8679bd626d49e7bed31d191cb1ee751c4ea01383
Zongheng Yang <zongheng.y@gmail.com>
2021-10-06 10:53:11 -0700

    Factor out sky/optimizer.py.

--- prototype/sky/__init__.py
+++ prototype/sky/__init__.py
@@ -2,10 +2,12 @@ from sky import clouds
 from sky.dag import *
 from sky.resources import *
 from sky.task import *
+from sky.optimizer import *
 
 __all__ = [
     'Dag',
     'DagContext',
+    'Optimizer',
     'Resources',
     'Task',
 ]
