84a3eefe602ef2866da59d93f8a1125c6a83f515
Zongheng Yang <zongheng.y@gmail.com>
2021-10-06 10:37:59 -0700

    Factor our sky/resources.py.

--- prototype/sky/__init__.py
+++ prototype/sky/__init__.py
@@ -1,5 +1,11 @@
 from sky import clouds
 from sky.dag import *
+from sky.resources import *
 from sky.task import *
 
-__all__ = ['Dag', 'DagContext', 'Task']
+__all__ = [
+    'Dag',
+    'DagContext',
+    'Resources',
+    'Task',
+]
