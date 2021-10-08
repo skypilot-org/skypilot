2b5af1f9e84803789355f0c2b05be2d079ac3fe4
Frank Luan <lsf@berkeley.edu>
2021-10-07 17:14:49 -0700

    A simple task runner and logs handler to get rid of Airflow

--- prototype/README.md
+++ prototype/README.md
@@ -2,39 +2,6 @@
 
 ## Setup
 
-0. `pip install -e .`
+1. `pip install -e .`
 
-1. Install Airflow:
-
-    ```
-    AIRFLOW_VERSION=2.1.4
-    PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
-    CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
-    pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
-    ```
-
-2. `pip install -r requirements.txt`
-
-3. Initialize Airflow
-
-    ```
-    airflow db init
-      
-    airflow users create \
-        --username admin \
-        --firstname Peter \
-        --lastname Parker \
-        --role Admin \
-        --email spiderman@superhero.org
-    ```
-
-4. Config Airflow: open `~/airflow/airflow.cfg`. On line 841, set `dag_dir_list_interval = 1`.
-
-5. Run Airflow
-
-    ```
-    airflow webserver --port 8080
-    airflow scheduler
-    ```
-
-6. Run `python main.py`, which will submit your job to Airflow and open http://localhost:8080/dagrun/list/ for monitoring progress.
+2. No more Airflow! Just run `python -u main.py`.
--- prototype/main.py
+++ prototype/main.py
@@ -1,15 +1,18 @@
 import dataclasses
 import datetime
 import enum
+import json
+import logging
 import os
+import sys
 import subprocess
 import time
-from typing import Optional
-import webbrowser
+from typing import Dict, List, Optional
 
 from absl import app
 from absl import flags
-from absl import logging
+import colorama
+from colorama import Fore, Style
 import jinja2
 
 
@@ -20,14 +23,9 @@ flags.DEFINE_string(
     "path to the cluster config file relative to repository root",
 )
 flags.DEFINE_string(
-    "dag_template",
-    "config/sky_dag.py.j2",
-    "path to the Airflow DAG file template",
-)
-flags.DEFINE_string(
-    "airflow_dags_path",
-    "_airflow_dags",
-    "path to the Airflow DAGs location",
+    "logs_directory",
+    "logs",
+    "path to runtime logs",
 )
 
 
@@ -44,22 +42,12 @@ class ExecutionPlanNode:
     cloud: CloudProvider
     region: str
     instance_type: str
-    working_dir: str
+    working_dir: Optional[str]
+    setup_commands: List[str]
     run_command: str
     output_path: str
     output_cloud_uri: str
     timeout_seconds: int
-
-
-def run(cmd, **kwargs) -> subprocess.CompletedProcess:
-    logging.info("$ " + cmd)
-    ret = subprocess.run(cmd, shell=True, **kwargs)
-    ret.check_returncode()
-    return ret
-
-
-def get_run_id() -> str:
-    return "sky_" + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 
 
 def get_execution_plan_node():
@@ -68,13 +56,92 @@ def get_execution_plan_node():
         "test run",
         CloudProvider.AWS,
         "us-west-2",
-        "t3.micro",
-        "/Users/lsf/projects/sky-experiments/prototype/user/",
-        "echo hello world > task_output",
-        "task_output",
-        "s3://intercloud-data/test_output",
+        "p3.2xlarge",
+        None,
+        [
+            "git clone https://github.com/concretevitamin/tpu.git; cd tpu; git checkout -f gpu_train",
+        ],
+        "python ~/tpu/models/official/resnet/resnet_main.py --use_tpu=False --mode=train --model_dir=~/resnet_model --data_dir=gs://cloud-tpu-test-datasets/fake_imagenet --train_batch_size=256 --train_steps=112590 --amp --xla --loss_scale=128 --iterations_per_loop=1251 2>&1 | tee ~/run.log",
+        "run.log",
+        "s3://intercloud-data/run.log",
         3600,
     )
+
+
+class JsonLogger:
+    def __init__(self, logger_name: str, log_file_path: str):
+        self.logger = logging.getLogger(logger_name)
+        self.logger.handlers = []
+        self.logger.addHandler(logging.FileHandler(log_file_path))
+
+    def log(self, event: str, payload: Dict = {}):
+        now = time.time()
+        json_payload = {"time": time.time(), "event": event}
+        json_payload.update(payload)
+        self.logger.debug(json.dumps(json_payload))
+        msg = f"{Fore.CYAN}{Style.BRIGHT}{event}{Style.RESET_ALL}"
+        if len(payload) > 0:
+            for k, v in payload.items():
+                msg += f" {Fore.MAGENTA}{k}={v}"
+        msg += f"{Fore.RESET}"
+        logging.info(msg)
+
+
+class Step:
+    def __init__(self, runner: "Runner", step_id: str, shell_command: str):
+        self.runner = runner
+        self.step_id = str(step_id)
+        self.shell_command = shell_command
+
+    def run(self, **kwargs) -> subprocess.CompletedProcess:
+        logfile = os.path.join(self.runner.logs_root, f"{self.step_id}.log")
+        self.shell_command += f" 2>&1 | tee {logfile}"
+        proc = subprocess.Popen(
+            self.shell_command,
+            shell=True,
+            **kwargs,
+        )
+        proc.communicate()
+        if proc.returncode != 0:
+            raise subprocess.CalledProcessError(proc.returncode, proc.args)
+
+
+class Runner:
+    """
+    FIXME: This is a linear sequence of steps for now. Will upgrade to a DAG.
+    """
+
+    def __init__(self, run_id: Optional[str] = None, steps: List[Step] = []):
+        if run_id is None:
+            run_id = get_run_id()
+        self.run_id = run_id
+        self.steps = steps
+        self.next_step_id = 0
+        self.logs_root = os.path.join(FLAGS.logs_directory, run_id)
+        os.makedirs(self.logs_root, exist_ok=True)
+        self.logger = JsonLogger(run_id, os.path.join(self.logs_root, "runner.log"))
+
+    def add_step(self, shell_command: str, step_id: str = "") -> "Runner":
+        step_id = f"{self.next_step_id:03}_{step_id}"
+        self.next_step_id += 1
+        self.steps.append(Step(self, step_id, shell_command))
+        return self
+
+    def run(self) -> "Runner":
+        self.logger.log("start_run")
+        for step in self.steps:
+            self.logger.log(
+                "start_step",
+                {"step_id": step.step_id, "shell_command": step.shell_command},
+            )
+            step.run()
+            self.logger.log("finish_step")
+        self.logger.log("finish_run")
+        return self
+
+
+def get_run_id() -> str:
+    return "sky_" + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 
 
 def create_from_template(
@@ -99,39 +166,33 @@ def write_cluster_config(node: ExecutionPlanNode) -> str:
         FLAGS.cluster_config_template,
         {
             "instance_type": node.instance_type,
+            "setup_commands": node.setup_commands,
             "working_dir": node.working_dir,
         },
     )
 
 
-def start_airflow(node: ExecutionPlanNode):
+def execute_node(node: ExecutionPlanNode) -> Runner:
     cluster_config_file = write_cluster_config(node)
-    dag_id = get_run_id()
-    os.makedirs(FLAGS.airflow_dags_path, exist_ok=True)
-    output_path = f"{FLAGS.airflow_dags_path}/{dag_id}.py"
-    create_from_template(
-        FLAGS.dag_template,
-        {
-            "cluster_config_file": cluster_config_file,
-            "dag_id": dag_id,
-            "output_cloud_uri": node.output_cloud_uri,
-            "output_path": node.output_path,
-            "run_command": node.run_command,
-            "timeout_seconds": node.timeout_seconds,
-        },
-        output_path,
+    runner = (
+        Runner()
+        .add_step(f"ray up -y {cluster_config_file} --no-config-cache", "setup")
+        .add_step(f"ray exec {cluster_config_file} '{node.run_command}'", "exec")
+        .add_step(
+            f"ray exec {cluster_config_file} "
+            f"'aws s3 cp {node.output_path} {node.output_cloud_uri}'",
+            "save_result_to_cloud",
+        )
+        .add_step(f"ray down -y {cluster_config_file}", "teardown")
     )
-    airflow_wait_seconds = 2
-    logging.info(f"Waiting {airflow_wait_seconds}s for Airflow to pick up the DAG")
-    time.sleep(airflow_wait_seconds)
-    run(f"airflow dags trigger {dag_id}")
-    webbrowser.open("http://localhost:8080/dagrun/list/")
+    return runner.run()
 
 
 def main(argv):
     del argv  # Unused.
+    colorama.init()
     node = get_execution_plan_node()
-    start_airflow(node)
+    execute_node(node)
 
 
 if __name__ == "__main__":
