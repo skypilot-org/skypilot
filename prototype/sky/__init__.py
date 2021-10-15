0ecfeed362e4c3b12d689a6324cfa9c07e1b3d3e
Zongheng Yang <zongheng.y@gmail.com>
2021-10-14 20:06:11 -0700

    Support for resource filtering (e.g., V100 -> p3.2xlarge)

--- prototype/sky/__init__.py
+++ prototype/sky/__init__.py
@@ -3,6 +3,7 @@ from sky.dag import Dag, DagContext
 from sky.execution import execute
 from sky.resources import Resources
 from sky.task import Task
+from sky.registry import fill_in_launchable_resources
 from sky.optimizer import Optimizer
 
 __all__ = [
@@ -12,4 +13,5 @@ __all__ = [
     'Resources',
     'Task',
     'execute',
+    'fill_in_launchable_resources',
 ]
---
+++ prototype/sky/registry.py
@@ -0,0 +1,28 @@
+"""Service registry."""
+import collections
+
+import sky
+from sky import clouds
+
+_CLOUDS = [
+    clouds.AWS(),
+    clouds.GCP(),
+]
+
+
+def fill_in_launchable_resources(task: sky.Task):
+    launchable = collections.defaultdict(list)
+    for resources in task.get_resources():
+        if resources.is_launchable():
+            launchable[resources] = [resource]
+            continue
+        if resources.cloud is not None:
+            print(resources.cloud)
+            launchable[
+                resources] = resources.cloud.get_feasible_launchable_resources(
+                    resources)
+        else:
+            for cloud in _CLOUDS:
+                launchable[resources].extend(
+                    cloud.get_feasible_launchable_resources(resources))
+    return launchable
