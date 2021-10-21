de69ad689f223d35f29fdf1797feb6e46dec874c
Michael Luo <michaelluo@dhcp-132-50.EECS.Berkeley.EDU>
2021-10-20 23:22:14 -0700

    Multi-node done, clean up done

--- prototype/resnet_app.py
+++ prototype/resnet_app.py
@@ -1,3 +1,4 @@
+import json 
 import sky
 from sky import clouds
 
@@ -28,29 +29,57 @@ with sky.Dag() as dag:
     # The working directory contains all code and will be synced to remote.
     workdir = '~/Downloads/tpu'
 
+    docker_image = 'rayproject/ray-ml:latest-gpu'
+    container_name = 'resnet_container'
+
     # The setup command.  Will be run under the working directory.
     setup = 'pip install --upgrade pip && \
-        conda activate resnet || \
-          "conda create -n resnet python=3.7 -y && \
+           pip install ray[default] awscli && \
+           conda create -n resnet python=3.7 -y && \
            conda activate resnet && \
-           pip install tensorflow==2.4.0 pyyaml && \
-           cd models && pip install -e ."'
+           pip install tensorflow==2.5.0 pyyaml ray[default] awscli && \
+           cd models && pip install -e .'
+
+
+    # Post setup function. Run after `ray up *.yml` completes. Returns dictionary of commands to be run on each corresponding node.
+    def post_setup_fn(ip_dict):
+        command_dict = {}
+        ip_list = [ip_dict['head']] + ip_dict['workers']
+        tf_config = {'cluster': {'worker': [ip + ':8008' for ip in ip_list]}, 'task': {'type': 'worker', 'index': -1}}
+        for i, ip in enumerate(ip_list):
+            tf_config['task']['index'] = i
+            str_tf_config = json.dumps(tf_config).replace('"', '\\"')
+            command_dict[ip] = "echo \"export TF_CONFIG='" + str_tf_config + "'\" >> ~/.bashrc"
+        return command_dict
 
     # The command to run.  Will be run under the working directory.
-    run = 'conda activate resnet && \
-        python models/official/resnet/resnet_main.py --use_tpu=False \
-        --mode=train --train_batch_size=256 --train_steps=250 \
-        --iterations_per_loop=125 \
-        --data_dir=gs://cloud-tpu-test-datasets/fake_imagenet \
-        --model_dir=resnet-model-dir \
-        --amp --xla --loss_scale=128'
+    def run_fn(ip_dict):
+        ip_list = [ip_dict['head']] + ip_dict['workers']
+        run_dict = {}
+        for i, ip in enumerate(ip_list):
+            run_dict[ip] = 'source ~/.bashrc && \
+            source activate resnet && \
+            rm -rf resnet_model-dir && \
+            python models/official/resnet/resnet_main.py --use_tpu=False \
+            --mode=train --train_batch_size=4 --train_steps=2000 \
+            --iterations_per_loop=125 \
+            --data_dir=gs://cloud-tpu-test-datasets/fake_imagenet \
+            --model_dir=resnet-model-dir \
+            --amp --xla --loss_scale=128'
+        return run_dict
+
+    run = run_fn
 
     train = sky.Task(
         'train',
         workdir=workdir,
         setup=setup,
+        post_setup_fn = post_setup_fn,
+        docker_image = docker_image,
+        container_name = container_name,
         run=run,
     )
+
     train.set_inputs('gs://cloud-tpu-test-datasets/fake_imagenet',
                      estimated_size_gigabytes=70)
     train.set_outputs('resnet-model-dir', estimated_size_gigabytes=0.1)
