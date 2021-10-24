c58ad45408b5f55a4dcafeb98c253e61fc556d65
Zongheng Yang <zongheng.y@gmail.com>
2021-10-24 14:08:43 -0700

    Add examples/huggingface_glue_imdb_app.py.

---
+++ prototype/examples/huggingface_glue_imdb_app.py
@@ -0,0 +1,42 @@
+"""Huggingface.
+
+Fine-tunes a pretrained BERT model on the IMDB dataset:
+https://github.com/huggingface/transformers/tree/master/examples/pytorch/text-classification
+
+The dataset is downloaded automatically by huggingface, and saved to
+~/.cache/huggingface.
+"""
+import sky
+
+with sky.Dag() as dag:
+    # The setup command.  Will be run under the working directory.
+    # https://github.com/huggingface/transformers/tree/master/examples#important-note
+    setup = 'git clone https://github.com/huggingface/transformers/ && \
+      cd transformers && pip install . && \
+      cd examples/pytorch/text-classification && \
+      pip install -r requirements.txt'
+
+    # The command to run.  Will be run under the working directory.
+    # https://github.com/huggingface/transformers/tree/master/examples/pytorch/text-classification
+    run = 'cd transformers/examples/pytorch/text-classification && \
+    python run_glue.py \
+  --model_name_or_path bert-base-cased \
+  --dataset_name imdb  \
+  --do_train \
+  --do_predict \
+  --max_seq_length 128 \
+  --per_device_train_batch_size 32 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 3 \
+  --output_dir /tmp/imdb/ \
+  --fp16'
+
+    train = sky.Task(
+        'train',
+        setup=setup,
+        run=run,
+    )
+    train.set_resources({sky.Resources(accelerators='V100')})
+
+dag = sky.Optimizer.optimize(dag)
+sky.execute(dag)
