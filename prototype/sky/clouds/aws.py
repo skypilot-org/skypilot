de936205d620834538e2f3fbd9f714bccc491171
Zongheng Yang <zongheng.y@gmail.com>
2021-10-25 13:07:04 -0700

    Minor: docs, readme, comments

--- prototype/sky/clouds/aws.py
+++ prototype/sky/clouds/aws.py
@@ -22,6 +22,7 @@ class AWS(clouds.Cloud):
     }
 
     # TODO: add other GPUs c.f. https://aws.amazon.com/ec2/instance-types/
+    # e.g., g4 instances with T4 GPUs
     _ACCELERATORS_DIRECTORY = {
         ('V100', 1): 'p3.2xlarge',
         ('V100', 4): 'p3.8xlarge',
@@ -88,6 +89,8 @@ class AWS(clouds.Cloud):
 
         assert len(accelerators) == 1, resources
         acc, acc_count = list(accelerators.items())[0]
+        # TODO: support bin-packing; if requesting 2 V100s, should allow 2x
+        # p3.2xlarge rather than not returning anything.
         instance_type = AWS._ACCELERATORS_DIRECTORY.get((acc, acc_count))
         if instance_type is None:
             return []
