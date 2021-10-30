312181cb61c393c7350734cf79b656fbbb805f2c
Wei-Lin Chiang <infwinston@gmail.com>
2021-10-29 23:17:24 -0700

    Hint user how to teardown tpu

--- prototype/sky/execution.py
+++ prototype/sky/execution.py
@@ -92,14 +92,15 @@ def _write_cluster_config(run_id: RunId, task, cluster_config_template: str):
     )
     if resources_vars['tpu_type'] is not None:
         # FIXME: replace hard-coding paths
-        config_dict['gcloud'] = _fill_template(
-            'config/gcp-tpu-create.sh.j2',
-            dict(resources_vars)
-        )
-        # FIXME: return this somewhere
-        _fill_template(
-            'config/gcp-tpu-delete.sh.j2',
-            dict(resources_vars)
+        config_dict['gcloud'] = (
+            _fill_template(
+                'config/gcp-tpu-create.sh.j2',
+                dict(resources_vars)
+            ),
+            _fill_template(
+                'config/gcp-tpu-delete.sh.j2',
+                dict(resources_vars)
+            )
         )
     return config_dict
 
@@ -355,7 +356,7 @@ def execute(dag: sky.Dag, dryrun: bool = False, teardown: bool = False):
     if task.best_resources.accelerator_args['tpu_name'] is not None:
         assert 'gcloud' in config_dict, 'Expect TPU provisioning with gcloud'
         runner.add_step('provision', 'Provision resources with gcloud',
-                        f'bash {config_dict["gcloud"]}')
+                        f'bash {config_dict["gcloud"][0]}')
         runner.add_step(
             'setup', 'TPU setup',
             f"ray exec {cluster_config_file} \'echo \"export TPU_NAME={task.best_resources.accelerator_args['tpu_name']}\" >> ~/.bashrc\'"
@@ -407,6 +408,9 @@ def execute(dag: sky.Dag, dryrun: bool = False, teardown: bool = False):
     if teardown:
         runner.add_step('teardown', 'Tear down resources',
                         f'ray down -y {cluster_config_file}')
+        if task.best_resources.accelerator_args['tpu_name'] is not None:
+            runner.add_step('teardown', 'Tear down resources with gcloud',
+                            f'bash {config_dict["gcloud"][1]}')
     runner.run()
     if not teardown:
         print(
@@ -415,3 +419,7 @@ def execute(dag: sky.Dag, dryrun: bool = False, teardown: bool = False):
         print(
             f'  To teardown the resources:\t{Style.BRIGHT}ray down {cluster_config_file} -y {Style.RESET_ALL}\n'
         )
+        if task.best_resources.accelerator_args['tpu_name'] is not None:
+            print(
+                f'  To teardown the TPU resources:\t{Style.BRIGHT}bash {config_dict["gcloud"][1]} {Style.RESET_ALL}\n'
+            )
