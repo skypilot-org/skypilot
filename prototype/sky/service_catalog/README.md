7aad484b52318191075c32a91248041ad64a6827
Frank Luan <lsf@berkeley.edu>
2021-11-05 15:29:07 -0700

    Reorganize

--- prototype/sky/service_catalog/README.md
+++ prototype/sky/service_catalog/README.md
@@ -1,4 +1,9 @@
-# Sky Service Catalog
+# Service Catalog
 
-This module provides the list of all cloud instance offerings and their prices. Currently, each `{aws,azure,gcp}_probe.py` is a standalone program that respectively produces `data/{aws,azure,gcp}.csv` files.
+The Sky supports 3 major clouds: AWS, Azure and GCP.
 
+This module provides information including the instance type offerings, their pricing and data transfer costs. It also provides functions to query these information, and to select the most suitable instance types based on resource requirements.
+
+- `data/{aws,azure,gcp}.csv`: the prefilled offerings and pricing list files.
+- `probes/{aws,azure,gcp}_probe.py`: each file is a standalone script that queries the cloud APIs to produce the data files.
+- `{aws,azure,gcp}_catalog.py`: Singleton-classes that load the data files and provide functions to query for instance offerings based on resource requirements. Used by `clouds/{aws,azure,gcp}.py`.
--- prototype/sky/service_catalog/probes/aws_probe.py
+++ prototype/sky/service_catalog/probes/aws_probe.py
@@ -1,3 +1,8 @@
+"""A script that queries AWS API to get instance types and pricing information.
+This script takes about 1 minute to finish.
+"""
+from typing import Tuple
+
 from absl import app
 from absl import flags
 from absl import logging
@@ -7,6 +12,8 @@ import pandas as pd
 import ray
 
 REGIONS = ["us-west-1", "us-west-2", "us-east-1", "us-east-2"]
+# NOTE: the hard-coded us-east-1 URL is not a typo. AWS pricing endpoint is
+# only available in this region, but it serves pricing information for all regions.
 PRICING_TABLE_URL_FMT = "https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/current/{region}/index.csv"
 
 
@@ -39,6 +46,7 @@ def get_instance_types_df(region: str) -> pd.DataFrame:
     df, pricing_df = ray.get(
         [get_instance_types.remote(region),
          get_pricing_table.remote(region)])
+    print(f"{region} Processing dataframes")
 
     def get_price(row):
         t = row["InstanceType"]
@@ -48,8 +56,24 @@ def get_instance_types_df(region: str) -> pd.DataFrame:
             print(f"{region} WARNING: cannot find pricing for {t}")
             return np.nan
 
-    df["PricePerHour"] = df.apply(get_price, axis=1)
+    def get_gpu_info(row) -> Tuple[str, float]:
+        info = row["GpuInfo"]
+        if not isinstance(info, dict):
+            return None, np.nan
+        gpu = info["Gpus"][0]
+        return gpu["Name"], gpu["Count"]
+
+    def get_additional_columns(row):
+        gpu_name, gpu_count = get_gpu_info(row)
+        return pd.Series({
+            "PricePerHour": get_price(row),
+            "GpuName": gpu_name,
+            "GpuCount": gpu_count,
+        })
+
     df["Region"] = region
+    df = pd.concat([df, df.apply(get_additional_columns, axis="columns")],
+                   axis="columns")
     return df
 
 
@@ -66,7 +90,7 @@ def main(argv):
     logging.set_verbosity(logging.DEBUG)
     df = get_all_regions_instance_types_df()
     df.to_csv("aws.csv", index=False)
-    print("AWS Service Catalogue saved to aws.csv")
+    print("AWS Service Catalog saved to aws.csv")
 
 
 if __name__ == "__main__":
