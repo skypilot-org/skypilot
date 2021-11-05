efc7c26ce88e5ba77976ac42c4ad6bf0da972bdc
Frank Luan <lsf@berkeley.edu>
2021-11-05 16:27:14 -0700

    " -> '

--- prototype/sky/clouds/data_fetchers/fetch_aws.py
+++ prototype/sky/clouds/data_fetchers/fetch_aws.py
@@ -11,34 +11,34 @@ import numpy as np
 import pandas as pd
 import ray
 
-REGIONS = ["us-west-1", "us-west-2", "us-east-1", "us-east-2"]
+REGIONS = ['us-west-1', 'us-west-2', 'us-east-1', 'us-east-2']
 # NOTE: the hard-coded us-east-1 URL is not a typo. AWS pricing endpoint is
 # only available in this region, but it serves pricing information for all regions.
-PRICING_TABLE_URL_FMT = "https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/current/{region}/index.csv"
+PRICING_TABLE_URL_FMT = 'https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/current/{region}/index.csv'
 
 
 @ray.remote
 def get_instance_types(region: str) -> pd.DataFrame:
-    client = boto3.client("ec2", region_name=region)
-    paginator = client.get_paginator("describe_instance_types")
+    client = boto3.client('ec2', region_name=region)
+    paginator = client.get_paginator('describe_instance_types')
     items = []
     for i, resp in enumerate(paginator.paginate()):
-        print(f"{region} getting instance types page {i}")
-        items += resp["InstanceTypes"]
+        print(f'{region} getting instance types page {i}')
+        items += resp['InstanceTypes']
     return pd.DataFrame(items)
 
 
 @ray.remote
 def get_pricing_table(region: str) -> pd.DataFrame:
-    print(f"{region} downloading pricing table")
+    print(f'{region} downloading pricing table')
     url = PRICING_TABLE_URL_FMT.format(region=region)
     df = pd.read_csv(url, skiprows=5, low_memory=False)
-    return df[(df["TermType"] == "OnDemand") &
-              (df["Operating System"] == "Linux") &
-              df["Pre Installed S/W"].isnull() &
-              (df["CapacityStatus"] == "Used") &
-              (df["Tenancy"].isin(["Host", "Shared"])) &
-              df["PricePerUnit"] > 0].set_index("Instance Type")
+    return df[(df['TermType'] == 'OnDemand') &
+              (df['Operating System'] == 'Linux') &
+              df['Pre Installed S/W'].isnull() &
+              (df['CapacityStatus'] == 'Used') &
+              (df['Tenancy'].isin(['Host', 'Shared'])) &
+              df['PricePerUnit'] > 0].set_index('Instance Type')
 
 
 @ray.remote
@@ -46,41 +46,41 @@ def get_instance_types_df(region: str) -> pd.DataFrame:
     df, pricing_df = ray.get(
         [get_instance_types.remote(region),
          get_pricing_table.remote(region)])
-    print(f"{region} Processing dataframes")
+    print(f'{region} Processing dataframes')
 
     def get_price(row):
-        t = row["InstanceType"]
+        t = row['InstanceType']
         try:
-            return pricing_df.loc[t]["PricePerUnit"]
+            return pricing_df.loc[t]['PricePerUnit']
         except KeyError:
-            print(f"{region} WARNING: cannot find pricing for {t}")
+            print(f'{region} WARNING: cannot find pricing for {t}')
             return np.nan
 
     def get_gpu_info(row) -> Tuple[str, float]:
-        info = row["GpuInfo"]
+        info = row['GpuInfo']
         if not isinstance(info, dict):
             return None, np.nan
-        gpu = info["Gpus"][0]
-        return gpu["Name"], gpu["Count"]
+        gpu = info['Gpus'][0]
+        return gpu['Name'], gpu['Count']
 
     def get_additional_columns(row):
         gpu_name, gpu_count = get_gpu_info(row)
         return pd.Series({
-            "PricePerHour": get_price(row),
-            "GpuName": gpu_name,
-            "GpuCount": gpu_count,
+            'PricePerHour': get_price(row),
+            'GpuName': gpu_name,
+            'GpuCount': gpu_count,
         })
 
-    df["Region"] = region
-    df = pd.concat([df, df.apply(get_additional_columns, axis="columns")],
-                   axis="columns")
+    df['Region'] = region
+    df = pd.concat([df, df.apply(get_additional_columns, axis='columns')],
+                   axis='columns')
     return df
 
 
 def get_all_regions_instance_types_df():
     dfs = ray.get([get_instance_types_df.remote(r) for r in REGIONS])
     df = pd.concat(dfs)
-    df.sort_values(["InstanceType", "Region"], inplace=True)
+    df.sort_values(['InstanceType', 'Region'], inplace=True)
     return df
 
 
@@ -89,9 +89,9 @@ def main(argv):
     ray.init()
     logging.set_verbosity(logging.DEBUG)
     df = get_all_regions_instance_types_df()
-    df.to_csv("aws.csv", index=False)
-    print("AWS Service Catalog saved to aws.csv")
+    df.to_csv('aws.csv', index=False)
+    print('AWS Service Catalog saved to aws.csv')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(main)
