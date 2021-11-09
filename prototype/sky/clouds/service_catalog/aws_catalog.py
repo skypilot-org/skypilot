b0533733167edbb89965ba0d4418bd13c1b8aa65
Frank Luan <lsf@berkeley.edu>
2021-11-09 11:46:29 -0800

    Fix region for `get_instance_type_for_gpu`

--- prototype/sky/clouds/service_catalog/aws_catalog.py
+++ prototype/sky/clouds/service_catalog/aws_catalog.py
@@ -11,20 +11,21 @@ _df = common.read_catalog('aws.csv')
 
 
 def get_hourly_cost(instance_type: str, region: str = 'us-west-2') -> float:
-    mask = _df['InstanceType'] == instance_type
-    if region is not None:
-        mask &= _df['Region'] == region
-    result = _df[mask]
+    result = _df[(_df['InstanceType'] == instance_type) &
+                 (_df['Region'] == region)]
     assert len(result) == 1, (result, instance_type, region)
     return result['PricePerHour'].iloc[0]
 
 
-def get_instance_type_for_gpu(gpu_name: str, count: int) -> Optional[str]:
+def get_instance_type_for_gpu(gpu_name: str,
+                              count: int,
+                              region: str = 'us-west-2') -> Optional[str]:
     """Returns the cheapest instance type that offers the required count of GPUs.
     """
     # TODO: Reorganize _df to support any accelerator (Inferentia, etc.)
-    result = _df[(_df['GpuName'] == gpu_name) & (_df['GpuCount'] == count)]
+    result = _df[(_df["GpuName"] == gpu_name) & (_df["GpuCount"] == count) &
+                 (_df["Region"] == region)]
     if len(result) == 0:
         return None
-    assert len(result) == 1, (result, gpu_name, count)
+    assert len(result) == 1, (result, gpu_name, count, region)
     return result.iloc[0]['InstanceType']
