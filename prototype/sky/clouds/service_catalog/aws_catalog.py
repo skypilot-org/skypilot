aa552e7798f92ceef0890b6673719da61ac9645f
Frank Luan <lsf@berkeley.edu>
2021-11-08 16:52:11 -0800

    Address comments

--- prototype/sky/clouds/service_catalog/aws_catalog.py
+++ prototype/sky/clouds/service_catalog/aws_catalog.py
@@ -3,29 +3,31 @@ instance types and pricing information for AWS.
 """
 from typing import Optional
 
-from sky.clouds.service_catalog.common import read_catalog
+import sky.clouds.service_catalog.common as common
 
 InstanceType = str
+Region = str
 
-_df = read_catalog("aws.csv")
+_df = common.read_catalog('aws.csv')
 
 
 def get_hourly_cost(instance_type: InstanceType,
-                    region: Optional[str] = None) -> float:
+                    region: Optional[Region] = 'us-west-2') -> float:
     mask = _df['InstanceType'] == instance_type
     if region is not None:
         mask &= _df['Region'] == region
     result = _df[mask]
-    return result['PricePerHour'].mean()
+    assert len(result) == 1, (result, instance_type, region)
+    return result['PricePerHour'].iloc[0]
 
 
 def get_instance_type_for_gpu(gpu_name: str,
                               count: int) -> Optional[InstanceType]:
+    """Returns the cheapest instance type that offers the required count of GPUs.
     """
-    Returns the cheapest instance type that offers the required count of GPUs.
-    """
+    # TODO: Reorganize _df to support any accelerator (Inferentia, etc.)
     result = _df[(_df['GpuName'] == gpu_name) &
-                 (_df['GpuCount'] >= count)].sort_values('PricePerHour')
+                 (_df['GpuCount'] == count)].sort_values('PricePerHour')
     if len(result) == 0:
         return None
     return result.iloc[0]['InstanceType']
