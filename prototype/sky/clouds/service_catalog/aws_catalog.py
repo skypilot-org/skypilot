3f86f4a531e0430397701c4cec6a74fdae6b6965
Zhanghao Wu <zhanghao.wu@outlook.com>
2021-11-13 12:19:12 -0800

    Fix format issues

--- prototype/sky/clouds/service_catalog/aws_catalog.py
+++ prototype/sky/clouds/service_catalog/aws_catalog.py
@@ -17,15 +17,14 @@ _df = common.read_catalog('aws.csv')
 def get_hourly_cost(instance_type: str,
                     region: str = 'us-west-2',
                     use_spot: bool = False) -> float:
-    """Returns the cost, or the cheapest cost among all zones for spot.
-    """
+    """Returns the cost, or the cheapest cost among all zones for spot."""
     result = _df[(_df['InstanceType'] == instance_type) &
                  (_df['Region'] == region)]
 
     assert len(set(result['Price'])) == 1, (result, instance_type, region)
     cheapest_idx = result['SpotPrice'].idxmin()
 
-    if  not use_spot or np.isnan(cheapest_idx):
+    if not use_spot or np.isnan(cheapest_idx):
         return result['Price'].iloc[0]
 
     cheapest = result.loc[cheapest_idx]
@@ -35,8 +34,7 @@ def get_hourly_cost(instance_type: str,
 def get_instance_type_for_gpu(gpu_name: str,
                               count: int,
                               region: str = 'us-west-2') -> Optional[str]:
-    """Returns the cheapest instance type that offers the required count of GPUs.
-    """
+    """Returns the cheapest instance type that offers the required count of GPUs."""
     # TODO: Reorganize _df to support any accelerator (Inferentia, etc.)
     result = _df[(_df['GpuName'] == gpu_name) & (_df['GpuCount'] == count) &
                  (_df['Region'] == region)]
