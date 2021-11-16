42dd2c01b46e081fcfe7c2cb9e893f823542e0f7
Gautam Mittal <gautam@mittal.net>
2021-11-16 14:59:26 -0800

    Break into 2 PRs

--- prototype/setup.py
+++ prototype/setup.py
@@ -4,13 +4,5 @@ setup(
     name='sky',
     version='0.1dev',
     packages=['sky'],
-    install_requires=[
-        'Click',
-    ],
-    entry_points={
-        'console_scripts': [
-            'sky = scripts.cli:cli',
-        ],
-    },
     include_package_data=True,
 )
