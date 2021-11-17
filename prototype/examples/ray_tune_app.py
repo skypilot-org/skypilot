2ba16793aa9e75b685da24b0aac02dd211a965d7
Frank Luan <lsf@berkeley.edu>
2021-11-16 20:49:27 -0800

    Tune example

---
+++ prototype/examples/ray_tune_app.py
@@ -0,0 +1,38 @@
+import os
+from typing import Dict, List
+
+import sky
+from sky import clouds
+
+IPAddr = str
+
+with sky.Dag() as dag:
+    # Total Nodes, INCLUDING Head Node
+    num_nodes = 2
+
+    workdir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                           'ray_tune_example')
+
+    # The setup command.  Will be run under the working directory.
+    setup = 'pip3 install ray[tune]'
+
+    # The command to run.  Will be run under the working directory.
+    def run_fn(ip_list: List[IPAddr]) -> Dict[IPAddr, str]:
+        return {
+            ip_list[0]: f'python3 tune_basic_example.py',
+        }
+
+    train = sky.Task(
+        'train',
+        workdir=workdir,
+        setup=setup,
+        num_nodes=num_nodes,
+        run=run_fn,
+    )
+
+    train.set_resources({
+        sky.Resources(clouds.AWS(), 'p3.2xlarge'),
+    })
+
+dag = sky.Optimizer.optimize(dag, minimize=sky.Optimizer.COST)
+sky.execute(dag)
