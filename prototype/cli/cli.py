fd96b16a42d7be3b4a53276344d58aebaba6073a
Gautam Mittal <gautam@mittal.net>
2021-11-20 11:31:32 -0800

    Add CLI base with sky run (#46)
    * Init
    
    * init
    
    * Init CLI
    
    * Add __init__.py
    
    * Add CLI base
    
    * Remove .gitignore
    
    * Rename and fix quoting

---
+++ prototype/cli/cli.py
@@ -0,0 +1,29 @@
+import click
+
+import sky
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument('config_path', required=True, type=str)
+@click.option('--dryrun', '-n', default=False, type=bool, help='Dry run.')
+def run(config_path, dryrun):
+    """Launch a job from a YAML config."""
+
+    with sky.Dag() as dag:
+        task = sky.Task.from_yaml(config_path)
+
+    dag = sky.optimize(dag, minimize=sky.Optimizer.COST)
+    sky.execute(dag, dryrun=dryrun)
+
+
+def main():
+    return cli()
+
+
+if __name__ == '__main__':
+    main()
