9fecda543a13b0dd61dec2a563b65d6e0ef1bc18
Romil Bhardwaj <romil.bhardwaj@gmail.com>
2021-11-22 19:44:01 -0800

    LocalDockerBackend for debugging (#35)
    working LocalDockerBackend

--- prototype/sky/backends/__init__.py
+++ prototype/sky/backends/__init__.py
@@ -1,7 +1,5 @@
 from sky.backends.backend import Backend
 from sky.backends.cloud_vm_ray_backend import CloudVmRayBackend
+from sky.backends.local_docker_backend import LocalDockerBackend
 
-__all__ = [
-    'Backend',
-    'CloudVmRayBackend',
-]
+__all__ = ['Backend', 'CloudVmRayBackend', 'LocalDockerBackend']
