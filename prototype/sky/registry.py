5b1bce9c4e49bfd0515bea71fb38518a425df1cf
Zhanghao Wu <zhanghao.wu@outlook.com>
2021-11-28 10:57:56 -0800

    Refactorize the blocked_resources removing

--- prototype/sky/registry.py
+++ prototype/sky/registry.py
@@ -17,6 +17,19 @@ def _resource_eq(__l: Resources, __r: Resources) -> bool:
     assert __l.cloud is not None and __l.instance_type is not None
     return type(__l.cloud) == type(__r.cloud) and __l.instance_type == __r.instance_type
 
+def _remove_task_blocked_resources(task: sky.Task, launchable: dict):
+    """Remove blocked resources from launchable resources."""
+    for orig_resources, launchable_list in launchable.items():
+        new_launchable_list = []            
+        for launchable_resources in launchable_list:
+            for blocked_resources in task.blocked_resources:
+                if _resource_eq(launchable_resources, blocked_resources):
+                    break
+            else: # Not blocked resources.
+                new_launchable_list.append(launchable_resources)
+        launchable[orig_resources] = new_launchable_list
+    return launchable
+
 def fill_in_launchable_resources(task: sky.Task):
     launchable = collections.defaultdict(list)
     for resources in task.get_resources():
@@ -32,15 +45,5 @@ def fill_in_launchable_resources(task: sky.Task):
                 launchable[resources].extend(
                     cloud.get_feasible_launchable_resources(resources))
 
-        # Remove blocked resources from launchable resources.
-        for orig_resources, launchable_list in launchable.items():
-            new_launchable_list = []            
-            for launchable_resources in launchable_list:
-                for blocked_resources in task.blocked_resources:
-                    if _resource_eq(launchable_resources, blocked_resources):
-                        break
-                else: # Not blocked resources.
-                    new_launchable_list.append(launchable_resources)
-            launchable[orig_resources] = new_launchable_list
-            
+        launchable = _remove_task_blocked_resources(task, launchable)
     return launchable
