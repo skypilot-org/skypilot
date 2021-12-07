ad2d5fb0c02cb5193ac054a19d526882bb360cdf
Zongheng Yang <zongheng.y@gmail.com>
2021-12-07 16:52:03 +0800

    TESTED: examples/run_smoke_tests.sh

--- prototype/examples/run_smoke_tests.sh
+++ prototype/examples/run_smoke_tests.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 # Run a few example apps as smoke tests.
-# For now, must manually (i) Ctrl-C as training starts (ii) comment out this
-# test and rerun this script for the remaining tests.
-#
+# NOTE: after tests, shut down clusters manually in UI / sky down --all.
+# Some tests are commented out due to long run time / being covered by others.
 # Usage:
-#
 #   bash examples/run_smoke_tests.sh 2>&1 | tee run.log
-#
 set -ex
 
 DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
@@ -16,35 +13,46 @@ python "$DIR"/example_app.py
 
 # Simple apps.
 time sky run -c min "$DIR"/minimal.yaml
-sky down min
+sky down min &
 time sky run -c fm "$DIR"/using_file_mounts.yaml
-sky down mh
+sky down fm &
 
 # Task(), 1 node.
+# 17:25.60 total
 # time sky run -c resnet "$DIR"/resnet_app.yaml
+# 13.770 total
 # time sky exec -c resnet "$DIR"/resnet_app.yaml
 # time python "$DIR"/resnet_app.py
-# sky down resnet
+# sky down resnet &
 # FIXME: if concurrently running two runs of resnet, the second run should be
 # waiting for the first run, but this is not happening.
 
 # Task(), 1 node.
+# 6:47.90 total
 time sky run -c huggingface "$DIR"/huggingface_glue_imdb_app.yaml
+# real    1m49.532s
 time sky exec -c huggingface "$DIR"/huggingface_glue_imdb_app.yaml
 # time python "$DIR"/huggingface_glue_imdb_app.py
-sky down huggingface
+sky down huggingface &
 
 # Task(), 1 node, TPU.
+# real    10m9.219s
 time sky run -c tpu "$DIR"/tpu_app.yaml
-time sky exec -c tpu "$DIR"/tpu_app.yaml
+# real    3m26.997s
+# time sky exec -c tpu "$DIR"/tpu_app.yaml
 # python "$DIR"/tpu_app.py
-sky down tpu
+sky down tpu &
 
 # Task(), n nodes.
+# real    4m17.406s
 time sky run -c mh "$DIR"/multi_hostname.yaml
+# real    0m50.811s
 time sky exec -c mh "$DIR"/multi_hostname.yaml
-sky down mh
+sky down mh &
 
 # ParTask.
+# 6:23.58 total
 time python "$DIR"/multi_echo.py
 # python "$DIR"/huggingface_glue_imdb_grid_search_app.py
+
+wait
