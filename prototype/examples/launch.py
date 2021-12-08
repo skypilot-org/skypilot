837dcb8b3a02dc28f935e09c4da90551adcfff92
Zongheng Yang <concretevitamin@users.noreply.github.com>
2021-12-08 14:14:50 +0800

    [CLI] Speed up 'sky exec' for faster iterative development (#91)
    * (Not back-compat) global_user_state: support pickle-able handles.
    
    This change is not binary back-compat.  Do this to update:
      rm ~/.sky/state.db
    
    * Speed up sky exec by not using 'ray exec/rsync_up'
    
    Changes:
    - Sane SSH options (control path has huge performance boost)
    - ResourceHandle: is now a tuple (head_ip, yaml, tpu).
      - Uses the cached head_ip to directly run our own 'rsync', 'ssh <cmd>'.
      - Thus tpu management is nicely handled
    
    * Add minimal.yaml
    
    * Update run_smoke_tests.sh
    
    * TESTED: examples/run_smoke_tests.sh
    
    * Add username to auto-generated cluster name
    
    * Fix cloud->remote file mount mkdir logic.
    
    * Better naming for 'managed_tpu' & address comments
    
    * Fix _create_interactive_node() use of handle; don't down the node.
    
    * lint

--- prototype/examples/launch.py
+++
@@ -1,8 +0,0 @@
-import sky
-
-with sky.Dag() as dag:
-    # The working directory contains all code and will be synced to remote.
-    task = sky.Task.from_yaml('examples/resnet_app.yaml')
-
-# sky.execute(dag, dryrun=True)
-sky.execute(dag)
