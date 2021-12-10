ba113fd3a1a177a2f77478e16deed0dcaf2c0bd6
Michael Luo <michael.luo123456789@gmail.com>
2021-12-09 23:10:31 -0800

    Sky Data (#63)
    * Initial Commit
    
    * Some more changes, mounting is left, finish tmrw
    
    * Demo
    
    * Fix
    
    * MVP is Done
    
    * ok
    
    * squashed commits
    
    * Yapf Fix
    
    * Romil's Comments Addressed
    
    * Partially addressed Zongheng's comments
    
    * Temporary fix
    
    * Fixed Resnet Storage example
    
    * Yapf and Pylint
    
    * Addressed Zongheng's comments, linting time
    
    * Pylint fix
    
    * Done

---
+++ prototype/examples/playground/storage_playground.py
@@ -0,0 +1,31 @@
+from sky.data import storage
+
+
+def test_bucket_creation():
+    storage_1 = storage.Storage(name='mluo-data', source='~/Downloads/temp/')
+    storage_1.get_or_copy_to_s3()  # Transfers data from local to S3
+    storage_1.get_or_copy_to_gcs()  # Transfers data from local to GCS
+
+
+def test_bucket_deletion():
+    storage_1 = storage.Storage(name='mluo-data', source='~/Downloads/temp/')
+    storage_1.get_or_copy_to_s3()
+    storage_1.get_or_copy_to_gcs()
+    storage_1.delete()  # Deletes Data
+
+
+def test_bucket_transfer():
+    # First time upload to s3
+    storage_1 = storage.Storage(name='mluo-data', source='~/Downloads/temp/')
+    bucket_path = storage_1.get_or_copy_to_s3(
+    )  # Transfers data from local to S3
+
+    storage_2 = storage.Storage(name='mluo-data', source=bucket_path)
+    storage_2.get_or_copy_to_s3()
+    storage_2.get_or_copy_to_gcs()  # Transfer data from S3 to Gs bucket
+
+
+if __name__ == '__main__':
+    test_bucket_creation()
+    test_bucket_deletion()
+    test_bucket_transfer()
