76dbf79632dee6cc3cd15286fb6bc54e1a2f1f9c
Frank Luan <lsf@berkeley.edu>
2021-12-30 01:00:54 -0800

    Update GCP catalog with regions info

--- prototype/sky/clouds/service_catalog/data_fetchers/fetch_gcp.py
+++ prototype/sky/clouds/service_catalog/data_fetchers/fetch_gcp.py
@@ -7,8 +7,9 @@ TODO: Add support for regular VMs
 https://cloud.google.com/sdk/gcloud/reference/compute/machine-types/list
 """
 
+import json
+
 from absl import app
-from absl import flags
 from absl import logging
 import pandas as pd
 
@@ -16,25 +17,61 @@ import pandas as pd
 def get_gpu_tpu_df():
     # All prices are for us-central1 region.
     # https://cloud.google.com/compute/gpus-pricing
+    # Region availability only covers US regions (us-*).
     gpu_data = {
-        'A100': (2.939, 0.880, [1, 2, 4, 8, 16]),
-        'T4': (0.35, 0.11, [1, 2, 4]),
-        'P4': (0.60, 0.216, [1, 2, 4]),
-        'V100': (2.48, 0.74, [1, 2, 4, 8]),
-        'P100': (1.46, 0.43, [1, 2, 4]),
-        'K80': (0.45, 0.038, [1, 2, 4, 8]),
+        'A100': (
+            [1, 2, 4, 8, 16],
+            [('us-central1', 2.939, 0.880), ('us-west1', 2.939, 0.880),
+             ('us-east1', 2.939, 0.880)],
+        ),
+        'T4': (
+            [1, 2, 4],
+            [('us-central1', 0.35, 0.11), ('us-west1', 0.35, 0.11),
+             ('us-west2', 0.41, 0.11), ('us-west4', 0.37, 0.069841),
+             ('us-east1', 0.35, 0.11), ('us-east4', 0.35, 0.11)],
+        ),
+        'P4': (
+            [1, 2, 4],
+            [('us-central1', 0.60, 0.216), ('us-west2', 0.72, 0.2592),
+             ('us-west4', 0.60, 0.216), ('us-east4', 0.60, 0.216)],
+        ),
+        'V100': (
+            [1, 2, 4, 8],
+            [('us-central1', 2.48, 0.74), ('us-west1', 2.48, 0.74),
+             ('us-east1', 2.48, 0.74)],
+        ),
+        'P100': (
+            [1, 2, 4],
+            [('us-central1', 1.46, 0.43), ('us-west1', 1.46, 0.43),
+             ('us-east1', 1.46, 0.43)],
+        ),
+        'K80': (
+            [1, 2, 4, 8],
+            [('us-central1', 0.45, 0.038), ('us-west1', 0.45, 0.038),
+             ('us-east1', 0.45, 0.038)],
+        ),
     }
     # https://cloud.google.com/tpu/pricing
     tpu_data = {
-        'tpu-v2-8': (4.50, 1.35, [1]),
-        'tpu-v3-8': (8.00, 2.40, [1]),
+        'tpu-v2-8': (
+            [1],
+            [('us-central1', 4.50, 1.35), ('europe-west4', 4.95, 1.485),
+             ('asia-east1', 5.22, 1.566)],
+        ),
+        'tpu-v3-8': (
+            [1],
+            [('us-central1', 8.00, 2.40), ('europe-west4', 8.80, 2.64)],
+        ),
     }
     acc_data = dict(**gpu_data, **tpu_data)
     rows = []
-    for acc_name, (price, spot_price, counts) in acc_data.items():
-        for cnt in counts:
-            rows.append(
-                [None, acc_name, cnt, 0, True, price * cnt, spot_price * cnt])
+    for acc_name, (counts, regions) in acc_data.items():
+        for region, price, spot_price in regions:
+            for cnt in counts:
+                rows.append([
+                    None, acc_name, cnt, 0, acc_name, price * cnt, spot_price * cnt,
+                    region
+                ])
     df = pd.DataFrame(
         data=rows,
         columns=[
@@ -45,6 +82,7 @@ def get_gpu_tpu_df():
             'GpuInfo',
             'Price',
             'SpotPrice',
+            'Region',
         ],
     )
     return df
