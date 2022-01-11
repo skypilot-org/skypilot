8aa4c7fe9590cb60f2c1d716f49d1b9240eb988b
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-01-11 02:00:27 -0800

    Fix issues with jobs (#164)
    * Do not cancel succeeded jobs
    
    * Suppress the log for job submission from ray
    
    * Better info for sky queue on stopped/init cluster
    
    * Optimize the task status query with active status updates. (Also, fix the stale task caused by stop/start of the cluster).
    
    * Fix sky queue and job check logic

--- prototype/sky/backends/cloud_vm_ray_backend.py
+++ prototype/sky/backends/cloud_vm_ray_backend.py
@@ -353,7 +353,16 @@ class RayCodeGen:
         assert not self._has_epilogue, 'add_epilogue() called twice?'
         self._has_epilogue = True
 
-        self._code.append('ray.get(futures)')
+        self._code += [
+            textwrap.dedent(f"""\
+            try:
+                ray.get(futures)
+                job_lib.set_status({self.job_id!r}, job_lib.JobStatus.SUCCEEDED)
+            except ray.exceptions.WorkerCrashedError:
+                job_lib.set_status({self.job_id!r}, job_lib.JobStatus.FAILED)
+                raise RuntimeError('Command failed, please check the logs.')
+            """)
+        ]
 
     def build(self) -> str:
         """Returns the entire generated program."""
@@ -1188,6 +1197,7 @@ class CloudVmRayBackend(backends.Backend):
             detach_run: bool = False,
     ) -> None:
         """Executes generated code on the head node."""
+        del stream_logs  # unused
         with tempfile.NamedTemporaryFile('w', prefix='sky_app_') as fp:
             fp.write(codegen)
             fp.flush()
@@ -1212,16 +1222,22 @@ class CloudVmRayBackend(backends.Backend):
             f'--address=127.0.0.1:8265 --job-id {job_id} -- '
             f'"{executable} -u {script_path} > {remote_log_path} 2>&1"')
 
-        self._run_command_on_head_via_ssh(handle, f'{cd} && {job_submit_cmd}',
-                                          job_log_path, stream_logs)
+        self._run_command_on_head_via_ssh(handle,
+                                          f'{cd} && {job_submit_cmd}',
+                                          job_log_path,
+                                          stream_logs=False,
+                                          check=True)
+
+        colorama.init()
+        style = colorama.Style
+        fore = colorama.Fore
+        logger.info('Job submitted with Job ID: '
+                    f'{style.BRIGHT}{job_id}{style.RESET_ALL}')
 
         try:
             if not detach_run:
                 backend_utils.run(f'sky logs -c {handle.cluster_name} {job_id}')
         finally:
-            colorama.init()
-            style = colorama.Style
-            fore = colorama.Fore
             name = handle.cluster_name
             logger.info('NOTE: ctrl-c does not stop the job.\n'
                         f'\n{fore.CYAN}Job ID: '
@@ -1248,7 +1264,7 @@ class CloudVmRayBackend(backends.Backend):
                               r'(\d+)', job_id_str)
         job_id = int(re_match[0])
 
-        logger.info(f'Job_id: {job_id}')
+        logger.info(f'Reserved Job ID: {job_id}')
         job_id = int(job_id)
         return job_id
 
@@ -1522,7 +1538,12 @@ class CloudVmRayBackend(backends.Backend):
             ssh_private_key,
             self._ssh_control_path(handle)) + [f'{ssh_user}@{handle.head_ip}']
 
-    def _run_command_on_head_via_ssh(self, handle, cmd, log_path, stream_logs):
+    def _run_command_on_head_via_ssh(self,
+                                     handle: ResourceHandle,
+                                     cmd: str,
+                                     log_path: str,
+                                     stream_logs: bool,
+                                     check=False):
         """Uses 'ssh' to run 'cmd' on a cluster's head node."""
         base_ssh_command = self.ssh_head_command(handle)
         command = base_ssh_command + [
@@ -1533,7 +1554,10 @@ class CloudVmRayBackend(backends.Backend):
             shlex.quote(f'true && source ~/.bashrc && export OMP_NUM_THREADS=1 '
                         f'PYTHONWARNINGS=ignore && ({cmd})'),
         ]
-        return backend_utils.run_with_log(command, log_path, stream_logs)
+        return backend_utils.run_with_log(command,
+                                          log_path,
+                                          stream_logs,
+                                          check=check)
 
     def run_on_head(self,
                     handle: ResourceHandle,
