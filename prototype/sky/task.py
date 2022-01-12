fc3bd45eaaa490f89bdf739309bc0af6b00f3928
Michael Luo <michael.luo@berkeley.edu>
2022-01-12 01:36:01 -0800

    Add back resource to task.py

--- prototype/sky/task.py
+++ prototype/sky/task.py
@@ -98,7 +98,7 @@ class Task:
         self.estimated_inputs_size_gigabytes = None
         self.estimated_outputs_size_gigabytes = None
         # Default to CPUNode
-        self.resources = None
+        self.resources = {sky.Resources()}
         self.time_estimator_func = None
         self.file_mounts = None
         # Filled in by the optimizer.  If None, this Task is not planned.
