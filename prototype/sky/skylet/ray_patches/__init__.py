dbad4f45113f8cc78c249aab8e94f0375249c2ca
Siyuan (Ryans) Zhuang <suquark@gmail.com>
2022-01-16 23:11:42 -0800

    Fix skylet setup issue (skylet failed to be imported locally by a third-party package) (#191)
    hide imports of specific clouds
    
    skip patched source files
    
    upload and install the sky wheel
    
    patch files
    
    update setup.py
    
    fix pycryptodome version

---
+++ prototype/sky/skylet/ray_patches/__init__.py
@@ -0,0 +1,13 @@
+"""Patch files"""
+import os
+import shutil
+
+
+def patch() -> None:
+    # Patch the buggy ray file. This should only be called
+    # from an isolated python process, because once imported
+    # the python module would persist in the memory.
+    from ray._private import log_monitor
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                        'log_monitor.py')
+    shutil.copyfile(path, log_monitor.__file__)
