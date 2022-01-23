64051b9fca67dbcf6b7398561358c128bfd037a3
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-01-22 20:26:31 -0800

    Format

--- prototype/sky/clouds/service_catalog/data_fetchers/fetch_aws.py
+++ prototype/sky/clouds/service_catalog/data_fetchers/fetch_aws.py
@@ -14,13 +14,7 @@ import ray
 from sky.clouds.service_catalog import common
 from sky.cloud_adaptors import aws
 
-
-REGIONS = [
-    'us-east-1',
-    'us-east-2',
-    'us-west-1',
-    'us-west-2'
-]
+REGIONS = ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2']
 # NOTE: the hard-coded us-east-1 URL is not a typo. AWS pricing endpoint is
 # only available in this region, but it serves pricing information for all regions.
 PRICING_TABLE_URL_FMT = 'https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/current/{region}/index.csv'
@@ -90,7 +84,6 @@ def get_instance_types_df(region: str) -> pd.DataFrame:
         try:
             return pricing_df.loc[t]['PricePerUnit']
         except KeyError:
-            # print(f'{region} WARNING: cannot find pricing for {t}')
             return np.nan
 
     def get_spot_price(row):
@@ -99,9 +92,6 @@ def get_instance_types_df(region: str) -> pd.DataFrame:
         try:
             return spot_pricing_df.loc[(instance, zone)]['SpotPrice']
         except KeyError:
-            # print(
-            #     f'{region} WARNING: cannot find spot pricing for {instance} {zone}'
-            # )
             return np.nan
 
     def get_acc_info(row) -> Tuple[str, float]:
