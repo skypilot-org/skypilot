ddb70897eded7c54d6158c0e528b5e6971ed1ee0
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-01-28 22:46:15 -0800

    Fix progress bar by patching ray/worker.py (#261)
    * Fix progress bar by patching ray/worker
    
    * Fix comments

--- prototype/sky/skylet/ray_patches/__init__.py
+++ prototype/sky/skylet/ray_patches/__init__.py
@@ -11,3 +11,7 @@ def patch() -> None:
     path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                         'log_monitor.py')
     shutil.copyfile(path, log_monitor.__file__)
+
+    from ray import worker
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'worker.py')
+    shutil.copyfile(path, worker.__file__)
