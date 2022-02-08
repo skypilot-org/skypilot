21c2d4207e560b95c92f32070a79646ce2c9a629
Gautam Mittal <gautam@mittal.net>
2022-02-07 21:09:06 -0800

    Delete test.py

--- prototype/test.py
+++
@@ -1,9 +0,0 @@
-import sky
-
-with sky.Dag() as dag:
-    train = sky.Task(
-        'train',
-        setup=setup,
-        run=run,
-    )
-    train.set_resources({sky.Resources(accelerators='V100')})
