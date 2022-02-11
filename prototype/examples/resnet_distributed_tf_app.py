4463bfea671f4e5d07d3fb024049c5d3f4ed579c
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-02-11 11:40:28 -0800

    Support multi-node tasks with any num_nodes smaller than the cluster (#272)
    * Use SKY_NODE_IPS and SKY_NODE_RANK
    
    * Update torch distributed training
    
    * Fix torch distirbuted training yaml
    
    * Fix node id in distributed torch yaml
    
    * Fix torch distributed yaml
    
    * Change python API for multi-node
    
    * Fix run_fn
    
    * Fix distributed tf
    
    * Add ray tune yaml
    
    * Support task that requires less nodes
    
    * Update comment
    
    * rename the run_fn
    
    * yaml for ray tune
    
    * Add todo to horovod example
    
    * Fix the keyboard interrupt when sky logs
    
    * Update file_mounts on worker nodes

--- prototype/examples/resnet_distributed_tf_app.py
+++ prototype/examples/resnet_distributed_tf_app.py
@@ -1,10 +1,7 @@
-import json
 import subprocess
-from typing import Dict, List
+from typing import List, Optional
 
 import sky
-
-IPAddr = str
 
 with sky.Dag() as dag:
     # The working directory contains all code and will be synced to remote.
@@ -16,8 +13,6 @@ with sky.Dag() as dag:
         shell=True,
         check=True)
 
-    docker_image = None  # 'rayproject/ray-ml:latest-gpu'
-
     # Total Nodes, INCLUDING Head Node
     num_nodes = 2
 
@@ -26,52 +21,45 @@ with sky.Dag() as dag:
            pip3 install ray[default] awscli botocore boto3 && \
            conda create -n resnet python=3.7 -y && \
            conda activate resnet && \
-           pip install tensorflow==2.4.0 pyyaml ray[default] awscli botocore boto3 && \
+           pip install tensorflow==2.4.0 pyyaml && \
            cd models && pip install -e .'
 
-    # Post setup function. Run after `ray up *.yml` completes. Returns
-    # dictionary of commands to be run on each corresponding node.
-    # 'ip_list': List of IPs, 0th index denoting head worker.
-    def post_setup_fn(ip_list: List[IPAddr]) -> Dict[IPAddr, str]:
-        command_dict = {}
+    # The command to run.  Will be run under the working directory.
+    # If a str, run the same command on all nodes.
+    # Generates per-node commands.  Must be a self-contained lambda
+    # that doesn't refer to any external variables.
+    #
+    # Will be run under the working directory.
+    def run_fn(node_rank: int, ip_list: List[str]) -> Optional[str]:
+        import json
         tf_config = {
             'cluster': {
                 'worker': [ip + ':8008' for ip in ip_list]
             },
             'task': {
                 'type': 'worker',
-                'index': -1
+                'index': node_rank
             }
         }
-        for i, ip in enumerate(ip_list):
-            tf_config['task']['index'] = i
-            str_tf_config = json.dumps(tf_config).replace('"', '\\"')
-            command_dict[
-                ip] = "echo \"export TF_CONFIG='" + str_tf_config + "'\" >> ~/.bashrc"
-        return command_dict
-
-    # The command to run.  Will be run under the working directory.
-    # If a str, run the same command on all nodes.
-    # If a function, run per-node command on each node.
-    def run_fn(ip_list: List[IPAddr]) -> Dict[IPAddr, str]:
-        run = 'conda activate resnet && \
-            rm -rf resnet_model-dir && \
-            export XLA_FLAGS=\'--xla_gpu_cuda_data_dir=/usr/local/cuda/\' && \
+        str_tf_config = json.dumps(tf_config)
+        print(f'{str_tf_config!r}')
+        run = f"""
+            conda activate resnet
+            rm -rf resnet_model-dir
+            export TF_CONFIG={str_tf_config!r}
+            export XLA_FLAGS='--xla_gpu_cuda_data_dir=/usr/local/cuda/'
             python models/official/resnet/resnet_main.py --use_tpu=False \
-            --mode=train --train_batch_size=256 --train_steps=500 \
-            --iterations_per_loop=125 \
-            --data_dir=gs://cloud-tpu-test-datasets/fake_imagenet \
-            --model_dir=resnet-model-dir \
-            --amp --xla --loss_scale=128'
-
-        return {ip: run for ip in ip_list}
+                --mode=train --train_batch_size=256 --train_steps=500 \
+                --iterations_per_loop=125 \
+                --data_dir=gs://cloud-tpu-test-datasets/fake_imagenet \
+                --model_dir=resnet-model-dir \
+                --amp --xla --loss_scale=128"""
+        return run
 
     train = sky.Task(
         'train',
         workdir=workdir,
         setup=setup,
-        post_setup_fn=post_setup_fn,
-        docker_image=docker_image,
         num_nodes=num_nodes,
         run=run_fn,
     )
@@ -83,3 +71,4 @@ with sky.Dag() as dag:
 
 # sky.launch(dag, dryrun=True)
 sky.launch(dag, cluster_name='dtf')
+# sky.exec(dag, cluster_name='dtf')
