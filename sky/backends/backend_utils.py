8e9d891853936efb3606abbb6c7e3f3f2dffaedd
Wei-Lin Chiang <weichiang@berkeley.edu>
2022-02-17 21:17:31 -0800

    Fix Azure's default disk size and make all clouds disk sizes configurable (#350)
    * Fix Azure's default disk size and make all clouds configurable
    
    * yapf
    
    * yapf
    
    * Add example
    
    * Set a lowerbound for os disk size
    
    * Rename os_disk_size to disk_size
    
    * Add disk size option for CLI
    
    * yapf
    
    * lsblk
    
    * rebuild docs
    
    * Add FIXME

--- sky/backends/backend_utils.py
+++ sky/backends/backend_utils.py
@@ -428,6 +428,7 @@ def write_cluster_config(task: task_lib.Task,
                 'workdir': task.workdir,
                 'docker_image': task.docker_image,
                 'num_nodes': num_nodes,
+                'disk_size': to_provision.disk_size,
                 # File mounts handling.
                 'file_mounts': wrapped_file_mounts,
                 'initialization_commands': initialization_commands or None,
