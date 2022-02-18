62794a7a6a36f12eab1043588b333f600938f54d
Wei-Lin Chiang <weichiang@berkeley.edu>
2022-02-17 21:17:31 -0800

    Fix Azure's default disk size and make all clouds disk sizes configurable (#350)
    * Fix Azure's default disk size and make all clouds configurable
    
    * yapf
    
    * yapf
    
    * Add example
    
    * Set a lowerbound for os disk size
    
    * Rename os_disk_size to disk_size
    
    * Add disk size option for CLI
    
    * yapf
    
    * lsblk
    
    * rebuild docs
    
    * Add FIXME

--- sky/resources.py
+++ sky/resources.py
@@ -6,6 +6,8 @@ from sky import sky_logging
 
 logger = sky_logging.init_logger(__name__)
 
+DEFAULT_DISK_SIZE = 256
+
 
 class Resources:
     """A cloud resource bundle.
@@ -38,6 +40,7 @@ class Resources:
         accelerators: Union[None, str, Dict[str, int]] = None,
         accelerator_args: Optional[Dict[str, str]] = None,
         use_spot: Optional[bool] = None,
+        disk_size: Optional[int] = None,
     ):
         self.cloud = cloud
         self.instance_type = instance_type
@@ -66,6 +69,11 @@ class Resources:
         self._use_spot_specified = use_spot is not None
         self.use_spot = use_spot if use_spot is not None else False
 
+        if disk_size is not None:
+            assert disk_size >= 50, 'OS disk size must be larger than 50GB.'
+            self.disk_size = disk_size
+        else:
+            self.disk_size = DEFAULT_DISK_SIZE
         self._try_validate_accelerators()
 
     def __repr__(self) -> str:
