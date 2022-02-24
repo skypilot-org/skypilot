0bd2031caac0f3bad5eb460b790f073b883eb05a
Siyuan <suquark@gmail.com>
2022-02-23 18:08:30 -0800

    update CI & formatter

--- format.sh
+++ format.sh
@@ -1,100 +1,228 @@
 #!/usr/bin/env bash
-# YAPF formatter, adapted from ray.
+# Formatter adapted from ray.
 #
 # Usage:
 #    # Do work and commit your work.
 
 #    # Format files that differ from origin/master.
-#    bash format.sh
+#    ./format.sh
 
-#    # Commit changed files with message 'Run yapf and pylint'
+#    # Commit changed files with message 'lint'
 #
 #
-# YAPF + Clang formatter (if installed). This script formats all changed files from the last mergebase.
+# Black + Flake8 formatter. This script formats all changed files from the last mergebase.
 # You are encouraged to run this locally before pushing changes for review.
 
 # Cause the script to exit if a single command fails
-set -eo pipefail
+set -euo pipefail
 
-# this stops git rev-parse from failing if we run this from the .git directory
-builtin cd "$(dirname "${BASH_SOURCE:-$0}")"
-ROOT="$(git rev-parse --show-toplevel)"
-builtin cd "$ROOT" || exit 1
+FLAKE8_VERSION_REQUIRED="3.9.1"
+BLACK_VERSION_REQUIRED="21.12b0"
+MYPY_VERSION_REQUIRED="0.782"
 
-YAPF_VERSION=$(yapf --version | awk '{print $2}')
-PYLINT_VERSION=$(pylint --version | head -n 1 | awk '{print $2}')
-PYLINT_QUOTES_VERSION=$(pip list | grep pylint-quotes | awk '{print $2}')
-
-# # params: tool name, tool version, required version
-tool_version_check() {
-    if [[ $2 != $3 ]]; then
-        echo "Wrong $1 version installed: $3 is required, not $2."
+check_python_command_exist() {
+    VERSION=""
+    case "$1" in
+        black)
+            VERSION=$BLACK_VERSION_REQUIRED
+            ;;
+        flake8)
+            VERSION=$FLAKE8_VERSION_REQUIRED
+            ;;
+        mypy)
+            VERSION=$MYPY_VERSION_REQUIRED
+            ;;
+        *)
+            echo "$1 is not a required dependency"
+            exit 1
+    esac
+    if ! [ -x "$(command -v "$1")" ]; then
+        echo "$1 not installed. Install the python package with: pip install $1==$VERSION"
         exit 1
     fi
 }
 
-tool_version_check "yapf" $YAPF_VERSION "0.32.0"
-tool_version_check "pylint" $PYLINT_VERSION "2.8.2"
-tool_version_check "pylint-quotes" $PYLINT_QUOTES_VERSION "0.2.3"
+check_python_command_exist black
+check_python_command_exist flake8
+check_python_command_exist mypy
 
-YAPF_FLAGS=(
-    '--style' "$ROOT/.style.yapf"
-    '--recursive'
-    '--parallel'
+# this stops git rev-parse from failing if we run this from the .git directory
+builtin cd "$(dirname "${BASH_SOURCE:-$0}")"
+
+ROOT="$(git rev-parse --show-toplevel)"
+builtin cd "$ROOT" || exit 1
+
+FLAKE8_VERSION=$(flake8 --version | head -n 1 | awk '{print $1}')
+BLACK_VERSION=$(black --version | awk '{print $2}')
+MYPY_VERSION=$(mypy --version | awk '{print $2}')
+
+# params: tool name, tool version, required version
+tool_version_check() {
+    if [ "$2" != "$3" ]; then
+        echo "WARNING: Ray uses $1 $3, You currently are using $2. This might generate different results."
+    fi
+}
+
+tool_version_check "flake8" "$FLAKE8_VERSION" "$FLAKE8_VERSION_REQUIRED"
+tool_version_check "black" "$BLACK_VERSION" "$BLACK_VERSION_REQUIRED"
+tool_version_check "mypy" "$MYPY_VERSION" "$MYPY_VERSION_REQUIRED"
+
+if [[ $(flake8 --version) != *"flake8_quotes"* ]]; then
+    echo "WARNING: Sky uses flake8 with flake8_quotes. Might error without it. Install with: pip install flake8-quotes"
+fi
+
+if [[ $(flake8 --version) != *"flake8-bugbear"* ]]; then
+    echo "WARNING: Sky uses flake8 with flake8-bugbear. Might error without it. Install with: pip install flake8-bugbear"
+fi
+
+# TODO(dmitri): When more of the codebase is typed properly, the mypy flags
+# should be set to do a more stringent check.
+MYPY_FLAGS=(
+    '--follow-imports=skip'
+    '--ignore-missing-imports'
 )
 
-YAPF_EXCLUDES=(
-    # '--exclude' 'python/build/*'
+MYPY_FILES=(
+  # TODO(suquark): Add more files later. Our code is pretty far from
+  # passing mypy currently.
+  '__init__.py'
 )
+
+BLACK_EXCLUDES=(
+    '--extend-exclude' 'build'
+    '--extend-exclude' 'sky/templates/*'
+)
+
+GIT_LS_EXCLUDES=(
+  ":(exclude)build/**"
+  ":(exclude).egg/**"
+  ":(exclude)*.egg-info/**"
+  ":(exclude)sky/templates/*"
+  ":(exclude)examples/*"
+  ":(exclude)sky/skylet/**"
+  ":(exclude)sky/clouds/service_catalog/data_fetchers/*"
+)
+
+# Runs mypy on each argument in sequence. This is different than running mypy
+# once on the list of arguments.
+mypy_on_each() {
+    pushd sky
+    for file in "$@"; do
+       echo "Running mypy on $file"
+       mypy ${MYPY_FLAGS[@]+"${MYPY_FLAGS[@]}"} "$file"
+    done
+    popd
+}
 
 # Format specified files
-format() {
-    yapf --in-place "${YAPF_FLAGS[@]}" "$@"
+format_files() {
+    local shell_files=() python_files=() bazel_files=()
+
+    local name
+    for name in "$@"; do
+      local base="${name%.*}"
+      local suffix="${name#${base}}"
+
+      local shebang=""
+      read -r shebang < "${name}" || true
+      case "${shebang}" in
+        '#!'*)
+          shebang="${shebang#/usr/bin/env }"
+          shebang="${shebang%% *}"
+          shebang="${shebang##*/}"
+          ;;
+      esac
+
+      if [ "${base}" = "WORKSPACE" ] || [ "${base}" = "BUILD" ] || [ "${suffix}" = ".BUILD" ] || [ "${suffix}" = ".bazel" ] || [ "${suffix}" = ".bzl" ]; then
+        bazel_files+=("${name}")
+      elif [ -z "${suffix}" ] && [ "${shebang}" != "${shebang#python}" ] || [ "${suffix}" != "${suffix#.py}" ]; then
+        python_files+=("${name}")
+      elif [ -z "${suffix}" ] && [ "${shebang}" != "${shebang%sh}" ] || [ "${suffix}" != "${suffix#.sh}" ]; then
+        shell_files+=("${name}")
+      else
+        echo "error: failed to determine file type: ${name}" 1>&2
+        return 1
+      fi
+    done
+
+    if [ 0 -lt "${#python_files[@]}" ]; then
+      black "${python_files[@]}" --skip-string-normalization
+    fi
+}
+
+format_all_scripts() {
+    command -v flake8 &> /dev/null;
+    HAS_FLAKE8=$?
+
+    echo "$(date)" "Black...."
+    if [ -z "${FORMAT_SH_PRINT_DIFF-}" ]; then
+      git ls-files -- '*.py' "${GIT_LS_EXCLUDES[@]}" | xargs -P 10 \
+        black "${BLACK_EXCLUDES[@]}" --skip-string-normalization
+    fi
+    echo "$(date)" "MYPY...."
+    mypy_on_each "${MYPY_FILES[@]}"
+    if [ $HAS_FLAKE8 ]; then
+      echo "$(date)" "Flake8...."
+      git ls-files -- '*.py' "${GIT_LS_EXCLUDES[@]}" | xargs -P 5 \
+        flake8 --config=.flake8
+    fi
+}
+
+# Format all files, and print the diff to stdout for travis.
+# Mypy is run only on files specified in the array MYPY_FILES.
+format_all() {
+    format_all_scripts "${@}"
+    echo "$(date)" "done!"
 }
 
 # Format files that differ from main branch. Ignores dirs that are not slated
 # for autoformat yet.
 format_changed() {
     # The `if` guard ensures that the list of filenames is not empty, which
-    # could cause yapf to receive 0 positional arguments, making it hang
-    # waiting for STDIN.
+    # could cause the formatter to receive 0 positional arguments, making
+    # Black error.
     #
-    # `diff-filter=ACM` and $MERGEBASE is to ensure we only format files that
+    # `diff-filter=ACRM` and $MERGEBASE is to ensure we only format files that
     # exist on both branches.
     MERGEBASE="$(git merge-base origin/master HEAD)"
 
-    if ! git diff --diff-filter=ACM --quiet --exit-code "$MERGEBASE" -- '*.py' &>/dev/null; then
-        git diff --name-only --diff-filter=ACM "$MERGEBASE" -- '*.py' | xargs -P 5 \
-             yapf --in-place "${YAPF_EXCLUDES[@]}" "${YAPF_FLAGS[@]}"
+    if ! git diff --diff-filter=ACRM --quiet --exit-code "$MERGEBASE" -- '*.py' "${GIT_LS_EXCLUDES[@]}" &>/dev/null; then
+        git diff --name-only --diff-filter=ACRM "$MERGEBASE" -- '*.py' "${GIT_LS_EXCLUDES[@]}" | xargs -P 5 \
+            black "${BLACK_EXCLUDES[@]}" --skip-string-normalization
+        if which flake8 >/dev/null; then
+            git diff --name-only --diff-filter=ACRM "$MERGEBASE" -- '*.py' "${GIT_LS_EXCLUDES[@]}" | xargs -P 5 \
+                 flake8 --config=.flake8
+        fi
     fi
-
 }
 
-# Format all files, and print the diff to stdout for travis.
-format_all() {
-    yapf --diff "${YAPF_FLAGS[@]}" "${YAPF_EXCLUDES[@]}" test python
-}
 
-## This flag formats individual files. --files *must* be the first command line
-## arg to use this option.
-if [[ "$1" == '--files' ]]; then
-   format "${@:2}"
-   # If `--all` is passed, then any further arguments are ignored and the
-   # entire python directory is formatted.
-elif [[ "$1" == '--all' ]]; then
-   format_all
+# This flag formats individual files. --files *must* be the first command line
+# arg to use this option.
+if [ "${1-}" == '--files' ]; then
+    format_files "${@:2}"
+# If `--all` or `--scripts` are passed, then any further arguments are ignored.
+# Format the entire python directory and other scripts.
+elif [ "${1-}" == '--all-scripts' ]; then
+    format_all_scripts "${@}"
+    if [ -n "${FORMAT_SH_PRINT_DIFF-}" ]; then git --no-pager diff; fi
+# Format the all Python, C++, Java and other script files.
+elif [ "${1-}" == '--all' ]; then
+    format_all "${@}"
+    if [ -n "${FORMAT_SH_PRINT_DIFF-}" ]; then git --no-pager diff; fi
 else
-   # Format only the files that changed in last commit.
-   format_changed
+    # Only fetch master since that's the branch we're diffing against.
+    git fetch origin master || true
+
+    # Format only the files that changed in last commit.
+    format_changed
 fi
 
-# Run Pylint
-pylint --load-plugins pylint_quotes sky
-
 if ! git diff --quiet &>/dev/null; then
-    echo 'Reformatted files. Please review and stage the changes.'
-    echo 'Changes not staged for commit:'
+    echo 'Reformatted changed files. Please review and stage the changes.'
+    echo 'Files updated:'
     echo
+
     git --no-pager diff --name-only
 
     exit 1
