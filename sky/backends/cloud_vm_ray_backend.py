0bf079cc1c9ea12b3f286a98373b6aec70990dce
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-02-25 15:50:20 -0800

    fix

--- sky/backends/cloud_vm_ray_backend.py
+++ sky/backends/cloud_vm_ray_backend.py
@@ -1174,7 +1174,7 @@ class CloudVmRayBackend(backends.Backend):
         ip_list = self._get_node_ips(handle.cluster_yaml, handle.launched_nodes)
         ssh_user, ssh_private_key = self._get_ssh_credential(
             handle.cluster_yaml)
-        
+
         log_path = os.path.join(self.log_dir, 'file_mounts.log')
 
         def sync_to_all_nodes(src: str,
@@ -1291,7 +1291,7 @@ class CloudVmRayBackend(backends.Backend):
                 symlink_command,
                 ssh_user=ssh_user,
                 ssh_private_key=ssh_private_key,
-                log_path=log_path
+                log_path=log_path,
                 ssh_control_name=self._ssh_control_name(handle))
             backend_utils.handle_returncode(returncode, symlink_command,
                                             'Failed to create symlinks.')
