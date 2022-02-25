8044c4842593abbe71f8e54e998eecf5615b6241
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-02-25 12:18:32 -0800

    Add comment

--- sky/cli.py
+++ sky/cli.py
@@ -650,6 +650,7 @@ def exec(cluster: str, entrypoint: str, detach_run: bool,
                         handle,
                         entrypoint,
                         stream_logs=True,
+                        # Allocate a pseudo-terminal to disable output buffering.
                         ssh_mode=backend_utils.SshMode.INTERACTIVE,
                         under_remote_workdir=True,
                         redirect_stdout_stderr=False)
