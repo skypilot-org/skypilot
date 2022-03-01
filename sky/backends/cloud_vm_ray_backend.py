536a61cad11b4060e990e28b4e6fb482b83e4a04
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-02-28 17:49:27 -0800

    Add warning for ctrl-c

--- sky/backends/cloud_vm_ray_backend.py
+++ sky/backends/cloud_vm_ray_backend.py
@@ -1438,7 +1438,7 @@ class CloudVmRayBackend(backends.Backend):
                 self.tail_logs(handle, job_id)
         finally:
             name = handle.cluster_name
-            logger.info(f'\n{fore.CYAN}Job ID: '
+            logger.info(f'{fore.CYAN}Job ID: '
                         f'{style.BRIGHT}{job_id}{style.RESET_ALL}'
                         '\nTo cancel the job:\t'
                         f'{backend_utils.BOLD}sky cancel {name} {job_id}'
@@ -1455,17 +1455,17 @@ class CloudVmRayBackend(backends.Backend):
         codegen.tail_logs(job_id)
         code = codegen.build()
         click.secho('Start streaming logs...', fg='yellow')
-        try:
-            self.run_on_head(
-                handle,
-                code,
-                stream_logs=True,
-                redirect_stdout_stderr=False,
-                # Allocate a pseudo-terminal to disable output buffering.
-                ssh_mode=backend_utils.SshMode.INTERACTIVE)
-        except KeyboardInterrupt:
-            # Do nothing. When receiving ctrl-c.
-            pass
+        returncode = self.run_on_head(
+            handle,
+            code,
+            stream_logs=True,
+            redirect_stdout_stderr=False,
+            # Allocate a pseudo-terminal to disable output buffering.
+            ssh_mode=backend_utils.SshMode.INTERACTIVE)
+        if returncode != 0:
+            # TODO (zhwu): find a way to distinguish ctrl-c from other errors.
+            logger.warning(f'\n{colorama.Fore.LIGHTBLACK_EX}The job will keep '
+                           f'running after ctrl-c.{colorama.Style.RESET_ALL}')
 
     def _add_job(self, handle: ResourceHandle, job_name: str) -> int:
         codegen = backend_utils.JobLibCodeGen()
