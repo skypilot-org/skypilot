fc779353bfec903c4984b773296c877bd37d5cab
Gautam Mittal <gautam@mittal.net>
2022-03-01 21:57:45 -0800

    Centralize sky_logs, polish logging, and update `sky start` documentation (#432)

--- sky/skylet/job_lib.py
+++ sky/skylet/job_lib.py
@@ -15,8 +15,7 @@ import pendulum
 
 from sky.skylet import util_lib
 
-SKY_LOGS_DIRECTORY = 'sky_logs'
-SKY_REMOTE_LOGS_ROOT = '~'
+SKY_LOGS_DIRECTORY = '~/sky_logs'
 
 
 class JobStatus(enum.Enum):
