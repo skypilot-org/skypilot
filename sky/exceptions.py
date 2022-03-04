51833cbd744972c51c7c8031fef82593b162a3e4
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-03-04 15:09:34 -0800

    Add exception

--- sky/exceptions.py
+++ sky/exceptions.py
@@ -12,3 +12,11 @@ class ResourcesUnavailableError(Exception):
 class ResourcesMismatchError(Exception):
     """Raised when resources are mismatched."""
     pass
+
+class CommandError(Exception):
+    """Raised when a command fails."""
+    def __init__(self, returncode: int, command: str, error_msg: str) -> None:
+        super().__init__()
+        self.returncode = returncode
+        self.command = command
+        self.error_msg = error_msg
