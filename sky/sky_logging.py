822ea6824f70a10f015184a703c7c7a4a5a6684a
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-03-09 11:16:20 -0800

    change back to info mode

--- sky/sky_logging.py
+++ sky/sky_logging.py
@@ -29,5 +29,5 @@ def init_logger(name: str):
 
     logger = logging.getLogger(name)
     logger.addHandler(h)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
     return logger
