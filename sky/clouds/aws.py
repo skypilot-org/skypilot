0b6c64ee666563e8890d34689b8030ee02b00497
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-03-10 11:48:42 -0800

    Fix help str for aws/azure credential check (#532)
    * Fix help str for azure credential check
    
    * Fix aws check
    
    * format

--- sky/clouds/aws.py
+++ sky/clouds/aws.py
@@ -206,6 +206,9 @@ class AWS(clouds.Cloud):
     def check_credentials(self) -> Tuple[bool, Optional[str]]:
         """Checks if the user has access credentials to this cloud."""
         help_str = (
+            ' Run the following commands:'
+            '\n    $ pip install boto3'
+            '\n    $ aws configure'
             '\n    For more info: '
             'https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-quickstart.html'  # pylint: disable=line-too-long
         )
@@ -213,9 +216,7 @@ class AWS(clouds.Cloud):
         # `aws` to access private storage buckets.
         # `aws configure list` does not guarantee this file exists.
         if not os.path.isfile(os.path.expanduser('~/.aws/credentials')):
-            return (False,
-                    '~/.aws/credentials does not exist. Run `aws configure`.' +
-                    help_str)
+            return (False, '~/.aws/credentials does not exist.' + help_str)
         try:
             output = _run_output('aws configure list')
         except subprocess.CalledProcessError:
@@ -241,7 +242,7 @@ class AWS(clouds.Cloud):
                     secret_key_ok = True
         if access_key_ok and secret_key_ok:
             return True, None
-        return False, 'AWS credentials not set. Run `aws configure`.' + help_str
+        return False, 'AWS credentials not set.' + help_str
 
     def get_credential_file_mounts(self) -> Tuple[Dict[str, str], List[str]]:
         return {'~/.aws': '~/.aws'}, []
