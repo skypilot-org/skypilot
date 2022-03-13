5a30f05c1572497f343c38efcd28a26c5cffec6d
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-03-12 23:17:33 -0800

    Fix the dpkg lock for apt install (#554)
    * Disable unattended-upgrade
    
    * minor: set ray version constraint
    
    * Disable unattended-upgrades for azure as well
    
    * Address comments
    
    * fix comments

--- sky/setup_files/setup.py
+++ sky/setup_files/setup.py
@@ -21,7 +21,9 @@ install_requires = [
     'pycryptodome==3.12.0',
     'pendulum',
     'PrettyTable',
-    'ray[default]',
+    # Lower local ray version is not fully supported, due to the
+    # autoscaler issues (also tracked in #537).
+    'ray[default]>=1.9.0',
     'rich',
     'tabulate',
     # This is used by ray. The latest 1.44.0 will generate an error
