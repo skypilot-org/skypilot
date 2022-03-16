f3dc00ab65b28d7e142ddf0db7dfd8d1bcd0d0b8
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-03-16 10:47:44 -0700

    add hint for installing gcloud

--- sky/backends/cloud_vm_ray_backend.py
+++ sky/backends/cloud_vm_ray_backend.py
@@ -737,6 +737,13 @@ class RetryingVmProvisioner(object):
                     f'  TPU type {tpu_type} is not available in this zone.')
                 return False
 
+            if 'gcloud: command not found' in stderr:
+                logger.info(
+                    f'  TPU provision requires library \'gcloud\'. '
+                    'To install gcloud, run the following commands:\n'
+                    '    $ conda install -c conda-forge google-cloud-sdk')
+                raise exceptions.ResourcesUnavailableError(no_retry=True)
+
             logger.error(stderr)
             raise e
 
