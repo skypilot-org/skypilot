5e98f2e9337da4aa23abff104118fbd152dec93b
Zongheng Yang <zongheng.y@gmail.com>
2022-03-18 09:31:45 -0700

    Remove job_submit.log

--- sky/backends/cloud_vm_ray_backend.py
+++ sky/backends/cloud_vm_ray_backend.py
@@ -1650,7 +1650,6 @@ class CloudVmRayBackend(backends.Backend):
                            stream_logs=False)
         remote_log_dir = self.log_dir
         local_log_dir = os.path.expanduser(remote_log_dir)
-        job_log_path = os.path.join(local_log_dir, 'job_submit.log')
         remote_log_path = os.path.join(remote_log_dir, 'run.log')
 
         assert executable == 'python3', executable
@@ -1663,7 +1662,6 @@ class CloudVmRayBackend(backends.Backend):
 
         returncode = self.run_on_head(handle,
                                       f'{cd} && {job_submit_cmd}',
-                                      log_path=job_log_path,
                                       stream_logs=False)
         backend_utils.handle_returncode(returncode, job_submit_cmd,
                                         f'Failed to submit job {job_id}.')
