eef3f1b0d29147a123a59ab17d3691a36f200165
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-03-19 19:32:40 -0700

    format

--- sky/backends/cloud_vm_ray_backend.py
+++ sky/backends/cloud_vm_ray_backend.py
@@ -1298,9 +1298,8 @@ class CloudVmRayBackend(backends.Backend):
                 # know the actual previous status of the cluster.
                 cmd = backend_utils.JobLibCodeGen.update_status()
                 with console.status('[bold cyan]Preparing Job Queue'):
-                    returncode, _, stderr = self.run_on_head(handle,
-                                                            cmd,
-                                                            require_outputs=True)
+                    returncode, _, stderr = self.run_on_head(
+                        handle, cmd, require_outputs=True)
                 backend_utils.handle_returncode(returncode, cmd,
                                                 'Failed to update job status.',
                                                 stderr)
