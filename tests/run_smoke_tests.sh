70e0416f7d3b3ef8c76250944b87181b60f09084
Zongheng Yang <zongheng.y@gmail.com>
2022-03-22 10:31:03 -0700

    Support test_azure_start_stop_two_nodes() under --runslow.

--- tests/run_smoke_tests.sh
+++ tests/run_smoke_tests.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-time pytest -s -n 16 -q --tb=short --disable-warnings tests/test_smoke.py
+pytest -s -n 16 -q --tb=short --disable-warnings tests/test_smoke.py
+
+# To run all tests including the slow ones, add the --runslow flag:
+# pytest --runslow -s -n 16 -q --tb=short --disable-warnings tests/test_smoke.py
