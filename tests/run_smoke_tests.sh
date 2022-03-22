904be72b70754d03da2c6a1d99f14cff04835843
Zongheng Yang <zongheng.y@gmail.com>
2022-03-22 13:08:30 -0700

    Support passing a test name.

--- tests/run_smoke_tests.sh
+++ tests/run_smoke_tests.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-pytest -s -n 16 -q --tb=short --disable-warnings tests/test_smoke.py
+# Usage:
+#
+#   # Run everything
+#   bash tests/run_smoke_tests.sh
+#
+#   # Re-run a failed test
+#   bash tests/run_smoke_tests.sh test_azure_start_stop
+#
+
+test=${1:-""}
+if [ -z "$test" ]
+then
+    test_spec=tests/test_smoke.py
+else
+    test_spec=tests/test_smoke.py::"$test"
+fi
+
+pytest -s -n 16 -q --tb=short --disable-warnings "$test_spec"
 
 # To run all tests including the slow ones, add the --runslow flag:
 # pytest --runslow -s -n 16 -q --tb=short --disable-warnings tests/test_smoke.py
