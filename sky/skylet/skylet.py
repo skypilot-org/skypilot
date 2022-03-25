41f3d411a13a743ba5bf6f2bcedd35018e1ab4e2
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-03-25 11:45:00 -0700

    address comments

--- sky/skylet/skylet.py
+++ sky/skylet/skylet.py
@@ -1,8 +1,11 @@
-"""skylet: the daemon function for skylet."""
+"""skylet: a daemon running on the head node of a cluster."""
 
 import time
+
 from sky.skylet import job_lib
 
+timestamp = time.strftime('[%Y-%m-%d %H:%M:%S]', time.localtime())
+print(f'[{timestamp}] skylet started')
 while True:
     time.sleep(20)
     job_lib.update_status()
