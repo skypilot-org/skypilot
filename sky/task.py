d6e809ca4731c6b38ed4431dbe96eb687e1de9b7
Gautam Mittal <gautam@mittal.net>
2022-04-09 17:02:08 -0700

    similar keys

--- sky/task.py
+++ sky/task.py
@@ -62,6 +62,26 @@ def _is_valid_env_var(name: str) -> bool:
     return bool(re.fullmatch(_VALID_ENV_VAR_REGEX, name))
 
 
+def _get_similar_keys(name: str, keys: List[str]) -> List[str]:
+    """Return a list of keys that are similar to name.
+
+    Original source:
+    https://github.com/tensorflow/lingvo/blob/master/lingvo/core/hyperparams.py
+    """
+    def overlaps(name: str, key: str) -> float:
+      """The fraction of 3-char substrings in <name> that appear in key."""
+      matches = 0
+      trials = 0
+      for i in range(len(name) - 3):
+        trials += 1
+        if name[i:i + 3] in key:
+          matches += 1
+      if trials:
+        return float(matches) / trials
+      return 0
+    return [key for key in keys if overlaps(name, key) > 0.5]
+
+
 class Task:
     """Task: a coarse-grained stage in an application."""
 
