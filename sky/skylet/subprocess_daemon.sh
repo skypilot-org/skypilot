e8bb7af3181607e4c8d13bb8c96651ce3305df06
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-04-22 14:16:31 -0700

    fix PIPE kill

--- sky/skylet/subprocess_daemon.sh
+++ sky/skylet/subprocess_daemon.sh
@@ -6,9 +6,9 @@ proc_pid=$2
 
 while kill -s 0 ${parent_pid}; do sleep 1; done 
 
+# This is to avoid the PIPE outputing to the console after being killed in next line.
+pkill -PIPE -P ${proc_pid}
 if [[ $OSTYPE == 'darwin'* ]]; then
-    # This is to avoid the PIPE outputing to the console after being killed in next line.
-    pkill -PIPE -P ${proc_pid}
     # MacOS do not have the ps --forest option, it is enough to do pkill here. (pkill
     # only kills the first level descendant of the process, not recursively find all
     # of the descendants.)
