0a608d00efa0185e4df609396072acb18083db19
Siyuan (Ryans) Zhuang <suquark@gmail.com>
2022-04-29 11:03:17 -0700

    Reuse sky wheels for efficiency (#769)
    * cache sky wheels

--- tests/test_wheels.py
+++ tests/test_wheels.py
@@ -1,5 +1,16 @@
+import time
+import shutil
 from sky.backends import wheel_utils
 
 
 def test_build_wheels():
+    shutil.rmtree(wheel_utils.WHEEL_DIR, ignore_errors=True)
+    start = time.time()
     assert wheel_utils.build_sky_wheel().exists()
+    duration = time.time() - start
+
+    start = time.time()
+    assert wheel_utils.build_sky_wheel().exists()
+    duration_cached = time.time() - start
+
+    assert duration_cached < duration
