ead56409f40dcf86dfddca5625d05a455ea20f09
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-05-04 21:49:26 -0700

    File mounts for managed spot jobs (#788)
    * Fix storage from_yaml_config
    
    * Add sky storage support for managed spot jobs
    
    * add example for storage in managed spot job
    
    * Fix testing for the spot storage
    
    * format
    
    * format
    
    * Add todos
    
    * Fix test
    
    * Fix comments
    
    * format
    
    * Fix retry cnt
    
    * Fix test name
    
    * Fix test by adding flush
    
    * delete storage after spot task finish
    
    * persistent=false
    
    * Add new line at the end
    
    * address comments

--- sky/data/storage.py
+++ sky/data/storage.py
@@ -607,18 +607,17 @@ class Storage(object):
 
     @classmethod
     def from_yaml_config(cls, config: Dict[str, str]) -> 'Storage':
-        name = config.get('name')
-        source = config.get('source')
-        store = config.get('store')
-        mode_str = config.get('mode')
+        name = config.pop('name', None)
+        source = config.pop('source', None)
+        store = config.pop('store', None)
+        mode_str = config.pop('mode', None)
         if isinstance(mode_str, str):
             # Make mode case insensitive, if specified
             mode = StorageMode(mode_str.upper())
         else:
             # Make sure this keeps the same as the default mode in __init__
             mode = StorageMode.MOUNT
-        persistent = True if config.get(
-            'persistent') is None else config['persistent']
+        persistent = config.pop('persistent', True)
         # Validation of the config object happens on instantiation.
         storage_obj = cls(name=name,
                           source=source,
@@ -626,6 +625,9 @@ class Storage(object):
                           mode=mode)
         if store is not None:
             storage_obj.add_store(StoreType(store.upper()))
+        if config:
+            raise exceptions.StorageSpecError(
+                f'Invalid storage spec. Unknown fields: {config.keys()}')
         return storage_obj
 
     def to_yaml_config(self) -> Dict[str, str]:
@@ -635,7 +637,12 @@ class Storage(object):
             if value is not None:
                 config[key] = value
 
-        add_if_not_none('name', self.name)
+        name = self.name
+        if self.source is not None and data_utils.is_cloud_store_url(
+                self.source):
+            # Remove name if source is a cloud store URL
+            name = None
+        add_if_not_none('name', name)
         add_if_not_none('source', self.source)
 
         stores = None
