2310637c0e5415dc3abd3a0fba2f96e478860a4f
Siyuan <suquark@gmail.com>
2022-05-06 18:28:57 -0700

    fix a rare case

--- tests/test_wheels.py
+++ tests/test_wheels.py
@@ -1,3 +1,4 @@
+import os
 import time
 import shutil
 from sky.backends import wheel_utils
@@ -14,3 +15,13 @@ def test_build_wheels():
     duration_cached = time.time() - start
 
     assert duration_cached < duration
+
+    # simulate uncleaned symlinks due to interruption
+    shutil.rmtree(wheel_utils.WHEEL_DIR, ignore_errors=True)
+    wheel_utils.WHEEL_DIR.mkdir()
+    (wheel_utils.WHEEL_DIR / 'sky').symlink_to(wheel_utils.SKY_PACKAGE_PATH,
+                                               target_is_directory=True)
+    for root, _, _ in os.walk(str(wheel_utils.WHEEL_DIR)):
+        # set file date to 1970-01-01 00:00 UTC
+        os.utime(root, (0, 0))
+    assert wheel_utils.build_sky_wheel().exists()
