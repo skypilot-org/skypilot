d9bd3b028d0d89a6dd8fc3f528c1c92112c346cf
Zongheng Yang <concretevitamin@users.noreply.github.com>
2022-05-09 21:04:19 -0700

    Fix autoscaler benign assertion by patching (#815)
    * Patch resource_demand_scheduler.py from Ray 1.10.0.
    
    * Make multi_echo test autoscaler bug.
    
    * Fix LICENSE, comments, test
    
    * Change examples/multi_echo.py to use thread pool

--- sky/skylet/ray_patches/__init__.py
+++ sky/skylet/ray_patches/__init__.py
@@ -4,9 +4,14 @@ import shutil
 
 
 def patch() -> None:
-    # Patch the buggy ray file. This should only be called
+    # Patch the buggy ray files. This should only be called
     # from an isolated python process, because once imported
     # the python module would persist in the memory.
     from ray import worker
     path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'worker.py')
     shutil.copyfile(path, worker.__file__)
+
+    from ray.autoscaler._private import resource_demand_scheduler
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                        'resource_demand_scheduler.py')
+    shutil.copyfile(path, resource_demand_scheduler.__file__)
