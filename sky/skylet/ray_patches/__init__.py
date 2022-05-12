8ee5e40e9031be34022e6a6e0a2f9137a8bda8b5
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-05-11 19:51:40 -0700

    Fix the upscaling=0

--- sky/skylet/ray_patches/__init__.py
+++ sky/skylet/ray_patches/__init__.py
@@ -49,3 +49,7 @@ def patch() -> None:
     from ray.autoscaler._private import resource_demand_scheduler
     _run_patch(resource_demand_scheduler.__file__,
                _to_absolute('resource_demand_scheduler.py.patch'))
+
+    from ray.autoscaler._private import autoscaler
+    _run_patch(autoscaler.__file__,
+               _to_absolute('autoscaler.py.patch'))
