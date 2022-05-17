0f986a1bb03cfc4ca570813cbb06ab4efabe3f3e
Wei-Lin Chiang <weichiang@berkeley.edu>
2022-05-16 21:27:39 -0700

    Fix sky cancel for on-prem mode (#775)
    * Replace ps forest with pstree to handle on-prem
    
    * remove pgid
    
    * Fix
    
    * Add tests for all clouds
    
    * Python based subprocess daemon
    
    * yapf
    
    * rm subprocess_daemon.sh
    
    * comment
    
    * add setup and fix test
    
    * replace workdir with git clone for test_distributed_tf
    
    * Fix sky spot issue
    
    * Needs more time for sky cancel to work
    
    * Fix bug
    
    * address comments

---
+++ sky/skylet/subprocess_daemon.py
@@ -0,0 +1,52 @@
+"""Sky subprocess daemon.
+
+Wait for parent_pid to exit, then SIGTERM (or SIGKILL if needed) the child
+processes of proc_pid.
+"""
+
+import argparse
+import time
+import sys
+
+import psutil
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--parent-pid', type=int, required=True)
+    parser.add_argument('--proc-pid', type=int, required=True)
+    args = parser.parse_args()
+
+    parent_process = psutil.Process(args.parent_pid)
+    process = psutil.Process(args.proc_pid)
+
+    if process is None or parent_process is None:
+        sys.exit()
+
+    # Wait for either parent or target process to exit.
+    while True:
+        time.sleep(1)
+        if not process.is_running() or not parent_process.is_running():
+            break
+
+    try:
+        children = process.children(recursive=True)
+        children.append(process)
+    except psutil.NoSuchProcess:
+        sys.exit()
+
+    for pid in children:
+        try:
+            pid.terminate()
+        except psutil.NoSuchProcess:
+            pass
+
+    # Wait 30s for the processes to exit gracefully.
+    time.sleep(30)
+
+    # SIGKILL if they're still running.
+    for pid in children:
+        try:
+            pid.kill()
+        except psutil.NoSuchProcess:
+            pass
