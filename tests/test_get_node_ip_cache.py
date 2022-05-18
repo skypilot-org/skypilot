76ccc44e6fbc69abb66e0ecae95c1cd6e8864e7c
Siyuan <suquark@gmail.com>
2022-05-18 14:52:01 -0700

    unit test

---
+++ tests/test_get_node_ip_cache.py
@@ -0,0 +1,26 @@
+import pytest
+
+import sky
+from sky import backends
+from sky import exceptions
+from sky import global_user_state
+from sky.backends import backend_utils
+
+
+def test_head_ip_cache_invalidated():
+    # This unittest ensures 'get_node_ips' will error with the
+    # cached IP but actually the IP is
+    cluster_name = 'test-head-ip-cache-invalidated'
+    handle = backends.CloudVmRayBackend.ResourceHandle(
+        cluster_name=cluster_name,
+        cluster_yaml='/tmp/cluster1.yaml',
+        head_ip='169.254.0.0',
+        launched_nodes=1,
+        launched_resources=sky.Resources(sky.AWS(),
+                                         instance_type='p3.2xlarge',
+                                         region='us-east-1'),
+    )
+    global_user_state.add_or_update_cluster(cluster_name, handle, ready=True)
+    with pytest.raises(exceptions.FetchIPError):
+        backend_utils.get_node_ips('', 1, cluster_name=cluster_name)
+    global_user_state.remove_cluster(cluster_name, terminate=True)
