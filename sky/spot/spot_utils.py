67459e9b2ac5b16f21f0c073ed928059309ead09
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-05-22 19:44:08 -0700

    message for cluster failed as well

--- sky/spot/spot_utils.py
+++ sky/spot/spot_utils.py
@@ -183,7 +183,10 @@ def stream_logs_by_id(job_id: int) -> str:
 
     if job_status.is_terminal():
         job_msg = ''
-        if job_status == spot_state.SpotStatus.FAILED:
+        if job_status in [
+                spot_state.SpotStatus.FAILED,
+                spot_state.SpotStatus.CLUSTER_FAILED
+        ]:
             job_msg = ('\nFor detailed error message, please check: '
                        f'{colorama.Style.BRIGHT}sky logs '
                        f'{constants.SPOT_CONTROLLER_NAME} {job_id}'
