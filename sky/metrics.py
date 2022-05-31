2ae167b9a37261725b40af3ab56240b821a5de4a
mraheja <mraheja@berkeley.edu>
2022-05-31 01:53:01 -0700

    yapf

--- sky/metrics.py
+++ sky/metrics.py
@@ -8,10 +8,12 @@ from sky.utils import base_utils
 PROM_PUSHGATEWAY_URL = '3.216.190.117:9091'
 CURRENT_CLUSTER_NAME = 'NONE'
 
+
 class Label:
     """
     Label for prometheus metric
     """
+
     def __init__(self, name):
         self.name = name
         self.val = 'null'
@@ -24,6 +26,7 @@ class Metric:
     """
     Single prometheus Gauge metric
     """
+
     def __init__(self, name, desc):
         self.name = name
         self.desc = desc
@@ -46,11 +49,12 @@ class MetricLogger:
     """
     Provides decorator to wrap functions that need to be logged
     """
+
     def __init__(self,
                  func_name,
                  labels=None,
                  metrics=None,
-                 with_cluster_name = False,
+                 with_cluster_name=False,
                  with_return_code=False,
                  with_runtime=False):
 
@@ -74,7 +78,7 @@ class MetricLogger:
             self.return_code_metric = self.func_name + '_return_code'
             self.metrics.append(
                 Metric(self.return_code_metric,
-                    f'Return code for {self.func_name}'))
+                       f'Return code for {self.func_name}'))
 
         if with_runtime:
             self.runtime_metric = self.func_name + '_runtime'
@@ -95,9 +99,7 @@ class MetricLogger:
 
             @functools.wraps(func)
             def wrapper_logging(*args, **kwargs):
-                self.set_labels({
-                    'user': base_utils.get_user()
-                })
+                self.set_labels({'user': base_utils.get_user()})
                 saved_ex = None
                 try:
                     start = time.time()
@@ -107,7 +109,7 @@ class MetricLogger:
                             {self.runtime_metric: time.time() - start})
                     if self.with_cluster_name:
                         self.set_labels({'cluster_name': CURRENT_CLUSTER_NAME})
-                except Exception as ex: # pylint: disable=broad-except
+                except Exception as ex:  # pylint: disable=broad-except
                     if with_return_code:
                         self.set_return_code(-1)
                     cloud_logging.send_trace()
@@ -136,20 +138,28 @@ class MetricLogger:
     def set_return_code(self, value):
         self.set_metrics({self.return_code_metric: value})
 
+
 #### USER METRICS ###
 ### Labels: User ID, Cluster ID
 
+
 class TimerLogger(MetricLogger):
+
     def __init__(self, func_name):
         super().__init__(func_name, with_cluster_name=True, with_runtime=True)
+
     def __call__(self, func):
         return self.decorator(func)
+
 
 #### USER METRICS ###
 ### Labels: User ID
 
+
 class ReturnCodeLogger(MetricLogger):
+
     def __init__(self, func_name):
-        super().__init__(func_name, with_return_code = True)
+        super().__init__(func_name, with_return_code=True)
+
     def __call__(self, func):
         return self.decorator(func)
