a5fa8d3d28c89bef0bec363e860af20515ff9403
Michael Luo <michael.luo@berkeley.edu>
2022-06-02 12:49:54 -0700

    Fix storage delete if local is gone

--- sky/data/storage.py
+++ sky/data/storage.py
@@ -384,7 +384,8 @@ class Storage(object):
                     self.add_store(StoreType.GCS)
 
     @staticmethod
-    def _validate_source(source: str, mode: StorageMode) -> Tuple[str, bool]:
+    def _validate_source(source: str, mode: StorageMode,
+                         sync_on_reconstruction: bool) -> Tuple[str, bool]:
         """Validates the source path.
 
         Args:
@@ -408,7 +409,7 @@ class Storage(object):
                     f'Found source={source}')
             # Local path, check if it exists
             source = os.path.abspath(os.path.expanduser(source))
-            if not os.path.exists(source):
+            if not os.path.exists(source) and sync_on_reconstruction:
                 raise exceptions.StorageSourceError('Local source path does not'
                                                     f' exist: {source}')
             # Raise warning if user's path is a symlink
@@ -459,7 +460,7 @@ class Storage(object):
                     return
         elif self.source is not None:
             source, is_local_source = Storage._validate_source(
-                self.source, self.mode)
+                self.source, self.mode, self.sync_on_reconstruction)
 
             if not self.name:
                 if is_local_source:
