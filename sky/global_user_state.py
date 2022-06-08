7c503005cb411ced355075a9c6d6bc30162b7d98
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-06-08 01:18:11 -0700

    keep the latest cluster on the top

--- sky/global_user_state.py
+++ sky/global_user_state.py
@@ -238,7 +238,7 @@ def get_cluster_from_name(
 
 
 def get_clusters() -> List[Dict[str, Any]]:
-    rows = _DB.cursor.execute('select * from clusters')
+    rows = _DB.cursor.execute('select * from clusters order by launched_at desc')
     records = []
     for name, launched_at, handle, last_use, status, autostop in rows:
         # TODO: use namedtuple instead of dict
