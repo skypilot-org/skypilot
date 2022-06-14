0242b8d275f50ea74bf7417de2a79ba15f171173
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-06-13 21:00:52 -0700

    fix

--- sky/backends/cloud_vm_ray_backend.py
+++ sky/backends/cloud_vm_ray_backend.py
@@ -252,8 +252,8 @@ class RayCodeGen:
                 plural = 's' if {num_nodes} > 1 else ''
                 node_str = f'{num_nodes} node' + plural + '.'
                 print('SKY INFO: Waiting for task resources on ' + node_str + 
-                      '\\nSKY INFO: This will block if the cluster is full. '
-                      'Use Ctrl-C to exit log streaming (task will not be killed).',
+                      ' This will block if the cluster is full.\n'
+                      'SKY INFO: Use Ctrl-C to exit log streaming (task will not be killed).',
                       file=sys.stderr,
                       flush=True)
                 # FIXME: This will print the error message from autoscaler if
