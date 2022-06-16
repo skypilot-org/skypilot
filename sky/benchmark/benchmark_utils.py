11e995d60183699ff9521cfbfc46a569d93e3009
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2022-06-16 16:17:07 -0700

    Minor fix

--- sky/benchmark/benchmark_utils.py
+++ sky/benchmark/benchmark_utils.py
@@ -182,16 +182,16 @@ def generate_benchmark_configs(
         if 'setup' not in candidate_config:
             candidate_config['setup'] = ''
         candidate_config['setup'] = (
-            f'mkdir -p {benchmark_dir}; ' +
-            f'ln -s {benchmark_dir} {_SKY_REMOTE_BENCHMARK_DIR_SYMLINK}; ' +
-            candidate_config['setup'])
+            f'mkdir -p {benchmark_dir}; '
+            f'ln -s {benchmark_dir} {_SKY_REMOTE_BENCHMARK_DIR_SYMLINK}; '
+            + candidate_config['setup'])
 
         # Copy the sky callback wheel to ~/sky_utils.
         wheel_dir = backend_utils.SKY_REMOTE_PATH
         candidate_config['setup'] = (
-            'mkdir -p ~/sky_utils; ' +
-            f'cp "$(echo {wheel_dir}/sky_callback-*.whl)" ~/sky_utils; ' +
-            candidate_config['setup'])
+            'mkdir -p ~/sky_utils; '
+            f'cp "$(echo {wheel_dir}/sky_callback-*.whl)" ~/sky_utils; '
+            + candidate_config['setup'])
         candidate_configs.append(candidate_config)
 
     candidate_resources = _get_optimized_resources(candidate_configs)
