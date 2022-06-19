297ad4c651d38f266550f18208dceec11cfd337b
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2022-06-18 23:35:53 -0700

    Check sky_callback initialization

--- sky/callbacks/sky_callback/api.py
+++ sky/callbacks/sky_callback/api.py
@@ -25,23 +25,31 @@ from typing import Optional
 from sky_callback import base
 
 _DISABLE_CALLBACK = os.environ.get('SKY_DISABLE_CALLBACK', 'False').lower() in ('true', '1')
-_SKY_CALLBACK = None
+_sky_callback = None
+_initialized = False
 
 
 def init(global_rank: int = 0, log_dir: Optional[str] = None, total_steps: Optional[int] = None) -> None:
     if global_rank == 0 and not _DISABLE_CALLBACK:
-        global _SKY_CALLBACK
-        _SKY_CALLBACK = base.BaseCallback(log_dir=log_dir, total_steps=total_steps)
+        global _sky_callback, _initialized
+        _sky_callback = base.BaseCallback(log_dir=log_dir, total_steps=total_steps)
+    _initialized = True
 
 
 def on_step_begin() -> None:
-    if _SKY_CALLBACK is not None:
-        _SKY_CALLBACK.on_step_begin()
+    if not _initialized:
+        raise RuntimeError('sky_callback is not initialized. '
+            'Please call `sky_callback.init` before using sky_callback.')
+    if _sky_callback is not None:
+        _sky_callback.on_step_begin()
 
 
 def on_step_end() -> None:
-    if _SKY_CALLBACK is not None:
-        _SKY_CALLBACK.on_step_end()
+    if not _initialized:
+        raise RuntimeError('sky_callback is not initialized. '
+            'Please call `sky_callback.init` before using sky_callback.')
+    if _sky_callback is not None:
+        _sky_callback.on_step_end()
 
 
 @contextlib.contextmanager
@@ -54,16 +62,19 @@ def step():
 class timer:
 
     def __init__(self, iterable: collections.Iterable) -> None:
+        if not _initialized:
+            raise RuntimeError('sky_callback is not initialized. '
+                'Please call `sky_callback.init` before using sky_callback.')
         self._iterable = iterable
 
     def __iter__(self):
         # Inlining for speed optimization.
         iterable = self._iterable
-        if _SKY_CALLBACK is None:
+        if _sky_callback is None:
             for obj in iterable:
                 yield obj
         else:
             for obj in iterable:
-                _SKY_CALLBACK.on_step_begin()
+                _sky_callback.on_step_begin()
                 yield obj
-                _SKY_CALLBACK.on_step_end()
+                _sky_callback.on_step_end()
