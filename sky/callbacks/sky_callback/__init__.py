ada19e03fefe038cf9da95f88f1e92292cac8749
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2022-06-18 22:11:13 -0700

    Add lazy import

--- sky/callbacks/sky_callback/__init__.py
+++ sky/callbacks/sky_callback/__init__.py
@@ -1,19 +1,48 @@
+from typing import Optional
+
 from sky_callback.api import (
     init,
-    config,
     on_step_begin,
     on_step_end,
     train_step,
     step,
 )
-from sky_callback.base import SkyCallback
+from sky_callback.base import BaseCallback
+
+
+# FIXME(woosuk): Find a better way for lazy imports.
+class CallbackLoader:
+
+    @staticmethod
+    def keras(log_dir: Optional[str] = None):
+        from sky_callback.integrations.keras import SkyKerasCallback
+        return SkyKerasCallback(log_dir=log_dir)
+
+    @staticmethod
+    def pytorch_lightning(log_dir: Optional[str] = None):
+        from sky_callback.integrations.pytorch_lightning import SkyPLCallback
+        return SkyPLCallback(log_dir=log_dir)
+
+    @staticmethod
+    def transformers(log_dir: Optional[str] = None):
+        from sky_callback.integrations.transformers import SkyHFCallback
+        return SkyHFCallback(log_dir=log_dir)
+
+
+SkyKerasCallback = CallbackLoader.keras
+SkyPLCallback = CallbackLoader.pytorch_lightning
+SkyHFCallback = CallbackLoader.transformers
 
 __all__ = [
+    # APIs
     'init',
-    'config',
     'on_step_begin',
     'on_step_end',
     'train_step',
     'step',
-    'SkyCallback',
+    # Callbacks
+    'BaseCallback',
+    'SkyKerasCallback',
+    'SkyPLCallback',
+    'SkyHFCallback',
 ]
