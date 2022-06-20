543f5e10e1053715fc6e8815576b609091299cba
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2022-06-20 01:14:43 -0700

    Implement PyTorch Lightning Callback

--- sky/callbacks/sky_callback/integrations/pytorch_lightning.py
+++ sky/callbacks/sky_callback/integrations/pytorch_lightning.py
@@ -29,8 +29,27 @@ class SkyPLCallback(pl.Callback):
             return
         assert self.sky_callback is None
         if trainer.global_rank == 0:
+            max_epochs = trainer.max_epochs
+            max_steps = trainer.max_steps
+            # This is safe because `on_train_start` is always called after
+            # `reset_train_dataloader` which sets `num_trainig_batches`.
+            num_training_batches = trainer.num_training_batches
+
+            if max_epochs == -1 and max_steps == -1:
+                # Infinite training.
+                total_steps = None
+            elif num_training_batches == float('inf'):
+                # Iterable dataset. `total_steps` is known only if
+                # `max_steps` is set.
+                total_steps = max_steps if max_steps != -1 else None
+            else:
+                total_steps = num_training_batches * max(max_epochs, 1)
+                if max_steps != -1:
+                    total_steps = min(total_steps, max_steps)
+            # FIXME(woosuk): Check the early stopping flag.
+            # If it is set, total_steps should be None.
             self.sky_callback = base.BaseCallback(log_dir=self.log_dir,
-                                                  total_steps=trainer.max_steps)
+                                                  total_steps=total_steps)
 
     def on_train_batch_start(
         self,
