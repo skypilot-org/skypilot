c4fa2c0f9d3c832bc5d36d5b463a94ca1cd3a69a
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2022-06-24 14:22:43 -0700

    Implement Keras Callback

--- sky/callbacks/sky_callback/integrations/keras.py
+++ sky/callbacks/sky_callback/integrations/keras.py
@@ -1,4 +1,6 @@
 """SkyCallback integration with Keras."""
+from typing import Optional
+
 from tensorflow import keras
 
 from sky_callback import base
@@ -21,18 +23,32 @@ class SkyKerasCallback(keras.callbacks.Callback):
         total_steps: A total number of steps.
     """
 
-    def __init__(self, log_dir=None, total_steps=None):
+    def __init__(self, log_dir: Optional[str] = None, total_steps: Optional[int] = None):
+        super(SkyKerasCallback, self).__init__()
         self.log_dir = log_dir
         self.total_steps = total_steps
         self.sky_callback = None
+
+    def _infer_total_steps(self):
+        if self.total_steps is not None:
+            return self.total_steps
+
+        epochs = self.params['epochs']
+        steps_per_epoch = self.params['steps']
+        if steps_per_epoch is None:
+            total_steps = None
+        else:
+            total_steps = epochs * steps_per_epoch
+        return total_steps
 
     def on_train_begin(self, logs=None):
         del logs  # Unused.
         if DISABLE_CALLBACK:
             return
-        # TODO(woosuk): Add support for distributed training.
         assert self.sky_callback is None
-        self.sky_callback = base.BaseCallback(log_dir=self.log_dir)
+        # TODO(woosuk): Add support for distributed training.
+        total_steps = self._infer_total_steps()
+        self.sky_callback = base.BaseCallback(log_dir=self.log_dir, total_steps=total_steps)
 
     def on_train_batch_begin(self, batch, logs=None):
         del batch, logs  # Unused.
