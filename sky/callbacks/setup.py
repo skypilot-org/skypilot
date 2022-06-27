318f2f2a586964a03a5636bad5d5550c5a9dbf21
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2022-06-27 04:27:14 -0700

    Fix SkyCallback version

--- sky/callbacks/setup.py
+++ sky/callbacks/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='sky-callback',
-    version='0.1.dev0',
+    version='0.1.0-dev0',
     packages=setuptools.find_packages(),
     install_requires=['psutil'],
 )
