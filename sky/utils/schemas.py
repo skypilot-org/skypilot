ad50526dfce54118aa2d822dd9aa46fd3e2859dc
Wei-Lin Chiang <infwinston@gmail.com>
2022-06-27 13:50:33 -0700

    fix schema check for sky spot

--- sky/utils/schemas.py
+++ sky/utils/schemas.py
@@ -23,7 +23,19 @@ def get_resources_schema():
                 'type': 'string',
             },
             'accelerators': {
-                'type': 'string',
+                'anyOf': [
+                    {
+                        "type": "string",
+                    },
+                    {
+                        "type": "object",
+                        'required': [],
+                        'maxProperties': 1,
+                        'additionalProperties': {
+                            'type': 'number'
+                        }
+                    }
+                ]
             },
             'instance_type': {
                 'type': 'string',
