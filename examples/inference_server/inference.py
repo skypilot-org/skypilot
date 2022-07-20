641e8fa4327727986365ce138593c894cce8636c
Isaac Ong <isaacong.jw@gmail.com>
2022-07-19 20:30:42 -0700

    Add data init and image transfer functionality

--- examples/inference_server/inference.py
+++ examples/inference_server/inference.py
@@ -3,13 +3,13 @@ import sys
 INFERENCE_RESULT_MARKER = "INFERENCE RESULT:"
 
 
-def run_inference(input):
-    # Perform some computation on input here
+def run_inference(image_path):
+    # Perform some computation on the image located at image_path
 
     # Instead of returning the result,
     # print it to stdout so that the server can retrieve the result from the logs
     print(
-        f"{INFERENCE_RESULT_MARKER}This is the result of running inference on '{input}'"
+        f"{INFERENCE_RESULT_MARKER}This is the result of running inference on the image at '{image_path}'"
     )
 
 
--- examples/inference_server/server.py
+++ examples/inference_server/server.py
@@ -12,11 +12,17 @@ import string
 import subprocess
 
 from flask import Flask, request, abort
-
-from inference import INFERENCE_RESULT_MARKER
+from werkzeug.utils import secure_filename
 import sky
 
+from inference import INFERENCE_RESULT_MARKER
+
+LOCAL_UPLOAD_FOLDER = '/local/path/to/folder'
+REMOTE_UPLOAD_FOLDER = '/remote/path/to/folder'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = LOCAL_UPLOAD_FOLDER
 
 
 def run_output(cmd: str, **kwargs) -> str:
@@ -28,38 +34,66 @@ def run_output(cmd: str, **kwargs) -> str:
     return completed_process.stdout.decode("ascii").strip()
 
 
-@app.route("/run-inference", methods=["GET"])
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/", methods=["GET", "POST"])
 def run_inference():
-    input = request.args.get("input")
-    if not input:
-        abort(400, "input query parameter is required")
+    if request.method == 'POST':
+        image = request.files['file']
+        if not image or not allowed_file(image.filename):
+            abort(400, "Invalid image upload")
 
-    with sky.Dag() as dag:
-        workdir = os.path.dirname(os.path.abspath(__file__))
-        task_name = "inference_task"
-        setup = 'pip3 install --upgrade pip'
-        run_fn = f"python inference.py {input}"
+        filename = secure_filename(image.filename)
+        local_image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        remote_image_path = os.path.join(REMOTE_UPLOAD_FOLDER, filename)
 
-        task = sky.Task(name=task_name,
-                        setup=setup,
-                        workdir=workdir,
-                        run=run_fn)
+        image.save(local_image_path)
 
-        resources = sky.Resources(cloud=sky.Azure())
-        task.set_resources(resources)
+        with sky.Dag() as dag:
+            workdir = os.path.dirname(os.path.abspath(__file__))
+            task_name = "inference_task"
+            setup = 'pip3 install --upgrade pip'
+            run_fn = f"python inference.py {remote_image_path}"
 
-    cluster_name = f"inference-cluster-{''.join(random.choice(string.ascii_lowercase) for _ in range(10))}"
-    sky.launch(
-        dag,
-        cluster_name=cluster_name,
-        detach_run=True,
-    )
+            task = sky.Task(name=task_name,
+                            setup=setup,
+                            workdir=workdir,
+                            run=run_fn)
 
-    cmd_output = run_output(f"sky logs {cluster_name}")
-    inference_result = re.findall(f'{INFERENCE_RESULT_MARKER}((?:[^\n])+)',
-                                  cmd_output)
+            resources = sky.Resources(cloud=sky.Azure())
+            task.set_resources(resources)
+            task.set_file_mounts({
+                # Copy model weights to the cluster
+                # Instead of local path, can also specify cloud object store URI
+                '/remote/path/to/model-weights': 'local/path/to/model-weights',
+                # Copy image to the cluster
+                remote_image_path: local_image_path,
+            })
 
-    # Down the cluster in the background
-    subprocess.Popen(f"sky down -y {cluster_name}", shell=True)
+        cluster_name = f"inference-cluster-{''.join(random.choice(string.ascii_lowercase) for _ in range(10))}"
+        sky.launch(
+            dag,
+            cluster_name=cluster_name,
+            detach_run=True,
+        )
 
-    return {"result": inference_result}
+        cmd_output = run_output(f"sky logs {cluster_name}")
+        inference_result = re.findall(f'{INFERENCE_RESULT_MARKER}((?:[^\n])+)',
+                                      cmd_output)
+
+        # Down the cluster in the background
+        subprocess.Popen(f"sky down -y {cluster_name}", shell=True)
+
+        return {"result": inference_result}
+    elif request.method == 'GET':
+        return '''
+            <title>Upload Image</title>
+            <h1>Upload Image</h1>
+            <form method=post enctype=multipart/form-data>
+            <input type=file name=file>
+            <input type=submit value=Upload>
+            </form>
+            '''
