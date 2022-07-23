9dfd2462f5d2d65d9f55b801eec12679b0378c54
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2022-07-22 22:02:11 -0700

    Minor fix

--- sky/clouds/service_catalog/data_fetchers/fetch_gcp_accelerators.py
+++ sky/clouds/service_catalog/data_fetchers/fetch_gcp_accelerators.py
@@ -27,7 +27,7 @@ TPU_ZONES = GCP_DATA_DIR + 'zones/tpu.csv'
 #    support them in the region.
 # 3. We used estimated prices for on-demand tpu-v3-{64,...,2048} as their
 #    prices are not publicly open.
-# 4. We applied 70% discount to preemptible TPUs
+# 4. We applied 70% discount to preemptible TPUs.
 TPU_PRICING = GCP_DATA_DIR + 'pricing/tpu.csv'
 
 COLS = [
