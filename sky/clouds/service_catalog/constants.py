6051c586ce383712fa0be972e3a10edde798b858
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2022-07-24 20:12:09 -0700

    Add constants.py

---
+++ sky/clouds/service_catalog/constants.py
@@ -0,0 +1,5 @@
+"""Constants used for service catalog."""
+
+HOSTED_CATALOG_DIR_URL = 'https://raw.githubusercontent.com/sky-proj/skypilot-catalog/master/catalogs'  # pylint: disable=line-too-long
+CATALOG_SCHEMA_VERSION = 'v1'
+LOCAL_CATALOG_DIR = '~/.sky/catalogs/'
