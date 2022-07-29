b20ffffdd586496c3ec660b11c96bcb39339427a
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-07-29 10:35:28 -0700

    Fix patch installation

--- sky/skylet/ray_patches/__init__.py
+++ sky/skylet/ray_patches/__init__.py
@@ -28,7 +28,8 @@ def _run_patch(target_file, patch_file):
     # .orig is the original file that is not patched.
     orig_file = os.path.abspath(target_file + '.orig')
     script = f"""\
-    which patch >/dev/null 2>&1 || sudo yum install -y patch
+    which patch >/dev/null 2>&1 || sudo yum install -y patch || true
+    which patch >/dev/null 2>&1 || echo '`patch` is not found. Failed to setup ray.' && exit 1
     if [ ! -f {orig_file} ]; then
         echo Create backup file {orig_file}
         cp {target_file} {orig_file}
