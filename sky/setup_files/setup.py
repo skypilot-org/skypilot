92ed4c4051f25712303dce5f35886b6eb464ebe9
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-08-09 15:42:42 -0700

    Install aws by default (#1055) (#1056)
    * Install aws by default (#1055)
    
    * Fix github action for PR on releases branches (#1058)

--- sky/setup_files/setup.py
+++ sky/setup_files/setup.py
@@ -94,6 +94,10 @@ extras_require = {
 
 extras_require['all'] = sum(extras_require.values(), [])
 
+# Install aws requirements by default, as it is the most common cloud provider,
+# and the installation is quick.
+install_requires += extras_require['aws']
+
 long_description = ''
 readme_filepath = 'README.md'
 # When sky/backends/wheel_utils.py builds wheels, it will not contain the README.
