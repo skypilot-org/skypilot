5e68724f156cf96ca347967016c817cd58647259
Isaac Ong <isaacong.jw@gmail.com>
2022-08-12 16:40:27 +1000

    Fix imports and style

--- examples/inference_server/server.py
+++ examples/inference_server/server.py
@@ -11,17 +11,17 @@ import re
 import string
 import subprocess
 
-from flask import Flask, request, abort
-from werkzeug.utils import secure_filename
+import flask
 import sky
+from werkzeug import utils
 
-from inference import INFERENCE_RESULT_MARKER
+import inference
 
 LOCAL_UPLOAD_FOLDER = '/Users/isaac/Dropbox/Berkeley/Sky/sky/examples/inference_server/uploads'
 REMOTE_UPLOAD_FOLDER = '/remote/path/to/folder'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 app.config['UPLOAD_FOLDER'] = LOCAL_UPLOAD_FOLDER
 
 
@@ -41,12 +41,12 @@ def allowed_file(filename):
 
 @app.route("/", methods=["GET", "POST"])
 def run_inference():
-    if request.method == 'POST':
-        image = request.files['file']
+    if flask.request.method == 'POST':
+        image = flask.request.files['file']
         if not image or not allowed_file(image.filename):
-            abort(400, "Invalid image upload")
+            flask.abort(400, "Invalid image upload")
 
-        filename = secure_filename(image.filename)
+        filename = utils.secure_filename(image.filename)
         local_image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         remote_image_path = os.path.join(REMOTE_UPLOAD_FOLDER, filename)
 
@@ -54,9 +54,9 @@ def run_inference():
 
         with sky.Dag() as dag:
             workdir = os.path.dirname(os.path.abspath(__file__))
-            task_name = "inference_task"
+            task_name = 'inference_task'
             setup = 'pip3 install --upgrade pip'
-            run_fn = f"python inference.py {remote_image_path}"
+            run_fn = f'python inference.py {remote_image_path}'
 
             task = sky.Task(name=task_name,
                             setup=setup,
@@ -73,22 +73,22 @@ def run_inference():
                 remote_image_path: local_image_path,
             })
 
-        cluster_name = f"inference-cluster-{''.join(random.choice(string.ascii_lowercase) for _ in range(10))}"
+        cluster_name = f'inference-cluster-{" ".join(random.choice(string.ascii_lowercase) for _ in range(10))}'
         sky.launch(
             dag,
             cluster_name=cluster_name,
             detach_run=True,
         )
 
-        cmd_output = run_output(f"sky logs {cluster_name}")
-        inference_result = re.findall(f'{INFERENCE_RESULT_MARKER}((?:[^\n])+)',
-                                      cmd_output)
+        cmd_output = run_output(f'sky logs {cluster_name}')
+        inference_result = re.findall(
+            f'{inference.INFERENCE_RESULT_MARKER}((?:[^\n])+)', cmd_output)
 
         # Down the cluster in the background
-        subprocess.Popen(f"sky down -y {cluster_name}", shell=True)
+        subprocess.Popen(f'sky down -y {cluster_name}', shell=True)
 
-        return {"result": inference_result}
-    elif request.method == 'GET':
+        return {'result': inference_result}
+    elif flask.request.method == 'GET':
         return '''
             <title>Upload Image</title>
             <h1>Upload Image</h1>
