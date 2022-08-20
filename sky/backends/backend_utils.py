5ee04d6a1c5200b41443bcfe068a44f0900c1196
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-08-20 12:59:56 -0700

    Fix duplicate folders in file mounts

--- sky/backends/backend_utils.py
+++ sky/backends/backend_utils.py
@@ -194,7 +194,12 @@ def fill_template(template_name: str,
         assert dst_parent_dir, f'Found relative destination path: {dst}'
 
         mkdir_parent = f'mkdir -p {dst_parent_dir}'
-        mv = (f'cp -r {_REMOTE_RUNTIME_FILES_DIR}/{src_basename} '
+        if os.path.isdir(os.path.expanduser(src)):
+            # Special case for directories. If the dst already exists as a folder,
+            # directly copy the folder will create a subfolder under the dst.
+            mkdir_parent = f'mkdir -p {dst}'
+            src_basename = f'{src_basename}/*'
+        mv = (f'rsync -az {_REMOTE_RUNTIME_FILES_DIR}/{src_basename} '
               f'{dst_parent_dir}/{dst_basename}')
         fragment = f'({mkdir_parent} && {mv})'
         commands.append(fragment)
