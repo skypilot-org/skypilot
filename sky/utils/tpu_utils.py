aa85d8e93ad7df3747d0325d98caae2292002b94
Wei-Lin Chiang <infwinston@gmail.com>
2022-08-25 19:18:14 -0700

    add tpu utils

---
+++ sky/utils/tpu_utils.py
@@ -0,0 +1,24 @@
+"""Utility functions for TPUs."""
+from typing import Optional
+
+from sky import resources as resources_lib
+
+def is_tpu_vm(resources: resources_lib.Resources) -> bool:
+    if resources.accelerator_args is None:
+        return False
+    return resources.accelerator_args.get('tpu_vm', False)
+
+
+def is_tpu_pod(resources: resources_lib.Resources) -> bool:
+    if resources.accelerators is None:
+        return False
+    acc, _ = list(resources.accelerators.items())[0]
+    return acc.startswith('tpu') and acc not in ['tpu-v2-8', 'tpu-v3-8']
+
+
+def get_num_tpu_devices(resources: resources_lib.Resources) -> Optional[bool]:
+    if not is_tpu_vm(resources):
+        return None
+    acc, _ = list(resources.accelerators.items())[0]
+    num_tpu_dev = int(int(acc.split('-')[2]) / 8)
+    return num_tpu_dev
