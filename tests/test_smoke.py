fc182783b856f2ebab3a3374fa13b4694a370f81
Wei-Lin Chiang <infwinston@gmail.com>
2022-08-27 00:55:39 -0700

    mark slow

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -387,6 +387,8 @@ def test_tpu_vm():
 
 
 # ---------- TPU VM Pod. ----------
+# Mark slow because it's expensive to run.
+@pytest.mark.slow
 def test_tpu_vm_pod():
     name = _get_cluster_name()
     test = Test(
