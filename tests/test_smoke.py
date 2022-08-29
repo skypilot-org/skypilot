285dcfe855e4e5f2e5aa272bb10f3b78db669248
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-08-29 10:23:35 -0700

    format

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -318,7 +318,7 @@ def test_large_job_queue():
             f'for i in `seq 1 75`; do sky exec {name} -d "echo $i; sleep 100000000"; done',
             f'sky cancel {name} 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16',
             'sleep 20',
-            # Each job takes 0.5 CPU and the default VM has 8 CPUs, so there should be 8 / 0.5 = 16 jobs running. 
+            # Each job takes 0.5 CPU and the default VM has 8 CPUs, so there should be 8 / 0.5 = 16 jobs running.
             f'sky queue {name} | grep -v grep | grep RUNNING | wc -l | grep 16',
         ],
         f'sky down -y {name}',
