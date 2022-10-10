6e839db52d6d923167b277577c4c1e01346f76c5
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-10-10 13:38:36 -0700

    increase version number

--- sky/clouds/service_catalog/constants.py
+++ sky/clouds/service_catalog/constants.py
@@ -2,5 +2,5 @@
 import os
 
 HOSTED_CATALOG_DIR_URL = 'https://raw.githubusercontent.com/skypilot-org/skypilot-catalog/master/catalogs'  # pylint: disable=line-too-long
-CATALOG_SCHEMA_VERSION = 'v2'
+CATALOG_SCHEMA_VERSION = 'v3'
 LOCAL_CATALOG_DIR = os.path.expanduser('~/.sky/catalogs/')
