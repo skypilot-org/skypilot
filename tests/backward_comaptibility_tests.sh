8e14254819a2f68d4ac0a5635bfeb18d063a6b24
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-10-13 00:21:50 -0700

    fix backward_compatibility_test

--- tests/backward_comaptibility_tests.sh
+++ tests/backward_comaptibility_tests.sh
@@ -12,7 +12,7 @@ conda create -n sky-back-compat-master -y --clone sky-dev
 conda create -n sky-back-compat-current -y --clone sky-dev
 
 conda activate sky-back-compat-master
-rm -r  ~/.sky/sky_wheels || true
+rm -r  ~/.sky/wheels || true
 pip uninstall skypilot -y
 
 cd sky-master
@@ -21,39 +21,39 @@ pip install -e ".[all]"
 cd -
 
 conda activate sky-back-compat-current
-rm -r  ~/.sky/sky_wheels || true
+rm -r  ~/.sky/wheels || true
 pip uninstall -y skypilot
 pip install -e ".[all]"
 
 
 # exec + launch
 conda activate sky-back-compat-master
-rm -r  ~/.sky/sky_wheels || true
+rm -r  ~/.sky/wheels || true
 which sky
 sky launch --cloud gcp -c ${CLUSTER_NAME} examples/minimal.yaml
 
 conda activate sky-back-compat-current
-rm -r  ~/.sky/sky_wheels || true
+rm -r  ~/.sky/wheels || true
 sky exec --cloud gcp ${CLUSTER_NAME} examples/minimal.yaml
 sky launch --cloud gcp -c ${CLUSTER_NAME} examples/minimal.yaml
 sky queue
 
 # sky stop + sky start + sky exec
 conda activate sky-back-compat-master
-rm -r  ~/.sky/sky_wheels || true
+rm -r  ~/.sky/wheels || true
 sky launch --cloud gcp -y -c ${CLUSTER_NAME}-2 examples/minimal.yaml
 conda activate sky-back-compat-current
-rm -r  ~/.sky/sky_wheels || true
+rm -r  ~/.sky/wheels || true
 sky stop -y ${CLUSTER_NAME}-2
 sky start -y ${CLUSTER_NAME}-2
 sky exec --cloud gcp ${CLUSTER_NAME}-2 examples/minimal.yaml
 
 # `sky autostop` + `sky status -r`
 conda activate sky-back-compat-master
-rm -r  ~/.sky/sky_wheels || true
+rm -r  ~/.sky/wheels || true
 sky launch --cloud gcp -y -c ${CLUSTER_NAME}-3 examples/minimal.yaml
 conda activate sky-back-compat-current
-rm -r  ~/.sky/sky_wheels || true
+rm -r  ~/.sky/wheels || true
 sky autostop -y -i0 ${CLUSTER_NAME}-3
 sleep 100
 sky status -r | grep ${CLUSTER_NAME}-3 | grep STOPPED
@@ -61,11 +61,11 @@ sky status -r | grep ${CLUSTER_NAME}-3 | grep STOPPED
 
 # (1 node) sky launch + sky exec + sky queue + sky logs
 conda activate sky-back-compat-master
-rm -r  ~/.sky/sky_wheels || true
+rm -r  ~/.sky/wheels || true
 sky launch --cloud gcp -y -c ${CLUSTER_NAME}-4 examples/minimal.yaml
 sky stop -y ${CLUSTER_NAME}-4
 conda activate sky-back-compat-current
-rm -r  ~/.sky/sky_wheels || true
+rm -r  ~/.sky/wheels || true
 sky launch --cloud gcp -y -c ${CLUSTER_NAME}-4 examples/minimal.yaml
 sky queue ${CLUSTER_NAME}-4
 sky logs ${CLUSTER_NAME}-4 1 --status
@@ -75,11 +75,11 @@ sky logs ${CLUSTER_NAME}-4 2
 
 # (1 node) sky start + sky exec + sky queue + sky logs
 conda activate sky-back-compat-master
-rm -r  ~/.sky/sky_wheels || true
+rm -r  ~/.sky/wheels || true
 sky launch --cloud gcp -y -c ${CLUSTER_NAME}-5 examples/minimal.yaml
 sky stop -y ${CLUSTER_NAME}-5
 conda activate sky-back-compat-current
-rm -r  ~/.sky/sky_wheels || true
+rm -r  ~/.sky/wheels || true
 sky start -y ${CLUSTER_NAME}-5
 sky queue ${CLUSTER_NAME}-5
 sky logs ${CLUSTER_NAME}-5 1 --status
@@ -91,11 +91,11 @@ sky logs ${CLUSTER_NAME}-5 2
 
 # (2 nodes) sky launch + sky exec + sky queue + sky logs
 conda activate sky-back-compat-master
-rm -r  ~/.sky/sky_wheels || true
+rm -r  ~/.sky/wheels || true
 sky launch --cloud gcp -y -c ${CLUSTER_NAME}-6 examples/multi_hostname.yaml
 sky stop -y ${CLUSTER_NAME}-6
 conda activate sky-back-compat-current
-rm -r  ~/.sky/sky_wheels || true
+rm -r  ~/.sky/wheels || true
 sky start -y ${CLUSTER_NAME}-6
 sky queue ${CLUSTER_NAME}-6
 sky logs ${CLUSTER_NAME}-6 1 --status
