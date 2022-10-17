5b86d15cb9d1738f18dc11a0b3aa9e54342317f1
Isaac Ong <isaacong.jw@gmail.com>
2022-10-17 12:43:13 -0700

    Update setup.py

--- sky/setup_files/setup.py
+++ sky/setup_files/setup.py
@@ -56,9 +56,9 @@ def parse_footnote(readme: str) -> str:
 
 install_requires = [
     'wheel',
-    # NOTE: ray 1.13.0 requires click<=8.0.4,>=7.0; our use of shell_complete
-    # requires >= 8.
-    'click>=7.0,<=8.0.4',
+    # NOTE: ray 1.13.0 requires click<=8.0.4,>=7.0; We disable the
+    # shell completion for click<8.0 for backward compatibility.
+    'click<=8.0.4,>=7.0',
     'colorama',
     'cryptography',
     'jinja2',
