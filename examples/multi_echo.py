aecf468aec7e402d237d285f12838bfb96c8a136
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-10-23 14:10:42 -0700

    update examples

--- examples/multi_echo.py
+++ examples/multi_echo.py
@@ -27,11 +27,10 @@ def run(cluster: Optional[str] = None):
 
     # Submit multiple tasks in parallel to trigger queueing behaviors.
     def _exec(i):
-        with sky.Dag() as dag:
-            task = sky.Task(run=f'echo {i}; sleep 5')
-            resources = sky.Resources(accelerators={'K80': 0.5})
-            task.set_resources(resources)
-        sky.exec(dag, cluster_name=cluster, detach_run=True)
+        task = sky.Task(run=f'echo {i}; sleep 5')
+        resources = sky.Resources(accelerators={'K80': 0.5})
+        task.set_resources(resources)
+        sky.exec(task, cluster_name=cluster, detach_run=True)
 
     with pool.ThreadPool(8) as p:
         list(p.imap(_exec, range(32)))
--- examples/resnet_distributed_torch_app.py
+++ examples/resnet_distributed_torch_app.py
@@ -2,52 +2,53 @@ from typing import List, Optional
 
 import sky
 
-with sky.Dag() as dag:
-    # Total Nodes, INCLUDING Head Node
-    num_nodes = 2
+# Total Nodes, INCLUDING Head Node
+num_nodes = 2
 
-    # The setup command.  Will be run under the working directory.
-    setup = 'echo \"alias python=python3\" >> ~/.bashrc && pip3 install --upgrade pip && \
-      [ -d pytorch-distributed-resnet ] || \
-      (git clone https://github.com/michaelzhiluo/pytorch-distributed-resnet && \
-      cd pytorch-distributed-resnet && pip3 install -r requirements.txt && \
-      mkdir -p data  && mkdir -p saved_models && cd data && \
-      wget -c --quiet https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz && \
-      tar -xvzf cifar-10-python.tar.gz)'
+# The setup command.  Will be run under the working directory.
+setup = 'echo \"alias python=python3\" >> ~/.bashrc && pip3 install --upgrade pip && \
+    [ -d pytorch-distributed-resnet ] || \
+    (git clone https://github.com/michaelzhiluo/pytorch-distributed-resnet && \
+    cd pytorch-distributed-resnet && pip3 install -r requirements.txt && \
+    mkdir -p data  && mkdir -p saved_models && cd data && \
+    wget -c --quiet https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz && \
+    tar -xvzf cifar-10-python.tar.gz)'
 
-    # The command to run.  Will be run under the working directory.
-    def run_fn(node_rank: int, ip_list: List[str]) -> Optional[str]:
-        num_nodes = len(ip_list)
-        return f"""\
-        cd pytorch-distributed-resnet
-        python3 -m torch.distributed.launch --nproc_per_node=1 \
-        --nnodes={num_nodes} --node_rank={node_rank} --master_addr={ip_list[0]} \
-        --master_port=8008 resnet_ddp.py --num_epochs 20
-        """
 
-    train = sky.Task(
-        'train',
-        setup=setup,
-        num_nodes=num_nodes,
-        run=run_fn,
-    )
+# The command to run.  Will be run under the working directory.
+def run_fn(node_rank: int, ip_list: List[str]) -> Optional[str]:
+    num_nodes = len(ip_list)
+    return f"""\
+    cd pytorch-distributed-resnet
+    python3 -m torch.distributed.launch --nproc_per_node=1 \
+    --nnodes={num_nodes} --node_rank={node_rank} --master_addr={ip_list[0]} \
+    --master_port=8008 resnet_ddp.py --num_epochs 20
+    """
 
-    train.set_resources({
-        ##### Fully specified
-        sky.Resources(sky.AWS(), 'p3.2xlarge'),
-        # sky.Resources(sky.GCP(), 'n1-standard-16'),
-        #sky.Resources(
-        #     sky.GCP(),
-        #     'n1-standard-8',
-        # Options: 'V100', {'V100': <num>}.
-        #     'V100',
-        #),
-        ##### Partially specified
-        #sky.Resources(accelerators='V100'),
-        # sky.Resources(accelerators='tpu-v3-8'),
-        # sky.Resources(sky.AWS(), accelerators={'V100': 4}),
-        # sky.Resources(sky.AWS(), accelerators='V100'),
-    })
 
-sky.launch(dag, cluster_name='dth')
-# sky.exec(dag, cluster_name='dth')
+train = sky.Task(
+    'train',
+    setup=setup,
+    num_nodes=num_nodes,
+    run=run_fn,
+)
+
+train.set_resources({
+    ##### Fully specified
+    sky.Resources(sky.AWS(), 'p3.2xlarge'),
+    # sky.Resources(sky.GCP(), 'n1-standard-16'),
+    #sky.Resources(
+    #     sky.GCP(),
+    #     'n1-standard-8',
+    # Options: 'V100', {'V100': <num>}.
+    #     'V100',
+    #),
+    ##### Partially specified
+    #sky.Resources(accelerators='V100'),
+    # sky.Resources(accelerators='tpu-v3-8'),
+    # sky.Resources(sky.AWS(), accelerators={'V100': 4}),
+    # sky.Resources(sky.AWS(), accelerators='V100'),
+})
+
+sky.launch(train, cluster_name='dth')
+# sky.exec(train, cluster_name='dth')
