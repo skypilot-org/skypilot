dbea5b13346da4523a14962d378e431c2eeee336
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-11-13 15:09:27 -0800

    [Spot] Fix spot recovery for multi node (#1411)
    * Add cluster status check even job is RUNNING for multi-node
    
    * Disable autoscaler logs and fix finished when partially preempted
    
    * format
    
    * Add test
    
    * address comments
    
    * update
    
    * Add time

--- sky/spot/spot_utils.py
+++ sky/spot/spot_utils.py
@@ -260,12 +260,19 @@ def stream_logs_by_id(job_id: int, follow: bool = True) -> str:
             job_statuses = backend.get_job_status(handle, stream_logs=False)
             job_status = list(job_statuses.values())[0]
             assert job_status is not None, 'No job found.'
-            logger.info(f'Logs finished for job {job_id} '
-                        f'(status: {job_status.value}).')
-            break
-        logger.info(
-            f'INFO: (Log streaming) Got return code {returncode}. Retrying '
-            f'in {JOB_STATUS_CHECK_GAP_SECONDS} seconds.')
+            if job_status != job_lib.JobStatus.CANCELLED:
+                logger.info(f'Logs finished for job {job_id} '
+                            f'(status: {job_status.value}).')
+                break
+            # The job can be cancelled by the user or the controller (when the
+            # the cluster is partially preempted).
+            logger.info('INFO: (Log streaming) Job is cancelled. Waiting '
+                        'for the status update in '
+                        f'{JOB_STATUS_CHECK_GAP_SECONDS} seconds.')
+        else:
+            logger.info(
+                f'INFO: (Log streaming) Got return code {returncode}. Retrying '
+                f'in {JOB_STATUS_CHECK_GAP_SECONDS} seconds.')
         # If the tailing fails, it is likely that the cluster fails, so we wait
         # a while to make sure the spot state is updated by the controller, and
         # check the spot queue again.
