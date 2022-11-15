d7a47e5c0169b1b539ac6640975be049371b1227
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-11-14 23:14:28 -0800

    [Release] Fix pypi description (#1416)
    * Fix pypi description
    
    * fix
    
    * format

--- sky/setup_files/setup.py
+++ sky/setup_files/setup.py
@@ -47,11 +47,19 @@ def find_version(*filepath):
         raise RuntimeError('Unable to find version string.')
 
 
-def parse_footnote(readme: str) -> str:
-    """Parse the footnote from the README.md file."""
+def parse_readme(readme: str) -> str:
+    """Parse the README.md file to be pypi compatible."""
+    # Replace the footnotes.
     readme = readme.replace('<!-- Footnote -->', '#')
     footnote_re = re.compile(r'\[\^([0-9]+)\]')
-    return footnote_re.sub(r'<sup>[\1]</sup>', readme)
+    readme = footnote_re.sub(r'<sup>[\1]</sup>', readme)
+
+    # Remove the dark mode switcher
+    mode_re = re.compile(
+        r'<picture>[\n ]*<source media=.*>[\n ]*<img(.*)>[\n ]*</picture>',
+        re.MULTILINE)
+    readme = mode_re.sub(r'<img\1>', readme)
+    return readme
 
 
 install_requires = [
@@ -113,7 +121,7 @@ readme_filepath = 'README.md'
 # README.  Skip the description for that case.
 if os.path.exists(readme_filepath):
     long_description = io.open(readme_filepath, 'r', encoding='utf-8').read()
-    long_description = parse_footnote(long_description)
+    long_description = parse_readme(long_description)
 
 setuptools.setup(
     # NOTE: this affects the package.whl wheel name. When changing this (if
