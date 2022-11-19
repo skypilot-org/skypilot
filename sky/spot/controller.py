7bffd8414c1124dd82acfd491b9ebabce43edd3e
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-11-18 16:35:03 -0800

    Fix serialization problem

--- sky/spot/controller.py
+++ sky/spot/controller.py
@@ -56,171 +56,174 @@ class SpotController:
             self.task_name, self.job_id)
         self.strategy_executor = recovery_strategy.StrategyExecutor.make(
             self.cluster_name, self.backend, self.task, retry_until_up)
+        
+    def run(self):
+        """Busy loop monitoring spot cluster status and handling recovery."""
+        logger.info(f'Started monitoring spot task {self.task_name} '
+                    f'(id: {self.job_id})')
+        spot_state.set_starting(self.job_id)
+        start_at = self.strategy_executor.launch()
 
-    def start(self):
-        """Start the controller."""
-        try:
-            self._handle_signal()
-            controller_task = _controller_run.remote(self)
-            # Signal can interrupt the underlying controller process.
-            ready, _ = ray.wait([controller_task], timeout=0)
-            while not ready:
-                try:
-                    self._handle_signal()
-                except exceptions.SpotUserCancelledError as e:
-                    logger.info(f'Cancelling spot job {self.job_id}...')
-                    try:
-                        ray.cancel(controller_task)
-                        ray.get(controller_task)
-                    except ray.exceptions.RayTaskError:
-                        # When the controller task is cancelled, it will raise
-                        # ray.exceptions.RayTaskError, which can be ignored,
-                        # since the SpotUserCancelledError will be raised and
-                        # handled later.
-                        pass
-                    raise e
-                ready, _ = ray.wait([controller_task], timeout=1)
-            # Need this to get the exception from the controller task.
-            ray.get(controller_task)
-        except exceptions.SpotUserCancelledError as e:
-            logger.info(e)
-            spot_state.set_cancelled(self.job_id)
-        except exceptions.ResourcesUnavailableError as e:
-            logger.error(f'Resources unavailable: {colorama.Fore.RED}{e}'
-                         f'{colorama.Style.RESET_ALL}')
-            spot_state.set_failed(
-                self.job_id,
-                failure_type=spot_state.SpotStatus.FAILED_NO_RESOURCE)
-        except (Exception, SystemExit) as e:  # pylint: disable=broad-except
-            logger.error(traceback.format_exc())
-            logger.error(f'Unexpected error occurred: {type(e).__name__}: {e}')
-        finally:
-            self.strategy_executor.terminate_cluster()
-            job_status = spot_state.get_status(self.job_id)
-            # The job can be non-terminal if the controller exited abnormally,
-            # e.g. failed to launch cluster after reaching the MAX_RETRY.
-            if not job_status.is_terminal():
-                spot_state.set_failed(
-                    self.job_id,
-                    failure_type=spot_state.SpotStatus.FAILED_CONTROLLER)
+        spot_state.set_started(self.job_id, start_time=start_at)
+        while True:
+            time.sleep(spot_utils.JOB_STATUS_CHECK_GAP_SECONDS)
 
-            # Clean up Storages with persistent=False.
-            self.backend.teardown_ephemeral_storage(self.task)
-
-    def _handle_signal(self):
-        """Handle the signal if the user sent it."""
-        signal_file = pathlib.Path(
-            spot_utils.SIGNAL_FILE_PREFIX.format(self.job_id))
-        signal = None
-        if signal_file.exists():
-            # Filelock is needed to prevent race condition with concurrent
-            # signal writing.
-            # TODO(mraheja): remove pylint disabling when filelock version
-            # updated
-            # pylint: disable=abstract-class-instantiated
-            with filelock.FileLock(str(signal_file) + '.lock'):
-                with signal_file.open(mode='r') as f:
-                    signal = f.read().strip()
-                    signal = spot_utils.UserSignal(signal)
-                # Remove the signal file, after reading the signal.
-                signal_file.unlink()
-        if signal is None:
-            return
-        if signal == spot_utils.UserSignal.CANCEL:
-            raise exceptions.SpotUserCancelledError(
-                f'User sent {signal.value} signal.')
-
-        raise RuntimeError(f'Unknown SkyPilot signal received: {signal.value}.')
-
-
-@ray.remote(num_cpus=0)
-def _controller_run(spot_controller: SpotController):
-    """Busy loop monitoring spot cluster status and handling recovery."""
-    logger.info(f'Started monitoring spot task {spot_controller.task_name} '
-                f'(id: {spot_controller.job_id})')
-    spot_state.set_starting(spot_controller.job_id)
-    start_at = spot_controller.strategy_executor.launch()
-
-    spot_state.set_started(spot_controller.job_id, start_time=start_at)
-    while True:
-        time.sleep(spot_utils.JOB_STATUS_CHECK_GAP_SECONDS)
-
-        # Check the network connection to avoid false alarm for job failure.
-        # Network glitch was observed even in the VM.
-        try:
-            backend_utils.check_network_connection()
-        except exceptions.NetworkError:
-            logger.info('Network is not available. Retrying again in '
-                        f'{spot_utils.JOB_STATUS_CHECK_GAP_SECONDS} seconds.')
-            continue
-
-        # NOTE: we do not check cluster status first because race condition
-        # can occur, i.e. cluster can be down during the job status check.
-        job_status = spot_utils.get_job_status(spot_controller.backend,
-                                               spot_controller.cluster_name)
-
-        if job_status is not None and not job_status.is_terminal():
-            need_recovery = False
-            if spot_controller.task.num_nodes > 1:
-                # Check the cluster status for multi-node jobs, since the
-                # job may not be set to FAILED immediately when only some
-                # of the nodes are preempted.
-                (cluster_status,
-                 handle) = backend_utils.refresh_cluster_status_handle(
-                     spot_controller.cluster_name, force_refresh=True)
-                if cluster_status != global_user_state.ClusterStatus.UP:
-                    # recover the cluster if it is not up.
-                    logger.info(f'Cluster status {cluster_status.value}. '
-                                'Recovering...')
-                    need_recovery = True
-            if not need_recovery:
-                # The job and cluster are healthy, continue to monitor the
-                # job status.
+            # Check the network connection to avoid false alarm for job failure.
+            # Network glitch was observed even in the VM.
+            try:
+                backend_utils.check_network_connection()
+            except exceptions.NetworkError:
+                logger.info('Network is not available. Retrying again in '
+                            f'{spot_utils.JOB_STATUS_CHECK_GAP_SECONDS} seconds.')
                 continue
 
-        if job_status == job_lib.JobStatus.SUCCEEDED:
-            end_time = spot_utils.get_job_timestamp(
-                spot_controller.backend,
-                spot_controller.cluster_name,
-                get_end_time=True)
-            # The job is done.
-            spot_state.set_succeeded(spot_controller.job_id, end_time=end_time)
-            break
+            # NOTE: we do not check cluster status first because race condition
+            # can occur, i.e. cluster can be down during the job status check.
+            job_status = spot_utils.get_job_status(self.backend,
+                                                self.cluster_name)
 
-        if job_status == job_lib.JobStatus.FAILED:
-            # Check the status of the spot cluster. If it is not UP,
-            # the cluster is preempted.
-            (cluster_status,
-             handle) = backend_utils.refresh_cluster_status_handle(
-                 spot_controller.cluster_name, force_refresh=True)
-            if cluster_status == global_user_state.ClusterStatus.UP:
-                # The user code has probably crashed.
+            if job_status is not None and not job_status.is_terminal():
+                need_recovery = False
+                if self.task.num_nodes > 1:
+                    # Check the cluster status for multi-node jobs, since the
+                    # job may not be set to FAILED immediately when only some
+                    # of the nodes are preempted.
+                    (cluster_status,
+                    handle) = backend_utils.refresh_cluster_status_handle(
+                        self.cluster_name, force_refresh=True)
+                    if cluster_status != global_user_state.ClusterStatus.UP:
+                        # recover the cluster if it is not up.
+                        logger.info(f'Cluster status {cluster_status.value}. '
+                                    'Recovering...')
+                        need_recovery = True
+                if not need_recovery:
+                    # The job and cluster are healthy, continue to monitor the
+                    # job status.
+                    continue
+
+            if job_status == job_lib.JobStatus.SUCCEEDED:
                 end_time = spot_utils.get_job_timestamp(
-                    spot_controller.backend,
-                    spot_controller.cluster_name,
+                    self.backend,
+                    self.cluster_name,
                     get_end_time=True)
-                logger.info(
-                    'The user job failed. Please check the logs below.\n'
-                    '== Logs of the user job (ID: '
-                    f'{spot_controller.job_id}) ==\n')
-                spot_controller.backend.tail_logs(
-                    handle, None, spot_job_id=spot_controller.job_id)
-                logger.info(
-                    f'\n== End of logs (ID: {spot_controller.job_id}) ==')
-                spot_state.set_failed(spot_controller.job_id,
-                                      failure_type=spot_state.SpotStatus.FAILED,
-                                      end_time=end_time)
+                # The job is done.
+                spot_state.set_succeeded(self.job_id, end_time=end_time)
                 break
-        # cluster can be down, INIT or STOPPED, based on the interruption
-        # behavior of the cloud.
-        # Failed to connect to the cluster or the cluster is partially down.
-        # job_status is None or job_status == job_lib.JobStatus.FAILED
-        logger.info('The cluster is preempted.')
-        spot_state.set_recovering(spot_controller.job_id)
-        recovered_time = spot_controller.strategy_executor.recover()
-        spot_state.set_recovered(spot_controller.job_id,
-                                 recovered_time=recovered_time)
 
+            if job_status == job_lib.JobStatus.FAILED:
+                # Check the status of the spot cluster. If it is not UP,
+                # the cluster is preempted.
+                (cluster_status,
+                handle) = backend_utils.refresh_cluster_status_handle(
+                    self.cluster_name, force_refresh=True)
+                if cluster_status == global_user_state.ClusterStatus.UP:
+                    # The user code has probably crashed.
+                    end_time = spot_utils.get_job_timestamp(
+                        self.backend,
+                        self.cluster_name,
+                        get_end_time=True)
+                    logger.info(
+                        'The user job failed. Please check the logs below.\n'
+                        '== Logs of the user job (ID: '
+                        f'{self.job_id}) ==\n')
+                    self.backend.tail_logs(
+                        handle, None, spot_job_id=self.job_id)
+                    logger.info(
+                        f'\n== End of logs (ID: {self.job_id}) ==')
+                    spot_state.set_failed(self.job_id,
+                                        failure_type=spot_state.SpotStatus.FAILED,
+                                        end_time=end_time)
+                    break
+            # cluster can be down, INIT or STOPPED, based on the interruption
+            # behavior of the cloud.
+            # Failed to connect to the cluster or the cluster is partially down.
+            # job_status is None or job_status == job_lib.JobStatus.FAILED
+            logger.info('The cluster is preempted.')
+            spot_state.set_recovering(self.job_id)
+            recovered_time = self.strategy_executor.recover()
+            spot_state.set_recovered(self.job_id,
+                                    recovered_time=recovered_time)
+
+@ray.remote(num_cpus=0)
+def _run_controller(job_id: int, task_yaml: str, retry_until_up: bool):
+    try:
+        spot_controller = SpotController(job_id, task_yaml, retry_until_up)
+        spot_controller.run()
+    except KeyboardInterrupt as e:
+        logger.error(e)
+        spot_state.set_cancelled(job_id)
+    except exceptions.ResourcesUnavailableError as e:
+        logger.error(f'Resources unavailable: {colorama.Fore.RED}{e}'
+                        f'{colorama.Style.RESET_ALL}')
+        spot_state.set_failed(
+            job_id,
+            failure_type=spot_state.SpotStatus.FAILED_NO_RESOURCE)
+    except (Exception, SystemExit) as e:  # pylint: disable=broad-except
+        logger.error(traceback.format_exc())
+        logger.error(f'Unexpected error occurred: {type(e).__name__}: {e}')
+    finally:
+        spot_controller.strategy_executor.terminate_cluster()
+        job_status = spot_state.get_status(job_id)
+        # The job can be non-terminal if the controller exited abnormally,
+        # e.g. failed to launch cluster after reaching the MAX_RETRY.
+        if not job_status.is_terminal():
+            print(job_status)
+            spot_state.set_failed(
+                job_id,
+                failure_type=spot_state.SpotStatus.FAILED_CONTROLLER)
+
+        # Clean up Storages with persistent=False.
+        spot_controller.backend.teardown_ephemeral_storage(spot_controller.task)
+    
+
+def start(job_id, task_yaml, retry_until_up):
+    """Start the controller."""
+    _handle_signal(job_id)
+    controller_task = _run_controller.remote(job_id, task_yaml, retry_until_up)
+    # Signal can interrupt the underlying controller process.
+    ready, _ = ray.wait([controller_task], timeout=0)
+    while not ready:
+        try:
+            _handle_signal(job_id)
+        except exceptions.SpotUserCancelledError as e:
+            logger.info(f'Cancelling spot job {job_id}...')
+            try:
+                ray.cancel(controller_task)
+                ray.get(controller_task)
+            except ray.exceptions.RayTaskError:
+                # When the controller task is cancelled, it will raise
+                # ray.exceptions.RayTaskError, which can be ignored,
+                # since the SpotUserCancelledError will be raised and
+                # handled later.
+                pass
+        ready, _ = ray.wait([controller_task], timeout=1)
+
+    
+
+def _handle_signal(job_id):
+    """Handle the signal if the user sent it."""
+    signal_file = pathlib.Path(
+        spot_utils.SIGNAL_FILE_PREFIX.format(job_id))
+    signal = None
+    if signal_file.exists():
+        # Filelock is needed to prevent race condition with concurrent
+        # signal writing.
+        # TODO(mraheja): remove pylint disabling when filelock version
+        # updated
+        # pylint: disable=abstract-class-instantiated
+        with filelock.FileLock(str(signal_file) + '.lock'):
+            with signal_file.open(mode='r') as f:
+                signal = f.read().strip()
+                signal = spot_utils.UserSignal(signal)
+            # Remove the signal file, after reading the signal.
+            signal_file.unlink()
+    if signal is None:
+        return
+    if signal == spot_utils.UserSignal.CANCEL:
+        raise exceptions.SpotUserCancelledError(
+            f'User sent {signal.value} signal.')
+
+    raise RuntimeError(f'Unknown SkyPilot signal received: {signal.value}.')
 
 if __name__ == '__main__':
     ray.init()
@@ -237,6 +240,4 @@ if __name__ == '__main__':
                         help='The path to the user spot task yaml file. '
                         'The file name is the spot task name.')
     args = parser.parse_args()
-    controller = SpotController(args.job_id, args.task_yaml,
-                                args.retry_until_up)
-    controller.start()
+    start(args.job_id, args.task_yaml, args.retry_until_up)
