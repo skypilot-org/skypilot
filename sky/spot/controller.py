0cd55aa379b1b21d8f37f7125fca3b5138eddd19
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-11-30 20:49:41 -0800

    format

--- sky/spot/controller.py
+++ sky/spot/controller.py
@@ -117,7 +117,9 @@ class SpotController:
                 spot_state.set_succeeded(self._job_id, end_time=end_time)
                 break
 
-            if job_status in [job_lib.JobStatus.FAILED, job_lib.JobStatus.FAILED_SETUP]:
+            if job_status in [
+                    job_lib.JobStatus.FAILED, job_lib.JobStatus.FAILED_SETUP
+            ]:
                 # Check the status of the spot cluster. If it is not UP,
                 # the cluster is preempted.
                 (cluster_status,
