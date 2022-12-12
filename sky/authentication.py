dee55905f7e415fa41b0ef211b61ec61ea9485c3
Zongheng Yang <zongheng.y@gmail.com>
2022-12-12 08:08:32 +0800

    Minor: add a FIXME in authentication.py.

--- sky/authentication.py
+++ sky/authentication.py
@@ -77,7 +77,10 @@ def get_or_generate_keys() -> Tuple[str, str]:
         _save_key_pair(private_key_path, public_key_path, private_key,
                        public_key)
     else:
-        assert os.path.exists(public_key_path)
+        # FIXME(skypilot): sky-launched instances fail this assert (has private
+        # key; but has not generated public key).
+        #   AssertionError: /home/ubuntu/.ssh/sky-key.pub
+        assert os.path.exists(public_key_path), public_key_path
     return private_key_path, public_key_path
 
 
