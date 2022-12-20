c73555b8891b7a993854c9ee1c26af39da1261ee
Zongheng Yang <zongheng.y@gmail.com>
2022-12-20 17:59:50 +0800

    authentication.py: reword comment

--- sky/authentication.py
+++ sky/authentication.py
@@ -77,10 +77,12 @@ def get_or_generate_keys() -> Tuple[str, str]:
         _save_key_pair(private_key_path, public_key_path, private_key,
                        public_key)
     else:
-        # FIXME(skypilot): sky-launched instances fail this assert (has private
-        # key; but has not generated public key).
+        # FIXME(skypilot): ran into failing this assert once, but forgot the
+        # reproduction (has private key; but has not generated public key).
         #   AssertionError: /home/ubuntu/.ssh/sky-key.pub
-        assert os.path.exists(public_key_path), public_key_path
+        assert os.path.exists(public_key_path), (
+            'Private key found, but associated public key '
+            f'{public_key_path} does not exist.')
     return private_key_path, public_key_path
 
 
