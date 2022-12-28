9a91db3b3f0171e2d6f928767c190f565fbd1d31
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-12-28 11:30:10 -0800

    support newer version of ray

--- sky/setup_files/setup.py
+++ sky/setup_files/setup.py
@@ -80,7 +80,7 @@ install_requires = [
     'PrettyTable',
     # Lower local ray version is not fully supported, due to the
     # autoscaler issues (also tracked in #537).
-    'ray[default]>=1.9.0,<=2.0.1',
+    'ray[default]>=1.9.0,<=2.2.0',
     'rich',
     'tabulate',
     'filelock',  # TODO(mraheja): Enforce >=3.6.0 when python version is >= 3.7
