662e702ec4dc400c17b03c9aaa6ba49622cc693b
Cohen-J-Omer <cohen.j.omer@gmail.com>
2023-01-01 20:06:22 +0200

    update ibm specific libraries

--- sky/setup_files/setup.py
+++ sky/setup_files/setup.py
@@ -123,7 +123,7 @@ extras_require: Dict[str, List[str]] = {
         'azure-mgmt-network'
     ],
     'gcp': ['google-api-python-client', 'google-cloud-storage'],
-    'ibm':['ibm_cloud_sdk_core','ibm_vpc'],
+    'ibm':['ibm-cloud-sdk-core','ibm-vpc', 'ibm-platform-services'],
     'docker': ['docker'],
     'lambda': [],
     'cloudflare': aws_dependencies
