a0a83d85224bda29cf6a1f3b20a3c69754f1bd03
Cohen-J-Omer <cohen.j.omer@gmail.com>
2023-01-05 12:09:06 +0200

    replace mockup csv with default data frame

--- sky/clouds/service_catalog/ibm_catalog.py
+++ sky/clouds/service_catalog/ibm_catalog.py
@@ -1,8 +1,8 @@
 from sky.clouds import cloud
 from sky.clouds.service_catalog import common
 from typing import Dict, List, Optional, Tuple
-
-_df = common.read_catalog('ibm/vms.csv')
+import pandas as pd
+from numpy import nan 
 
 def instance_type_exists(instance_type: str) -> bool:
     return common.instance_type_exists_impl(_df, instance_type)
@@ -62,3 +62,47 @@ def list_accelerators(gpus_only: bool,
     """Returns all instance types in IBM offering accelerators."""
     return common.list_accelerators_impl('IBM', _df, gpus_only, name_filter,
                                          case_sensitive)
+
+def get_default_data():
+    """ 
+    temporarily hard-coded until the implementation of clouds/service_catalog/data_fetchers/fetch_ibm.py.
+    replacing the equivalent call: _df = common.read_catalog('ibm/vms.csv')
+    """
+
+    data = [
+    ['gx2-8x64x1v100', 'V100', 1.0, 8, 64, 'V100', 2.497, 0, 'us-south', 'us-south-1'],
+    ['gx2-8x64x1v100', 'V100', 1.0, 8, 64, 'V100', 2.497, 0, 'us-south', 'us-south-2'],
+    ['gx2-8x64x1v100', 'V100', 1.0, 8, 64, 'V100', 2.497, 0, 'us-south','us-south-3'],
+    ['gx2-8x64x1v100', 'V100', 1.0, 8, 64, 'V100', 2.497, 0, 'us-east', 'us-east-1'],
+    ['gx2-8x64x1v100', 'V100', 1.0, 8, 64, 'V100', 2.497, 0, 'us-east', 'us-east-2'],
+    ['gx2-8x64x1v100', 'V100', 1.0, 8, 64, 'V100', 2.497, 0, 'us-east', 'us-east-3'],
+    ['gx2-8x64x1v100', 'V100', 1.0, 8, 64, 'V100', 2.572, 0, 'ca-tor', 'ca-tor-1'],
+    ['gx2-8x64x1v100', 'V100', 1.0, 8, 64, 'V100', 2.572, 0, 'ca-tor', 'ca-tor-2'],
+    ['gx2-8x64x1v100', 'V100', 1.0, 8, 64, 'V100', 2.572, 0, 'ca-tor', 'ca-tor-3'],
+    ['gx2-8x64x1v100', 'V100', 1.0, 8, 64, 'V100', 2.746, 0, 'eu-de', 'eu-de-1'],
+    ['gx2-8x64x1v100', 'V100', 1.0, 8, 64, 'V100', 2.746, 0, 'eu-de', 'eu-de-2'],
+    ['gx2-8x64x1v100', 'V100', 1.0, 8, 64, 'V100', 2.746, 0, 'eu-de','eu-de-3'],
+    ['gx2-8x64x1v100', 'V100', 1.0, 8, 64, 'V100', 2.672, 0, 'eu-gb', 'eu-gb-1'],
+    ['gx2-8x64x1v100', 'V100', 1.0, 8, 64, 'V100', 2.672, 0, 'eu-gb', 'eu-gb-2'],
+    ['gx2-8x64x1v100', 'V100', 1.0, 8, 64, 'V100', 2.672, 0, 'eu-gb','eu-gb-3'],
+
+    ['bx2d-8x32', nan, nan, 8, 32, nan, 0.417, 0, 'us-east', 'us-east-1'],
+    ['bx2d-8x32', nan, nan, 8, 32, nan, 0.417, 0, 'us-east', 'us-east-2'],
+    ['bx2d-8x32', nan, nan, 8, 32, nan, 0.417, 0, 'us-east', 'us-east-3'],
+    ['bx2d-8x32', nan, nan, 8, 32, nan, 0.417, 0, 'us-south', 'us-south-1'],
+    ['bx2d-8x32', nan, nan, 8, 32, nan, 0.417, 0, 'us-south', 'us-south-2'],
+    ['bx2d-8x32', nan, nan, 8, 32, nan, 0.417, 0, 'us-south', 'us-south-3'],
+    ['bx2d-8x32', nan, nan, 8, 32, nan, 0.430, 0, 'ca-tor', 'ca-tor-1'],
+    ['bx2d-8x32', nan, nan, 8, 32, nan, 0.430, 0, 'ca-tor', 'ca-tor-2'],
+    ['bx2d-8x32', nan, nan, 8, 32, nan, 0.430, 0, 'ca-tor', 'ca-tor-3'],
+    ['bx2d-8x32', nan, nan, 8, 32, nan, 0.455, 0, 'eu-de', 'eu-de-1'],
+    ['bx2d-8x32', nan, nan, 8, 32, nan, 0.455, 0, 'eu-de', 'eu-de-2'],
+    ['bx2d-8x32', nan, nan, 8, 32, nan, 0.455, 0, 'eu-de', 'eu-de-3'],
+    ['bx2d-8x32', nan, nan, 8, 32, nan, 0.444, 0, 'eu-gb', 'eu-gb-1'],
+    ['bx2d-8x32', nan, nan, 8, 32, nan, 0.444, 0, 'eu-gb', 'eu-gb-2'],
+    ['bx2d-8x32', nan, nan, 8, 32, nan, 0.444, 0, 'eu-gb', 'eu-gb-3'],
+    ]
+    columns = ['InstanceType', 'AcceleratorName', 'AcceleratorCount', 'vCPUs', 'MemoryGiB', 'GpuInfo', 'Price', 'SpotPrice', 'Region', 'AvailabilityZone']
+    return pd.DataFrame(data, columns=columns)
+
+_df = get_default_data()
