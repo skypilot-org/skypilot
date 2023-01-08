e92bd47c86249d434ed5e19a9bb2e4a8b4c79dbd
Zhanghao Wu <zhanghao.wu@outlook.com>
2023-01-08 11:55:48 -0800

    create once

--- sky/skylet/configs.py
+++ sky/skylet/configs.py
@@ -9,6 +9,8 @@ from typing import Optional
 _DB_PATH = os.path.expanduser('~/.sky/skylet_config.db')
 os.makedirs(pathlib.Path(_DB_PATH).parents[0], exist_ok=True)
 
+_table_created = False
+
 
 @contextlib.contextmanager
 def _safe_cursor():
@@ -22,20 +24,28 @@ def _safe_cursor():
         conn.commit()
         conn.close()
 
+
 def ensure_table(func: callable):
     """Ensure the table exists before calling the function."""
+
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        with _safe_cursor() as c:  # Call it 'c' to avoid pylint complaining.
-            # Use WAL mode to avoid locking problem in #1507.
-            # Reference: https://stackoverflow.com/a/39265148
-            c.execute('PRAGMA journal_mode=WAL')
-            c.execute("""\
-                CREATE TABLE IF NOT EXISTS config (
-                    key TEXT PRIMARY KEY,
-                    value TEXT)""")
+        global _table_created
+        if not _table_created:
+            with _safe_cursor(
+            ) as c:  # Call it 'c' to avoid pylint complaining.
+                # Use WAL mode to avoid locking problem in #1507.
+                # Reference: https://stackoverflow.com/a/39265148
+                c.execute('PRAGMA journal_mode=WAL')
+                c.execute("""\
+                    CREATE TABLE IF NOT EXISTS config (
+                        key TEXT PRIMARY KEY,
+                        value TEXT)""")
+        _table_created = True
         return func(*args, **kwargs)
+
     return wrapper
+
 
 @ensure_table
 def get_config(key: str) -> Optional[str]:
@@ -43,6 +53,7 @@ def get_config(key: str) -> Optional[str]:
         rows = cursor.execute('SELECT value FROM config WHERE key = ?', (key,))
         for (value,) in rows:
             return value
+
 
 @ensure_table
 def set_config(key: str, value: str) -> None:
