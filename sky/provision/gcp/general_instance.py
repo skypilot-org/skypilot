b69b5977029b5a0a0591b64c3b54b275e119f97d
Siyuan <suquark@gmail.com>
2023-01-26 01:19:27 -0800

    update

--- sky/provision/gcp/general_instance.py
+++ sky/provision/gcp/general_instance.py
@@ -2,6 +2,7 @@ from typing import List, Dict, Optional, Any
 
 import copy
 import logging
+import re
 import time
 
 from sky.provision.gcp import config, utils
@@ -19,6 +20,39 @@ def _construct_label_filter_expr(label_filters: dict) -> str:
 def _construct_status_filter_expr(status_filter: list) -> str:
     exprs = [f'(status = {status})' for status in status_filter]
     return f'({" OR ".join(exprs)})'
+
+
+def _convert_resources_to_urls(configuration_dict: Dict[str,
+                                                        Any], project_id: str,
+                               availability_zone: str) -> Dict[str, Any]:
+    """Ensures that resources are in their full URL form.
+
+    GCP expects machineType and accleratorType to be a full URL (e.g.
+    `zones/us-west1/machineTypes/n1-standard-2`) instead of just the
+    type (`n1-standard-2`)
+
+    Args:
+        configuration_dict: Dict of options that will be passed to GCP
+    Returns:
+        Input dictionary, but with possibly expanding `machineType` and
+            `acceleratorType`.
+    """
+    configuration_dict = copy.deepcopy(configuration_dict)
+    existing_machine_type = configuration_dict['machineType']
+    if not re.search('.*/machineTypes/.*', existing_machine_type):
+        configuration_dict[
+            'machineType'] = 'zones/{zone}/machineTypes/{machine_type}'.format(
+                zone=availability_zone,
+                machine_type=configuration_dict['machineType'],
+            )
+
+    for accelerator in configuration_dict.get('guestAccelerators', []):
+        gpu_type = accelerator['acceleratorType']
+        if not re.search('.*/acceleratorTypes/.*', gpu_type):
+            accelerator[
+                'acceleratorType'] = f'projects/{project_id}/zones/{availability_zone}/acceleratorTypes/{gpu_type}'  # noqa: E501
+
+    return configuration_dict
 
 
 def _wait_for_operation(
@@ -130,10 +164,88 @@ def resume_instances(region: str, cluster_name: str, tags: Dict[str, str],
     return instances
 
 
-def create_or_resume_instances(region: str, cluster_name: str,
-                               node_config: Dict[str, Any],
-                               tags: Dict[str, str], count: int,
-                               resume_stopped_nodes: bool) -> Dict[str, Any]:
+def create_instances(region: str, cluster_name: str,
+                     node_config: Dict[str, Any], tags: Dict[str, str],
+                     count: int, provider_config: Dict[str,
+                                                       Any]) -> Dict[str, Any]:
+    project_id = provider_config['project_id']
+    availability_zone = provider_config.get('availability_zone')
+    compute_client = config.construct_compute_client_from_provider_config(
+        provider_config)
+
+    if availability_zone is None:
+        zones = utils.get_zones_from_regions(region, project_id, compute_client)
+        availability_zone = zones[0]
+
+    labels = tags
+
+    config = _convert_resources_to_urls(node_config, project_id,
+                                        availability_zone)
+    # removing TPU-specific default key set in config.py
+    config.pop('networkConfig', None)
+    name = utils.generate_node_name(labels, 'compute')
+
+    labels = dict(config.get('labels', {}), **labels)
+
+    config.update({
+        'labels': dict(labels, **{utils.TAG_RAY_CLUSTER_NAME: cluster_name}),
+        'name': name,
+    })
+
+    # Allow Google Compute Engine instance templates.
+    #
+    # Config example:
+    #
+    #     ...
+    #     node_config:
+    #         sourceInstanceTemplate: global/instanceTemplates/worker-16
+    #         machineType: e2-standard-16
+    #     ...
+    #
+    # node_config parameters override matching template parameters, if any.
+    #
+    # https://cloud.google.com/compute/docs/instance-templates
+    # https://cloud.google.com/compute/docs/reference/rest/v1/instances/insert
+    source_instance_template = config.pop('sourceInstanceTemplate', None)
+
+    # Here we use the zonal API for bulk instance creation.
+    # There is also regional instance creation API.
+    # https://cloud.google.com/compute/docs/instances/multiple/create-in-bulk
+
+    if count > 1:
+        # 'name' is a field for creating a single instance, we pop it here
+        #  and use a pattern instead.
+        name_pattern = config.pop('name') + '-' + '#' * len(str(count))
+        body = {
+            'count': count,  # this is the max count
+            'minCount': count,
+            'namePattern': name_pattern,
+            'instanceProperties': config,
+            'sourceInstanceTemplate': source_instance_template,
+        }
+
+        operation = compute_client.instances().bulkInsert(
+            project=project_id,
+            zone=availability_zone,
+            body=body,
+        ).execute()
+    else:
+        operation = compute_client.instances().insert(
+            project=project_id,
+            zone=availability_zone,
+            sourceInstanceTemplate=source_instance_template,
+            body=config,
+        ).execute()
+
+    _wait_for_operation(compute_client, operation, project_id,
+                        availability_zone)
+    return name
+
+
+def create_or_resume_instances(
+        region: str, cluster_name: str, node_config: Dict[str, Any],
+        tags: Dict[str, str], count: int, resume_stopped_nodes: bool,
+        provider_config: Optional[Dict]) -> Dict[str, Any]:
     """Creates instances.
 
     Returns dict mapping instance id to ec2.Instance object for the created
@@ -145,12 +257,14 @@ def create_or_resume_instances(region: str, cluster_name: str,
     all_created_nodes = {}
     # Try to reuse previously stopped nodes with compatible configs
     if resume_stopped_nodes:
-        all_created_nodes = resume_instances(region, cluster_name, tags, count)
+        all_created_nodes = resume_instances(region, cluster_name, tags, count,
+                                             provider_config)
 
     remaining_count = count - len(all_created_nodes)
     if remaining_count > 0:
         created_nodes_dict = create_instances(region, cluster_name, node_config,
-                                              tags, remaining_count)
+                                              tags, remaining_count,
+                                              provider_config)
         all_created_nodes.update(created_nodes_dict)
     return all_created_nodes
 
--- sky/provision/gcp/utils.py
+++ sky/provision/gcp/utils.py
@@ -3,9 +3,13 @@ from typing import List, Union, Tuple, Optional
 import functools
 import re
 import time
+import uuid
 
+# Tag for the name of the node
+TAG_RAY_NODE_NAME = "ray-node-name"
 # Tag uniquely identifying all nodes of a cluster
 TAG_RAY_CLUSTER_NAME = "ray-cluster-name"
+
 MAX_POLLS = 12
 # TPUs take a long while to respond, so we increase the MAX_POLLS
 # considerably - this probably could be smaller
@@ -14,6 +18,9 @@ MAX_POLLS_TPU = MAX_POLLS * 8
 # Stopping instances can take several minutes, so we increase the timeout
 MAX_POLLS_STOP = MAX_POLLS * 8
 POLL_INTERVAL = 5
+
+INSTANCE_NAME_MAX_LEN = 64
+INSTANCE_NAME_UUID_LEN = 8
 
 
 def retry_on_exception(
@@ -57,4 +64,21 @@ def get_zones_from_regions(region: str, project_id: str,
                            compute_client) -> List[str]:
     response = compute_client.zones().list(
         project=project_id, filter=f'name eq "^{region}-.*"').execute()
-    return [zone['name'] for zone in response.get('items', [])]
+    return sorted([zone['name'] for zone in response.get('items', [])])
+
+
+def generate_node_name(labels: dict, node_suffix: str) -> str:
+    """Generate node name from labels and suffix.
+
+    This is required so that the correct resource can be selected
+    when the only information autoscaler has is the name of the node.
+
+    The suffix is expected to be one of 'compute' or 'tpu'
+    (as in ``GCPNodeType``).
+    """
+    name_label = labels[TAG_RAY_NODE_NAME]
+    if len(name_label) > INSTANCE_NAME_MAX_LEN - INSTANCE_NAME_UUID_LEN - 1:
+        raise ValueError(
+            f'Instance name ({name_label}) too long ({len(name_label)})')
+    uid = uuid.uuid4().hex[:INSTANCE_NAME_UUID_LEN]
+    return f'{name_label}-{uid}-{node_suffix}'
