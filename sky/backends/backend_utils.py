d3d27b774782f94fc093ef218879748245b265db
Zhanghao Wu <zhanghao.wu@outlook.com>
2023-01-26 13:56:10 -0800

    fix 'to_down'

--- sky/backends/backend_utils.py
+++ sky/backends/backend_utils.py
@@ -1973,7 +1973,7 @@ def _update_cluster_status_no_lock(
             else:
                 ux_utils.console_newline()
                 operation_str = 'autodowning' if record[
-                    'down'] else 'autostopping'
+                    'to_down'] else 'autostopping'
                 logger.info(
                     f'Cluster {cluster_name!r} is {operation_str}, it will remain '
                     'in INIT state until the autostop is finished.')
