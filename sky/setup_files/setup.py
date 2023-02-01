0cb298b0ee6207d42e37a2aca553dca9068dafd4
Zhanghao Wu <zhanghao.wu@outlook.com>
2023-01-31 21:26:45 -0800

    address comments

--- sky/setup_files/setup.py
+++ sky/setup_files/setup.py
@@ -64,9 +64,9 @@ def parse_readme(readme: str) -> str:
 
 install_requires = [
     'wheel',
-    # NOTE: ray 2.2.0 requires click<=8.0.4,>=7.0; We disable the
+    # NOTE: ray 2.2.0 requires click>=7.0; We disable the
     # shell completion for click<8.0 for backward compatibility.
-    'click<=8.0.4,>=7.0',
+    'click>=7.0',
     # NOTE: required by awscli. To avoid ray automatically installing
     # the latest version.
     'colorama<0.4.5',
--- sky/skylet/providers/aws/cloudwatch/cloudwatch_helper.py
+++ sky/skylet/providers/aws/cloudwatch/cloudwatch_helper.py
@@ -9,7 +9,7 @@ from typing import Any, Callable, Dict, List, Union
 
 import botocore
 
-from ray.autoscaler._private.aws.utils import client_cache, resource_cache
+from sky.skylet.providers.aws.utils import client_cache, resource_cache
 from ray.autoscaler.tags import NODE_KIND_HEAD, TAG_RAY_CLUSTER_NAME, TAG_RAY_NODE_KIND
 
 logger = logging.getLogger(__name__)
