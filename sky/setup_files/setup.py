ae1937f762f42bc855e3dbf82a2343856625fa23
Edward Zeng <edwardzeng@berkeley.edu>
2023-02-05 10:21:03 -0800

    [Lambda Cloud] Update docs and add `[lambda]` install option (#1659)
    * Update docs and add [lambda] option
    
    * Update docs
    
    * Nit

--- sky/setup_files/setup.py
+++ sky/setup_files/setup.py
@@ -116,6 +116,7 @@ extras_require = {
     'azure': ['azure-cli>=2.31.0', 'azure-core', 'azure-identity'],
     'gcp': ['google-api-python-client', 'google-cloud-storage'],
     'docker': ['docker'],
+    'lambda': [],
 }
 
 extras_require['all'] = sum(extras_require.values(), [])
