8269594951c8ae24874b6cc7184e2cdeaa17effb
Sumanth <sumanth@MacBook-Pro-5.local>
2023-02-08 19:19:58 -0800

    pr changes

--- sky/global_user_state.py
+++ sky/global_user_state.py
@@ -426,7 +426,7 @@ def _get_cluster_launch_time(cluster_hash: str) -> Optional[Dict[str, Any]]:
     return usage_intervals[0][0]
 
 
-def _get_cluster_duration(cluster_hash: str) -> Optional[Dict[str, Any]]:
+def _get_cluster_duration(cluster_hash: str) -> int:
     total_duration = 0
     usage_intervals = _get_cluster_usage_intervals(cluster_hash)
 
@@ -435,8 +435,6 @@ def _get_cluster_duration(cluster_hash: str) -> Optional[Dict[str, Any]]:
 
     for i, (start_time, end_time) in enumerate(usage_intervals):
         # duration from latest start time to time of query
-        if start_time is None:
-            continue
         if end_time is None:
             assert i == len(usage_intervals) - 1, i
             end_time = int(time.time())
