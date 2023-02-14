c0994287e08e39a7f47e550af59c6f9974abbf1c
WoosukKwon <woosuk.kwon@berkeley.edu>
2023-02-14 12:27:10 -0800

    Add a comment on service id

--- sky/clouds/service_catalog/data_fetchers/fetch_gcp.py
+++ sky/clouds/service_catalog/data_fetchers/fetch_gcp.py
@@ -19,6 +19,7 @@ import pandas as pd
 
 GCP_METADATA_URL = 'https://raw.githubusercontent.com/skypilot-org/skypilot-catalog/fix-gcp/metadata/gcp/'  # pylint: disable=line-too-long
 
+# Service IDs found in https://cloud.google.com/skus 
 GCE_SERVICE_ID = '6F81-5844-456A'
 TPU_SERVICE_ID = 'E000-3F24-B8AA'
 
