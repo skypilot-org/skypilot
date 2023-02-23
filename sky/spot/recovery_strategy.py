67290dbb2030e7d951158c98b0ca7bf0590dc4b4
Zhanghao Wu <zhanghao.wu@outlook.com>
2023-02-22 22:07:29 -0800

    format

--- sky/spot/recovery_strategy.py
+++ sky/spot/recovery_strategy.py
@@ -247,9 +247,8 @@ class StrategyExecutor:
                            _is_launched_by_spot_controller=True)
                 logger.info('Spot cluster launched.')
             except exceptions.InvalidClusterNameError as e:
-                logger.error(
-                        'Failure happened before provisioning. '
-                        f'{common_utils.format_exception(e)}')
+                logger.error('Failure happened before provisioning. '
+                             f'{common_utils.format_exception(e)}')
                 if raise_on_failure:
                     raise exceptions.ProvisionPrechecksError(reasons=[e])
                 return None
