590912204fd272f57988d89b69393b8e06826696
doyoungkim <doyoung.kim@zoom.us>
2023-02-27 15:35:19 -0800

    add gs:// to test_smoke

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1882,7 +1882,7 @@ class TestStorageWithCredentials:
         assert tmp_public_storage_obj.name not in out.decode('utf-8')
 
     @pytest.mark.parametrize('nonexist_bucket_url',
-                             ['s3://{random_name}', 'r2://{random_name}'])
+                             ['s3://{random_name}', 'gs://{random_name}','r2://{random_name}'])
     def test_nonexistent_bucket(self, nonexist_bucket_url):
         # Attempts to create fetch a stroage with a non-existent source.
         # Generate a random bucket name and verify it doesn't exist:
@@ -1936,7 +1936,7 @@ class TestStorageWithCredentials:
                 random_name=nonexist_bucket_name))
 
     @pytest.mark.parametrize('private_bucket',
-                             [f's3://imagenet'])
+                             [f's3://imagenet','gs://imagenet'])
     def test_private_bucket(self, private_bucket):
         # Attempts to access private buckets not belonging to the user.
         # These buckets are known to be private, but may need to be updated if
