b2249fe123f99bcbf2496f9f33f33f0998d89aa6
Sumanth <sumanth@MacBook-Pro-5.local>
2023-03-15 17:25:15 -0700

    fix PR comment

--- sky/cli.py
+++ sky/cli.py
@@ -1663,8 +1663,9 @@ def cost_report(all: bool):  # pylint: disable=redefined-builtin
         status_utils.show_cost_report_table(
             [cluster_record], all, reserved_group_name=cluster_group_name)
     click.secho(
-        'NOTE: This feature is experimental.'
-        '\nCosts for autostop clusters may not be accurate.',
+        'NOTE: This feature is experimental. '
+        'Costs for clusters with auto{stop,down} '
+        'scheduled may not be accurate.',
         fg='yellow')
 
 
