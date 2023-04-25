496ab76dd4f70fad7cb05630a685f18bb3cc74ce
Isaac Ong <isaacong.jw@gmail.com>
2023-04-25 12:05:16 -0700

    Add docstr for validation method

--- sky/backends/backend_utils.py
+++ sky/backends/backend_utils.py
@@ -2472,7 +2472,20 @@ def stop_handler(signum, frame):
 
 
 def validate_schema(obj, schema, err_msg_prefix='', skip_none=False):
-    # yaml.safe_load() loads value as None if the field is empty.
+    """Validates an object against a given JSON schema.
+
+    Args:
+        obj: The object to validate.
+        schema: The JSON schema against which to validate the object.
+        err_msg_prefix: The string to prepend to the error message if
+          validation fails.
+        skip_none: If True, removes fields with value None from the object
+          before validation. This is useful for objects that will never contain
+          None because yaml.safe_load() loads empty fields as None.
+
+    Raises:
+        ValueError: if the object does not match the schema.
+    """
     if skip_none:
         obj = {k: v for k, v in obj.items() if v is not None}
     err_msg = None
