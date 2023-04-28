82f210f348f28af2cd0f67a81530cff3406973c7
Doyoung Kim <kei0112777@gmail.com>
2023-04-27 23:52:54 -0700

    format

--- sky/backends/cloud_vm_ray_backend.py
+++ sky/backends/cloud_vm_ray_backend.py
@@ -3570,14 +3570,10 @@ class CloudVmRayBackend(backends.Backend['CloudVmRayResourceHandle']):
                 )
             except exceptions.CommandError as e:
                 if e.returncode == exceptions.MOUNT_PATH_NON_EMPTY_CODE:
-                    mount_path = (f'{colorama.Fore.RED}'
-                                  f'{colorama.Style.BRIGHT}{dst}'
-                                  f'{colorama.Style.RESET_ALL}')
+                    mount_path = f'{colorama.Fore.RED}{colorama.Style.BRIGHT}{dst}{colorama.Style.RESET_ALL}'
                     raise exceptions.StorageMountPathError(
-                        f'Mount path {mount_path} you set in ENTRYPOINT ' 
-                        f'is non-empty. {mount_path} may have been already'
-                        f' taken by the Kernel. Please set the mount path '
-                        f'to another name.') from None
+                        f'Mount path {mount_path} is non-empty. {mount_path} may have been already taken by the Kernel. Please set the mount path to another name.'
+                    ) from None
 
         end = time.time()
         logger.debug(f'Storage mount sync took {end - start} seconds.')
