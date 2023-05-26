02902f66a514655a99064bf625c99f4b341a41f7
Saikrishna Achalla <saikrishnaachalla@Saikrishnas-MBP.fritz.box>
2023-05-26 21:28:58 +0200

    Added few tests for show_gpus. Still missing tests on zero and negative quantities. Missing tests on lambda because of the cloud output compared to others

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -73,3 +73,4 @@ def test_accelerator_mismatch(enable_all_clouds):
         _capture_match_gpus_spec(f.name, 'v100:1')
         _capture_match_gpus_spec(f.name, 'V100:0.5')
         _capture_match_gpus_spec(f.name, 'V100')
+        
