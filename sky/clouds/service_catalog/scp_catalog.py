58d5af92453903925430cd822d783620805aa82a
WoosukKwon <woosuk.kwon@berkeley.edu>
2023-05-31 11:17:10 -0700

    Fix list_accelerators

--- sky/clouds/service_catalog/scp_catalog.py
+++ sky/clouds/service_catalog/scp_catalog.py
@@ -121,14 +121,15 @@ def get_region_zones_for_instance_type(instance_type: str,
 
 
 def list_accelerators(
-        gpus_only: bool,
-        name_filter: Optional[str],
-        region_filter: Optional[str],
-        case_sensitive: bool = True
+    gpus_only: bool,
+    name_filter: Optional[str],
+    region_filter: Optional[str],
+    quantity_filter: Optional[int],
+    case_sensitive: bool = True,
 ) -> Dict[str, List[common.InstanceTypeInfo]]:
     """Returns all instance types in SCP offering GPUs."""
     return common.list_accelerators_impl('scp', _df, gpus_only, name_filter,
-                                         region_filter, case_sensitive)
+                                         region_filter, quantity_filter, case_sensitive)
 
 
 def get_image_id_from_tag(tag: str, region: Optional[str]) -> Optional[str]:
