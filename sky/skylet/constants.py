2fa6a00d72c55163fa37536f93f9997e59fcf7be
Zhanghao Wu <zhanghao.wu@outlook.com>
2023-07-14 00:52:05 -0700

    Avoid install python 3.11 conda

--- sky/skylet/constants.py
+++ sky/skylet/constants.py
@@ -46,3 +46,10 @@ SKYLET_VERSION_FILE = '~/.sky/skylet_version'
 #
 # Port on the remote spot controller that the dashboard is running on.
 SPOT_DASHBOARD_REMOTE_PORT = 5000
+
+CONDA_INSTALLATION_COMMANDS = (
+    '(which conda > /dev/null 2>&1 && conda init > /dev/null) || '
+    '(wget -nc https://repo.anaconda.com/miniconda/Miniconda3-py39_23.5.2-0-Linux-x86_64.sh -O Miniconda3-Linux-x86_64.sh && '  # pylint: disable=line-too-long
+    'bash Miniconda3-Linux-x86_64.sh -b && '
+    'eval "$(~/miniconda3/bin/conda shell.bash hook)" && conda init && '
+    'conda config --set auto_activate_base true);')
