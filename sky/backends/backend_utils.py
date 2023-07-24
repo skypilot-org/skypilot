04c8939eb8a445c942a6b4f2cd83c34ab65397fd
cblmemo <cblmemo@gmail.com>
2023-07-24 14:51:09 -0700

    rename default service name

--- sky/backends/backend_utils.py
+++ sky/backends/backend_utils.py
@@ -1330,9 +1330,7 @@ def generate_cluster_name():
 
 
 def generate_service_name():
-    # TODO: change this ID formatting to something more pleasant.
-    # User name is helpful in non-isolated accounts, e.g., GCP, Azure.
-    return f'sky-service-{uuid.uuid4().hex[:4]}-{get_cleaned_username()}'
+    return f'service-{uuid.uuid4().hex[:4]}'
 
 
 def get_cleaned_username() -> str:
