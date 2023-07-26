02e3415b0b856a06d32087334231653de92285cc
Romil Bhardwaj <romil.bhardwaj@gmail.com>
2023-07-26 10:00:19 -0700

    lint

--- sky/skylet/providers/kubernetes/node_provider.py
+++ sky/skylet/providers/kubernetes/node_provider.py
@@ -10,7 +10,6 @@ from sky.skylet.providers.kubernetes import config
 from sky.skylet.providers.kubernetes import get_head_ssh_port
 from sky.skylet.providers.kubernetes import utils
 from ray.autoscaler._private.command_runner import SSHCommandRunner
-from ray.autoscaler._private.cli_logger import cli_logger
 from ray.autoscaler.node_provider import NodeProvider
 from ray.autoscaler.tags import NODE_KIND_HEAD, TAG_RAY_CLUSTER_NAME, TAG_RAY_NODE_KIND
 
