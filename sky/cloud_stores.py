e4424db1ba12bf99f0ff8fbeeaab1dc8445b7523
Avi Weit <weit@il.ibm.com>
2023-07-30 13:57:44 +0300

    Fix invalid bucket name.
    - At make_sync_file_command:
      add replacement of minio:// -> s3:// and remove s3 prefix from source

--- sky/cloud_stores.py
+++ sky/cloud_stores.py
@@ -261,9 +261,11 @@ class MinioCloudStorage(CloudStorage):
     def make_sync_file_command(self, source: str, destination: str) -> str:
         """Downloads a file using AWS CLI."""
         endpoint_url = minio.create_endpoint()
+        if 'minio://' in source:
+            source = source.replace('minio://', 's3://')
         download_via_awscli = ('AWS_SHARED_CREDENTIALS_FILE='
                                f'{minio.MINIO_CREDENTIALS_PATH} '
-                               f'aws s3 cp s3://{source} {destination} '
+                               f'aws s3 cp {source} {destination} '
                                f'--endpoint {endpoint_url} '
                                f'--profile={minio.MINIO_PROFILE_NAME}')
 
