5b0d2b56fd3455dce14e9b59613a9374d3a360da
cblmemo <cblmemo@gmail.com>
2023-08-08 22:06:05 -0700

    new log logic: use cluster status == UP instead of waiting 10s; early quit for replica not exist; skip all detailed file sync log

--- sky/serve/serve_utils.py
+++ sky/serve/serve_utils.py
@@ -7,7 +7,7 @@ import re
 import requests
 import shlex
 import time
-from typing import Any, Dict, List, Optional, Iterator, TextIO, Callable, Union
+from typing import Any, Dict, List, Optional, Iterator, TextIO, Callable
 
 from sky import backends
 from sky import global_user_state
@@ -16,6 +16,7 @@ from sky import status_lib
 from sky.utils import common_utils
 
 _CONTROL_PLANE_URL = f'http://localhost:{constants.CONTROL_PLANE_PORT}'
+_SKYPILOT_PROVISION_LOG_PATTERN = r'.*tail -n100 -f (.*provision\.log).*'
 _SKYPILOT_LOG_PATTERN = r'.*tail -n100 -f (.*\.log).*'
 _FAILED_TO_FIND_REPLICA_MSG = (
     f'{colorama.Fore.RED}Failed to find replica '
@@ -64,54 +65,81 @@ def load_terminate_service_result(payload: str) -> Any:
     return terminate_resp
 
 
-def _follow_logs(
-        file: TextIO,
-        finish_stream: Union[int, Callable[[], bool]]) -> Iterator[str]:
+def _follow_logs(file: TextIO,
+                 cluster_name: str,
+                 *,
+                 finish_stream: Callable[[], bool],
+                 no_new_content_timeout: Optional[int] = None) -> Iterator[str]:
     line = ''
     log_file = None
     no_new_content_cnt = 0
+
+    def cluster_is_up() -> bool:
+        cluster_record = global_user_state.get_cluster_from_name(cluster_name)
+        if cluster_record is None:
+            return False
+        return cluster_record['status'] == status_lib.ClusterStatus.UP
+
     while True:
         tmp = file.readline()
         if tmp is not None and tmp != '':
+            no_new_content_cnt = 0
             line += tmp
             if '\n' in line or '\r' in line:
                 # Tailing detailed progress for user. All logs in skypilot is
                 # of format `To view detailed progress: tail -n100 -f *.log`.
-                x = re.match(_SKYPILOT_LOG_PATTERN, line)
+                x = re.match(_SKYPILOT_PROVISION_LOG_PATTERN, line)
                 if x is not None:
                     log_file = os.path.expanduser(x.group(1))
-                else:
+                elif re.match(_SKYPILOT_LOG_PATTERN, line) is None:
+                    # Not print other logs (file sync logs) since we lack
+                    # utility to determine when these log files are finished
+                    # writing.
+                    # TODO(tian): Not skip these logs since there are small
+                    # chance that error will happen in file sync. Need to find
+                    # a better way to do this.
                     yield line
-                    # Output next line first since it usually is some sentence
-                    # indicating the process is starting. For example:
+                    # Output next line first since it indicates the process is
+                    # starting. For our launching logs, it's always:
                     # Launching on <cloud> <region> (<zone>)
                     if log_file is not None:
                         with open(log_file, 'r', newline='') as f:
-                            # TODO(tian): Hack here for exit if more than 10
-                            # seconds without new content. Need to find a better
-                            # way to do this. Maybe add some callback to sky?
-                            for l in _follow_logs(f, 10):
+                            # We still exit if more than 10 seconds without new
+                            # content to avoid any internal bug that causes
+                            # the launch failed and cluster status remains INIT.
+                            for l in _follow_logs(f,
+                                                  cluster_name,
+                                                  finish_stream=cluster_is_up,
+                                                  no_new_content_timeout=10):
                                 yield l
                         log_file = None
                 line = ''
         else:
-            if isinstance(finish_stream, int):
-                if no_new_content_cnt >= finish_stream:
+            if finish_stream():
+                break
+            if no_new_content_timeout is not None:
+                if no_new_content_cnt >= no_new_content_timeout:
                     break
                 no_new_content_cnt += 1
                 time.sleep(1)
-            else:
-                if finish_stream():
-                    break
-
-
-def _stream_file(filename: str, finish_stream: Callable[[], bool]) -> None:
-    with open(filename, 'r', newline='') as f:
-        for line in _follow_logs(f, finish_stream):
-            print(line, end='', flush=True)
 
 
 def stream_logs(service_name: str, replica_id: int, follow: bool) -> str:
+    print(f'{colorama.Fore.YELLOW}Start streaming logs for launching process '
+          f'of replica {replica_id}.{colorama.Style.RESET_ALL}')
+    replica_cluster_name = generate_replica_cluster_name(
+        service_name, replica_id)
+    handle = global_user_state.get_handle_from_cluster_name(
+        replica_cluster_name)
+    if handle is None:
+        return _FAILED_TO_FIND_REPLICA_MSG.format(replica_id=replica_id)
+    assert isinstance(handle, backends.CloudVmRayResourceHandle), handle
+
+    launch_log_file_name = generate_replica_launch_log_file_name(
+        replica_cluster_name)
+    if not os.path.exists(launch_log_file_name):
+        return (f'{colorama.Fore.RED}Replica {replica_id} doesn\'t exist.'
+                f'{colorama.Style.RESET_ALL}')
 
     def _get_replica_status() -> status_lib.ReplicaStatus:
         resp = requests.get(_CONTROL_PLANE_URL +
@@ -132,24 +160,18 @@ def stream_logs(service_name: str, replica_id: int, follow: bool) -> str:
                 _FAILED_TO_FIND_REPLICA_MSG.format(replica_id=replica_id))
         return target_info['status']
 
-    print(f'{colorama.Fore.YELLOW}Start streaming logs for launching process '
-          f'of replica {replica_id}.{colorama.Style.RESET_ALL}')
-    replica_cluster_name = generate_replica_cluster_name(
-        service_name, replica_id)
-    _stream_file(
-        generate_replica_launch_log_file_name(replica_cluster_name),
-        finish_stream=lambda: follow and
-        (_get_replica_status() != status_lib.ReplicaStatus.PROVISIONING))
+    replica_is_provisioning = (lambda: follow and _get_replica_status() !=
+                               status_lib.ReplicaStatus.PROVISIONING)
+    with open(launch_log_file_name, 'r', newline='') as f:
+        for line in _follow_logs(f,
+                                 replica_cluster_name,
+                                 finish_stream=replica_is_provisioning):
+            print(line, end='', flush=True)
     if not follow and _get_replica_status(
     ) == status_lib.ReplicaStatus.PROVISIONING:
         # Early exit if not following the logs.
         return ''
 
-    handle = global_user_state.get_handle_from_cluster_name(
-        replica_cluster_name)
-    if handle is None:
-        return _FAILED_TO_FIND_REPLICA_MSG.format(replica_id=replica_id)
-    assert isinstance(handle, backends.CloudVmRayResourceHandle), handle
     backend = backends.CloudVmRayBackend()
     # Always tail the logs of the first job, which represent user setup & run.
     returncode = backend.tail_logs(handle, job_id=1, follow=follow)
