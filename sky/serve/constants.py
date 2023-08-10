e0054acbc4bf6ed91ca11cf8763a73bb73a701c3
cblmemo <cblmemo@gmail.com>
2023-08-09 17:34:17 -0700

    change controller resources vcpu to 4+ to avoid no 4 vcpu cloud

--- sky/serve/constants.py
+++ sky/serve/constants.py
@@ -11,4 +11,4 @@ CONTROL_PLANE_PORT = 31001
 CONTROL_PLANE_SYNC_INTERVAL = 20
 
 # TODO(tian): Remove cloud == GCP.
-CONTROLLER_RESOURCES = {'cloud': 'gcp', 'disk_size': 100, 'cpus': 4}
+CONTROLLER_RESOURCES = {'cloud': 'gcp', 'disk_size': 100, 'cpus': '4+'}
