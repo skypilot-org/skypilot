ace1f748cc8c8380bda6ae7d5ddad95ef9685999
cblmemo <cblmemo@gmail.com>
2023-08-11 21:18:21 -0700

    add comment for DEFAULT_INITIAL_DELAY_SECONDS

--- sky/serve/constants.py
+++ sky/serve/constants.py
@@ -12,5 +12,7 @@ CONTROL_PLANE_SYNC_INTERVAL = 20
 
 CONTROLLER_RESOURCES = {'disk_size': 100, 'cpus': '4+'}
 
+# A period of time to initialize your service. Any readiness probe failures
+# during this period will be ignored.
 DEFAULT_INITIAL_DELAY_SECONDS = 1200
 DEFAULT_MIN_REPLICA = 1
