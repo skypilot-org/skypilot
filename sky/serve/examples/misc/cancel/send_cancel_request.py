f1198fd747258870cd4d47e6891a567487e1ec6e
cblmemo <cblmemo@gmail.com>
2023-08-17 13:05:38 -0700

    add cancel and gorilla example

---
+++ sky/serve/examples/misc/cancel/send_cancel_request.py
@@ -0,0 +1,29 @@
+import aiohttp
+import asyncio
+
+redirector_endpoint = input('Enter redirector endpoint: ')
+
+async def fetch(session, url):
+    try:
+        async with session.get(url) as response:
+            print("Got response!")
+            return await response.text()
+    except asyncio.CancelledError:
+        print("Request was cancelled!")
+        raise
+
+async def main():
+    timeout = 2
+
+    async with aiohttp.ClientSession() as session:
+        task = asyncio.create_task(fetch(session, f'http://{redirector_endpoint}/'))
+
+        await asyncio.sleep(timeout)
+        task.cancel()
+
+        try:
+            await task
+        except asyncio.CancelledError:
+            print("Main function caught the cancelled exception.")
+
+asyncio.run(main())
