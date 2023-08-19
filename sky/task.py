8e427af72d77727d6e675ad3554f9f8278740819
cblmemo <cblmemo@gmail.com>
2023-08-18 17:10:33 -0700

    move check_and_fill_in_docker_login_config to from_yaml_config

--- sky/task.py
+++ sky/task.py
@@ -104,6 +104,27 @@ def _fill_in_env_vars_in_file_mounts(
     return json.loads(file_mounts_str)
 
 
+def _with_docker_login_config(
+    resources: 'resources_lib.Resources',
+    task_envs: Dict[str, str],
+) -> 'resources_lib.Resources':
+    all_keys = {
+        constants.DOCKER_USERNAME_ENV_KEY,
+        constants.DOCKER_PASSWORD_ENV_KEY,
+        constants.DOCKER_REPO_URI_ENV_KEY,
+    }
+    existing_keys = all_keys & set(task_envs.keys())
+    if not existing_keys:
+        return
+    if len(existing_keys) != len(all_keys):
+        with ux_utils.print_exception_no_traceback():
+            raise ValueError('If any of DOCKER_USERNAME, DOCKER_PASSWORD, '
+                             'DOCKER_REPO_URI is set, all of them must be set. '
+                             f'Missing envs: {all_keys - existing_keys}')
+    return resources.copy(
+        _docker_login_config={k: task_envs.pop(k) for k in existing_keys})
+
+
 class Task:
     """Task: a computation to be run on the cloud."""
 
@@ -370,6 +391,7 @@ class Task:
 
         resources = config.pop('resources', None)
         resources = sky.Resources.from_yaml_config(resources)
+        resources = _with_docker_login_config(resources, task.envs)
 
         task.set_resources({resources})
         assert not config, f'Invalid task args: {config.keys()}'
@@ -552,25 +574,6 @@ class Task:
                 f'Node [{self}] does not have a cost model set; '
                 'call set_time_estimator() first')
         return self.time_estimator_func(resources)
-
-    def check_and_fill_in_docker_login_config(self) -> None:
-        all_keys = {
-            constants.DOCKER_USERNAME_ENV_KEY,
-            constants.DOCKER_PASSWORD_ENV_KEY,
-            constants.DOCKER_REPO_URI_ENV_KEY,
-        }
-        existing_keys = all_keys & set(self.envs.keys())
-        if not existing_keys:
-            return
-        if len(existing_keys) != len(all_keys):
-            with ux_utils.print_exception_no_traceback():
-                raise ValueError(
-                    'If any of DOCKER_USERNAME, DOCKER_PASSWORD, '
-                    'DOCKER_REPO_URI is set, all of them must be set. '
-                    f'Missing envs: {all_keys - existing_keys}')
-        for resource in self.resources:
-            resource.set_docker_login_config(
-                {k: self.envs.pop(k) for k in existing_keys})
 
     def set_file_mounts(self, file_mounts: Optional[Dict[str, str]]) -> 'Task':
         """Sets the file mounts for this task.
