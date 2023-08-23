c6b116676d83a0d223d0253c901a9249a94b9153
Zhanghao Wu <zhanghao.wu@outlook.com>
2023-08-23 15:45:01 -0700

    format

--- sky/backends/cloud_vm_ray_backend.py
+++ sky/backends/cloud_vm_ray_backend.py
@@ -3864,10 +3864,12 @@ class CloudVmRayBackend(backends.Backend['CloudVmRayResourceHandle']):
                   'SKYPILOT_ERROR_NO_NODES_LAUNCHED: ' not in stderr and
                   '(ResourceGroupNotFound)' not in stderr):
                 raise RuntimeError(
-                    _TEARDOWN_FAILURE_MESSAGE.format(extra_reason='',
-                                                     cluster_name=common_utils.cluster_name_in_hint(cluster_name, cluster_name_on_cloud),
-                                                     stdout=stdout,
-                                                     stderr=stderr))
+                    _TEARDOWN_FAILURE_MESSAGE.format(
+                        extra_reason='',
+                        cluster_name=common_utils.cluster_name_in_hint(
+                            cluster_name, cluster_name_on_cloud),
+                        stdout=stdout,
+                        stderr=stderr))
 
         # No need to clean up if the cluster is already terminated
         # (i.e., prev_status is None), as the cleanup has already been done
@@ -3916,7 +3918,8 @@ class CloudVmRayBackend(backends.Backend['CloudVmRayResourceHandle']):
                     raise RuntimeError(
                         _TEARDOWN_FAILURE_MESSAGE.format(
                             extra_reason='It is caused by TPU failure.',
-                            cluster_name=common_utils.cluster_name_in_hint(handle.cluster_name, cluster_name_on_cloud),
+                            cluster_name=common_utils.cluster_name_in_hint(
+                                handle.cluster_name, cluster_name_on_cloud),
                             stdout=tpu_stdout,
                             stderr=tpu_stderr))
         if (terminate and handle.launched_resources.is_image_managed is True):
