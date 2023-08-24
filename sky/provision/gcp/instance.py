1a00d0781a29050b3ea3ddb9fe83baa70e9a1443
Zhanghao Wu <zhanghao.wu@outlook.com>
2023-08-24 14:17:49 -0700

    format

--- sky/provision/gcp/instance.py
+++ sky/provision/gcp/instance.py
@@ -180,7 +180,8 @@ def cleanup_ports(
         # No new ports were opened, so there is nothing to clean up.
         return
     project_id = provider_config['project_id']
-    cluster_name_hash = common_utils.make_cluster_name_on_cloud(cluster_name_on_cloud)
+    cluster_name_hash = common_utils.make_cluster_name_on_cloud(
+        cluster_name_on_cloud)
     for port in provider_config['ports']:
         rule_name = f'user-ports-{cluster_name_hash}-{port}'
         instance_utils.GCPComputeInstance.delete_firewall_rule(
