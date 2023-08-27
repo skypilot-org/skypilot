8bd58d7915d60b440eb99f29cd17f434d4a71dda
Doyoung Kim <kei0112777@gmail.com>
2023-08-27 23:30:29 +0000

    format

--- sky/skylet/providers/kubernetes/node_provider.py
+++ sky/skylet/providers/kubernetes/node_provider.py
@@ -224,7 +224,9 @@ class KubernetesNodeProvider(NodeProvider):
                     'Cluster may be out of resources or '
                     'may be too slow to autoscale.')
                 lack_resource_msg = (
-                    'Insufficient {resource}. Other SkyPilot tasks or pods on the cluster may be using resources. Check resource usage by running `kubectl describe nodes`.'
+                    'Insufficient {resource}. Other SkyPilot tasks or pods on '
+                    'the cluster may be using resources. Check resource usage '
+                    'by running `kubectl describe nodes`.')
                 if event_message is not None:
                     if pod_status == 'Pending':
                         if 'Insufficient cpu' in event_message:
