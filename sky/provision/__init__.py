748242e034fc5eb258d2163baf190bb2b6c035f8
Siyuan <suquark@gmail.com>
2023-08-27 18:11:09 -0700

    fix

--- sky/provision/__init__.py
+++ sky/provision/__init__.py
@@ -54,7 +54,7 @@ def query_instances(
 
 
 @_route_to_cloud_impl
-def bootstrap(provider_name: str, region: str, cluster_name: str,
+def bootstrap_instances(provider_name: str, region: str, cluster_name: str,
               config: common.InstanceConfig) -> common.InstanceConfig:
     """Bootstrap configurations for a cluster.
 
