797ec58e824f8c9ef067fc83e49eddd3c3b976c9
cblmemo <cblmemo@gmail.com>
2023-09-03 12:37:22 -0700

    fix

--- sky/provision/azure/__init__.py
+++ sky/provision/azure/__init__.py
@@ -1,4 +1,4 @@
 """Azure provisioner for SkyPilot."""
 
-from sky.provision.aws.instance import cleanup_ports
+from sky.provision.azure.instance import cleanup_ports
 from sky.provision.azure.instance import open_ports
