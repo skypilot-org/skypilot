84e1b6538f3176f167b5f9c5b4b5afba7d29509a
Doyoung Kim <kei0112777@gmail.com>
2023-09-25 07:00:20 +0000

    nit

--- sky/backends/backend_utils.py
+++ sky/backends/backend_utils.py
@@ -2895,8 +2895,7 @@ def wait_and_terminate_csync(cluster_name: str) -> None:
         cluster_name: Cluster name (see `sky status`)
     """
     record = global_user_state.get_cluster_from_name(cluster_name)
-    if record is None:
-        return
+    assert record is not None
     handle = record['handle']
     if not isinstance(handle, backends.CloudVmRayResourceHandle):
         return
@@ -2918,13 +2917,15 @@ def wait_and_terminate_csync(cluster_name: str) -> None:
         rc, _, stderr = runner.run(csync_terminate_cmd,
                                    stream_logs=False,
                                    require_outputs=True)
+
         if rc:
+            #TODO(Doyoung): Set the following to 'warning' when #2504 is resolved
+            # Currently, the logger interrupts the rich progress bar.
             logger.debug(
                 f'CSYNC: failed to terminate the CSYNC on {runner.ip}. '
                 f'Details: {stderr}')
 
     #TODO(Doyoung): Set the following to 'info' when #2504 is resolved
-    # Currently, the logger interrupts the rich progress bar.
     logger.debug(f'CSYNC termination initiated for {cluster_name}. If a '
                  'sync process is currently running, CSYNC will terminate '
                  'after it completes.\n')
