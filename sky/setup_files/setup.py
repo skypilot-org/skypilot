7bb01f6810e1817a3f88f76bfddbc91038d40474
cblmemo <cblmemo@gmail.com>
2023-09-28 10:51:34 -0700

    revert

--- sky/setup_files/setup.py
+++ sky/setup_files/setup.py
@@ -135,9 +135,7 @@ extras_require: Dict[str, List[str]] = {
         'azure-cli>=2.31.0', 'azure-core', 'azure-identity>=1.13.0',
         'azure-mgmt-network'
     ],
-    # We need google-api-python-client>=2.19.1 to enable 'reason' attribute
-    # of googleapiclient.errors.HttpError, which is widely used in our system.
-    'gcp': ['google-api-python-client >= 2.19.1', 'google-cloud-storage'],
+    'gcp': ['google-api-python-client', 'google-cloud-storage'],
     'ibm': [
         'ibm-cloud-sdk-core', 'ibm-vpc', 'ibm-platform-services', 'ibm-cos-sdk'
     ],
