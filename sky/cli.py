1d9c3ec6e5256cae05da352c4a686ffca1d4cb5e
Tian Xia <cblmemo@gmail.com>
2023-10-03 10:10:20 -0700

    [Core] Fix type of `job_id` when querying job status (#2541)
    * fix
    
    * apply suggestions from code review
    
    * Apply suggestions from code review
    
    Co-authored-by: Zhanghao Wu <zhanghao.wu@outlook.com>
    
    * lint
    
    ---------
    
    Co-authored-by: Zhanghao Wu <zhanghao.wu@outlook.com>

--- sky/cli.py
+++ sky/cli.py
@@ -2027,12 +2027,12 @@ def queue(clusters: List[str], skip_finished: bool, all_users: bool):
                 required=True,
                 type=str,
                 **_get_shell_complete_args(_complete_cluster_name))
-@click.argument('job_ids', type=str, nargs=-1)
+@click.argument('job_ids', type=int, nargs=-1)
 # TODO(zhwu): support logs by job name
 @usage_lib.entrypoint
 def logs(
     cluster: str,
-    job_ids: Tuple[str],
+    job_ids: Tuple[int],
     sync_down: bool,
     status: bool,  # pylint: disable=redefined-outer-name
     follow: bool,
@@ -2059,8 +2059,9 @@ def logs(
             '(ambiguous). To fix: specify at most one of them.')
 
     if len(job_ids) > 1 and not sync_down:
+        job_ids_str = ', '.join([str(id) for id in job_ids])
         raise click.UsageError(
-            f'Cannot stream logs of multiple jobs (IDs: {", ".join(job_ids)}).'
+            f'Cannot stream logs of multiple jobs (IDs: {job_ids_str}).'
             '\nPass -s/--sync-down to download the logs instead.')
 
     job_ids = None if not job_ids else job_ids
@@ -2073,12 +2074,14 @@ def logs(
     job_id = None
     if job_ids:
         job_id = job_ids[0]
-        if not job_id.isdigit():
-            raise click.UsageError(f'Invalid job ID {job_id}. '
-                                   'Job ID must be integers.')
     if status:
         job_statuses = core.job_status(cluster, job_ids)
         job_id = list(job_statuses.keys())[0]
+        # If job_ids is None and no job has been submitted to the cluster,
+        # it will return {None: None}.
+        if job_id is None:
+            click.secho(f'No job found on cluster {cluster!r}.', fg='red')
+            sys.exit(1)
         job_status = list(job_statuses.values())[0]
         job_status_str = job_status.value if job_status is not None else 'None'
         click.echo(f'Job {job_id}: {job_status_str}')
