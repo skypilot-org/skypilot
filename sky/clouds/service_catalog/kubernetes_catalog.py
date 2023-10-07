6b0b40d2ad0c4fba5b4614554026612eb51b5099
hemildesai <hemil.desai10@gmail.com>
2023-10-06 17:52:05 -0700

    Pylint fixes part 4

--- sky/clouds/service_catalog/kubernetes_catalog.py
+++ sky/clouds/service_catalog/kubernetes_catalog.py
@@ -98,6 +98,6 @@ def list_accelerators(
 def validate_region_zone(
     region_name: Optional[str],
     zone_name: Optional[str],
-    clouds: CloudFilter = None  # type: ignore
+    clouds: CloudFilter = None  # pylint: disable=unused-argument
 ) -> Tuple[Optional[str], Optional[str]]:
     return (region_name, zone_name)
