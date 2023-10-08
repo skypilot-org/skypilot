59ef51872d659c39ea8630ca99a71d2eeb1bac17
Zhanghao Wu <zhanghao.wu@outlook.com>
2023-10-07 17:35:49 -0700

    add unit test for aws resources memory leakage

---
+++ tests/unit_tests/sky/clouds/test_aws.py
@@ -0,0 +1,18 @@
+import memory_profiler
+
+from sky.provision.aws import instance
+
+regions = ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2']
+
+
+def test_aws_resources_memory_leakage():
+    memory_usage_before = memory_profiler.memory_usage(-1,
+                                                       interval=0.1,
+                                                       timeout=1)[0]
+    for i in range(int(1e5)):
+        instance._default_ec2_resource(regions[i % len(regions)])
+    mem_usage_after = memory_profiler.memory_usage(-1, interval=0.1,
+                                                   timeout=1)[0]
+    delta = mem_usage_after - memory_usage_before
+    assert delta <= 10, (
+        f'Function used {delta:.2f}MB which is more than the allowed 10MB')
