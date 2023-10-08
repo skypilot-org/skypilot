ca9be38de5c8f262ccc7619065d845fba88a5a59
Zhanghao Wu <zhanghao.wu@outlook.com>
2023-10-07 18:18:26 -0700

    add requirement for memory profiler

--- tests/unit_tests/sky/clouds/test_aws.py
+++ tests/unit_tests/sky/clouds/test_aws.py
@@ -1,5 +1,6 @@
-import memory_profiler
 import os
+
+import memory_profiler
 
 from sky.provision.aws import instance
 
@@ -12,7 +13,8 @@ def test_aws_resources_memory_leakage():
                                                        timeout=1)[0]
     for i in range(int(1e6)):
         instance._default_ec2_resource(regions[i % len(regions)])
-    mem_usage_after = memory_profiler.memory_usage(os.getpid(), interval=0.1,
+    mem_usage_after = memory_profiler.memory_usage(os.getpid(),
+                                                   interval=0.1,
                                                    timeout=1)[0]
     delta = mem_usage_after - memory_usage_before
     print('memory usage:', delta)
