47006daa4578dd7de5a292273a29a94050aeb1c9
Zhanghao Wu <zhanghao.wu@outlook.com>
2023-10-07 18:07:59 -0700

    Fix unit test

--- tests/unit_tests/sky/clouds/test_aws.py
+++ tests/unit_tests/sky/clouds/test_aws.py
@@ -1,4 +1,5 @@
 import memory_profiler
+import os
 
 from sky.provision.aws import instance
 
@@ -6,13 +7,14 @@ regions = ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2']
 
 
 def test_aws_resources_memory_leakage():
-    memory_usage_before = memory_profiler.memory_usage(-1,
+    memory_usage_before = memory_profiler.memory_usage(os.getpid(),
                                                        interval=0.1,
                                                        timeout=1)[0]
-    for i in range(int(1e4)):
+    for i in range(int(1e6)):
         instance._default_ec2_resource(regions[i % len(regions)])
-    mem_usage_after = memory_profiler.memory_usage(-1, interval=0.1,
+    mem_usage_after = memory_profiler.memory_usage(os.getpid(), interval=0.1,
                                                    timeout=1)[0]
     delta = mem_usage_after - memory_usage_before
-    assert delta <= 40, (
-        f'Function used {delta:.2f}MB which is more than the allowed 40MB')
+    print('memory usage:', delta)
+    assert delta <= 120, (
+        f'Function used {delta:.2f}MB which is more than the allowed 120MB')
