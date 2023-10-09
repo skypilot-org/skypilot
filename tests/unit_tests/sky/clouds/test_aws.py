4b6b6af65597687276acd89b117d47607a5e20ac
Zhanghao Wu <zhanghao.wu@outlook.com>
2023-10-08 23:48:31 -0700

    fail early for memory exceeding

--- tests/unit_tests/sky/clouds/test_aws.py
+++ tests/unit_tests/sky/clouds/test_aws.py
@@ -18,12 +18,14 @@ def test_aws_resources_memory_leakage():
                                                        timeout=1)[0]
     for i in range(int(1e6)):
         instance._default_ec2_resource(regions[i % len(regions)])
-    mem_usage_after = memory_profiler.memory_usage(os.getpid(),
-                                                   interval=0.1,
-                                                   timeout=1)[0]
-    delta = mem_usage_after - memory_usage_before
-    print('memory usage:', delta)
-    # 120MB is chosen by only creating 4 EC2 resources, and the memory usage
-    # should not grow when creating more.
-    assert delta <= 120, (
-        f'Function used {delta:.2f}MB which is more than the allowed 120MB')
+        if i % int(1e3) == 0:
+            mem_usage_after = memory_profiler.memory_usage(os.getpid(),
+                                                           interval=0.1,
+                                                           timeout=1)[0]
+            delta = mem_usage_after - memory_usage_before
+            print('memory usage:', delta)
+            # 120MB is chosen by only creating 4 EC2 resources, and the memory
+            # usage should not grow when creating more.
+            assert delta <= 120, (
+                f'Function used {delta:.2f}MB which is more than the '
+                'allowed 120MB')
