427bd6068ed46703f7e377a1227d119e23dd082f
Zhanghao Wu <zhanghao.wu@outlook.com>
2023-10-12 09:34:31 -0700

    [Core] enable traceback for better debugging (#2696)
    * enable traceback for better debugging
    
    * Update aws.py

--- sky/skylet/events.py
+++ sky/skylet/events.py
@@ -17,6 +17,7 @@ from sky.skylet import autostop_lib
 from sky.skylet import job_lib
 from sky.spot import spot_utils
 from sky.utils import common_utils
+from sky.utils import ux_utils
 
 # Seconds of sleep between the processing of skylet events.
 EVENT_CHECKING_INTERVAL_SECONDS = 20
@@ -48,7 +49,8 @@ class SkyletEvent:
             except Exception as e:  # pylint: disable=broad-except
                 # Keep the skylet running even if an event fails.
                 logger.error(f'{self.__class__.__name__} error: {e}')
-                logger.error(traceback.format_exc())
+                with ux_utils.enable_traceback():
+                    logger.error(traceback.format_exc())
 
     def _run(self):
         raise NotImplementedError
