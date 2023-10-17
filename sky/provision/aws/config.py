66e4a1096cf8a0967c7b6a2bba472c470177a214
cblmemo <cblmemo@gmail.com>
2023-10-17 13:57:10 -0700

    lint

--- sky/provision/aws/config.py
+++ sky/provision/aws/config.py
@@ -11,7 +11,7 @@ import copy
 import json
 import logging
 import time
-from typing import Any, Dict, List, Optional, Set, Tuple, Union
+from typing import Any, Dict, List, Optional, Set, Tuple
 
 import colorama
 
