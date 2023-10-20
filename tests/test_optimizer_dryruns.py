2e768281374fba26dbc1b5efb43ee44970e55eb4
MaoZiming <ziming.mao@yale.edu>
2023-10-20 15:50:48 -0700

    did the tests pass

--- tests/test_optimizer_dryruns.py
+++ tests/test_optimizer_dryruns.py
@@ -719,37 +719,31 @@ def test_infer_cloud_from_region_or_zone(monkeypatch):
 
 def test_ordered_resources():
 
-    dag = sky.Dag()
-    task = sky.Task(run='nvidia-smi')
-    dag.add(task)
-
-    task.set_resources([
-        sky.Resources(accelerators={'V100': 1}),
-        sky.Resources(accelerators={'T4': 1}),
-        sky.Resources(accelerators={'V100': 1}),
-        sky.Resources(accelerators={'K80': 1}),
-        sky.Resources(accelerators={'T4': 4}),
-    ])
-
-    dag = sky.optimize(dag, quiet=True)
+    with sky.Dag() as dag:
+        task = sky.Task('test_task')
+        task.set_resources([
+            sky.Resources(accelerators={'V100': 1}),
+            sky.Resources(accelerators={'T4': 1}),
+            sky.Resources(accelerators={'V100': 1}),
+            sky.Resources(accelerators={'K80': 1}),
+            sky.Resources(accelerators={'T4': 4}),
+        ])
+    dag = sky.optimize(dag)
     # 'V100' is picked because it is the first in the list.
     assert 'V100' in repr(task.best_resources)
 
 
 def test_unordered_resources():
 
-    dag = sky.Dag()
-    task = sky.Task(run='nvidia-smi')
-    dag.add(task)
-
-    task.set_resources({
-        sky.Resources(accelerators={'V100': 1}),
-        sky.Resources(accelerators={'T4': 1}),
-        sky.Resources(accelerators={'V100': 1}),
-        sky.Resources(accelerators={'K80': 1}),
-        sky.Resources(accelerators={'T4': 4}),
-    })
-
-    dag = sky.optimize(dag, quiet=True)
+    with sky.Dag() as dag:
+        task = sky.Task('test_task')
+        task.set_resources({
+            sky.Resources(accelerators={'V100': 1}),
+            sky.Resources(accelerators={'T4': 1}),
+            sky.Resources(accelerators={'V100': 1}),
+            sky.Resources(accelerators={'K80': 1}),
+            sky.Resources(accelerators={'T4': 4}),
+        })
+    dag = sky.optimize(dag)
     # 'K80' is picked because it is the cheapest.
     assert 'K80' in repr(task.best_resources)
