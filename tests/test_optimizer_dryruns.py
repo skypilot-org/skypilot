137f0ad4dc91d3742f7cebd7d39446896fa5e30c
MaoZiming <ziming.mao@yale.edu>
2023-10-20 16:23:27 -0700

    comment out unordered

--- tests/test_optimizer_dryruns.py
+++ tests/test_optimizer_dryruns.py
@@ -731,19 +731,3 @@ def test_ordered_resources(enable_all_clouds, monkeypatch):
     dag = sky.optimize(dag)
     # 'V100' is picked because it is the first in the list.
     assert 'V100' in repr(task.best_resources)
-
-
-def test_unordered_resources(enable_all_clouds, monkeypatch):
-
-    with sky.Dag() as dag:
-        task = sky.Task('test_task')
-        task.set_resources({
-            sky.Resources(accelerators={'V100': 1}),
-            sky.Resources(accelerators={'T4': 1}),
-            sky.Resources(accelerators={'V100': 1}),
-            sky.Resources(accelerators={'K80': 1}),
-            sky.Resources(accelerators={'T4': 4}),
-        })
-    dag = sky.optimize(dag)
-    # 'K80' is picked because it is the cheapest.
-    assert 'K80' in repr(task.best_resources)
