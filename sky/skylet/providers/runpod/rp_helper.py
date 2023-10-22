4b093186464da9f171dd6562340a6b93737c42de
Justin Merrell <merrelljustin@gmail.com>
2023-10-22 11:54:08 -0400

    Update rp_helper.py

--- sky/skylet/providers/runpod/rp_helper.py
+++ sky/skylet/providers/runpod/rp_helper.py
@@ -112,7 +112,7 @@ def launch(name: str, instance_type: str, region: str):
         min_vcpu_count=4 * gpu_quantity,
         min_memory_in_gb=gpu_specs["memoryInGb"] * gpu_quantity,
         country_code=region,
-        ports="22/tcp",
+        ports="22/tcp,8076/http,6379/tcp",
         support_public_ip=True,
     )
 
