eeadad30b59d18b69cbb35841343b1c147ed7c39
Zhanghao Wu <zhanghao.wu@outlook.com>
2023-10-24 21:17:50 -0700

    Fix sky api stop

--- sky/api/sdk.py
+++ sky/api/sdk.py
@@ -14,6 +14,7 @@ from sky import backends
 from sky import optimizer
 from sky import sky_logging
 from sky.skylet import constants
+from sky.usage import usage_lib
 from sky.utils import status_lib
 
 if typing.TYPE_CHECKING:
@@ -25,7 +26,7 @@ DEFAULT_SERVER_URL = 'http://127.0.0.1:8000'
 API_SERVER_CMD = 'uvicorn sky.api.rest:app'
 
 
-def get_server_url():
+def _get_server_url():
     return os.environ.get(constants.SKY_API_SERVER_URL_ENV_VAR,
                           DEFAULT_SERVER_URL)
 
@@ -35,17 +36,18 @@ def _start_uvicorn_in_background():
     os.makedirs(os.path.dirname(log_path), exist_ok=True)
     # The command to run uvicorn. Adjust the app:app to your application's
     # location.
-    cmd = f'uvicorn sky.api.rest:app --reload &> {log_path}'
+    cmd = f'{API_SERVER_CMD} &> {log_path}'
 
     # Start the uvicorn process in the background and don't wait for it.
     subprocess.Popen(cmd, shell=True)
-    time.sleep(1)
+    # Wait for the server to start.
+    time.sleep(5)
 
 
 def _handle_response(response):
     if response.status_code != 200:
         raise RuntimeError(
-            f'Failed to connect to SkyPilot server at {get_server_url()}. '
+            f'Failed to connect to SkyPilot server at {_get_server_url()}. '
             f'Response: {response.content}')
     return response.json()
 
@@ -54,10 +56,10 @@ def _check_health(func):
 
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        server_url = get_server_url()
+        server_url = _get_server_url()
 
         try:
-            response = requests.get(f'{get_server_url()}/health', timeout=5)
+            response = requests.get(f'{_get_server_url()}/health', timeout=5)
         except requests.exceptions.ConnectionError:
             response = None
         if (response is None or response.status_code != 200):
@@ -79,6 +81,7 @@ def _check_health(func):
     return wrapper
 
 
+@usage_lib.entrypoint
 @_check_health
 def launch(
     task: 'task_lib.Task',
@@ -100,7 +103,7 @@ def launch(
     # TODO(zhwu): For all the file_mounts, we need to handle them properly
     # similarly to how we deal with it for spot_launch.
     requests.post(
-        f'{get_server_url()}/launch',
+        f'{_get_server_url()}/launch',
         json={
             'task': task.to_yaml_config(),
             'cluster_name': cluster_name,
@@ -120,12 +123,13 @@ def launch(
     )
 
 
+@usage_lib.entrypoint
 @_check_health
 def status(cluster_names: Optional[List[str]] = None,
            refresh: bool = False) -> List[dict]:
     # TODO(zhwu): this does not stream the logs output by logger back to the
     # user
-    response = requests.get(f'{get_server_url()}/status',
+    response = requests.get(f'{_get_server_url()}/status',
                             json={
                                 'cluster_names': cluster_names,
                                 'refresh': refresh,
@@ -142,16 +146,18 @@ def status(cluster_names: Optional[List[str]] = None,
 
 
 # === API server management ===
+@usage_lib.entrypoint
 @_check_health
 def api_start():
     """Start the API server."""
-    logger.info(f'SkyPilot API server: {get_server_url()}')
+    logger.info(f'SkyPilot API server: {_get_server_url()}')
 
 
+@usage_lib.entrypoint
 def api_stop():
     """Kill the API server."""
     # Kill the uvicorn process by name: uvicorn sky.api.rest:app
-    server_url = get_server_url()
+    server_url = _get_server_url()
     if server_url != DEFAULT_SERVER_URL:
         raise RuntimeError(
             f'Cannot kill the API server at {server_url} because it is not '
@@ -161,28 +167,32 @@ def api_stop():
     for process in psutil.process_iter(attrs=['pid', 'cmdline']):
         cmdline = process.info['cmdline']
         if cmdline and API_SERVER_CMD in ' '.join(cmdline):
-            print(process)
-            process.kill()
-            logger.info(f'{colorama.Fore.GREEN}SkyPilot API server stopped.'
-                        f'{colorama.Style.RESET_ALL}')
-            found = True
-    if not found:
+            process.terminate()
+        found = True
+
+    if found:
+        logger.info(f'{colorama.Fore.GREEN}SkyPilot API server stopped.'
+                    f'{colorama.Style.RESET_ALL}')
+    else:
         logger.info('SkyPilot API server is not running.')
 
 
-# Use the same args as tail -f
+# Use the same args as `docker logs`
+@usage_lib.entrypoint
 def api_logs(follow: bool = True, tail: str = 'all'):
     """Stream the API server logs."""
-    server_url = get_server_url()
+    server_url = _get_server_url()
     if server_url != DEFAULT_SERVER_URL:
         raise RuntimeError(
             f'Cannot kill the API server at {server_url} because it is not '
             f'the default SkyPilot API server started locally.')
 
     tail_args = ['-f'] if follow else []
-    if tail != 'all':
+    if tail == 'all':
+        tail_args.extend(['-n', '+1'])
+    else:
         try:
-            tail_args.append(f'-n {int(tail)}')
+            tail_args.extend(['-n', f'{int(tail)}'])
         except ValueError as e:
             raise ValueError(f'Invalid tail argument: {tail}') from e
     log_path = os.path.expanduser(constants.API_SERVER_LOGS)
