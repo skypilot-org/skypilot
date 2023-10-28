689624ac5b948bee52ae5e8f4eef32a705a32718
MaoZiming <ziming.mao@yale.edu>
2023-10-27 22:18:29 -0700

    fix compare_optimization_results

--- tests/test_optimizer_random_dag.py
+++ tests/test_optimizer_random_dag.py
@@ -126,10 +126,10 @@ def find_min_objective(dag: sky.Dag, minimize_cost: bool) -> float:
 def compare_optimization_results(dag: sky.Dag, minimize_cost: bool):
     copy_dag = copy.deepcopy(dag)
     if minimize_cost:
-        optimizer_plan = sky.Optimizer._optimize_objective(
+        optimizer_plan = sky.Optimizer._optimize_dag(
             dag, sky.OptimizeTarget.COST)
     else:
-        optimizer_plan = sky.Optimizer._optimize_objective(
+        optimizer_plan = sky.Optimizer._optimize_dag(
             dag, sky.OptimizeTarget.TIME)
     if minimize_cost:
         objective = sky.Optimizer._compute_total_cost(dag.get_graph(),
