30059e00cd06180513bf61f03bf124f627ecf487
jackyk02 <jackykwok@berkeley.edu>
2023-11-06 14:46:46 -0800

    add disk resizing for TPUVMs

--- sky/skylet/providers/gcp/node.py
+++ sky/skylet/providers/gcp/node.py
@@ -895,3 +895,51 @@ class GCPTPU(GCPResource):
             result = operation
 
         return result
+
+    def resize_disk(
+        self, base_config: dict, instance_name: str, wait_for_operation: bool = True
+    ) -> dict:
+        """Resize a Google Cloud disk based on the provided configuration."""
+
+        # Extract the specified disk size from the configuration
+        new_size_gb = base_config["disks"][0]["initializeParams"]["diskSizeGb"]
+
+        # Fetch the instance details to get the disk name and current disk size
+        response = (
+            self.resource.instances()
+            .get(
+                project=self.project_id,
+                zone=self.availability_zone,
+                instance=instance_name,
+            )
+            .execute()
+        )
+        disk_name = response["disks"][0]["source"].split("/")[-1]
+
+        try:
+            # Execute the resize request and return the response
+            operation = (
+                self.resource.disks()
+                .resize(
+                    project=self.project_id,
+                    zone=self.availability_zone,
+                    disk=disk_name,
+                    body={
+                        "sizeGb": str(new_size_gb),
+                    },
+                )
+                .execute()
+            )
+        except HttpError as e:
+            # Catch HttpError when provided with invalid value for new disk size.
+            # Raises error when the new disk size is smaller than the exisiting
+            # disk size
+            logger.warning(f"googleapiclient.errors.HttpError: {e.reason}")
+            return {}
+
+        if wait_for_operation:
+            result = self.wait_for_operation(operation)
+        else:
+            result = operation
+
+        return result
