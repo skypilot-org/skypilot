d5ab23252c9f671c02b8e03adbec68fc1974c5cd
Zhanghao Wu <zhanghao.wu@outlook.com>
2023-12-25 08:22:18 +0000

    add tpu filter for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -61,6 +61,10 @@ def pytest_addoption(parser):
                      action='store_true',
                      default=False,
                      help='Only run tests for sky serve.')
+    parser.addoption('--tpu',
+                    action='store_true',
+                     default=False,
+                     help='Only run tests for TPU.')
     parser.addoption(
         '--generic-cloud',
         type=str,
@@ -111,6 +115,8 @@ def pytest_collection_modifyitems(config, items):
         reason='skipped, because --managed-spot option is set')
     skip_marks['sky_serve'] = pytest.mark.skip(
         reason='skipped, because --sky-serve option is set')
+    skip_marks['tpu'] = pytest.mark.skip(
+        reason='skipped, because --tpu option is set')
     for cloud in all_clouds_in_smoke_tests:
         skip_marks[cloud] = pytest.mark.skip(
             reason=f'tests for {cloud} is skipped, try setting --{cloud}')
@@ -137,6 +143,9 @@ def pytest_collection_modifyitems(config, items):
         if (not 'managed_spot'
                 in item.keywords) and config.getoption('--managed-spot'):
             item.add_marker(skip_marks['managed_spot'])
+        if (not 'tpu'
+                in item.keywords) and config.getoption('--tpu'):
+            item.add_marker(skip_marks['tpu'])
         if (not 'sky_serve'
                 in item.keywords) and config.getoption('--sky-serve'):
             item.add_marker(skip_marks['sky_serve'])
