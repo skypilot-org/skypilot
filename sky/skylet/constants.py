8f18a6d83d09886b3ac3dbc820f17281b6239a47
Zhanghao Wu <zhanghao.wu@outlook.com>
2023-12-28 14:35:34 +0800

    [Setup] Fix conda init (#2912)
    Fix conda init

--- sky/skylet/constants.py
+++ sky/skylet/constants.py
@@ -74,7 +74,7 @@ CONDA_INSTALLATION_COMMANDS = (
     'which conda > /dev/null 2>&1 || '
     '(wget -nc https://repo.anaconda.com/miniconda/Miniconda3-py310_23.5.2-0-Linux-x86_64.sh -O Miniconda3-Linux-x86_64.sh && '  # pylint: disable=line-too-long
     'bash Miniconda3-Linux-x86_64.sh -b && '
-    'eval "$(~/miniconda3/bin/conda shell.bash hook)" && '
+    'eval "$(~/miniconda3/bin/conda shell.bash hook)" && conda init && '
     'conda config --set auto_activate_base true); '
     'grep "# >>> conda initialize >>>" ~/.bashrc || conda init;')
 
