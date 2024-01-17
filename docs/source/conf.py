60ef296d9acc1b6555a39283575ddc4478fefdd5
Rémy Léone <rleone@scaleway.com>
2024-01-17 14:34:09 +0100

    single quote for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -97,5 +97,5 @@ html_favicon = '_static/favicon.ico'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named 'default.css' will overwrite the builtin 'default.css'.
 html_static_path = ['_static']
-html_js_files = ["custom.js"]
-html_css_files = ["custom.css"]
+html_js_files = ['custom.js']
+html_css_files = ['custom.css']
