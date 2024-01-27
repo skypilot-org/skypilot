d9e79e56362dc9fbce93a6f3945fd875d2c4e83f
Romil Bhardwaj <romil.bhardwaj@gmail.com>
2024-01-26 18:07:18 -0800

    Add custom image tests

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -2573,7 +2573,7 @@ def test_aws_custom_image():
     test = Test(
         'test-aws-custom-image',
         [
-            f'sky launch -c {name} --retry-until-up -y examples/custom_image.yaml',
+            f'sky launch -c {name} --retry-until-up -y tests/test_yamls/test_custom_image.yaml --cloud aws --region us-east-2 --image-id ami-062ddd90fb6f8267a', # Nvidia image
             f'sky logs {name} 1 --status',
         ],
         f'sky down -y {name}',
@@ -2582,14 +2582,22 @@ def test_aws_custom_image():
     run_one_test(test)
 
 @pytest.mark.kubernetes
-def test_kubernetes_custom_image():
+@pytest.mark.parametrize("image_id", [
+    "docker:nvidia/cuda:11.8.0-devel-ubuntu18.04",
+    "docker:ubuntu:18.04",
+])
+def test_kubernetes_custom_image(image_id):
     """Test Kubernetes custom image"""
     name = _get_cluster_name()
     test = Test(
         'test-kubernetes-custom-image',
         [
-            f'sky launch -c {name} --retry-until-up -y examples/custom_image.yaml',
+            f'sky launch -c {name} --retry-until-up -y tests/test_yamls/test_custom_image.yaml --cloud kubernetes --image-id {image_id} --region None --gpus T4:1',
             f'sky logs {name} 1 --status',
+            # Try exec to run again and check if the logs are printed
+            f'sky exec {name} tests/test_yamls/test_custom_image.yaml --cloud kubernetes --image-id {image_id} --region None --gpus T4:1 | grep "Hello 100"',
+            # Make sure ssh is working with custom username
+            # f'ssh {name} echo hi | grep hi',
         ],
         f'sky down -y {name}',
         timeout=30 * 60,
