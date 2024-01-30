3df3d780c4f77d5775cf2b525d736db7b4791f91
Zhanghao Wu <zhanghao.wu@outlook.com>
2024-01-30 23:51:56 +0000

    fix gui

--- llm/codellama/complete.py
+++ llm/codellama/complete.py
@@ -1,4 +1,5 @@
 import openai
+
 import sky
 from sky import serve
 
@@ -8,18 +9,20 @@ endpoint = serve.get_endpoint(service_records[0])
 print('Using endpoint:', endpoint)
 
 client = openai.OpenAI(
-    base_url = f'http://{endpoint}/v1',
+    base_url=f'http://{endpoint}/v1',
     # No API key is required when self-hosted.
-    api_key='EMPTY'
-)
+    api_key='EMPTY')
 
 chat_completion = client.chat.completions.create(
     model='codellama/CodeLlama-70b-Instruct-hf',
-    messages=[{'role': 'system', 'content': 'You are ahelpful and honest code assistant expert in Python.'},
-              {'role': 'user', 'content': 'Show me the code for quick sort a list of integers.'}],
+    messages=[{
+        'role': 'system',
+        'content': 'You are ahelpful and honest code assistant expert in Python.'
+    }, {
+        'role': 'user',
+        'content': 'Show me the code for quick sort a list of integers.'
+    }],
     max_tokens=300,
 )
 
 print(chat_completion.model_dump())
-
-
