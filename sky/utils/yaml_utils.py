bc78dd4dc728ee47b7275b0c4cc518d70f20b871
MaoZiming <ziming.mao@yale.edu>
2024-01-30 21:28:13 -0800

    format

--- sky/utils/yaml_utils.py
+++ sky/utils/yaml_utils.py
@@ -7,6 +7,8 @@ import re
 import yaml
 
 path_matcher = re.compile(r'\$\{([^}^{]+)\}')
+
+
 def path_constructor(loader, node):
     del loader
     value = node.value
@@ -14,11 +16,14 @@ def path_constructor(loader, node):
     env_var = match.group()[2:-1]
     return os.environ.get(env_var) + value[match.end():]
 
+
 yaml.add_implicit_resolver('!path', path_matcher, None, yaml.SafeLoader)
 yaml.add_constructor('!path', path_constructor, yaml.SafeLoader)
 
+
 def yaml_safe_load_with_env(file_path):
     return yaml.safe_load(file_path)
+
 
 def yaml_safe_load_all_with_env(file_path):
     return yaml.safe_load_all(file_path)
