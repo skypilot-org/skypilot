59454cadac1ed44ab8d3c3389838fb2b73074542
Mahmoud Sehsah <46302138+mahmoudrasmyfathy1@users.noreply.github.com>
2024-02-07 01:46:40 +0000

    remove unused variable username

--- sky/utils/command_runner.py
+++ sky/utils/command_runner.py
@@ -36,8 +36,9 @@ def _ssh_control_path(ssh_control_filename: Optional[str]) -> Optional[str]:
     """Returns a temporary path to be used as the ssh control path."""
     if ssh_control_filename is None:
         return None
-    username = getpass.getuser()
-    path = (f'/tmp/skypilot_ssh_{common_utils.get_user_hash()}/{ssh_control_filename}')
+    path = (
++        f'/tmp/skypilot_ssh_{common_utils.get_user_hash()}/{ssh_control_filename}'
+    )
     os.makedirs(path, exist_ok=True)
     return path
 
