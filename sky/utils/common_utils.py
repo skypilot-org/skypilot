342de1ca1827d0db6a8eb3ce96e2f0f3e4f6baf9
David Tran <davidtran@Davids-MacBook-Pro-2.local>
2024-02-09 10:05:34 -0500

    [core] update docstr

--- sky/utils/common_utils.py
+++ sky/utils/common_utils.py
@@ -123,14 +123,18 @@ def make_cluster_name_on_cloud(display_name: str,
                                add_user_hash: bool = True) -> str:
     """Generate valid cluster name on cloud that is unique to the user.
 
-    This is to map the cluster name to a valid length for cloud providers, e.g.
-    GCP limits the length of the cluster name to 35 characters. If the cluster
-    name with user hash is longer than max_length:
+    This is to map the cluster name to a valid length and character set for
+    cloud providers,
+    - e.g. GCP limits the length of the cluster name to 35 characters. If the
+    cluster name with user hash is longer than max_length:
       1. Truncate it to max_length - cluster_hash - user_hash_length.
       2. Append the hash of the cluster name
+    - e.g. Some cloud providers don't allow for uppercase letters, periods,
+    or underscores, so we map these to hyphens
 
     Args:
-        display_name: The cluster name to be truncated and hashed.
+        display_name: The cluster name to be truncated, hashed, and
+            transformed.
         max_length: The maximum length of the cluster name. If None, no
             truncation is performed.
         add_user_hash: Whether to append user hash to the cluster name.
