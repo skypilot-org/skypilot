470c51f104d1f08fac368674d2832da17e5a8ad7
David Tran <davidtran@Davids-MacBook-Pro-2.local>
2024-02-08 22:27:24 -0500

    [core] change regex to allow for uppercase, periods, and underscores for cluster name

--- tests/unit_tests/test_cloud.py
+++ tests/unit_tests/test_cloud.py
@@ -1,5 +1,6 @@
 import pytest
 
+from sky import exceptions
 from sky.clouds.cloud import Cloud
 
 
@@ -12,3 +13,23 @@ def test_cloud_get_reservations_available_resources(specific_reservations,
     available_resources = Cloud().get_reservations_available_resources(
         "instance_type", "region", "zone", specific_reservations)
     assert available_resources == expected
+
+
+class TestCheckClusterNameIsValid:
+
+    def test_simple_check(self):
+        Cloud.check_cluster_name_is_valid("lora")
+
+    def test_check_with_hyphen(self):
+        Cloud.check_cluster_name_is_valid("seed-1")
+
+    def test_check_with_characters_to_transform(self):
+        Cloud.check_cluster_name_is_valid("Cuda_11.8")
+
+    def test_check_when_starts_with_number(self):
+        with pytest.raises(exceptions.InvalidClusterNameError):
+            Cloud.check_cluster_name_is_valid("11.8cuda")
+
+    def test_check_with_invalid_characters(self):
+        with pytest.raises(exceptions.InvalidClusterNameError):
+            Cloud.check_cluster_name_is_valid("lor@")
