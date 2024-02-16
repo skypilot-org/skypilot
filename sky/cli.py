2cc3023404fa68dea6037dc9dafc3d6767e05dfd
Zhanghao Wu <zhanghao.wu@outlook.com>
2024-02-16 22:13:49 +0000

    Fix logging for sky queue

--- sky/cli.py
+++ sky/cli.py
@@ -2237,8 +2237,9 @@ def queue(clusters: List[str], skip_finished: bool, all_users: bool):
     for cluster in clusters:
         try:
             job_table = core.queue(cluster, skip_finished, all_users)
-        except (exceptions.CommandError, ValueError, exceptions.NotSupportedError,
-                exceptions.ClusterNotUpError, exceptions.CloudUserIdentityError,
+        except (exceptions.CommandError, ValueError,
+                exceptions.NotSupportedError, exceptions.ClusterNotUpError,
+                exceptions.CloudUserIdentityError,
                 exceptions.ClusterOwnerIdentityMismatchError) as e:
             if isinstance(e, exceptions.NotSupportedError):
                 unsupported_clusters.append(cluster)
