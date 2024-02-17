b85cbf92c10d51249b4e5f0697ee4e468fab0769
Sheth <shethhriday29@berkeley.edu>
2024-02-16 23:17:10 -0800

    comments

--- examples/unsloth/unsloth.py
+++ examples/unsloth/unsloth.py
@@ -1,14 +1,16 @@
+# Use the unsloth library to fine-tune a Mistral model
+
 from unsloth import FastLanguageModel
 import torch
 from trl import SFTTrainer
 from transformers import TrainingArguments
 from datasets import load_dataset
-max_seq_length = 2048 # Supports RoPE Scaling interally, so choose any!
-# Get LAION dataset
+max_seq_length = 2048
+
+# [1] Get LAION dataset
 url = "https://huggingface.co/datasets/laion/OIG/resolve/main/unified_chip2.jsonl"
 dataset = load_dataset("json", data_files = {"train" : url}, split = "train")
 
-# 4bit pre quantized models we support - 4x faster downloading!
 fourbit_models = [
     "unsloth/mistral-7b-bnb-4bit",
     "unsloth/llama-2-7b-bnb-4bit",
@@ -16,28 +18,30 @@ fourbit_models = [
     "unsloth/codellama-34b-bnb-4bit",
     "unsloth/tinyllama-bnb-4bit",
 ]
-# Load Llama model
+
+# [2] Load Mistral model
 model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name = "unsloth/mistral-7b-bnb-4bit", # Supports Llama, Mistral - replace this!
+    model_name = "unsloth/mistral-7b-bnb-4bit",
     max_seq_length = max_seq_length,
     dtype = None,
     load_in_4bit = True,
 )
 
-# Do model patching and add fast LoRA weights
+# [3] Do model patching and add fast LoRA weights
 model = FastLanguageModel.get_peft_model(
     model,
     r = 16,
     target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
                       "gate_proj", "up_proj", "down_proj",],
     lora_alpha = 16,
-    lora_dropout = 0, # Supports any, but = 0 is optimized
-    bias = "none",    # Supports any, but = "none" is optimized
+    lora_dropout = 0,
+    bias = "none",
     use_gradient_checkpointing = True,
     random_state = 3407,
     max_seq_length = max_seq_length,
 )
 
+# [4] Initialize and train the model using the SFTTrainer
 trainer = SFTTrainer(
     model = model,
     train_dataset = dataset,
