25b5db4ae4bcace1de133090a45223b9ebd943b4
Romil Bhardwaj <romil.bhardwaj@gmail.com>
2024-02-21 10:39:33 -0800

    update build_image to use latest or date tag

--- tests/kubernetes/build_image.sh
+++ tests/kubernetes/build_image.sh
@@ -4,17 +4,20 @@
 # Uses buildx to build the image for both amd64 and arm64.
 # If -p flag is specified, pushes the image to the registry.
 # If -g flag is specified, builds the GPU image in Dockerfile_k8s_gpu. GPU image is built only for amd64.
+# If -l flag is specified, uses the latest tag instead of the date tag. Date tag is of the form YYYYMMDD.
 # Usage: ./build_image.sh [-p] [-g]
 # -p: Push the image to the registry
 # -g: Build the GPU image
+# -l: Use latest tag
 
 TAG=us-central1-docker.pkg.dev/skypilot-375900/skypilotk8s/skypilot
 
 push=false
 gpu=false
+latest=false
 
 # Parse command line arguments
-while getopts ":pg" opt; do
+while getopts ":pgl" opt; do
   case ${opt} in
     p )
       push=true
@@ -22,21 +25,39 @@ while getopts ":pg" opt; do
     g )
       gpu=true
       ;;
+    l )
+      latest=true
+      ;;
     \? )
-      echo "Usage: ./build_image.sh [-p] [-g]"
+      echo "Usage: ./build_image.sh [-p] [-g] [-l]"
       echo "-p: Push the image to the registry"
       echo "-g: Build the GPU image"
+      echo "-l: Use latest tag instead of the date tag"
       exit 1
       ;;
   esac
 done
 
+echo "Options:"
+echo "Push: $push"
+echo "GPU: $gpu"
+echo "Latest: $latest"
+
+# Set the version tag. If the latest flag is used, use the latest tag
+if [[ $latest == "true" ]]; then
+  VERSION_TAG=latest
+else
+  VERSION_TAG=$(date +%Y%m%d)
+fi
+
 # Add -gpu to the tag if the GPU image is being built
 if [[ $gpu == "true" ]]; then
-  TAG=$TAG-gpu:latest
+  TAG=$TAG-gpu:${VERSION_TAG}
 else
-  TAG=$TAG:latest
+  TAG=$TAG:${VERSION_TAG}
 fi
+
+echo "Building image: $TAG"
 
 # Shift off the options
 shift $((OPTIND-1))
@@ -71,7 +92,7 @@ fi
 
 echo "Tagging image."
 if [[ $gpu ]]; then
-  docker tag $TAG skypilot:latest-gpu
+  docker tag $TAG skypilot-gpu:latest
 else
   docker tag $TAG skypilot:latest
 fi
