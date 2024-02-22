4a20256562f4cbfd158d9beab6649f82f488652b
Zhanghao Wu <zhanghao.wu@outlook.com>
2024-02-21 18:03:55 -0800

    Fix unit tests

--- tests/unit_tests/test_controller_utils.py
+++ tests/unit_tests/test_controller_utils.py
@@ -1,19 +1,61 @@
 """Test the controller_utils module."""
 import pytest
+import textwrap
 
 from sky.utils import controller_utils
+from sky.spot import constants as spot_constants
+from sky.serve import constants as serve_constants
+from sky import skypilot_config
 
-@pytest.mark.parametrize(('controller_type', 'controller_resources_config'), [
-    ('spot', {}, {'cpus': '8', 'memory': '32', 'disk_size': 50}),
-    ('spot', {'cpus': '8+', 'memory': '3x', 'disk_size': 100}, {'cpus': '8', 'memory': '32', 'disk_size': 100}),
-    ('serve', {}, {'cpus': '4', 'memory': '16', 'disk_size': 50}),
-])
-def test_get_controller_resources_spot(controller_type, controller_resources_config, expected, enable_all_clouds):
-    controller_resources = controller_utils.get_controller_resources(controller_type, controller_resources_config)
+
+
+@pytest.mark.parametrize(
+    ('controller_type', 'custom_controller_resources_config', 'expected'), [
+        ('spot', {}, {
+            'cpus': '8+',
+            'memory': '3x',
+            'disk_size': 50
+        }),
+        ('spot', {
+            'cpus': '4+',
+            'disk_size': 100
+        }, {
+            'cpus': '4+',
+            'memory': '3x',
+            'disk_size': 100
+        }),
+        ('serve', {}, {
+            'cpus': '4+',
+            'disk_size': 200
+        }),
+        ('serve', {
+            'memory': '32+',
+        }, {
+            'cpus': '4+',
+            'memory': '32+',
+            'disk_size': 200
+        }),
+    ])
+def test_get_controller_resources_spot(controller_type,
+                                       custom_controller_resources_config, expected,
+                                       enable_all_clouds,
+                                       monkeypatch):
+    if controller_type == 'spot':
+        controller_resources_config = spot_constants.CONTROLLER_RESOURCES
+    else:
+        controller_resources_config = serve_constants.CONTROLLER_RESOURCES
+
+    def get_custom_controller_resources(keys, default):
+        if keys == (controller_type, 'controller', 'resources'):
+            return custom_controller_resources_config
+        else:
+            return skypilot_config.get_nested(keys, default)
+    
+    monkeypatch.setattr(skypilot_config, 'get_nested', get_custom_controller_resources)
+
+    controller_resources = controller_utils.get_controller_resources(
+        controller_type, controller_resources_config)
     controller_resources_config = controller_resources.to_yaml_config()
     for k, v in expected.items():
-        assert controller_resources[k] == v, (k, v, controller_resources_config)
-
-
-
-
+        assert controller_resources_config[k] == v, (
+            k, v, controller_resources)
