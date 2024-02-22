b470ea93417d3b9145de03a9a7c7cb50ec2fcf3c
Zhanghao Wu <zhanghao.wu@outlook.com>
2024-02-22 03:11:46 +0000

    more output

--- tests/unit_tests/test_controller_utils.py
+++ tests/unit_tests/test_controller_utils.py
@@ -57,4 +57,5 @@ def test_get_controller_resources_spot(controller_type,
     controller_resources_config = controller_resources.to_yaml_config()
     for k, v in expected.items():
         assert controller_resources_config[k] == v, (
-            controller_type, custom_controller_resources_config, expected, k, v)
+            controller_type, custom_controller_resources_config, expected,
+            controller_resources_config, k, v)
