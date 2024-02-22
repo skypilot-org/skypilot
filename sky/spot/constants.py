3636abde2577708a98c0fb4d0681957623734a6b
Zhanghao Wu <zhanghao.wu@outlook.com>
2024-02-22 02:49:57 +0000

    update comments

--- sky/spot/constants.py
+++ sky/spot/constants.py
@@ -6,9 +6,9 @@ SPOT_CONTROLLER_YAML_PREFIX = '~/.sky/spot_controller'
 SPOT_TASK_YAML_PREFIX = '~/.sky/spot_tasks'
 
 # Resources as a dict for the spot controller.
-# Use default CPU instance type for spot controller, i.e.
+# Use default CPU instance type for spot controller with >= 24GB, i.e.
 # m6i.2xlarge (8vCPUs, 32 GB) for AWS, Standard_D8s_v4 (8vCPUs, 32 GB)
-# for Azure, and n1-standard-8 (8 vCPUs, 32 GB) for GCP.
+# for Azure, and n1-standard-8 (8 vCPUs, 32 GB) for GCP, etc.
 # Based on profiling, memory should be at least 3x (in GB) as num vCPUs to avoid
 # OOM (each vCPU can have 4 spot controller processes as we set the CPU
 # requirement to 0.25, and 3 GB is barely enough for 4 spot processes).
