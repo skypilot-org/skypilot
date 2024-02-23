2a26617b211179f677ad6ac742d250f3cfa08244
Zongheng Yang <zongheng.y@gmail.com>
2024-02-23 10:29:18 -0800

    Format

--- examples/cog/predict.py
+++ examples/cog/predict.py
@@ -5,15 +5,16 @@ from PIL import Image, ImageFilter
 
 
 class Predictor(BasePredictor):
+
     def predict(
-        self,
-        image: Path = Input(description="Input image"),
-        blur: float = Input(description="Blur radius", default=5),
+            self,
+            image: Path = Input(description='Input image'),
+            blur: float = Input(description='Blur radius', default=5),
     ) -> Path:
         if blur == 0:
             return input
         im = Image.open(str(image))
         im = im.filter(ImageFilter.BoxBlur(blur))
-        out_path = Path(tempfile.mkdtemp()) / "out.png"
+        out_path = Path(tempfile.mkdtemp()) / 'out.png'
         im.save(str(out_path))
         return out_path
