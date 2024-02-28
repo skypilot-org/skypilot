0bfb81b02bf45449419f9701946ec45c4b2a5262
cblmemo <cblmemo@gmail.com>
2024-02-28 03:33:22 -0800

    init

---
+++ examples/serve/authentication/server.py
@@ -0,0 +1,31 @@
+import fastapi
+from fastapi import security
+
+app = fastapi.FastAPI()
+
+bearer_security = security.HTTPBearer()
+STATIC_TOKEN = "static_secret_token"
+
+
+def verify_token(
+    credentials: security.HTTPAuthorizationCredentials = fastapi.Depends(
+        bearer_security),
+) -> None:
+    if credentials.credentials != STATIC_TOKEN:
+        raise fastapi.HTTPException(status_code=403,
+                                    detail="Invalid authentication credentials")
+
+
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
+
+
+@app.get("/compute")
+async def compute(_=fastapi.Depends(verify_token)):
+    return {"message": "Token validation successful!"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8087)
