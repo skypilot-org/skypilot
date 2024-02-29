c928a12e530d87725a221760aae2e125b808ff39
Zhanghao Wu <zhanghao.wu@outlook.com>
2024-02-29 02:36:20 +0000

    Fix backward compat

--- tests/backward_compatibility_tests.sh
+++ tests/backward_compatibility_tests.sh
@@ -161,17 +161,17 @@ fi
 if [ "$start_from" -le 7 ]; then
 conda activate sky-back-compat-master
 rm -r  ~/.sky/wheels || true
-sky spot launch --cloud ${CLOUD} -y --cpus 2 -n ${CLUSTER_NAME}-7 "echo hi; sleep 100"
+sky spot launch -d --cloud ${CLOUD} -y --cpus 2 -n ${CLUSTER_NAME}-7 "echo hi; sleep 100"
 conda activate sky-back-compat-current
 rm -r  ~/.sky/wheels || true
 s=$(sky spot logs --no-follow -n ${CLUSTER_NAME}-7)
 echo "$s"
 echo "$s" | grep "hi" || exit 1
-sky spot launch --cloud ${CLOUD} -y -n ${CLUSTER_NAME}-7 "echo hi; sleep 10"
+sky spot launch -d --cloud ${CLOUD} -y -n ${CLUSTER_NAME}-7 "echo hi; sleep 10"
 s=$(sky spot queue | grep ${CLUSTER_NAME}-7)
 echo "$s"
 echo "$s" | grep "RUNNING" | wc -l | grep 2 || exit 1
-sleep 50
+sleep 300
 s=$(sky spot queue | grep ${CLUSTER_NAME}-7 | grep "SUCCEEDED")
 echo "$s"
 echo "$s" | grep "SUCCEEDED" | wc -l | grep 2 || exit 1
