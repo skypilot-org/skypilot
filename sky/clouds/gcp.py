de7a8681925bb6046e5a093874051ffeace0d8f1
Zhanghao Wu <zhanghao.wu@outlook.com>
2024-03-06 14:16:36 -0800

    [GCP] Skip checking for `iam.serviceAccounts.actAs` (#3284)
    * check service account policy
    
    * Skip the check for actAs
    
    * add note in doc
    
    * format
    
    * Update docs/source/cloud-setup/cloud-permissions/gcp.rst
    
    Co-authored-by: Zongheng Yang <zongheng.y@gmail.com>
    
    * Address comments
    
    * comment
    
    ---------
    
    Co-authored-by: Zongheng Yang <zongheng.y@gmail.com>

--- sky/clouds/gcp.py
+++ sky/clouds/gcp.py
@@ -743,13 +743,13 @@ class GCP(clouds.Cloud):
 
         # This takes user's credential info from "~/.config/gcloud/application_default_credentials.json".  # pylint: disable=line-too-long
         credentials, project = google.auth.default()
-        service = googleapiclient.discovery.build('cloudresourcemanager',
-                                                  'v1',
-                                                  credentials=credentials)
+        crm = googleapiclient.discovery.build('cloudresourcemanager',
+                                              'v1',
+                                              credentials=credentials)
         gcp_minimal_permissions = gcp_utils.get_minimal_permissions()
         permissions = {'permissions': gcp_minimal_permissions}
-        request = service.projects().testIamPermissions(resource=project,
-                                                        body=permissions)
+        request = crm.projects().testIamPermissions(resource=project,
+                                                    body=permissions)
         ret_permissions = request.execute().get('permissions', [])
 
         diffs = set(gcp_minimal_permissions).difference(set(ret_permissions))
