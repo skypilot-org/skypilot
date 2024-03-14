03d5241f34cca8469df3f0ab2c9a2477ff508b4b
Zhanghao Wu <zhanghao.wu@outlook.com>
2024-03-14 19:05:19 +0000

    Add comment

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -105,4 +105,5 @@ html_static_path = ['_static']
 html_js_files = ['custom.js']
 html_css_files = ['custom.css']
 
+# Allowing cross references in markdown files to be parsed
 myst_heading_anchors = 3
