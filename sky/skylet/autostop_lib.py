c180b2b6637e14bab6347ae7d2adca7f4ecd7ccc
Zhanghao Wu <zhanghao.wu@outlook.com>
2024-03-19 09:45:20 -0700

    [Core] Use original Python path for SkyPilot runtime (#3326)
    * Use original python path
    
    * Fix python
    
    * cmd
    
    * fix patch
    
    * add test for custom default env
    
    * Fix test
    
    * address comments
    
    * format
    
    * Fix backward compat
    
    * format
    
    * Update sky/backends/backend_utils.py
    
    Co-authored-by: Zongheng Yang <zongheng.y@gmail.com>
    
    ---------
    
    Co-authored-by: Zongheng Yang <zongheng.y@gmail.com>

--- sky/skylet/autostop_lib.py
+++ sky/skylet/autostop_lib.py
@@ -8,6 +8,7 @@ import psutil
 
 from sky import sky_logging
 from sky.skylet import configs
+from sky.skylet import constants
 from sky.utils import common_utils
 
 logger = sky_logging.init_logger(__name__)
@@ -121,4 +122,4 @@ class AutostopCodeGen:
     def _build(cls, code: List[str]) -> str:
         code = cls._PREFIX + code
         code = ';'.join(code)
-        return f'python3 -u -c {shlex.quote(code)}'
+        return f'{constants.SKY_PYTHON_CMD} -u -c {shlex.quote(code)}'
