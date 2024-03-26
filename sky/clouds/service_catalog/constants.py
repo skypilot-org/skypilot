a46134584c6bc1ee5fe01d139af7acc63365219c
Romil Bhardwaj <romil.bhardwaj@gmail.com>
2024-03-26 11:01:02 -0700

    merge

--- sky/clouds/service_catalog/constants.py
+++ sky/clouds/service_catalog/constants.py
@@ -3,4 +3,5 @@ HOSTED_CATALOG_DIR_URL = 'https://raw.githubusercontent.com/skypilot-org/skypilo
 CATALOG_SCHEMA_VERSION = 'v5'
 CATALOG_DIR = '~/.sky/catalogs'
 ALL_CLOUDS = ('aws', 'azure', 'gcp', 'ibm', 'lambda', 'scp', 'oci',
-              'kubernetes', 'runpod', 'vsphere', 'cudo', 'fluidstack')
+              'kubernetes', 'runpod', 'vsphere', 'cudo', 'fluidstack',
+              'paperspace')
