855bf6107f5e6ca7fa48142030f10fbb2d7e709c
Zhanghao Wu <zhanghao.wu@outlook.com>
2024-04-01 06:23:06 +0000

    comments

--- sky/adaptors/common.py
+++ sky/adaptors/common.py
@@ -4,22 +4,22 @@ from typing import Optional
 
 
 class LazyImport:
-    """Lazy importer for heavy modules."""
+    """Lazy importer for heavy modules or cloud modules only when enabled."""
 
-    def __init__(self, module_name, import_error_message: Optional[str] = None):
-        self.module_name = module_name
-        self.module = None
+    def __init__(self, module_name: str, import_error_message: Optional[str] = None):
+        self._module_name = module_name
+        self._module = None
         self._import_error_message = import_error_message
 
     def load_module(self):
-        if self.module is None:
+        if self._module is None:
             try:
-                self.module = importlib.import_module(self.module_name)
+                self._module = importlib.import_module(self._module_name)
             except ImportError as e:
                 if self._import_error_message is not None:
                     raise ImportError(self._import_error_message) from e
                 raise
-        return self.module
+        return self._module
 
     def __getattr__(self, name):
         # Attempt to access the attribute, if it fails, assume it's a submodule
@@ -28,7 +28,7 @@ class LazyImport:
             return getattr(self.load_module(), name)
         except AttributeError:
             # Dynamically create a new LazyImport instance for the submodule
-            submodule_name = f'{self.module_name}.{name}'
+            submodule_name = f'{self._module_name}.{name}'
             setattr(self, name,
                     LazyImport(submodule_name, self._import_error_message))
             return getattr(self, name)
