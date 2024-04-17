97d734300e6cdb6d4f9723f37a3c165784a21ee0
Romil Bhardwaj <romil.bhardwaj@gmail.com>
2024-04-17 00:17:39 -0700

    lint

--- sky/resources.py
+++ sky/resources.py
@@ -46,27 +46,28 @@ class Resources:
     _VERSION = 16
 
     def __init__(
-            self,
-            cloud: Optional[clouds.Cloud] = None,
-            instance_type: Optional[str] = None,
-            cpus: Union[None, int, float, str] = None,
-            memory: Union[None, int, float, str] = None,
-            accelerators: Union[None, str, Dict[str, int]] = None,
-            accelerator_args: Optional[Dict[str, str]] = None,
-            use_spot: Optional[bool] = None,
-            spot_recovery: Optional[str] = None,
-            region: Optional[str] = None,
-            zone: Optional[str] = None,
-            image_id: Union[Dict[str, str], str, None] = None,
-            disk_size: Optional[int] = None,
-            disk_tier: Optional[Union[str, resources_utils.DiskTier]] = None,
-            ports: Optional[Union[int, str, List[str], Tuple[str]]] = None,
-            # Internal use only.
-            # pylint: disable=invalid-name
-            _docker_login_config: Optional[
-                docker_utils.DockerLoginConfig] = None,
-            _is_image_managed: Optional[bool] = None,
-            _requires_fuse: Optional[bool] = None):
+        self,
+        cloud: Optional[clouds.Cloud] = None,
+        instance_type: Optional[str] = None,
+        cpus: Union[None, int, float, str] = None,
+        memory: Union[None, int, float, str] = None,
+        accelerators: Union[None, str, Dict[str, int]] = None,
+        accelerator_args: Optional[Dict[str, str]] = None,
+        use_spot: Optional[bool] = None,
+        spot_recovery: Optional[str] = None,
+        region: Optional[str] = None,
+        zone: Optional[str] = None,
+        image_id: Union[Dict[str, str], str, None] = None,
+        disk_size: Optional[int] = None,
+        disk_tier: Optional[Union[str, resources_utils.DiskTier]] = None,
+        ports: Optional[Union[int, str, List[str], Tuple[str]]] = None,
+        # Internal use only.
+        # pylint: disable=invalid-name
+        _docker_login_config: Optional[
+            docker_utils.DockerLoginConfig] = None,
+        _is_image_managed: Optional[bool] = None,
+        _requires_fuse: Optional[bool] = None
+    ):
         """Initialize a Resources object.
 
         All fields are optional.  ``Resources.is_launchable`` decides whether
