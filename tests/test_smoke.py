90ef5a4d74a6f1b3fcfb06630969a7ced9d27bee
Zhanghao Wu <zhanghao.wu@outlook.com>
2024-04-26 21:32:54 +0000

    longer wait time for autostop on azure

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1831,8 +1831,8 @@ def test_azure_start_stop():
             f'sky start -y {name} -i 1',
             f'sky exec {name} examples/azure_start_stop.yaml',
             f'sky logs {name} 3 --status',  # Ensure the job succeeded.
-            'sleep 200',
-            f's=$(sky status -r {name}) && echo $s && echo $s | grep "INIT\|STOPPED"'
+            'sleep 250',
+            f's=$(sky status -r {name}) && echo "$s" && echo "$s" | grep "INIT\|STOPPED"'
         ],
         f'sky down -y {name}',
         timeout=30 * 60,  # 30 mins
