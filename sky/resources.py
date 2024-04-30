c07d0e355013e70455aba1bb3e47e78523b031a9
Zhanghao Wu <zhanghao.wu@outlook.com>
2024-04-30 19:34:58 +0000

    revert version 3

--- sky/resources.py
+++ sky/resources.py
@@ -1389,6 +1389,11 @@ class Resources:
         if version < 2:
             self._region = None
 
+        # spot_recovery is deprecated. We do not need to set the _spot_recovery
+        # here any more.
+        # if version < 3:
+        #     self._spot_recovery = None
+
         if version < 4:
             self._image_id = None
 
