5a0b7d51a5f80a27135085259d46ed2a02306651
Andrew Aikawa <asai@berkeley.edu>
2024-05-01 20:47:30 +0000

    update paperspace images

--- sky/provision/paperspace/constants.py
+++ sky/provision/paperspace/constants.py
@@ -1,14 +1,20 @@
-"""Constants for Paperspace provisioner"""
+"""Constants for Paperspace provisioner
+(TODO) asaiacai: fetch default images using API
+The H100 and A100 family of GPUs use `Ubuntu 22.04 MLaiB`
+V100s use Ubuntu `20.04 MLaiB`
+CPU instances use `Ubuntu 22.04 Server`
+These instances are fetched using:
+    # curl -X GET 'https://api.paperspace.io/templates/getTemplates \
+    # -H 'X-Api-Key: ...'
+"""
 
 CPU_INSTANCES_TEMPLATEID = {f'C{i}': 't0nspur5' for i in range(5, 10)}
 
 INSTANCE_TO_TEMPLATEID = {
-    'H100x8': 'tvimtol9',
-    'H100': 'tiq8mhti',
+    'H100x8': 't7vp562h',
+    'H100': 'tilqt47t',
     'A100-80Gx8': 'tvimtol9',
-    'A100-80Gx4': 'tiq8mhti',
-    'A100-80Gx2': 'tiq8mhti',
-    'A100-80G': 'tiq8mhti',
+    'A100-80G': 'tqqsxr6b',
     'V100-32Gx4': 'twnlo3zj',
     'V100-32Gx2': 'twnlo3zj',
     'V100-32G': 'twnlo3zj',
