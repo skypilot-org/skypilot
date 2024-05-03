40868cdeaa3c59878b6efc65fa879a4ef8d00261
Zhanghao Wu <zhanghao.wu@outlook.com>
2024-05-03 13:20:28 -0700

    Update sky/clouds/cloud.py
    Co-authored-by: Zongheng Yang <zongheng.y@gmail.com>

--- sky/clouds/cloud.py
+++ sky/clouds/cloud.py
@@ -503,7 +503,7 @@ class Cloud:
         In most cases, spot resources do not need cleanup after preemption,
         as long as the cluster can be relaunched with the same name and tag,
         no matter the preemption behavior is to terminate or stop the cluster.
-        Similar for on-demand resources that goes into maintenance mode. The
+        Similar for on-demand resources that go into maintenance mode. The
         only exception by far is GCP's TPU VM. We override this method in
         gcp.py.
         """
