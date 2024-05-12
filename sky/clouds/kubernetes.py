54a8e5c2d59c7a1f69171ae172781f3b834fabb7
Andrew Aikawa <asai@berkeley.edu>
2024-05-12 09:17:20 +0000

    check priority class

--- sky/clouds/kubernetes.py
+++ sky/clouds/kubernetes.py
@@ -468,7 +468,16 @@ class Kubernetes(clouds.Cloud):
                 'kueue.x-k8s.io', 'v1beta1', current_ns, 'localqueues')
             local_queues = [queue['metadata']['name'] for queue in local_queues['items']]
             if not label_value in local_queues:
-                error_msg = (f'Invalid kueue.x-k8s.io/queue-name {label_value} specified. '
-                             f'These are the available queues `{local_queues}`')
+                error_msg = (f'{label_key} {label_value} does not exist. '
+                             f'These are the available {label_key} `{local_queues}`')
+                return False, error_msg
+        if label_key == 'kueue.x-k8s.io/priority-class':
+            current_ns = kubernetes_utils.get_current_kube_config_context_namespace()
+            priorities = kubernetes.custom_objects_api().list_namespaced_custom_object(
+                'kueue.x-k8s.io', 'v1beta1', current_ns, 'workloadpriorityclasses')
+            priorities = [priority['metadata']['name'] for priority in priorities['items']]
+            if not label_value in priorities:
+                error_msg = (f'{label_key} {label_value} does not exist. '
+                             f'These are the available {label_key} `{priorities}`')
                 return False, error_msg
         return True, None
