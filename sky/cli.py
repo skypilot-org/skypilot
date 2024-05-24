b75e471dd6976412e802232737d1aa981a002a51
Romil Bhardwaj <romil.bhardwaj@gmail.com>
2024-05-23 18:57:25 -0700

    lint

--- sky/cli.py
+++ sky/cli.py
@@ -3073,7 +3073,9 @@ def show_gpus(
         cloud_is_kubernetes = isinstance(cloud_obj, sky_clouds.Kubernetes)
         kubernetes_autoscaling = kubernetes_utils.get_autoscaler_type(
         ) is not None
-        kubernetes_is_enabled = sky_clouds.cloud_in_iterable(sky_clouds.Kubernetes(), global_user_state.get_cached_enabled_clouds())
+        kubernetes_is_enabled = sky_clouds.cloud_in_iterable(
+            sky_clouds.Kubernetes(),
+            global_user_state.get_cached_enabled_clouds())
 
         if accelerator_str is None:
             # If cloud is kubernetes, we want to show real-time capacity
@@ -3089,7 +3091,8 @@ def show_gpus(
             # common GPUs.
             clouds_to_list = cloud
             if cloud is None and not show_all:
-                clouds_to_list = (c for c in service_catalog.ALL_CLOUDS if c != 'kubernetes')
+                clouds_to_list = (
+                    c for c in service_catalog.ALL_CLOUDS if c != 'kubernetes')
             result = service_catalog.list_accelerator_counts(
                 gpus_only=True,
                 clouds=clouds_to_list,
@@ -3105,7 +3108,8 @@ def show_gpus(
             # Kubernetes GPUs with realtime information
             if kubernetes_is_enabled:
                 yield '\n\n'
-                yield from _kubernetes_realtime_gpu_output(gpu_col_name='KUBERNETES_GPU')
+                yield from _kubernetes_realtime_gpu_output(
+                    gpu_col_name='KUBERNETES_GPU')
 
             # Google TPUs
             for tpu in service_catalog.get_tpus():
@@ -3122,13 +3126,15 @@ def show_gpus(
                     other_table.add_row([gpu, _list_to_str(qty)])
                 yield from other_table.get_string()
                 yield '\n\n'
-                if cloud is None and kubernetes_is_enabled and kubernetes_autoscaling:
+                if (cloud is None and kubernetes_is_enabled
+                        and kubernetes_autoscaling):
                     yield kubernetes_utils.KUBERNETES_AUTOSCALER_NOTE
                     yield '\n\n'
             else:
                 yield ('\n\nHint: use -a/--all to see all accelerators '
                        '(including non-common ones) and pricing.')
-                if cloud is None and kubernetes_is_enabled and kubernetes_autoscaling:
+                if (cloud is None and kubernetes_is_enabled
+                        and kubernetes_autoscaling):
                     yield '\n'
                     yield kubernetes_utils.KUBERNETES_AUTOSCALER_NOTE
                 return
