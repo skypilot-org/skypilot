49b05669374f27049bbb32ab5c0d5730907d2d59
Anish Chopra <anish@komodoai.dev>
2024-05-24 19:15:37 -0700

    Create new AWSIdentityType for assume role types

--- sky/clouds/aws.py
+++ sky/clouds/aws.py
@@ -81,6 +81,10 @@ class AWSIdentityType(enum.Enum):
 
     IAM_ROLE = 'iam-role'
 
+    ASSUME_ROLE = 'assume-role'
+
+    ASSUME_ROLE_WITH_WEB_IDENTITY = 'assume-role-with-web-identity'
+
     #       Name                    Value             Type    Location
     #       ----                    -----             ----    --------
     #    profile                <not set>             None    None
@@ -606,6 +610,10 @@ class AWS(clouds.Cloud):
             return AWSIdentityType.IAM_ROLE
         elif _is_access_key_of_type(AWSIdentityType.ENV.value):
             return AWSIdentityType.ENV
+        elif _is_access_key_of_type(AWSIdentityType.ASSUME_ROLE_WITH_WEB_IDENTITY.value):
+            return AWSIdentityType.ASSUME_ROLE_WITH_WEB_IDENTITY
+        elif _is_access_key_of_type(AWSIdentityType.ASSUME_ROLE.value):
+            return AWSIdentityType.ASSUME_ROLE
         else:
             return AWSIdentityType.SHARED_CREDENTIALS_FILE
 
