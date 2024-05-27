83787eaba38a0406ee59bbb3001e680e2cd0bcc4
Romil Bhardwaj <romil.bhardwaj@gmail.com>
2024-05-27 13:39:57 -0700

    lint

--- sky/provision/kubernetes/network.py
+++ sky/provision/kubernetes/network.py
@@ -74,8 +74,7 @@ def _open_ports_using_ingress(
         )
 
     # Prepare service names, ports,  for template rendering
-    service_details = [(f'{cluster_name_on_cloud}--skypilot-svc--{port}',
-                        port,
+    service_details = [(f'{cluster_name_on_cloud}--skypilot-svc--{port}', port,
                         _PATH_PREFIX.format(
                             cluster_name_on_cloud=cluster_name_on_cloud,
                             port=port,
