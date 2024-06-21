3436b8cad511560d99251e55eb3996cf4f976193
Zhanghao Wu <zhanghao.wu@outlook.com>
2024-06-21 15:27:08 -0700

    [Core] Allow disabling ECC for nvidia-gpu (#3676)
    * Disable ECC for nvidia-gpu
    
    * Add config.rst
    
    * format
    
    * address
    
    * Note for the reboot overhead
    
    * address comments
    
    * fix fluidstack
    
    * Avoid disable ecc for clouds using ray autoscaler due to the lack of retry after reboot

--- sky/utils/schemas.py
+++ sky/utils/schemas.py
@@ -757,6 +757,17 @@ def get_config_schema():
         }
     }
 
+    gpu_configs = {
+        'type': 'object',
+        'required': [],
+        'additionalProperties': False,
+        'properties': {
+            'disable_ecc': {
+                'type': 'boolean',
+            },
+        }
+    }
+
     for cloud, config in cloud_configs.items():
         if cloud == 'aws':
             config['properties'].update(_REMOTE_IDENTITY_SCHEMA_AWS)
@@ -774,6 +785,7 @@ def get_config_schema():
             'spot': controller_resources_schema,
             'serve': controller_resources_schema,
             'allowed_clouds': allowed_clouds,
+            'nvidia_gpus': gpu_configs,
             **cloud_configs,
         },
         # Avoid spot and jobs being present at the same time.
