7633d2e829d351833e876974910f4eb82d283cfd
Romil Bhardwaj <romil.bhardwaj@berkeley.edu>
2024-06-30 10:50:15 -0700

    [k8s] Remove SSH jump pod for port-forward mode (#3657)
    * working prototype of direct-to-pod port-forwarding
    
    * lint
    
    * switch to using head as jump
    
    * removed ssh jump pod
    
    * remove sleep
    
    * update note
    
    * comments
    
    * remove vestiges
    
    * updates
    
    * remove slash
    
    * add ssh_user placeholder
    
    * fix private key
    
    * lint

--- sky/clouds/kubernetes.py
+++ sky/clouds/kubernetes.py
@@ -38,9 +38,6 @@ class Kubernetes(clouds.Cloud):
 
     SKY_SSH_KEY_SECRET_NAME = 'sky-ssh-keys'
     SKY_SSH_JUMP_NAME = 'sky-ssh-jump-pod'
-    PORT_FORWARD_PROXY_CMD_TEMPLATE = \
-        'kubernetes-port-forward-proxy-command.sh.j2'
-    PORT_FORWARD_PROXY_CMD_PATH = '~/.sky/port-forward-proxy-cmd.sh'
     # Timeout for resource provisioning. This timeout determines how long to
     # wait for pod to be in pending status before giving up.
     # Larger timeout may be required for autoscaling clusters, since autoscaler
@@ -323,6 +320,7 @@ class Kubernetes(clouds.Cloud):
             'k8s_namespace':
                 kubernetes_utils.get_current_kube_config_context_namespace(),
             'k8s_port_mode': port_mode.value,
+            'k8s_networking_mode': network_utils.get_networking_mode().value,
             'k8s_ssh_key_secret_name': self.SKY_SSH_KEY_SECRET_NAME,
             'k8s_acc_label_key': k8s_acc_label_key,
             'k8s_acc_label_value': k8s_acc_label_value,
