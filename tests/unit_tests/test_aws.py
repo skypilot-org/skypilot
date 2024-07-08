d379df1a77eda5ac964ba0bef3c3f7f9509dd246
Keith Stevens <keith@collinear.ai>
2024-07-08 11:55:43 -0700

    Style fixes to the unittest

--- tests/unit_tests/test_aws.py
+++ tests/unit_tests/test_aws.py
@@ -8,16 +8,16 @@ from sky.clouds.aws import AWS
 def test_aws_label():
     aws = AWS()
     # Invalid - AWS prefix
-    assert aws.is_label_valid("aws:whatever", "value")[0] == False
+    assert not aws.is_label_valid('aws:whatever', 'value')[0]
     # Valid - valid prefix
-    assert aws.is_label_valid("any:whatever", "value")[0] == True
+    assert aws.is_label_valid('any:whatever', 'value')[0]
     # Invalid - Too long
-    assert (aws.is_label_valid(
-        "sprinto:thisiexample_string_with_123_characters_length_thing_thing_thing_thing_thing_thing_thing_thin_thing_thing_thing_thing_thing_thing",
-        "value",
-    )[0] == False)
+    assert not (aws.is_label_valid(
+        'sprinto:thisiexample_string_with_123_characters_length_thing_thing_thing_thing_thing_thing_thing_thin_thing_thing_thing_thing_thing_thing',
+        'value',
+    )[0])
     # Invalid - Too long
-    assert (aws.is_label_valid(
-        "sprinto:short",
-        "thisiexample_string_with_123_characters_length_thing_thing_thing_thing_thing_thing_thing_thin_thing_thing_thing_thing_thing_thingthisiexample_string_with_123_characters_length_thing_thing_thing_thing_thing_thing_thing_thin_thing_thing_thing_thing_thing_thing",
-    )[0] == False)
+    assert not (aws.is_label_valid(
+        'sprinto:short',
+        'thisiexample_string_with_123_characters_length_thing_thing_thing_thing_thing_thing_thing_thin_thing_thing_thing_thing_thing_thingthisiexample_string_with_123_characters_length_thing_thing_thing_thing_thing_thing_thing_thin_thing_thing_thing_thing_thing_thing',
+    )[0])
