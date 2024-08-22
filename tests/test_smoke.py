cfb9e9dd150efee483a6f13845ece5314a378404
Yi Pan <conlesspan@outlook.com>
2024-08-22 11:30:16 +0800

    Add unittest for disk tier ultra.

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -3254,7 +3254,7 @@ def test_gcp_disk_tier():
 @pytest.mark.azure
 def test_azure_disk_tier():
     for disk_tier in list(resources_utils.DiskTier):
-        if disk_tier == resources_utils.DiskTier.HIGH:
+        if disk_tier == resources_utils.DiskTier.HIGH or disk_tier == resources_utils.DiskTier.ULTRA:
             # Azure does not support high disk tier.
             continue
         type = Azure._get_disk_type(disk_tier)
