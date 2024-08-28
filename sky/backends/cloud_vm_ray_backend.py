bafd4521381b257cb0f24f2b9cb692d51d13bec3
cblmemo <cblmemo@gmail.com>
2024-08-28 11:31:42 -0700

    fix

--- sky/backends/cloud_vm_ray_backend.py
+++ sky/backends/cloud_vm_ray_backend.py
@@ -1933,7 +1933,7 @@ class RetryingVmProvisioner(object):
         while True:
             if (isinstance(to_provision.cloud, clouds.Azure) and
                     to_provision.accelerators is not None and
-                    'A10' in to_provision.accelerators):
+                    'A10' in to_provision.accelerators and prev_handle is None):
                 logger.warning(f'{style.BRIGHT}{fore.YELLOW}Trying to launch '
                                'an A10 cluster on Azure. This may take ~20 '
                                'minutes due to driver installation.'
