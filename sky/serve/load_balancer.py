39444aaaed054d7df4cd76ecacb199e288d36874
cblmemo <cblmemo@gmail.com>
2024-09-11 00:18:38 -0700

    todo

--- sky/serve/load_balancer.py
+++ sky/serve/load_balancer.py
@@ -160,6 +160,10 @@ class SkyServeLoadBalancer:
         # SkyServe supports serving on Spot Instances. To avoid preemptions
         # during request handling, we add a retry here.
         retry_cnt = 0
+        # TODO(tian): Here we try to not retry those failed replicas for the
+        # case when the replica is in a NOT_READY state but does not sync-ed
+        # to the load balancer yet. We should have a better way to do the
+        # synchronization (e.g. cross process communication).
         failed_replica_urls: List[str] = []
         while True:
             retry_cnt += 1
