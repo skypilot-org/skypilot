994272b35f0c61df204d838793363fa01a1cd601
Zhanghao Wu <zhanghao.wu@outlook.com>
2024-09-22 19:43:29 +0000

    changes in examples

--- examples/admin_policy/example_policy/example_policy/skypilot_policy.py
+++ examples/admin_policy/example_policy/example_policy/skypilot_policy.py
@@ -5,15 +5,15 @@ import sky
 
 
 class TaskLabelPolicy(sky.AdminPolicy):
-    """Example policy: add label for task with the local user name."""
+    """Example policy: adds a label of the local user name to all tasks."""
 
     @classmethod
     def validate_and_mutate(
             cls, user_request: sky.UserRequest) -> sky.MutatedUserRequest:
-        """Add label for task with the local user name."""
+        """Adds a label for task with the local user name."""
         local_user_name = getpass.getuser()
 
-        # Add label for task with the local user name
+        # Adds a label for task with the local user name
         task = user_request.task
         for r in task.resources:
             r.labels['local_user'] = local_user_name
@@ -23,15 +23,15 @@ class TaskLabelPolicy(sky.AdminPolicy):
 
 
 class ConfigLabelPolicy(sky.AdminPolicy):
-    """Example policy: add label for skypilot_config with the local user name."""
+    """Example policy: adds a label for skypilot_config with local user name."""
 
     @classmethod
     def validate_and_mutate(
             cls, user_request: sky.UserRequest) -> sky.MutatedUserRequest:
-        """Add label for skypilot_config with the local user name."""
+        """Adds a label for skypilot_config with local user name."""
         local_user_name = getpass.getuser()
 
-        # Add label for skypilot_config with the local user name
+        # Adds label for skypilot_config with the local user name
         skypilot_config = copy.deepcopy(user_request.skypilot_config)
         skypilot_config.set_nested(('gcp', 'labels', 'local_user'),
                                    local_user_name)
@@ -40,12 +40,12 @@ class ConfigLabelPolicy(sky.AdminPolicy):
 
 
 class RejectAllPolicy(sky.AdminPolicy):
-    """Example policy: reject all user requests."""
+    """Example policy: rejects all user requests."""
 
     @classmethod
     def validate_and_mutate(
             cls, user_request: sky.UserRequest) -> sky.MutatedUserRequest:
-        """Reject all user requests."""
+        """Rejects all user requests."""
         del user_request
         raise RuntimeError('Reject all policy')
 
@@ -56,15 +56,17 @@ class EnforceAutostopPolicy(sky.AdminPolicy):
     @classmethod
     def validate_and_mutate(
             cls, user_request: sky.UserRequest) -> sky.MutatedUserRequest:
-        """Enforce autostop for all tasks."""
-        operation_args = user_request.operation_args
-        if operation_args is None:
+        """Enforces autostop for all tasks."""
+        request_options = user_request.request_options
+        # Request options is None when a task is executed with `jobs launch` or
+        # `sky serve up`.
+        if request_options is None:
             return sky.MutatedUserRequest(
                 task=user_request.task,
                 skypilot_config=user_request.skypilot_config)
-        idle_minutes_to_autostop = operation_args.idle_minutes_to_autostop
+        idle_minutes_to_autostop = request_options.idle_minutes_to_autostop
         # Enforce autostop/down to be set for all tasks for new clusters.
-        if not operation_args.cluster_exists and (
+        if not request_options.cluster_exists and (
                 idle_minutes_to_autostop is None or
                 idle_minutes_to_autostop < 0):
             raise RuntimeError('Autostop/down must be set for all newly '
