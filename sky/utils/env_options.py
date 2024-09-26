a63893b0811becf9212953554c712caf9826edba
Zongheng Yang <zongheng.y@gmail.com>
2024-09-26 08:40:12 -0700

    Revert "[UX] default to minimal logging (no module/line number/timestamp)." (#4003)
    Revert "[UX] default to minimal logging (no module/line number/timestamp). (#…"
    
    This reverts commit b96a5b42b65a7f08d41fd57e508052e9b20a2041.

--- sky/utils/env_options.py
+++ sky/utils/env_options.py
@@ -5,27 +5,17 @@ import os
 
 class Options(enum.Enum):
     """Environment variables for SkyPilot."""
-
-    # (env var name, default value)
-    IS_DEVELOPER = ('SKYPILOT_DEV', False)
-    SHOW_DEBUG_INFO = ('SKYPILOT_DEBUG', False)
-    DISABLE_LOGGING = ('SKYPILOT_DISABLE_USAGE_COLLECTION', False)
-    MINIMIZE_LOGGING = ('SKYPILOT_MINIMIZE_LOGGING', True)
+    IS_DEVELOPER = 'SKYPILOT_DEV'
+    SHOW_DEBUG_INFO = 'SKYPILOT_DEBUG'
+    DISABLE_LOGGING = 'SKYPILOT_DISABLE_USAGE_COLLECTION'
+    MINIMIZE_LOGGING = 'SKYPILOT_MINIMIZE_LOGGING'
     # Internal: this is used to skip the cloud user identity check, which is
     # used to protect cluster operations in a multi-identity scenario.
     # Currently, this is only used in the job and serve controller, as there
     # will not be multiple identities, and skipping the check can increase
     # robustness.
-    SKIP_CLOUD_IDENTITY_CHECK = ('SKYPILOT_SKIP_CLOUD_IDENTITY_CHECK', False)
+    SKIP_CLOUD_IDENTITY_CHECK = 'SKYPILOT_SKIP_CLOUD_IDENTITY_CHECK'
 
-    def __init__(self, env_var: str, default: bool) -> None:
-        self.env_var = env_var
-        self.default = default
-
-    def __repr__(self) -> str:
-        return self.env_var
-
-    def get(self) -> bool:
+    def get(self):
         """Check if an environment variable is set to True."""
-        return os.getenv(self.env_var,
-                         str(self.default)).lower() in ('true', '1')
+        return os.getenv(self.value, 'False').lower() in ('true', '1')
