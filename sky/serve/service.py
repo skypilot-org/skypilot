c585341bfb58ff78566f1e272df0ae65a496894f
Andy Lee <andylizf@outlook.com>
2024-09-30 10:37:41 -0700

    Update sky/serve/service.py
    Co-authored-by: Tian Xia <cblmemo@gmail.com>

--- sky/serve/service.py
+++ sky/serve/service.py
@@ -241,7 +241,7 @@ def _start(service_name: str, tmp_task_yaml: str, job_id: int):
     finally:
         # Kill load balancer process first since it will raise errors if failed
         # to connect to the controller. Then the controller process.
-        process_to_kill = filter(None,
+        process_to_kill = filter(lambda proc: proc is not None,
                                  [load_balancer_process, controller_process])
         subprocess_utils.kill_children_processes(
             [process.pid for process in process_to_kill], force=True)
