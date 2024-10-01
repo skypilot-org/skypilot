77a4044da468d8d23e7c38d893fc1f0d46ba6443
Warrick He <warrickhe@gmail.com>
2024-10-01 04:47:26 -0700

    Add Error Reporting

--- sky/backends/cloud_vm_ray_backend.py
+++ sky/backends/cloud_vm_ray_backend.py
@@ -4580,15 +4580,16 @@ class CloudVmRayBackend(backends.Backend['CloudVmRayResourceHandle']):
             zip_filename = (f'{constants.SKY_REMOTE_WORKDIR}'
                             '/skypilot-filemounts.zip')
             decompress_command = (
-                f'unzip {zip_filename} -d {constants.SKY_REMOTE_WORKDIR} '
-                '&& rm {zip_filename}')
+                f'[ -f {zip_filename} ] && '
+                f'(unzip {zip_filename} -d {constants.SKY_REMOTE_WORKDIR} '
+                f'&& rm {zip_filename}) || echo "Zip not on this node"')
             returncode = runner.run(decompress_command, log_path=log_path)
             subprocess_utils.handle_returncode(
                 returncode, decompress_command,
                 'Failed to inflate or remove skypilot-filemounts.zip, '
                 f'check permissions. Log: {log_path}')
-
-        subprocess_utils.run_in_parallel(_decompress_filemount_zips, runners)
+        subprocess_utils.run_in_parallel(_decompress_filemount_zips,
+                                             runners)
         end = time.time()
         logger.debug(f'File mount sync took {end - start} seconds.')
 
