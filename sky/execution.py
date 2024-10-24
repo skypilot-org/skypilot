51c98d3213bc9889fd2432270e6bedf0a0386f95
Christopher Cooper <cooperc@assemblesys.com>
2024-10-23 17:18:10 -0700

    [UX] skip provisioning stages if cluster is already available

--- sky/execution.py
+++ sky/execution.py
@@ -15,6 +15,7 @@ from sky import global_user_state
 from sky import optimizer
 from sky import sky_logging
 from sky.backends import backend_utils
+from sky.exceptions import ClusterNotUpError
 from sky.usage import usage_lib
 from sky.utils import admin_policy_utils
 from sky.utils import controller_utils
@@ -451,15 +452,47 @@ def launch(
         controller_utils.check_cluster_name_not_controller(
             cluster_name, operation_str='sky.launch')
 
+    handle = None
+    stages = None
+    # Check if cluster exists
+    if cluster_name is not None:
+        maybe_handle = global_user_state.get_handle_from_cluster_name(
+            cluster_name)
+        if maybe_handle is not None:
+            try:
+                # This will throw if the cluster is not available
+                backend_utils.check_cluster_available(
+                    cluster_name,
+                    operation='executing tasks',
+                    check_cloud_vm_ray_backend=False,
+                    dryrun=dryrun)
+                # If the cluster is available, restrict stages
+                handle = maybe_handle
+                stages = [
+                    # Stage.CLONE_DISK,
+                    # Stage.PROVISION,
+                    # Stage.OPTIMIZE,
+                    Stage.SYNC_WORKDIR,
+                    Stage.SYNC_FILE_MOUNTS,
+                    # Stage.SETUP,
+                    Stage.PRE_EXEC,
+                    Stage.EXEC,
+                    Stage.DOWN
+                ]
+            except ClusterNotUpError:
+                # Proceed with normal provisioning
+                pass
+
     return _execute(
         entrypoint=entrypoint,
         dryrun=dryrun,
         down=down,
         stream_logs=stream_logs,
-        handle=None,
+        handle=handle,
         backend=backend,
         retry_until_up=retry_until_up,
         optimize_target=optimize_target,
+        stages=stages,
         cluster_name=cluster_name,
         detach_setup=detach_setup,
         detach_run=detach_run,
