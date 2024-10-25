7aa2ba4af26808d52a9576b7dbe73e38d6f1f017
Yika Luo <yikaluo@assemblesys.com>
2024-10-25 07:50:22 -0700

    auto refresh local images.csv

--- sky/clouds/service_catalog/azure_catalog.py
+++ sky/clouds/service_catalog/azure_catalog.py
@@ -6,11 +6,13 @@ instance types and pricing information for Azure.
 import re
 from typing import Dict, List, Optional, Tuple
 
-from sky import clouds as cloud_lib
+from sky import clouds as cloud_lib, sky_logging
 from sky.clouds import Azure
 from sky.clouds.service_catalog import common
 from sky.utils import resources_utils
 from sky.utils import ux_utils
+
+logger = sky_logging.init_logger(__name__)
 
 # This list should match the list of regions in
 # skypilot image generation Packer script's replication_regions
@@ -191,9 +193,16 @@ def list_accelerators(
 
 def get_image_id_from_tag(tag: str, region: Optional[str]) -> Optional[str]:
     """Returns the image id from the tag."""
-    # Azure images are not region-specific.
-    del region  # Unused.
-    return common.get_image_id_from_tag_impl(_image_df, tag, None)
+    global _image_df
+    image_id = common.get_image_id_from_tag_impl(_image_df, tag, region)
+    if image_id is None:
+        # Refresh the image catalog and try again, if the image tag is not
+        # found.
+        logger.debug('Refreshing the image catalog and trying again.')
+        _image_df = common.read_catalog('azure/images.csv',
+                                        pull_frequency_hours=0)
+        image_id = common.get_image_id_from_tag_impl(_image_df, tag, region)
+    return image_id
 
 
 def is_image_tag_valid(tag: str, region: Optional[str]) -> bool:
