ab830355fc317c0027a03fa4ca1c229f5f7d1614
Andy Lee <andylizf@outlook.com>
2024-10-28 12:45:56 +0800

    Update cloud_vm_ray_backend.py

--- sky/backends/cloud_vm_ray_backend.py
+++ sky/backends/cloud_vm_ray_backend.py
@@ -3296,6 +3296,7 @@ class CloudVmRayBackend(backends.Backend['CloudVmRayResourceHandle']):
     ) -> None:
         """Executes generated code on the head node."""
         style = colorama.Style
+        fore = colorama.Fore
 
         script_path = os.path.join(SKY_REMOTE_APP_DIR, f'sky_job_{job_id}')
         remote_log_dir = self.log_dir
