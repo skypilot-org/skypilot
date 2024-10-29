1a6e2f60a5e2b60205dfdd2c0d1eb233cd6abfab
Zhanghao Wu <zhanghao.wu@outlook.com>
2024-10-28 20:54:32 -0700

    [k8s] Show all kubernetes clusters in optimizer table (#4013)
    * Show all kubernetes clusters in optimizer table
    
    * format
    
    * Add comment

--- sky/optimizer.py
+++ sky/optimizer.py
@@ -831,13 +831,17 @@ class Optimizer:
             return row
 
         def _get_resource_group_hash(resources: 'resources_lib.Resources'):
-            return json.dumps(
-                {
-                    'cloud': f'{resources.cloud}',
-                    'accelerators': f'{resources.accelerators}',
-                    'use_spot': resources.use_spot
-                },
-                sort_keys=True)
+            resource_key_dict = {
+                'cloud': f'{resources.cloud}',
+                'accelerators': f'{resources.accelerators}',
+                'use_spot': resources.use_spot
+            }
+            if isinstance(resources.cloud, clouds.Kubernetes):
+                # Region for Kubernetes is the context name, i.e. different
+                # Kubernetes clusters. We add region to the key to show all the
+                # Kubernetes clusters in the optimizer table for better UX.
+                resource_key_dict['region'] = resources.region
+            return json.dumps(resource_key_dict, sort_keys=True)
 
         # Print the list of resouces that the optimizer considered.
         resource_fields = [
