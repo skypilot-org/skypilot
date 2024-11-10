628edc765462b102d27a62344c9b916b93f02a8a
Zhanghao Wu <zhanghao.wu@outlook.com>
2024-11-10 20:15:55 +0000

    set cancelling first

--- sky/jobs/utils.py
+++ sky/jobs/utils.py
@@ -245,6 +245,11 @@ def cancel_jobs_by_id(job_ids: Optional[List[int]]) -> str:
             controller_process = job_lib.get_status_no_lock(job_id)
             if controller_process == job_lib.JobStatus.PENDING:
                 if job_lib.cancel_job_no_lock(job_id):
+                    managed_job_state.set_cancelling(
+                        job_id,
+                        callback_func=event_callback_func(job_id,
+                                                          task_id=0,
+                                                          task=None))
                     managed_job_state.set_cancelled(
                         job_id,
                         callback_func=event_callback_func(job_id,
