088d2b1635d131ff35e158e6bb1a33b791f51ee3
Zhanghao Wu <zhanghao.wu@outlook.com>
2024-11-18 23:17:11 +0000

    Remove K80 and M60 from GPU list

--- sky/clouds/service_catalog/__init__.py
+++ sky/clouds/service_catalog/__init__.py
@@ -324,13 +324,13 @@ def get_common_gpus() -> List[str]:
         'A100',
         'A100-80GB',
         'H100',
-        'K80',
         'L4',
-        'M60',
+        'L40S',
         'P100',
         'T4',
         'V100',
         'V100-32GB',
+        'MI25'
     ]
 
 
@@ -340,10 +340,10 @@ def get_tpus() -> List[str]:
     # There are many TPU configurations available, we show the three smallest
     # and the largest configuration for the latest gen TPUs.
     return [
-        'tpu-v2-512', 'tpu-v3-2048', 'tpu-v4-8', 'tpu-v4-16', 'tpu-v4-32',
-        'tpu-v4-3968', 'tpu-v5litepod-1', 'tpu-v5litepod-4', 'tpu-v5litepod-8',
+        'tpu-v3-2048', 'tpu-v4-8', 'tpu-v4-16', 'tpu-v4-32',
+        'tpu-v5litepod-1', 'tpu-v5litepod-4', 'tpu-v5litepod-8',
         'tpu-v5litepod-256', 'tpu-v5p-8', 'tpu-v5p-32', 'tpu-v5p-128',
-        'tpu-v5p-12288'
+        'tpu-v5p-12288', 'tpu-v6e-8', 'tpu-v6e-32', 'tpu-v6e-128',
     ]
 
 
