10c5f21a619a6c4f8d89f788824627888008195a
Zhanghao Wu <zhanghao.wu@outlook.com>
2024-11-25 20:06:50 +0000

    fix smoke

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -3540,7 +3540,7 @@ def test_managed_jobs_inline_env(generic_cloud: str):
             f'sky jobs launch -n {name} -y --cloud {generic_cloud} --env TEST_ENV="hello world" -- "echo "\\$TEST_ENV"; ([[ ! -z \\"\$TEST_ENV\\" ]] && [[ ! -z \\"\${constants.SKYPILOT_NODE_IPS}\\" ]] && [[ ! -z \\"\${constants.SKYPILOT_NODE_RANK}\\" ]] && [[ ! -z \\"\${constants.SKYPILOT_NUM_NODES}\\" ]]) || exit 1"',
             'sleep 20',
             f'{_GET_JOB_QUEUE} | grep {name} | grep SUCCEEDED',
-            f'JOB_ID=$(sky jobs list -n {name} | grep {name} | head -n1 | awk \'{{print $1}}\') && '
+            f'JOB_ID=$(sky jobs queue -n {name} | grep {name} | head -n1 | awk \'{{print $1}}\') && '
             # Test that logs are still available after the job finishes.
             's=$(sky jobs logs $JOB_ID) && echo "$s" && echo "$s" | grep "hello world" && '
             # Make sure we skip the unnecessary logs.
