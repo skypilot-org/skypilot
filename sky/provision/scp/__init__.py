33b77550d84d70d11e163c4c30d0ecb0c0819c7b
Kihyo Moon <36657450+hyoxt121@users.noreply.github.com>
2024-11-25 09:15:15 +0900

    [SC

--- sky/provision/scp/__init__.py
+++ sky/provision/scp/__init__.py
@@ -1,4 +1,4 @@
 """SCP provisioner for SkyPilot."""
 
-from sky.provision.scp.instance import open_ports
 from sky.provision.scp.instance import cleanup_ports
+from sky.provision.scp.instance import open_ports
