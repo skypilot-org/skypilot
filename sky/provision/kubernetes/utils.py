0838d955167af02b5eb2c1f75affed757563b8fd
Romil Bhardwaj <romil.bhardwaj@gmail.com>
2024-11-26 11:51:04 -0800

    Fix incluster auth namespace fetching

--- sky/provision/kubernetes/utils.py
+++ sky/provision/kubernetes/utils.py
@@ -1045,11 +1045,14 @@ def get_kube_config_context_namespace(
             the default namespace.
     """
     k8s = kubernetes.kubernetes
-    # Get namespace if using in-cluster config
     ns_path = '/var/run/secrets/kubernetes.io/serviceaccount/namespace'
-    if os.path.exists(ns_path):
-        with open(ns_path, encoding='utf-8') as f:
-            return f.read().strip()
+    # If using in-cluster context, get the namespace from the service account
+    # namespace file.
+    if (context_name == kubernetes.in_cluster_context_name() or
+            context_name is None):
+        if os.path.exists(ns_path):
+            with open(ns_path, encoding='utf-8') as f:
+                return f.read().strip()
     # If not in-cluster, get the namespace from kubeconfig
     try:
         contexts, current_context = k8s.config.list_kube_config_contexts()
