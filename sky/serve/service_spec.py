a78c7d84e26536ec064ea01289381006949cf098
Yi Pan <conlesspan@outlook.com>
2024-11-30 18:59:28 +0800

    Refactor the implementation.

--- sky/serve/service_spec.py
+++ sky/serve/service_spec.py
@@ -23,6 +23,7 @@ class SkyServiceSpec:
         readiness_timeout_seconds: int,
         min_replicas: int,
         max_replicas: Optional[int] = None,
+        port: Optional[int] = None,
         target_qps_per_replica: Optional[float] = None,
         post_data: Optional[Dict[str, Any]] = None,
         readiness_headers: Optional[Dict[str, str]] = None,
@@ -69,6 +70,7 @@ class SkyServiceSpec:
         self._readiness_timeout_seconds: int = readiness_timeout_seconds
         self._min_replicas: int = min_replicas
         self._max_replicas: Optional[int] = max_replicas
+        self._port: Optional[int] = port
         self._target_qps_per_replica: Optional[float] = target_qps_per_replica
         self._post_data: Optional[Dict[str, Any]] = post_data
         self._readiness_headers: Optional[Dict[str, str]] = readiness_headers
@@ -131,6 +133,16 @@ class SkyServiceSpec:
                     ) from e
         service_config['post_data'] = post_data
         service_config['readiness_headers'] = readiness_headers
+
+        port = config.get('port', None)
+        if port is not None:
+            if not isinstance(port, int):
+                with ux_utils.print_exception_no_traceback():
+                    raise ValueError('Port must be an integer.')
+            if not 1 <= port <= 65535:
+                with ux_utils.print_exception_no_traceback():
+                    raise ValueError('Port must be between 1 and 65535.')
+        service_config['port'] = port
 
         policy_section = config.get('replica_policy', None)
         simplified_policy_section = config.get('replicas', None)
@@ -219,6 +231,7 @@ class SkyServiceSpec:
                         self.downscale_delay_seconds)
         add_if_not_none('load_balancing_policy', None,
                         self._load_balancing_policy)
+        add_if_not_none('port', None, self.port)
         return config
 
     def probe_str(self):
@@ -293,6 +306,10 @@ class SkyServiceSpec:
     def max_replicas(self) -> Optional[int]:
         # If None, treated as having the same value of min_replicas.
         return self._max_replicas
+
+    @property
+    def port(self) -> Optional[int]:
+        return self._port
 
     @property
     def target_qps_per_replica(self) -> Optional[float]:
