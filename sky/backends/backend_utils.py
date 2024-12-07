aa6d2e9f8462c4e68196e9a6420c6781c9ff116b
Christopher Cooper <cooperc@assemblesys.com>
2024-12-06 16:16:53 -0800

    use usage_intervals for new cluster detection
    get_cluster_duration will include the total duration of the cluster since its
    initial launch, while launched_at may be reset by sky launch on an existing
    cluster. So this is a more accurate method to check.

--- sky/backends/backend_utils.py
+++ sky/backends/backend_utils.py
@@ -1836,10 +1836,10 @@ def _update_cluster_status_no_lock(
 
     # All cases below are transitioning the cluster to non-UP states.
 
-    if not node_statuses:
-        time_since_launch = time.time() - record['launched_at']
-        if (record['status'] == status_lib.ClusterStatus.INIT and
-                time_since_launch < _LAUNCH_DOUBLE_CHECK_WINDOW):
+    if not node_statuses and record['status'] == status_lib.ClusterStatus.INIT:
+        cluster_duration = global_user_state.get_cluster_duration(
+            record['cluster_hash'])
+        if cluster_duration < _LAUNCH_DOUBLE_CHECK_WINDOW:
             # It's possible the instances for this cluster were just created,
             # and haven't appeared yet in the cloud API/console. Wait for a bit
             # and check again. This is a best-effort leak prevention check.
