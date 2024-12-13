fc69f73d43f4d5edb0207db91962d65aa5ec876f
Chris McKenzie <kristopolous@yahoo.com>
2024-12-12 16:21:16 -0800

    Vast: Comment on the rammifications of searching for instances

--- sky/provision/vast/utils.py
+++ sky/provision/vast/utils.py
@@ -50,6 +50,12 @@ def launch(name: str, instance_type: str, region: str, disk_size: int,
          Since regardless of specificity, all our geolocations end 
          in two-letter country codes we just snip that to conform 
          to how many providers state their geolocation.
+
+      *  Since the catalog is cached, we can't gaurantee availability 
+         of any machine at the point of inquiry.  As a consequence we 
+         search for the machine again and potentially return a failure 
+         if there is no availability.
+
     """
     gpu_name = instance_type.split('-')[1].replace('_', ' ')
     num_gpus = int(instance_type.split('-')[0].replace('x', ''))
