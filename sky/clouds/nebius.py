89add551bbe120b23730501dab57a7a510160631
Alexander Salikov <Salikov57@gmail.com>
2024-12-19 13:06:21 +0000

    add custom_resources

--- sky/clouds/nebius.py
+++ sky/clouds/nebius.py
@@ -194,8 +194,14 @@ class Nebius(clouds.Cloud):
         logging.debug('Nebius cloud make deploy resources variables: %s', )
         del zones, dryrun, cluster_name
 
+        r = resources
+        acc_dict = self.get_accelerators_from_instance_type(r.instance_type)
+        custom_resources = resources_utils.make_ray_custom_resources_str(
+            acc_dict)
         return {
-            'instance_type': resources.instance_type
+            'instance_type': resources.instance_type,
+            'custom_resources': custom_resources,
+            'region': region.name
         }
 
     def _get_feasible_launchable_resources(
