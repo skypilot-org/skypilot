890945b7497056e5631622ce088b9446c33af2a6
Romil Bhardwaj <romil.bhardwaj@gmail.com>
2024-12-20 17:59:27 -0800

    Fix SVG in navbar rendering by hardcoding SVG + defining text color in css

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -85,13 +85,7 @@ html_theme_options = {
     ],
     'navbar_persistent': ['search-button-field'],
     'logo': {
-        # 'image_light': render_svg_logo('_static/SkyPilot_wide_light.svg'),
-        # 'image_dark': render_svg_logo('_static/SkyPilot_wide_dark.svg'),
-        'svg_light': render_svg_logo('_static/SkyPilot_wide_light.svg'),
-        'svg_dark': render_svg_logo('_static/SkyPilot_wide_dark.svg'),
-        # 'image_light': '_static/SkyPilot_wide_light.svg',
-        # 'image_dark': '_static/SkyPilot_wide_dark.svg',
-        # 'svg': render_svg_logo('_static/SkyPilot_wide_light.svg'),
+        'svg': render_svg_logo('_static/SkyPilot_wide_light.svg'),
     },
     'icon_links': [{
         'name': 'Slack',
