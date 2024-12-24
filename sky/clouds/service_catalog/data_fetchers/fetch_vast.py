046480925bbc2a5281d4493fadf31e56526f8003
Chris McKenzie <kristopolous@yahoo.com>
2024-12-23 16:44:55 -0800

    Vast: updating the catalog fetcher

--- sky/clouds/service_catalog/data_fetchers/fetch_vast.py
+++ sky/clouds/service_catalog/data_fetchers/fetch_vast.py
@@ -5,8 +5,17 @@ import csv
 import json
 import re
 import sys
+from typing import Any, Dict
 
 from vastai_sdk import VastAI
+
+_map = {
+    'TeslaV100': 'V100',
+    'TeslaT4': 'T4',
+    'TeslaP100': 'P100',
+    'QRTX6000': 'RTX6000',
+    'QRTX8000': 'RTX8000'
+}
 
 
 def create_instance_type(obj: Dict[str, Any]) -> str:
@@ -21,46 +30,58 @@ def dot_get(d: dict, key: str) -> Any:
     return d
 
 
-# InstanceType and gpuInfo are basically just stubs
-# so that the dictwriter is happy without weird code.
-mapped_keys = (('gpu_name', 'InstanceType'), ('gpu_name', 'AcceleratorName'),
-               ('num_gpus', 'AcceleratorCount'), ('cpu_cores', 'vCPUs'),
-               ('gpu_total_ram', 'MemoryGiB'), ('search.totalHour', 'Price'),
-               ('geolocation', 'Region'), ('gpu_name', 'GpuInfo'),
-               ('search.totalHour', 'SpotPrice'))
-writer = csv.DictWriter(sys.stdout, fieldnames=[x[1] for x in mapped_keys])
-writer.writeheader()
+if __name__ == '__main__':
+    # InstanceType and gpuInfo are basically just stubs
+    # so that the dictwriter is happy without weird
+    # code.
+    mapped_keys = (('gpu_name', 'InstanceType'), ('gpu_name',
+                                                  'AcceleratorName'),
+                   ('num_gpus', 'AcceleratorCount'), ('cpu_cores', 'vCPUs'),
+                   ('gpu_total_ram', 'MemoryGiB'), ('gpu_name', 'GpuInfo'),
+                   ('search.totalHour', 'Price'),
+                   ('search.totalHour', 'SpotPrice'), ('geolocation', 'Region'))
+    writer = csv.DictWriter(sys.stdout, fieldnames=[x[1] for x in mapped_keys])
+    writer.writeheader()
 
-offerList = VastAI().search_offers(limit=10000)
-for offer in offerList:
-    entry = {}
-    for ours, theirs in mapped_keys:
-        field = dot_get(offer, ours)
-        if 'Price' in theirs:
-            field = '{:.2f}'.format(field)
-        entry[theirs] = field
+    offerList = VastAI().search_offers(limit=10000)
+    for offer in offerList:
+        entry = {}
+        for ours, theirs in mapped_keys:
+            field = dot_get(offer, ours)
+            if 'Price' in theirs:
+                field = '{:.2f}'.format(field)
+            entry[theirs] = field
 
-    entry['InstanceType'] = create_instance_type(offer)
+        entry['InstanceType'] = create_instance_type(offer)
 
-    # the documentation says
-    # "{'gpus': [{
-    #   'name': 'v100',
-    #   'manufacturer': 'nvidia',
-    #   'count': 8.0,
-    #   'memoryinfo': {'sizeinmib': 16384}
-    #   }],
-    #   'totalgpumemoryinmib': 16384}",
-    # we can do that.
-    entry['MemoryGiB'] /= 1024
-    entry['GpuInfo'] = json.dumps({
-        'Gpus': [{
-            'Name': offer['gpu_name'],
-            'Count': offer['num_gpus'],
-            'MemoryInfo': {
-                'SizeInMiB': offer['gpu_total_ram']
-            }
-        }],
-        'TotalGpuMemoryInMiB': offer['gpu_total_ram']
-    }).replace('"', '\'')
+        # the documentation says
+        # "{'gpus': [{
+        #   'name': 'v100',
+        #   'manufacturer': 'nvidia',
+        #   'count': 8.0,
+        #   'memoryinfo': {'sizeinmib': 16384}
+        #   }],
+        #   'totalgpumemoryinmib': 16384}",
+        # we can do that.
+        entry['MemoryGiB'] /= 1024
 
-    writer.writerow(entry)
+        gpu = re.sub('Ada', '-Ada', re.sub(r'\s', '', offer['gpu_name']))
+        gpu = re.sub(r'(Ti|PCIE|SXM4|SXM|NVL)$', '', gpu)
+        gpu = re.sub(r'(RTX\d0\d0)(S|D)$', r'\1', gpu)
+
+        if gpu in _map:
+            gpu = _map[gpu]
+
+        entry['AcceleratorName'] = gpu
+        entry['GpuInfo'] = json.dumps({
+            'Gpus': [{
+                'Name': gpu,
+                'Count': offer['num_gpus'],
+                'MemoryInfo': {
+                    'SizeInMiB': offer['gpu_total_ram']
+                }
+            }],
+            'TotalGpuMemoryInMiB': offer['gpu_total_ram']
+        }).replace('"', '\'')
+
+        writer.writerow(entry)
