8236882916b15a9b9668dd44094a8efd9e9858af
cblmemo <cblmemo@gmail.com>
2024-12-27 00:04:54 -0800

    remove unnecessary

--- sky/adaptors/runpod.py
+++ sky/adaptors/runpod.py
@@ -6,11 +6,3 @@ runpod = common.LazyImport(
     'runpod',
     import_error_message='Failed to import dependencies for RunPod. '
     'Try running: pip install "skypilot[runpod]"')
-
-_LAZY_MODULES = (runpod,)
-
-
-@common.load_lazy_modules(_LAZY_MODULES)
-def query_exception():
-    """Query exception."""
-    return runpod.error.QueryError
