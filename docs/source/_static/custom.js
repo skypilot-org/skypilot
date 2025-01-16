e71e5a92ccd90a654662121d6f08c4e100377bbf
Romil Bhardwaj <romil.bhardwaj@berkeley.edu>
2025-01-15 16:00:15 -0800

    [Docs] Enable kapa (#4567)
    Enable kapa

--- docs/source/_static/custom.js
+++ docs/source/_static/custom.js
@@ -1,17 +1,17 @@
-// As of 2025-01-01, Kapa seems to be having issues loading on some ISPs, including comcast. Uncomment once resolved.
-// document.addEventListener('DOMContentLoaded', function () {
-//        var script = document.createElement('script');
-//        script.src = 'https://widget.kapa.ai/kapa-widget.bundle.js';
-//        script.setAttribute('data-website-id', '4223d017-a3d2-4b92-b191-ea4d425a23c3');
-//        script.setAttribute('data-project-name', 'SkyPilot');
-//        script.setAttribute('data-project-color', '#4C4C4D');
-//        script.setAttribute('data-project-logo', 'https://avatars.githubusercontent.com/u/109387420?s=100&v=4');
-//        script.setAttribute('data-modal-disclaimer', 'Results are automatically generated and may be inaccurate or contain inappropriate information. Do not include any sensitive information in your query.\n**To get further assistance, you can chat directly with the development team** by joining the [SkyPilot Slack](https://slack.skypilot.co/).');
-//        script.setAttribute('data-modal-title', 'SkyPilot Docs AI - Ask a Question.');
-//        script.setAttribute('data-button-position-bottom', '100px');
-//        script.async = true;
-//        document.head.appendChild(script);
-// });
+document.addEventListener('DOMContentLoaded', function () {
+       var script = document.createElement('script');
+       script.src = 'https://widget.kapa.ai/kapa-widget.bundle.js';
+       script.setAttribute('data-website-id', '4223d017-a3d2-4b92-b191-ea4d425a23c3');
+       script.setAttribute('data-project-name', 'SkyPilot');
+       script.setAttribute('data-project-color', '#4C4C4D');
+       script.setAttribute('data-project-logo', 'https://avatars.githubusercontent.com/u/109387420?s=100&v=4');
+       script.setAttribute('data-modal-disclaimer', 'Results are automatically generated and may be inaccurate or contain inappropriate information. Do not include any sensitive information in your query.\n**To get further assistance, you can chat directly with the development team** by joining the [SkyPilot Slack](https://slack.skypilot.co/).');
+       script.setAttribute('data-modal-title', 'SkyPilot Docs AI - Ask a Question.');
+       script.setAttribute('data-button-position-bottom', '100px');
+       script.setAttribute('data-user-analytics-fingerprint-enabled', 'true');
+       script.async = true;
+       document.head.appendChild(script);
+});
 
 (function(h,o,t,j,a,r){
        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
