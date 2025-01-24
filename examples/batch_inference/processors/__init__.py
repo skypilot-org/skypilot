afee86d3eec249ef9095f11d9908155bc777cbf1
Eric Kaiyuan Chen <kych@berkeley.edu>
2025-01-23 18:42:22 -0800

    Remove batch processing CLIP script and related files

--- examples/batch_inference/processors/__init__.py
+++
@@ -1,9 +0,0 @@
-from .base import BatchProcessor
-from .datasets import HuggingFaceDatasetMixin
-from .inference import BatchInferenceProcessor
-
-__all__ = [
-    'BatchProcessor',
-    'BatchInferenceProcessor',
-    'HuggingFaceDatasetMixin',
-]
