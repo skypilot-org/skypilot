1a58bd80bb4c11227c949c75e288e1fc9ab91439
bend <contact@bend.works>
2025-02-04 09:31:39 +0100

    remove extra API call

---
+++ tests/unit_tests/test_lambda.py
@@ -0,0 +1,15 @@
+import pytest
+
+from sky.provision.lambda_cloud.instance import _get_private_ip
+
+
+def test_get_private_ip():
+    valid_info = {'private_ip': '10.19.83.125'}
+    invalid_info = {}
+    assert _get_private_ip(valid_info, single_node=True) == valid_info['private_ip']
+    assert _get_private_ip(valid_info, single_node=False) == valid_info['private_ip']
+    assert _get_private_ip(invalid_info, single_node=True) == '127.0.0.1'
+    with pytest.raises(RuntimeError):
+        _get_private_ip(invalid_info, single_node=False)
+
+
