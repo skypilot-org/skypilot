09eeaa08333bdb9faa57d149b26c84c12b005af8
DanielZhangQD <zhanghailong810@aliyun.com>
2025-02-20 20:19:24 +0800

    update format

--- tests/unit_tests/kubernetes/test_instance_type.py
+++ tests/unit_tests/kubernetes/test_instance_type.py
@@ -20,28 +20,36 @@ def test_kubernetes_instance_type():
     ]
 
     for cpus, memory, accelerator_count, accelerator_type, expected in test_cases:
-        instance_type = KubernetesInstanceType(cpus=cpus, memory=memory, accelerator_count=accelerator_count, accelerator_type=accelerator_type)
+        instance_type = KubernetesInstanceType(
+            cpus=cpus,
+            memory=memory,
+            accelerator_count=accelerator_count,
+            accelerator_type=accelerator_type)
 
         assert instance_type.name == expected, f'Failed name check for {cpus}, {memory}, {accelerator_count}, {accelerator_type}'
 
-        assert KubernetesInstanceType.is_valid_instance_type(instance_type.name), f'Failed valid instance type check for {cpus}, {memory}, {accelerator_count}, {accelerator_type}'
+        assert KubernetesInstanceType.is_valid_instance_type(
+            instance_type.name
+        ), f'Failed valid instance type check for {cpus}, {memory}, {accelerator_count}, {accelerator_type}'
 
-        cpus, memory, accelerator_count, accelerator_type = KubernetesInstanceType._parse_instance_type(instance_type.name)
+        cpus, memory, accelerator_count, accelerator_type = KubernetesInstanceType._parse_instance_type(
+            instance_type.name)
         assert cpus == cpus, f'Failed parse check for {cpus}, {memory}, {accelerator_count}, {accelerator_type}'
         assert memory == memory, f'Failed parse check for {cpus}, {memory}, {accelerator_count}, {accelerator_type}'
         assert accelerator_count == accelerator_count, f'Failed parse check for {cpus}, {memory}, {accelerator_count}, {accelerator_type}'
         assert accelerator_type == accelerator_type, f'Failed parse check for {cpus}, {memory}, {accelerator_count}, {accelerator_type}'
 
-        instance_type_from_name = KubernetesInstanceType.from_instance_type(instance_type.name)
+        instance_type_from_name = KubernetesInstanceType.from_instance_type(
+            instance_type.name)
         assert instance_type_from_name.cpus == cpus, f'Failed from instance type check for {cpus}, {memory}, {accelerator_count}, {accelerator_type}'
         assert instance_type_from_name.memory == memory, f'Failed from instance type check for {cpus}, {memory}, {accelerator_count}, {accelerator_type}'
         assert instance_type_from_name.accelerator_count == accelerator_count, f'Failed from instance type check for {cpus}, {memory}, {accelerator_count}, {accelerator_type}'
         assert instance_type_from_name.accelerator_type == accelerator_type, f'Failed from instance type check for {cpus}, {memory}, {accelerator_count}, {accelerator_type}'
 
         if accelerator_count is not None:
-            instance_type_from_resources = KubernetesInstanceType.from_resources(cpus, memory, accelerator_count, accelerator_type)
+            instance_type_from_resources = KubernetesInstanceType.from_resources(
+                cpus, memory, accelerator_count, accelerator_type)
             assert instance_type_from_resources.cpus == cpus, f'Failed from resources check for {cpus}, {memory}, {accelerator_count}, {accelerator_type}'
             assert instance_type_from_resources.memory == memory, f'Failed from resources check for {cpus}, {memory}, {accelerator_count}, {accelerator_type}'
             assert instance_type_from_resources.accelerator_count == accelerator_count, f'Failed from resources check for {cpus}, {memory}, {accelerator_count}, {accelerator_type}'
             assert instance_type_from_resources.accelerator_type == accelerator_type, f'Failed from resources check for {cpus}, {memory}, {accelerator_count}, {accelerator_type}'
-
