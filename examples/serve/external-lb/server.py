31cf4d2627249fdca1d3facbe3f856bca91c0de4
Tian Xia <cblmemo@gmail.com>
2025-02-26 10:45:39 -0800

    upd

--- examples/serve/external-lb/server.py
+++ examples/serve/external-lb/server.py
@@ -12,16 +12,20 @@ sentence = 'The quick brown fox jumps over the lazy dog'
 words = sentence.split()
 _cached_ip = None
 
+
 @app.get('/health')
 async def health_check():
     return {'status': 'healthy'}
+
 
 @app.get('/ip')
 async def get_ip():
     return {'ip': _cached_ip}
 
+
 @app.get('/stream')
 async def stream_response():
+
     async def generate():
         base_length = len(words)
         for i in range(sentence_length):
@@ -31,13 +35,21 @@ async def stream_response():
 
     return StreamingResponse(generate(), media_type='text/plain')
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='FastAPI Server')
-    parser.add_argument('--port', type=int, default=8080, help='Port to run the server on')
-    parser.add_argument('--length', type=int, default=5, help='Length of sentence to stream')
+    parser.add_argument('--port',
+                        type=int,
+                        default=8080,
+                        help='Port to run the server on')
+    parser.add_argument('--length',
+                        type=int,
+                        default=5,
+                        help='Length of sentence to stream')
     args = parser.parse_args()
 
     sentence_length = args.length
-    _cached_ip = subprocess.check_output('curl -s https://checkip.amazonaws.com', 
-                                         shell=True).decode('utf-8').strip()
+    _cached_ip = subprocess.check_output(
+        'curl -s https://checkip.amazonaws.com',
+        shell=True).decode('utf-8').strip()
     uvicorn.run(app, host='0.0.0.0', port=args.port)
