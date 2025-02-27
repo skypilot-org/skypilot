478b2a4f3027ea235c11a19d13be48b598e66c38
Romil Bhardwaj <romil.bhardwaj@gmail.com>
2025-02-26 16:05:01 -0800

    lint

--- sky/client/sdk.py
+++ sky/client/sdk.py
@@ -418,7 +418,8 @@ def launch(
         if not clusters:
             # Show the optimize log before the prompt if the cluster does not
             # exist.
-            request_id = optimize(dag, admin_policy_request_options=request_options)
+            request_id = optimize(dag,
+                                  admin_policy_request_options=request_options)
             stream_and_get(request_id)
         else:
             cluster_record = clusters[0]
