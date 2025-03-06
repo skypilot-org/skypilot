e6991724450dc87958880b0e5ac4c00fa48d737d
Wei Chen <wei@aqueducthq.com>
2025-03-06 00:16:38 +0000

    add runllm chat widget to skypilot's documentation page

--- docs/source/_static/custom.js
+++ docs/source/_static/custom.js
@@ -1,26 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
-       var script = document.createElement('script');
-       script.src = 'https://widget.kapa.ai/kapa-widget.bundle.js';
-       script.setAttribute('data-website-id', '4223d017-a3d2-4b92-b191-ea4d425a23c3');
-       script.setAttribute('data-project-name', 'SkyPilot');
-       script.setAttribute('data-project-color', '#4C4C4D');
-       script.setAttribute('data-project-logo', 'https://avatars.githubusercontent.com/u/109387420?s=100&v=4');
-       script.setAttribute('data-modal-disclaimer', 'Results are automatically generated and may be inaccurate or contain inappropriate information. Do not include any sensitive information in your query.\n**To get further assistance, you can chat directly with the development team** by joining the [SkyPilot Slack](https://slack.skypilot.co/).');
-       script.setAttribute('data-modal-title', 'SkyPilot Docs AI - Ask a Question.');
-       script.setAttribute('data-button-position-bottom', '100px');
-       script.setAttribute('data-user-analytics-fingerprint-enabled', 'true');
-       script.async = true;
-       document.head.appendChild(script);
+    var script = document.createElement('script');
+    script.src = 'https://widget.runllm.com';
+    script.setAttribute('id', 'runllm-widget');
+    script.setAttribute('runllm-assistant-id', '9');
+    script.setAttribute('runllm-name', 'SkyPilot');
+    script.setAttribute('runllm-theme-color', '#4C4C4D');
+    script.setAttribute('runllm-brand-logo', 'https://avatars.githubusercontent.com/u/109387420?s=100&v=4');
+    script.setAttribute('runllm-disclaimer', 'Results are automatically generated and may be inaccurate or contain inappropriate information. Do not include any sensitive information in your query.');
+    script.setAttribute('runllm-keyboard-shortcut', 'Mod+k')
+    script.setAttribute('runllm-community-type', 'slack')
+    script.setAttribute('runllm-community-url', 'https://slack.skypilot.co/')
+    script.setAttribute('runllm-position-y', '100px');
+    script.async = true;
+    document.head.appendChild(script);
 });
 
-(function(h,o,t,j,a,r){
-       h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-       h._hjSettings={hjid:3768396,hjsv:6};
-       a=o.getElementsByTagName('head')[0];
-       r=o.createElement('script');r.async=1;
-       r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-       a.appendChild(r);
-})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+(function (h, o, t, j, a, r) {
+    h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
+    h._hjSettings = { hjid: 3768396, hjsv: 6 };
+    a = o.getElementsByTagName('head')[0];
+    r = o.createElement('script'); r.async = 1;
+    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
 
 // New items: add 'new-item' class for for new items.
 document.addEventListener('DOMContentLoaded', () => {
