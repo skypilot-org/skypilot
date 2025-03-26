274be7afe55f0a83fec107d85ecdc77ba0fd8efd
Marcin Polonis <polonis.marcin@gmail.com>
2025-03-26 15:10:26 +0100

    Added changes to make the simplepod cloud visible.

--- sky/backends/cloud_vm_ray_backend.py
+++ sky/backends/cloud_vm_ray_backend.py
@@ -193,7 +193,8 @@ def _get_cluster_config_template(cloud):
         clouds.Vsphere: 'vsphere-ray.yml.j2',
         clouds.Vast: 'vast-ray.yml.j2',
         clouds.Fluidstack: 'fluidstack-ray.yml.j2',
-        clouds.Nebius: 'nebius-ray.yml.j2'
+        clouds.Nebius: 'nebius-ray.yml.j2',
+        clouds.Simplepod: 'simplepod-ray.yml.j2'
     }
     return cloud_to_template[type(cloud)]
 
