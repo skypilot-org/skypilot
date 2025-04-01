3f6e8f7616d4f4d3278bb47820db39d634d7282b
ZePing Guo <zp0int@qq.com>
2025-04-01 16:39:37 +0800

    update command

--- .buildkite/generate_pipeline.py
+++ .buildkite/generate_pipeline.py
@@ -242,7 +242,7 @@ def _generate_pipeline(test_file: str,
                 # Skip duplicate nested function tests under the same class
                 continue
             if 'PYTHON_VERSION' in os.environ:
-                command = f'source ~/buildkite-envs/python-{os.environ["PYTHON_VERSION"]}/bin/activate; {command}'
+                command = f'source ~/buildkite-envs/python-{os.environ["PYTHON_VERSION"]}/bin/activate && {command}'
             step = {
                 'label': label,
                 'command': command,
