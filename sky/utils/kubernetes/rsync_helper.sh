9707b5c4e844728aac3be904735247d73c746fa6
Seung Jin Yang <seungjin219@gmail.com>
2025-04-11 15:23:14 -0700

    merge common logic

--- sky/utils/kubernetes/rsync_helper.sh
+++ sky/utils/kubernetes/rsync_helper.sh
@@ -6,34 +6,29 @@ if [ "$1" = "-l" ]; then
     shift
     pod=$1
     shift
-    echo "pod: $pod" >&2
     encoded_namespace_context=$1
+    shift
+    echo "pod: $pod" >&2
     # Revert the encoded namespace+context to the original string.
     namespace_context=$(echo "$encoded_namespace_context" | sed 's|%40|@|g' | sed 's|%3A|:|g' | sed 's|%2B|+|g' | sed 's|%2F|/|g')
     echo "namespace_context: $namespace_context" >&2
-    namespace=$(echo $namespace_context | cut -d+ -f1)
-    echo "namespace: $namespace" >&2
-    context=$(echo $namespace_context | grep '+' >/dev/null && echo $namespace_context | cut -d+ -f2- || echo "")
-    echo "context: $context" >&2
-    context_lower=$(echo "$context" | tr '[:upper:]' '[:lower:]')
-    shift
 else
     # pod@namespace+context ...
     # used by openrsync
     encoded_pod_namespace_context=$1
+    shift
     pod_namespace_context=$(echo "$encoded_pod_namespace_context" | sed 's|%40|@|g' | sed 's|%3A|:|g' | sed 's|%2B|+|g' | sed 's|%2F|/|g')
     echo "pod_namespace_context: $pod_namespace_context" >&2
     pod=$(echo $pod_namespace_context | cut -d@ -f1)
     echo "pod: $pod" >&2
     namespace_context=$(echo $pod_namespace_context | cut -d@ -f2-)
     echo "namespace_context: $namespace_context" >&2
-    namespace=$(echo $namespace_context | cut -d+ -f1)
-    echo "namespace: $namespace" >&2
-    context=$(echo $namespace_context | grep '+' >/dev/null && echo $namespace_context | cut -d+ -f2- || echo "")
-    echo "context: $context" >&2
-    context_lower=$(echo "$context" | tr '[:upper:]' '[:lower:]')
-    shift
 fi
+namespace=$(echo $namespace_context | cut -d+ -f1)
+echo "namespace: $namespace" >&2
+context=$(echo $namespace_context | grep '+' >/dev/null && echo $namespace_context | cut -d+ -f2- || echo "")
+echo "context: $context" >&2
+context_lower=$(echo "$context" | tr '[:upper:]' '[:lower:]')
 
 if [ -z "$context" ] || [ "$context_lower" = "none" ]; then
     # If context is none, it means we are using incluster auth. In this case,
