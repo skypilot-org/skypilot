454d460a7a33eda429fc8214860f97b5b0c96bb9
DanielZhang <zhanghailong810@aliyun.com>
2025-04-11 16:17:58 +0800

    update font color

--- sky/dashboard/src/components/ui/table.jsx
+++ sky/dashboard/src/components/ui/table.jsx
@@ -55,7 +55,7 @@ const TableHead = React.forwardRef(({ className, ...props }, ref) => (
   <th
     ref={ref}
     className={cn(
-      'h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0',
+      'h-12 px-4 text-left align-middle font-medium text-[hsl(var(--text-strong))] [&:has([role=checkbox])]:pr-0',
       className
     )}
     {...props}
