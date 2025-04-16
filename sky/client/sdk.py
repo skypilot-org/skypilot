2d444b8befc0a086146e7b7fae46ccc631e7b67c
Seung Jin Yang <seungjin219@gmail.com>
2025-04-16 11:11:38 -0700

    api login mutates global config only

--- sky/client/sdk.py
+++ sky/client/sdk.py
@@ -1819,8 +1819,8 @@ def api_login(endpoint: Optional[str] = None) -> None:
             config_path.touch()
             config = {'api_server': {'endpoint': endpoint}}
         else:
-            config = skypilot_config.set_nested(('api_server', 'endpoint'),
-                                                endpoint)
+            config = skypilot_config.get_user_config()
+            config.set_nested(('api_server', 'endpoint'), endpoint)
         common_utils.dump_yaml(str(config_path), config)
         click.secho(f'Logged in to SkyPilot API server at {endpoint}',
                     fg='green')
