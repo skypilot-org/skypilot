be571e771ef23772968ff177218b126efaefe792
Aylei <rayingecho@gmail.com>
2025-04-18 10:55:39 +0800

    Refine backward compat test on API version change
    Signed-off-by: Aylei <rayingecho@gmail.com>

--- tests/smoke_tests/test_backward_compat.py
+++ tests/smoke_tests/test_backward_compat.py
@@ -338,7 +338,7 @@ class TestBackwardCompatibility:
             f'{self.ACTIVATE_BASE} && {self.SKY_API_RESTART} && '
             f'{self.ACTIVATE_CURRENT} && result="$(sky status 2>&1)" || true; '
             'if echo "$result" | grep -q "SkyPilot API server is too old"; then '
-            '  echo "$result" && exit 1; '
+            '  echo "$result" && echo "API version bumped, skip compatibility test" && exit 0; '
             'fi',
             # managed job test
             f'{self.ACTIVATE_BASE} && {self.SKY_API_RESTART} && '
