ba957517408722e521f295e476c4c210f5f88b88
Vedran Vidović <vvidovic@croz.net>
2025-04-22 14:16:59 +0200

    Fixed `test_cli.py` formatting

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -11,7 +11,8 @@ from sky import exceptions
 from sky import server
 
 CLOUDS_TO_TEST = [
-    'aws', 'gcp', 'ibm', 'azure', 'lambda', 'scp', 'oci', 'vsphere', 'nebius', 'hyperstack'
+    'aws', 'gcp', 'ibm', 'azure', 'lambda', 'scp', 'oci', 'vsphere', 'nebius',
+    'hyperstack'
 ]
 
 
