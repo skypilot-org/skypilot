f3ef65a2b51405f947d30edc8bf0dbf1446e3ff5
Bohdan Kovalevskyi <kovsbo@gmail.com>
2025-04-23 11:07:47 -0700

    do not delete region

--- sky/provision/runpod/config.py
+++ sky/provision/runpod/config.py
@@ -4,7 +4,8 @@ from sky.provision import common
 
 
 def bootstrap_instances(
-        _region: str, cluster_name: str,
+        region: str,  # noqa: unused-argument
+        cluster_name: str,
         config: common.ProvisionConfig) -> common.ProvisionConfig:
     """Bootstraps instances for the given cluster."""
     del cluster_name  # unused
