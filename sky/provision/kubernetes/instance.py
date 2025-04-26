a83f5c175def8d13ed2b1b02411bc5b5e0863972
Romil Bhardwaj <romil.bhardwaj@gmail.com>
2025-04-25 17:14:48 -0700

    simulate --force

--- sky/provision/kubernetes/instance.py
+++ sky/provision/kubernetes/instance.py
@@ -936,14 +936,13 @@ def _terminate_node(namespace: str, context: Optional[str],
     # This is to ensure there are no leftover resources if this down is run
     # from within the pod, e.g., for autodown.
     # Note - some misbehaving pods may not terminate gracefully if they have
-    # open file descriptors. In this case, we force delete the pod after
-    # grace_period_seconds.
+    # open file descriptors. We force delete pods to avoid this.
     _delete_k8s_resource_with_retry(
         delete_func=lambda: kubernetes.core_api(context).delete_namespaced_pod(
             name=pod_name,
             namespace=namespace,
             _request_timeout=config_lib.DELETION_TIMEOUT,
-            grace_period_seconds=10),
+            grace_period_seconds=0),
         resource_type='pod',
         resource_name=pod_name)
 
