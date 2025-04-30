cbdc9d405dd26ef4b938e9bd4a2f7611f2855f9c
ZePing Guo <zp0int@qq.com>
2025-04-30 21:14:22 +0800

    fix flaky of test_cancel_launch_and_exec_async

--- tests/smoke_tests/test_basic.py
+++ tests/smoke_tests/test_basic.py
@@ -826,22 +826,24 @@ def test_launch_and_exec_async(generic_cloud: str):
 def test_cancel_launch_and_exec_async(generic_cloud: str):
     """Test if async launch and exec commands work correctly when cluster is shutdown"""
     name = smoke_tests_utils.get_cluster_name()
-    test = smoke_tests_utils.Test('cancel_launch_and_exec_async', [
-        f'sky launch -c {name} -y --cloud {generic_cloud} --async',
-        (f's=$(sky exec {name} echo --async) && '
-         'echo "$s" && '
-         'logs_cmd=$(echo "$s" | grep "Check logs with" | '
-         'sed -E "s/.*with: (sky api logs .*).*/\\1/") && '
-         'echo "Extracted logs command: $logs_cmd" && '
-         f'{smoke_tests_utils.get_cmd_wait_until_cluster_status_contains(name, [sky.ClusterStatus.INIT, sky.ClusterStatus.UP], 30)} && '
-         f'sky down -y {name} && '
-         'log_output=$(eval $logs_cmd || true) && '
-         'echo "===logs===" && echo "$log_output" && '
-         'echo "$log_output" | grep "cancelled"'),
-    ],
-                                  teardown=f'sky down -y {name}',
-                                  timeout=smoke_tests_utils.get_timeout(
-                                      generic_cloud))
+    test = smoke_tests_utils.Test(
+        'cancel_launch_and_exec_async',
+        [
+            f'sky launch -c {name} -y --cloud {generic_cloud} --async',
+            (
+                f's=$(sky exec {name} echo --async) && '
+                'echo "$s" && '
+                'logs_cmd=$(echo "$s" | grep "Check logs with" | '
+                'sed -E "s/.*with: (sky api logs .*).*/\\1/") && '
+                'echo "Extracted logs command: $logs_cmd" && '
+                # sky down as soon as possible, only in INIT state will the job be cancelled
+                f'sky down -y {name} && '
+                'log_output=$(eval $logs_cmd || true) && '
+                'echo "===logs===" && echo "$log_output" && '
+                'echo "$log_output" | grep "cancelled"'),
+        ],
+        teardown=f'sky down -y {name}',
+        timeout=smoke_tests_utils.get_timeout(generic_cloud))
     smoke_tests_utils.run_one_test(test)
 
 
