daaf92458b14124b5a512e71054394bc82e47e8a
JiangJiaWei1103 <waynechuang97@gmail.com>
2025-05-03 13:52:32 +0800

    feat: Add cloud skeleton for the Slurm cluster
    Signed-off-by: JiangJiaWei1103 <waynechuang97@gmail.com>

--- sky/clouds/service_catalog/constants.py
+++ sky/clouds/service_catalog/constants.py
@@ -2,6 +2,6 @@
 HOSTED_CATALOG_DIR_URL = 'https://raw.githubusercontent.com/skypilot-org/skypilot-catalog/master/catalogs'  # pylint: disable=line-too-long
 CATALOG_SCHEMA_VERSION = 'v7'
 CATALOG_DIR = '~/.sky/catalogs'
-ALL_CLOUDS = ('aws', 'azure', 'gcp', 'ibm', 'lambda', 'scp', 'oci',
+ALL_CLOUDS = ('aws', 'azure', 'gcp', 'ibm', 'lambda', 'scp', 'slurm', 'oci',
               'kubernetes', 'runpod', 'vast', 'vsphere', 'cudo', 'fluidstack',
               'paperspace', 'do', 'nebius')
