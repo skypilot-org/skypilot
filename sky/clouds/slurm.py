b8c7843da27811345fda0b0e7c1e33a78f87816e
Zhanghao Wu <zhanghao.wu@outlook.com>
2025-05-03 14:44:04 -0700

    fix test

--- sky/clouds/slurm.py
+++ sky/clouds/slurm.py
@@ -1,10 +1,14 @@
 """Kubernetes."""
 import subprocess
-from typing import Optional, Tuple
+import typing
+from typing import Dict, Optional, Tuple
 
 from sky import clouds
 from sky import sky_logging
 from sky.utils import registry
+
+if typing.TYPE_CHECKING:
+    from sky import resources as resources_lib
 
 logger = sky_logging.init_logger(__name__)
 
@@ -28,6 +32,16 @@ class Slurm(clouds.Cloud):
             return (True, None)
         except FileNotFoundError:
             return (False, 'Slurm command not found. To check, run: sinfo')
+
+    @classmethod
+    def _unsupported_features_for_resources(
+        cls, resources: 'resources_lib.Resources'
+    ) -> Dict[clouds.CloudImplementationFeatures, str]:
+        del resources  # Unused.
+        return {
+            feature: f'{cls._REPR} only supports show-gpus for now.'
+            for feature in clouds.CloudImplementationFeatures
+        }
 
     def get_credential_file_mounts(self) -> registry.Dict[str, str]:
         return {}
