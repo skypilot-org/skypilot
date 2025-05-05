301a693d0d7065384619a4ba30c0ca33042ae741
Zhanghao Wu <zhanghao.wu@outlook.com>
2025-05-04 20:13:35 -0700

    fix run gpu

--- examples/slurm_cloud_deploy/run-gpu.sh
+++ examples/slurm_cloud_deploy/run-gpu.sh
@@ -3,8 +3,7 @@
 #SBATCH --nodes=2                    # nodes
 #SBATCH --ntasks-per-node=1          # crucial - only 1 task per dist per node!
 #SBATCH --cpus-per-task=4
-#SBATCH --gres=gpu:A10G:2     # match the exact GPU type from scontrol
-#SBATCH --time 0:05:00               # maximum execution time (HH:MM:SS)
+#SBATCH --gres=gpu:L4:1     # match the exact GPU type from scontrol
 #SBATCH --output=%x-%j.out           # output file name
 #SBATCH --partition=gpu              # use the gpu partition
 
@@ -14,4 +13,4 @@ srun --jobid $SLURM_JOBID bash -c 'echo "=== $(hostname) ===" && nvidia-smi'
 # Print visible GPU devices
 srun --jobid $SLURM_JOBID bash -c 'echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"'
 
-sleep 100
+sleep 10000
