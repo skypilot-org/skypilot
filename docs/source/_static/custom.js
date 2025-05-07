d4b59859b386a845cd4f41386c43ff41fd0211a2
Andy Lee <andylizf@outlook.com>
2025-05-07 15:44:23 +0000

    revert: custom.js format

--- docs/source/_static/custom.js
+++ docs/source/_static/custom.js
@@ -13,14 +13,14 @@ document.addEventListener('DOMContentLoaded', function () {
     document.head.appendChild(script);
 });
 
-(function (h, o, t, j, a, r) {
-    h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
-    h._hjSettings = { hjid: 3768396, hjsv: 6 };
-    a = o.getElementsByTagName('head')[0];
-    r = o.createElement('script'); r.async = 1;
-    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:3768396,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
     a.appendChild(r);
-})(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 
 // New items: add 'new-item' class for for new items.
 document.addEventListener('DOMContentLoaded', () => {
