aa2f9f8beab5414ceac41c6ce2d2968f12cfc1ac
Kihyo Moon <36657450+hyoxt121@users.noreply.github.com>
2025-05-08 09:13:24 +0900

    Update __init__.py

--- sky/provision/__init__.py
+++ sky/provision/__init__.py
@@ -23,6 +23,7 @@ from sky.provision import lambda_cloud
 from sky.provision import nebius
 from sky.provision import oci
 from sky.provision import runpod
+from sky.provision import scp
 from sky.provision import vast
 from sky.provision import vsphere
 from sky.utils import command_runner
