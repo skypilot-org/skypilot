1e19a342608a1d3e13d05f258c52c746dc9a6800
ZePing Guo <zp0int@qq.com>
2025-05-08 17:05:25 +0800

    more fix

--- sky/clouds/nebius.py
+++ sky/clouds/nebius.py
@@ -195,8 +195,10 @@ class Nebius(clouds.Cloud):
         del dryrun, cluster_name
         assert zones is None, ('Nebius does not support zones', zones)
 
-        r = resources
-        acc_dict = self.get_accelerators_from_instance_type(r.instance_type)
+        assert resources.instance_type is not None, \
+            'Instance type must be specified'
+        acc_dict = self.get_accelerators_from_instance_type(
+            resources.instance_type)
         custom_resources = resources_utils.make_ray_custom_resources_str(
             acc_dict)
         platform, _ = resources.instance_type.split('_')
