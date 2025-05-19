9002c524c37ec515163df8b0f532e4eaa52dae19
ZePing Guo <zp0int@qq.com>
2025-05-19 14:33:53 +0800

    fix test

--- tests/test_failover.py
+++ tests/test_failover.py
@@ -5,14 +5,15 @@ import uuid
 import boto3
 import botocore.exceptions
 import moto
+import pandas as pd
 import pytest
 
 import sky
 from sky import global_user_state
 from sky import sky_logging
 from sky.backends import cloud_vm_ray_backend
+from sky.clouds.service_catalog import aws_catalog
 from sky.provision.aws import instance as aws_instance
-from sky.server import config as server_config
 from sky.utils import db_utils
 from sky.utils import env_options
 
@@ -32,6 +33,11 @@ def test_aws_region_failover(enable_all_clouds, _mock_db_conn, mock_aws_backend,
     monkeypatch.setattr(env_options.Options.SHOW_DEBUG_INFO, 'get',
                         lambda: True)
     sky_logging.reload_logger()
+
+    # Ensure AWS catalog dataframes are initialized before mock_aws
+    _ = aws_catalog._default_df._load_df()
+    _ = aws_catalog._image_df._load_df()
+    _ = aws_catalog._quotas_df._load_df()
 
     region_attempt_count = {'count': 0}
 
