89418d40d841ef5bd81aa22207ffead47190c295
Kihyo Moon <36657450+hyoxt121@users.noreply.github.com>
2025-05-19 09:36:33 +0900

    refactoring

--- sky/provision/scp/__init__.py
+++ sky/provision/scp/__init__.py
@@ -10,6 +10,6 @@ from sky.provision.scp.instance import stop_instances
 from sky.provision.scp.instance import terminate_instances
 from sky.provision.scp.instance import wait_instances
 
-__all__ = ('bootstrap_instances', 'run_instances', 'stop_instances',
-           'terminate_instances', 'wait_instances', 'get_cluster_info',
-           'open_ports', 'cleanup_ports', 'query_instances')
+__all__ = ('bootstrap_instances', 'cleanup_ports', 'get_cluster_info',
+           'open_ports', 'query_instances', 'run_instances', 'stop_instances',
+           'terminate_instances', 'wait_instances')
