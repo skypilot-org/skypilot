ceedf5638ed8af22c31bac874287482e2d7d8e9e
Luca Manolache <luca1230@gmail.com>
2025-06-12 17:42:13 -0700

    [UX] Increased robustness for rsync when estimating size of a folder (#5956)

--- sky/backends/backend_utils.py
+++ sky/backends/backend_utils.py
@@ -341,7 +341,13 @@ def path_size_megabytes(path: str) -> int:
                      f'{git_exclude_filter} --dry-run {path!r}')
     rsync_output = ''
     try:
-        rsync_output = str(subprocess.check_output(rsync_command, shell=True))
+        # rsync sometimes fails `--dry-run` for MacOS' rsync build, however this function is only used to display
+        # a warning message to the user if the size of a file/directory is too
+        # large, so we can safely ignore the error.
+        rsync_output = str(
+            subprocess.check_output(rsync_command,
+                                    shell=True,
+                                    stderr=subprocess.DEVNULL))
     except subprocess.CalledProcessError:
         logger.debug('Command failed, proceeding without estimating size: '
                      f'{rsync_command}')
