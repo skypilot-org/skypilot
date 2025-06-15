1d079fe92827a94fab4e01c9c7a5a4a0484158e8
Luca Manolache <luca1230@gmail.com>
2025-06-14 22:47:11 -0700

    Bump API version

--- sky/server/constants.py
+++ sky/server/constants.py
@@ -7,16 +7,16 @@ from sky.skylet import constants
 # API server version, whenever there is a change in API server that requires a
 # restart of the local API server or error out when the client does not match
 # the server version.
-API_VERSION = '9'
+API_VERSION = "10"
 
 # Prefix for API request names.
-REQUEST_NAME_PREFIX = 'sky.'
+REQUEST_NAME_PREFIX = "sky."
 # The memory (GB) that SkyPilot tries to not use to prevent OOM.
 MIN_AVAIL_MEM_GB = 2
 # Default encoder/decoder handler name.
-DEFAULT_HANDLER_NAME = 'default'
+DEFAULT_HANDLER_NAME = "default"
 # The path to the API request database.
-API_SERVER_REQUEST_DB_PATH = '~/.sky/api_server/requests.db'
+API_SERVER_REQUEST_DB_PATH = "~/.sky/api_server/requests.db"
 
 # The interval (seconds) for the cluster status to be refreshed in the
 # background.
@@ -24,11 +24,10 @@ CLUSTER_REFRESH_DAEMON_INTERVAL_SECONDS = 60
 
 # Environment variable for a file path to the API cookie file.
 # Keep in sync with websocket_proxy.py
-API_COOKIE_FILE_ENV_VAR = f'{constants.SKYPILOT_ENV_VAR_PREFIX}API_COOKIE_FILE'
+API_COOKIE_FILE_ENV_VAR = f"{constants.SKYPILOT_ENV_VAR_PREFIX}API_COOKIE_FILE"
 # Default file if unset.
 # Keep in sync with websocket_proxy.py
-API_COOKIE_FILE_DEFAULT_LOCATION = '~/.sky/cookies.txt'
+API_COOKIE_FILE_DEFAULT_LOCATION = "~/.sky/cookies.txt"
 
 # The path to the dashboard build output
-DASHBOARD_DIR = os.path.join(os.path.dirname(__file__), '..', 'dashboard',
-                             'out')
+DASHBOARD_DIR = os.path.join(os.path.dirname(__file__), "..", "dashboard", "out")
