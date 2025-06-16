691af63ecb93e856f6eaaf3f7a972543d01df3c6
ZePing Guo <zp0int@qq.com>
2025-06-16 17:15:44 +0800

    no resource heavy

--- tests/smoke_tests/test_sky_serve.py
+++ tests/smoke_tests/test_sky_serve.py
@@ -301,7 +301,6 @@ def test_skyserve_azure_http():
 
 @pytest.mark.kubernetes
 @pytest.mark.serve
-@pytest.mark.resource_heavy
 def test_skyserve_kubernetes_http():
     """Test skyserve on Kubernetes"""
     name = _get_service_name()
