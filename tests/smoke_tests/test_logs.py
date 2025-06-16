b45525355912eb09f3f19f0a3d6cf0e4f0eb989a
Aylei <rayingecho@gmail.com>
2025-06-16 22:57:04 +0800

    Fix
    Signed-off-by: Aylei <rayingecho@gmail.com>

--- tests/smoke_tests/test_logs.py
+++ tests/smoke_tests/test_logs.py
@@ -48,21 +48,21 @@ def test_log_collection_to_gcp(generic_cloud: str):
                     base.name),
                 f'sky logs {name} 1',
                 # Wait for the logs to be available in the GCP Cloud Logging.
-                'sleep 60',
+                'sleep 10',
                 # Use grep instead of jq to avoid the dependency on jq.
                 (f'output=$(gcloud logging read \'labels.skypilot_cluster_name={name} AND timestamp>="{one_hour_ago}"\' --order=asc --format=json | grep \'"log":\') && '
                  f'{validate_logs_cmd}'),
                 smoke_tests_utils.with_config(
                     f'sky jobs launch -y -n {name}-job --infra {generic_cloud} {smoke_tests_utils.LOW_RESOURCE_ARG} \'{logs_cmd}\'',
                     base.name),
-                'sleep 60',
+                'sleep 10',
                 (f'output=$(gcloud logging read \'jsonPayload.log_path:{name}-job AND timestamp>="{one_hour_ago}"\' --order=asc --format=json | grep \'"log":\') && '
                  f'{validate_logs_cmd}'),
                 f'sky down -y {name}',
                 smoke_tests_utils.with_config(
                     f'sky launch -y -c {name} --infra {generic_cloud} {smoke_tests_utils.LOW_RESOURCE_ARG} \'{logs_cmd}\'',
                     additional_labels.name),
-                'sleep 60',
+                'sleep 10',
                 (f'output=$(gcloud logging read \'labels.skypilot_smoke_test_case={name}-case AND timestamp>="{one_hour_ago}"\' --order=asc --format=json | grep \'"log":\') && '
                  f'{validate_logs_cmd}'),
             ],
