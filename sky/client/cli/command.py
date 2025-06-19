440674259c0005538a6ec257f78ea3dc44366bb5
Zhanghao Wu <zhanghao.wu@outlook.com>
2025-06-19 05:57:33 +0000

    revert cli

--- sky/client/cli/command.py
+++ sky/client/cli/command.py
@@ -1831,9 +1831,7 @@ def cost_report(all: bool):  # pylint: disable=redefined-builtin
             controller_name = controller.value.name
             # to display most recent entry for each controller cluster
             # TODO(sgurram): fix assumption of sorted order of clusters
-            print(controller_name, cluster_record['status'])
-            if (controller_name not in controllers and
-                    cluster_record['status'] is not None):
+            if controller_name not in controllers:
                 controllers[controller_name] = cluster_record
         else:
             normal_cluster_records.append(cluster_record)
