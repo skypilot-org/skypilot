6023694d39fa4447d6b90866c404f7c81892a8fc
Aylei <rayingecho@gmail.com>
2025-06-24 17:19:35 +0800

    Update policy server example
    Signed-off-by: Aylei <rayingecho@gmail.com>

--- examples/admin_policy/example_server/policy_server.py
+++ examples/admin_policy/example_server/policy_server.py
@@ -15,17 +15,28 @@ import sky
 app = FastAPI(title="Example Admin Policy Server", version="1.0.0")
 
 
+class DoNothingPolicy(sky.AdminPolicy):
+    """Example policy: do nothing."""
+
+    @classmethod
+    def validate_and_mutate(
+            cls, user_request: sky.UserRequest) -> sky.MutatedUserRequest:
+        """Returns the user request unchanged."""
+        return sky.MutatedUserRequest(user_request.task,
+                                      user_request.skypilot_config)
+
+
 @app.post('/')
 async def apply_policy(request: Request) -> JSONResponse:
     """Apply an admin policy loaded from external package to a user request"""
-    # Decode from request body
+    # Decode
     json_data = await request.json()
     user_request = sky.UserRequest.decode(json_data)
-    # Change the following list to apply one or more policies to the user
-    # request. You can also implement custom policies in server in addition to
-    # loading policies from external packages.
+    # Example: change the following list to apply different policies.
     policies: List[sky.AdminPolicy] = [
-        example_policy.SetMaxAutostopIdleMinutesPolicy,
+        # Example: policy that implemented in the server package.
+        DoNothingPolicy,
+        # Example: policy from third party packages.
         example_policy.UseSpotForGpuPolicy,
     ]
     try:
