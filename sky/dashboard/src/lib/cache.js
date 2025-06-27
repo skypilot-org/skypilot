85a92894de1c8328393467b23c19e3750a49639c
DanielZhang <zhanghailong810@aliyun.com>
2025-06-27 20:41:04 +0800

    format code

--- sky/dashboard/src/lib/cache.js
+++ sky/dashboard/src/lib/cache.js
@@ -11,7 +11,7 @@ const DEFAULT_CACHE_TTL = CACHE_CONFIG.DEFAULT_TTL;
 function simpleHash(str) {
   let hash = 5381;
   for (let i = 0; i < str.length; i++) {
-    hash = ((hash << 5) + hash) + str.charCodeAt(i);
+    hash = (hash << 5) + hash + str.charCodeAt(i);
   }
   return hash >>> 0;
 }
