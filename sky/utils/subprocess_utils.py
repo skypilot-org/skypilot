a033aac2eda63bf09387749800bc38d191995cdf
Zhanghao Wu <zhanghao.wu@outlook.com>
2025-06-27 11:26:23 -0700

    use current python path for daemon process

--- sky/utils/subprocess_utils.py
+++ sky/utils/subprocess_utils.py
@@ -6,6 +6,7 @@ import random
 import resource
 import shlex
 import subprocess
+import sys
 import threading
 import time
 import typing
@@ -322,12 +323,8 @@ def kill_process_daemon(process_pid: int) -> None:
     daemon_script = os.path.join(
         os.path.dirname(os.path.abspath(log_lib.__file__)),
         'subprocess_daemon.py')
-    python_path = subprocess.check_output(constants.SKY_GET_PYTHON_PATH_CMD,
-                                          shell=True,
-                                          stderr=subprocess.DEVNULL,
-                                          encoding='utf-8').strip()
     daemon_cmd = [
-        python_path,
+        sys.executable,
         daemon_script,
         '--parent-pid',
         str(parent_pid),
