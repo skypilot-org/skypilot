c583fe7426e318c3af1b08cd6092e69b8d3e3fe1
cblmemo <cblmemo@gmail.com>
2025-06-30 11:12:09 -0700

    lint

--- sky/jobs/controller.py
+++ sky/jobs/controller.py
@@ -581,8 +581,9 @@ def _cleanup(job_id: int, dag_yaml: str):
         when reaching here, as we currently only support chain DAGs, and only
         task is executed at a time.
     """
-    # Cleanup the HA recovery script first as it is possible that some error was raised when we
-    # construct the task object (e.g. sky.exceptions.ResourcesUnavailableError).
+    # Cleanup the HA recovery script first as it is possible that some error
+    # was raised when we construct the task object (e.g.,
+    # sky.exceptions.ResourcesUnavailableError).
     managed_job_state.remove_ha_recovery_script(job_id)
     dag, _ = _get_dag_and_name(dag_yaml)
     for task in dag.tasks:
