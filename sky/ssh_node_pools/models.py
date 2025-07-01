8fffe0c5fbe61f24758a1c01bc1676b2800cfd22
kyuds <kyuseung1016@gmail.com>
2025-07-01 10:15:17 +0900

    update orm models to use a single table

--- sky/ssh_node_pools/models.py
+++ sky/ssh_node_pools/models.py
@@ -1,7 +1,22 @@
 """Data Models for SSH Node Pools."""
 
 import dataclasses
+from enum import Enum
 from typing import Any, Dict, List, Optional
+
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy_json import mutable_json_type
+
+SSHBase = declarative_base()
+
+class SSHClusterStatus(Enum):
+    PENDING = 'pending'
+    STARTING = 'starting'
+    UPDATE = 'update'
+    ACTIVE = 'active'
+    TERMINATING = 'terminating'
 
 
 @dataclasses.dataclass
@@ -14,7 +29,7 @@ class SSHNode:
     use_ssh_config: bool
 
     def __repr__(self):
-        return f'<SSHNode(ip={self.ip})>'
+        return (f'<SSHNode(ip={self.ip})>')
 
     def to_dict(self) -> Dict[str, Any]:
         """Convert to dictionary for JSON storage"""
@@ -38,65 +53,112 @@ class SSHNode:
         )
 
     def __hash__(self):
-        return hash((self.ip, self.user, self.identity_file, self.password,
-                     self.use_ssh_config))
+        return hash((self.ip, self.user, self.identity_file, 
+                     self.password, self.use_ssh_config))
 
 
 @dataclasses.dataclass
-class SSHCluster:
+class SSHCluster(SSHBase):  # type: ignore[valid-type, misc]
     """SSH Node Pool"""
-    name: str  # pk
-    head_node_ip: str
-    alias: Optional[str] = None
-    _node_json: List[Dict[str, Any]] = dataclasses.field(default_factory=list)
+    __tablename__ = 'ssh_clusters'
+
+    name: Mapped[str] = mapped_column(sqlalchemy.Text, primary_key=True)
+    status: Mapped[SSHClusterStatus] = mapped_column(
+        sqlalchemy.Enum(
+            SSHClusterStatus, 
+            name='cluster_status_enum', 
+            native_enum=False),
+        nullable=False,
+        default=SSHClusterStatus.PENDING)
+    _head_node_ip: Mapped[Optional[str]] = mapped_column(
+        sqlalchemy.Text, nullable=True)
+
+    _current_state: Mapped[Optional[List[Dict[str, Any]]]] = mapped_column(
+        mutable_json_type(dbtype=sqlalchemy.JSON, nested=True),
+        nullable=True)
+    _update_state: Mapped[Optional[List[Dict[str, Any]]]] = mapped_column(
+        mutable_json_type(dbtype=sqlalchemy.JSON, nested=True),
+        nullable=True)
 
     @property
-    def num_nodes(self):
-        return len(self._node_json)
+    def current_nodes(self) -> List[SSHNode]:
+        return self._get_nodes(True)
 
     @property
-    def nodes(self) -> List['SSHNode']:
-        return [SSHNode.from_dict(j) for j in self._node_json]
+    def update_nodes(self) -> List[SSHNode]:
+        return self._get_nodes(False)
 
     @property
-    def node_json(self) -> List[Dict[str, Any]]:
-        return self._node_json
+    def num_current_nodes(self) -> int:
+        return len(self.current_nodes)
 
-    def set_nodes(self, nodes: List[SSHNode]):
-        self._node_json = []
-        found_head = False
+    @property
+    def num_update_nodes(self) -> int:
+        return len(self.update_nodes)
+    
+    @property
+    def head_node_ip(self) -> Optional[str]:
+        return self._head_node_ip
+
+    def __repr__(self):
+        return f'<SSHCluster(name={self.name})>'
+
+    def _get_nodes(self, current: bool) -> List[SSHNode]:
+        state = self._current_state if current else self._update_state
+        state_name = 'current' if current else 'update'
+        if state is None:
+            return []
+        if self.head_node_ip is None:
+            raise ValueError(f'SSHCluster {state_name} state has '
+                             f'null head node ip {self.name}')
+        found_head, nodes = False, []
+        for j in state:
+            node = SSHNode.from_dict(j)
+            nodes.append(node)
+            if node.ip == self.head_node_ip:
+                found_head = True
+        if not found_head:
+            raise ValueError(f'SSHCluster {self.name} {state_name} state '
+                             'does not have configured head node ip '
+                             f'{self.head_node_ip}')
+        return nodes
+    
+    def set_head_node_ip(self, ip: str):
+        """Set head node ip for SSHCluster. Note that head node ip is
+        immutable, so we can only set it once."""
+        if self.head_node_ip is not None and self.head_node_ip != ip:
+            raise ValueError('Attempting to override head node ip '
+                             f'{self._head_node_ip} on SSHCluster '
+                             f'{self.name} to {ip}')
+        self._head_node_ip = ip
+
+    def commit(self):
+        """After update is applied, remove previous configurations
+        and update current state"""
+        self._current_state = self._update_state
+        self._update_state = None
+
+    def set_update_nodes(self, nodes: List['SSHNode']):
+        """Set SSHCluster with update configurations. Note that current
+        state can only be set using the `commit` function, which transfers
+        the update state to the current state."""
+        if self.head_node_ip is None:
+            raise ValueError('Attempting to set update node configurations '
+                             f'on SSHCluster {self.name} without specifying '
+                             'head node')
+        self._update_state, found_head = [], False
         for node in nodes:
             if node.ip == self.head_node_ip:
                 found_head = True
-            self._node_json.append(node.to_dict())
+            self._update_state.append(node.to_dict())
         if not found_head:
-            raise ValueError('updating nodes list for ssh cluster '
-                             f'<{self.get_detailed_name()}> '
-                             'that doesn\'t include head node '
-                             f'{self.head_node_ip}')
+            raise ValueError(f'Update nodes list for SSHCluster {self.name} '
+                             f'does not include head node {self.head_node_ip}')
 
-    def __repr__(self):
-        return (f'<SSHCluster(name={self.name}, '
-                f'num_nodes={self.num_nodes}, '
-                f'head_node={self.head_node_ip})>')
+    def get_current_ips(self) -> List[str]:
+        """Get ip addresses for current SSH nodes"""
+        return [node.ip for node in self.current_nodes]
 
-    def get_detailed_name(self):
-        if self.alias is None:
-            return self.name
-        return f'{self.name}({self.alias})'
-
-    def get_head_node(self) -> SSHNode:
-        for node in self.nodes:
-            if node.ip == self.head_node_ip:
-                return node
-        raise ValueError(f'head node not found for {self.name}')
-
-    def get_worker_nodes(self) -> List[SSHNode]:
-        workers = []
-        for node in self.nodes:
-            if node.ip != self.head_node_ip:
-                workers.append(node)
-        return workers
-
-    def get_ips(self) -> List[str]:
-        return [node.ip for node in self.nodes]
+    def get_update_ips(self) -> List[str]:
+        """Get ip addresses for updating SSH nodes"""
+        return [node.ip for node in self.update_nodes]
--- sky/ssh_node_pools/state.py
+++ sky/ssh_node_pools/state.py
@@ -1,19 +1,13 @@
 """The database for ssh node pool information."""
-from abc import ABC
-from abc import ABCMeta
-from abc import abstractmethod
 import functools
 import os
 from pathlib import Path
 import threading
-from typing import List, Optional, Type, TypeVar
+from typing import List, Optional
 
 import sqlalchemy
 from sqlalchemy import exc as sqlalchemy_exc
 from sqlalchemy import orm
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.ext.declarative import DeclarativeMeta
-from sqlalchemy_json import mutable_json_type
 
 from sky import sky_logging
 from sky.ssh_node_pools import constants
@@ -30,82 +24,7 @@ _SQLALCHEMY_ENGINE: Optional[sqlalchemy.engine.Engine] = None
 _DB_INIT_LOCK = threading.Lock()
 
 
-class _DeclarativeABCMeta(DeclarativeMeta, ABCMeta):
-    pass
-
-
-_Base = declarative_base(metaclass=_DeclarativeABCMeta)
-_T = TypeVar('_T', bound='_BaseOrm')
-
-
-class _BaseOrm(ABC):
-    """Base class for Cluster ORM
-    Used for type hinting.
-    """
-
-    @abstractmethod
-    def to_model(self) -> models.SSHCluster:
-        pass
-
-    @classmethod
-    @abstractmethod
-    def from_model(cls: Type[_T], model: models.SSHCluster) -> _T:
-        pass
-
-
-# We have one table for staging (pools being "upped")
-# and one pool for history management. Schema is identical.
-class _SSHStagingOrm(_Base, _BaseOrm):  # type: ignore[valid-type, misc]
-    """ORM for SSH Cluster in Staging"""
-    __tablename__ = 'cluster_staging'
-
-    name = sqlalchemy.Column(sqlalchemy.Text, primary_key=True)
-    alias = sqlalchemy.Column(sqlalchemy.Text, nullable=True)
-    head_node_ip = sqlalchemy.Column(sqlalchemy.Text, nullable=False)
-    node_json = sqlalchemy.Column(
-        mutable_json_type(dbtype=sqlalchemy.JSON, nested=True))
-
-    def to_model(self) -> models.SSHCluster:
-        return models.SSHCluster(name=self.name,
-                                 head_node_ip=self.head_node_ip,
-                                 alias=self.alias,
-                                 _node_json=self.node_json)
-
-    @classmethod
-    def from_model(cls, model: models.SSHCluster) -> '_SSHStagingOrm':
-        return cls(name=model.name,
-                   head_node_ip=model.head_node_ip,
-                   alias=model.alias,
-                   node_json=model.node_json)
-
-
-class _SSHHistoryOrm(_Base, _BaseOrm):  # type: ignore[valid-type, misc]
-    """ORM for SSH Cluster History
-
-    Stores information of SSH Node Pools that are currently active."""
-    __tablename__ = 'cluster_history'
-
-    name = sqlalchemy.Column(sqlalchemy.Text, primary_key=True)
-    alias = sqlalchemy.Column(sqlalchemy.Text, nullable=True)
-    head_node_ip = sqlalchemy.Column(sqlalchemy.Text, nullable=False)
-    node_json = sqlalchemy.Column(
-        mutable_json_type(dbtype=sqlalchemy.JSON, nested=True))
-
-    def to_model(self) -> models.SSHCluster:
-        return models.SSHCluster(name=self.name,
-                                 head_node_ip=self.head_node_ip,
-                                 alias=self.alias,
-                                 _node_json=self.node_json)
-
-    @classmethod
-    def from_model(cls, model: models.SSHCluster) -> '_SSHHistoryOrm':
-        return cls(name=model.name,
-                   head_node_ip=model.head_node_ip,
-                   alias=model.alias,
-                   node_json=model.node_json)
-
-
-class _IpRegistry(_Base):  # type: ignore[valid-type, misc]
+class _IpRegistry(models.SSHBase):  # type: ignore[valid-type, misc]
     """Ips currently being used by SSH Clusters"""
     __tablename__ = 'ip_registry'
 
@@ -130,7 +49,7 @@ def _create_table():
                 raise
 
     # create table if they don't exist
-    _Base.metadata.create_all(bind=_SQLALCHEMY_ENGINE)
+    models.SSHBase.metadata.create_all(bind=_SQLALCHEMY_ENGINE)
 
 
 def _initialize_and_get_db() -> sqlalchemy.engine.Engine:
@@ -161,83 +80,47 @@ def _init_db(func):
 
 
 ## cluster db ##
-# generics
 @_init_db
-def _get_all_clusters_generic(orm_class: Type[_T]) -> List[models.SSHCluster]:
+def get_all_clusters() -> List[models.SSHCluster]:
+    """Get all SSHCluster Configurations"""
     assert _SQLALCHEMY_ENGINE is not None
     with orm.Session(_SQLALCHEMY_ENGINE) as session:
-        rows = session.query(orm_class).all()
-        return [row.to_model() for row in rows]
-
-
-@_init_db
-def _get_cluster_generic(name: str,
-                         orm_class: Type[_T]) -> Optional[models.SSHCluster]:
-    assert _SQLALCHEMY_ENGINE is not None
-    with orm.Session(_SQLALCHEMY_ENGINE) as session:
-        row = session.query(orm_class).filter_by(name=name).first()
-        if row is None:
-            return None
-        return row.to_model()
+        return session.query(models.SSHCluster).all()
 
 
 @_init_db
-def _add_or_update_cluster_generic(cluster: models.SSHCluster,
-                                   orm_class: Type[_T]):
+def get_cluster(cluster_name: str) -> Optional[models.SSHCluster]:
+    """Get a SSHCluster Configuration by Cluster Name"""
     assert _SQLALCHEMY_ENGINE is not None
     with orm.Session(_SQLALCHEMY_ENGINE) as session:
-        orm_cluster = orm_class.from_model(cluster)
-        session.merge(orm_cluster)
+        return (session.query(models.SSHCluster)
+                       .filter_by(name=cluster_name).first())
+
+
+@_init_db
+def get_cluster_by_status(status: models.SSHClusterStatus) -> List[models.SSHCluster]:
+    """Get SSHCluster Configurations by Status"""
+    assert _SQLALCHEMY_ENGINE is not None
+    with orm.Session(_SQLALCHEMY_ENGINE) as session:
+        return session.query(models.SSHCluster).filter_by(status=status).all()
+
+
+@_init_db
+def add_or_update_cluster(cluster: models.SSHCluster):
+    """Add or Update an Existing SSHCluster Configuration"""
+    assert _SQLALCHEMY_ENGINE is not None
+    with orm.Session(_SQLALCHEMY_ENGINE) as session:
+        session.merge(cluster)
         session.commit()
 
 
 @_init_db
-def _remove_cluster_generic(name: str, orm_class: Type[_T]):
+def remove_cluster(cluster_name: str):
+    """Remove SSHCluster Configuration"""
     assert _SQLALCHEMY_ENGINE is not None
     with orm.Session(_SQLALCHEMY_ENGINE) as session:
-        session.query(orm_class).filter_by(name=name).delete()
+        session.query(models.SSHCluster).filter_by(name=cluster_name).delete()
         session.commit()
-
-
-# staging / history
-def get_all_clusters_staging() -> List[models.SSHCluster]:
-    """Get all staged ssh clusters"""
-    return _get_all_clusters_generic(_SSHStagingOrm)
-
-
-def get_all_clusters_history() -> List[models.SSHCluster]:
-    """Get all deployed ssh clusters"""
-    return _get_all_clusters_generic(_SSHHistoryOrm)
-
-
-def get_cluster_staging(name: str) -> Optional[models.SSHCluster]:
-    """Get specific staged ssh cluster"""
-    return _get_cluster_generic(name, _SSHStagingOrm)
-
-
-def get_cluster_history(name: str) -> Optional[models.SSHCluster]:
-    """Get specific ssh cluster history"""
-    return _get_cluster_generic(name, _SSHHistoryOrm)
-
-
-def add_or_update_cluster_staging(cluster: models.SSHCluster):
-    """Upsert ssh cluster for staging"""
-    _add_or_update_cluster_generic(cluster, _SSHStagingOrm)
-
-
-def add_or_update_cluster_history(cluster: models.SSHCluster):
-    """Upsert ssh cluster history"""
-    _add_or_update_cluster_generic(cluster, _SSHHistoryOrm)
-
-
-def remove_cluster_staging(name: str):
-    """Remove ssh cluster from staging"""
-    _remove_cluster_generic(name, _SSHStagingOrm)
-
-
-def remove_cluster_history(name: str):
-    """Remove ssh cluster history"""
-    _remove_cluster_generic(name, _SSHHistoryOrm)
 
 
 ## ip registry ##
