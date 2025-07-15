085e06313e88c6fab041d4fadb36336c2e962c71
Seung Jin Yang <seungjin219@gmail.com>
2025-07-15 13:32:43 -0700

    add an sdk endpoint to reload config

--- sky/client/sdk.py
+++ sky/client/sdk.py
@@ -73,6 +73,9 @@ logging.getLogger('httpx').setLevel(logging.CRITICAL)
 _LINE_PROCESSED_KEY = 'line_processed'
 
 
+def reload_config() -> None:
+    skypilot_config.safe_reload_config()
+
 def stream_response(request_id: Optional[str],
                     response: 'requests.Response',
                     output_stream: Optional['io.TextIOBase'] = None,
