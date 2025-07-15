ecaa11cd2aab4d292fe55cfbc487574c194b5966
kyuds <kyuseung1016@gmail.com>
2025-07-15 14:16:55 +0900

    backwards compat for nebius

--- sky/utils/config_utils.py
+++ sky/utils/config_utils.py
@@ -257,6 +257,16 @@ def get_cloud_config_value_from_dict(
             keys=(cloud, region_key, region) + keys,
             default_value=None,
             override_configs=override_configs)
+        if not per_context_config and cloud == 'nebius':
+            # TODO (kyuds): remove later when this back-compat is not needed.
+            per_context_config = input_config.get_nested(
+                keys=(cloud, region) + keys,
+                default_value=None,
+                override_configs=override_configs)
+            logger.info('Legacy configuration yaml format for Nebius used.\n'
+                        'While this is currently ok, please visit '
+                        '`https://docs.skypilot.co/en/latest/reference/config.html#nebius` ' # pylint: disable=line-too-long
+                        'for more information.')
     # if no override found for specified region
     general_config = input_config.get_nested(keys=(cloud,) + keys,
                                              default_value=default_value,
