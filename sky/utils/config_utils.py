f8114630ebd158345eafd2fb5a3bc96ea08c3dfc
Seung Jin <seungjin219@gmail.com>
2025-07-21 10:31:17 -0700

    Apply suggestions from code review
    Co-authored-by: Romil Bhardwaj <romil.bhardwaj@gmail.com>

--- sky/utils/config_utils.py
+++ sky/utils/config_utils.py
@@ -258,16 +258,16 @@ def get_cloud_config_value_from_dict(
             default_value=None,
             override_configs=override_configs)
         if not per_context_config and cloud == 'nebius':
-            # TODO (kyuds): remove later when this back-compat is not needed.
+            # TODO (kyuds): Backward compatibility, remove after 0.11.0.
             per_context_config = input_config.get_nested(
                 keys=(cloud, region) + keys,
                 default_value=None,
                 override_configs=override_configs)
             logger.info(
-                'Legacy configuration yaml format for Nebius used.\n'
-                'While this is currently ok, please visit '
+                'Nebius configuration is using the legacy format. \n'
+                'This format will be deprecated after 0.11.0, refer to '
                 '`https://docs.skypilot.co/en/latest/reference/config.html#nebius` '  # pylint: disable=line-too-long
-                'for more information.')
+                'for the new format. Please use `region_configs` to specify region specific configuration.')
     # if no override found for specified region
     general_config = input_config.get_nested(keys=(cloud,) + keys,
                                              default_value=default_value,
