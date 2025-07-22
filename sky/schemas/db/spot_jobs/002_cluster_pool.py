c4cfda7aabdc87afdadc6282c51d229bfdcdd770
cblmemo <cblmemo@gmail.com>
2025-07-22 16:46:15 -0700

    alembnic

--- sky/schemas/db/spot_jobs/002_cluster_pool.py
+++ sky/schemas/db/spot_jobs/002_cluster_pool.py
@@ -6,6 +6,7 @@ Create Date: 2025-07-18
 
 """
 # pylint: disable=invalid-name
+from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
@@ -13,10 +14,10 @@ import sqlalchemy as sa
 from sky.utils.db import db_utils
 
 # revision identifiers, used by Alembic.
-revision = '002'
-down_revision = '001'
-branch_labels = None
-depends_on = None
+revision: str = '002'
+down_revision: Union[str, Sequence[str], None] = '001'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade():
@@ -38,4 +39,4 @@ def upgrade():
 
 def downgrade():
     """Remove columns for cluster pool."""
-    # TODO(tian): remove columns
+    pass
