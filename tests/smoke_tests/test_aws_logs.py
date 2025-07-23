0ca5e5647b8933040bdaee17d85eb862137d871c
Aylei <rayingecho@gmail.com>
2025-07-23 20:34:37 +0800

    Fix smoke test
    Signed-off-by: Aylei <rayingecho@gmail.com>

--- tests/smoke_tests/test_aws_logs.py
+++ tests/smoke_tests/test_aws_logs.py
@@ -25,8 +25,20 @@ def test_log_collection_to_aws_cloudwatch(generic_cloud: str):
             textwrap.dedent(f"""\
                 logs:
                   store: aws
+                  aws:
+                    region: us-east-1
                 """))
         base.flush()
+        additional_tags.write(
+            textwrap.dedent(f"""\
+                logs:
+                  store: aws
+                  aws:
+                    region: us-east-1
+                    additional_tags:
+                      skypilot_smoke_test_case: {name}-case
+                """))
+        additional_tags.flush()
         logs_cmd = 'for i in {1..10}; do echo $i; done'
         validate_logs_cmd = (
             'echo $output && echo "===Validate logs from AWS CloudWatch===" && '
@@ -41,8 +53,8 @@ def test_log_collection_to_aws_cloudwatch(generic_cloud: str):
                 # Wait for the logs to be available in AWS CloudWatch.
                 'sleep 10',
                 # Use AWS CLI to query CloudWatch logs
-                (f'output=$(aws logs filter-log-events --log-group-name skypilot-logs '
-                 f'--filter-pattern "skypilot_cluster_name={name}" '
+                (f'output=$(aws logs --region us-east-1 filter-log-events --log-group-name skypilot-logs '
+                 f'--filter-pattern \'skypilot.cluster_name = "{name}"\' '
                  f'--start-time $(date -d "{one_hour_ago}" +%s)000 '
                  f'--query "events[*].message" --output text) && '
                  f'{validate_logs_cmd}'),
@@ -51,7 +63,7 @@ def test_log_collection_to_aws_cloudwatch(generic_cloud: str):
                     base.name),
                 'sleep 10',
                 # Query logs for the job
-                (f'output=$(aws logs filter-log-events --log-group-name skypilot-logs '
+                (f'output=$(aws --region us-east-1 logs filter-log-events --log-group-name skypilot-logs '
                  f'--filter-pattern "{name}-job" '
                  f'--start-time $(date -d "{one_hour_ago}" +%s)000 '
                  f'--query "events[*].message" --output text) && '
@@ -62,13 +74,13 @@ def test_log_collection_to_aws_cloudwatch(generic_cloud: str):
                     additional_tags.name),
                 'sleep 10',
                 # Query logs with additional tags
-                (f'output=$(aws logs filter-log-events --log-group-name skypilot-logs '
-                 f'--filter-pattern "skypilot_smoke_test_case={name}-case" '
+                (f'output=$(aws --region us-east-1 logs filter-log-events --log-group-name skypilot-logs '
+                 f'--filter-pattern \'skypilot_smoke_test_case = "{name}-case"\' '
                  f'--start-time $(date -d "{one_hour_ago}" +%s)000 '
                  f'--query "events[*].message" --output text) && '
                  f'{validate_logs_cmd}'),
             ],
-            f'sky down -y {name}',
+            # f'sky down -y {name}',
             timeout=20 * 60,
         )
         smoke_tests_utils.run_one_test(test)
