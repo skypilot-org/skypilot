f0ebadd4c4343a20de04359b71a49dc91d3e5999
zpoint <zp0int@qq.com>
2025-08-05 21:59:59 +0800

    Fix test managed jobs storage failure (#6511)
    * change region
    
    * change region

--- tests/smoke_tests/test_managed_job.py
+++ tests/smoke_tests/test_managed_job.py
@@ -841,7 +841,7 @@ def test_managed_jobs_storage(generic_cloud: str):
     # However, we inject this testing only for AWS and GCP since they are the
     # supported object storage providers in SkyPilot.
     if generic_cloud == 'aws':
-        region = 'eu-central-1'
+        region = 'us-east-2'
         region_flag = f'/{region}'
         region_cmd = test_mount_and_storage.TestStorageWithCredentials.cli_region_cmd(
             storage_lib.StoreType.S3, bucket_name=output_storage_name)
@@ -944,7 +944,6 @@ def test_managed_jobs_storage(generic_cloud: str):
     # This is useful for jobs consolidation mode, where the job submission is
     # very fast (don't need to launch a controller VM) and the bucket might not
     # be created yet immediately after the job submission.
-    region_validation_cmd_with_retry = region_validation_base_cmd
     region_validation_timeout_for_consolidation = 30
     # Only apply to non-trivial region validation commands.
     if region_validation_base_cmd != 'true':
