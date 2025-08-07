a78684da7bdec51ef90eacaaf292bbe8dddd2c04
cblmemo <cblmemo@gmail.com>
2025-08-07 13:38:46 -0700

    revert

--- sky/schemas/db/global_user_state/003_fix_initial_revision.py
+++ sky/schemas/db/global_user_state/003_fix_initial_revision.py
@@ -9,6 +9,7 @@ Create Date: 2025-08-07
 from typing import Sequence, Union
 
 from alembic import op
+import sqlalchemy as sa
 
 from sky.utils.db import db_utils
 
@@ -22,6 +23,20 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     """Upgrade schema."""
     with op.get_context().autocommit_block():
+        # add missing columns to clusters table
+        db_utils.add_column_to_table_alembic('clusters',
+                                             'storage_mounts_metadata',
+                                             sa.LargeBinary(),
+                                             server_default=None)
+        db_utils.add_column_to_table_alembic('clusters',
+                                             'cluster_ever_up',
+                                             sa.Integer(),
+                                             server_default='0')
+        db_utils.add_column_to_table_alembic('clusters',
+                                             'status_updated_at',
+                                             sa.Integer(),
+                                             server_default=None)
+
         # remove mistakenly added columns
         db_utils.drop_column_from_table_alembic('clusters', 'launched_nodes')
         db_utils.drop_column_from_table_alembic('clusters', 'disk_tier')
