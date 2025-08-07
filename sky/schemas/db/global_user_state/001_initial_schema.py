5e45b5de73e04958a4f25e0a2505e3ca7f6218ee
cblmemo <cblmemo@gmail.com>
2025-08-06 18:31:33 -0700

    fix

--- sky/schemas/db/global_user_state/001_initial_schema.py
+++ sky/schemas/db/global_user_state/001_initial_schema.py
@@ -49,14 +49,37 @@ def upgrade():
                                              'cluster_hash',
                                              sa.Text(),
                                              server_default=None)
-        db_utils.add_column_to_table_alembic('clusters',
-                                             'launched_nodes',
-                                             sa.Integer(),
-                                             server_default='0')
-        db_utils.add_column_to_table_alembic('clusters',
-                                             'disk_tier',
-                                             sa.Text(),
-                                             server_default=None)
+        db_utils.add_column_to_table_alembic(
+            'clusters',
+            'storage_mounts_metadata',
+            sa.LargeBinary(),
+            server_default=None)
+        db_utils.add_column_to_table_alembic(
+            'clusters',
+            'cluster_ever_up',
+            sa.Integer(),
+            server_default='0',
+            # Set the value to 1 so that all the existing clusters before #2977
+            # are considered as ever up, i.e:
+            #   existing cluster's default (null) -> 1;
+            #   new cluster's default -> 0;
+            # This is conservative for the existing clusters: even if some INIT
+            # clusters were never really UP, setting it to 1 means they won't be
+            # auto-deleted during any failover.
+            value_to_replace_existing_entries=1)
+        db_utils.add_column_to_table_alembic(
+            'clusters',
+            'status_updated_at',
+            sa.Integer(),
+            server_default=None)
+        # db_utils.add_column_to_table_alembic('clusters',
+        #                                      'launched_nodes',
+        #                                      sa.Integer(),
+        #                                      server_default='0')
+        # db_utils.add_column_to_table_alembic('clusters',
+        #                                      'disk_tier',
+        #                                      sa.Text(),
+        #                                      server_default=None)
         db_utils.add_column_to_table_alembic('clusters',
                                              'config_hash',
                                              sa.Text(),
