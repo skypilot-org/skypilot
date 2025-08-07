99d93120400c7f0178aee9e2c1daadd8cbd474b9
Aylei <rayingecho@gmail.com>
2025-08-07 12:19:30 +0800

    debug retention hour
    Signed-off-by: Aylei <rayingecho@gmail.com>

--- sky/server/requests/requests.py
+++ sky/server/requests/requests.py
@@ -665,4 +665,4 @@ async def requests_gc_daemon():
             logger.error(f'Error running requests GC daemon: {e}')
         # Run the daemon at most once every hour to avoid too frequent
         # cleanup.
-        await asyncio.sleep(max(retention_seconds, 3600))
+        await asyncio.sleep(max(retention_seconds, 120))
