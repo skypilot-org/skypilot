61cead5243157c7b76648514ca09e16f074c9320
lloyd-brown <lloyd@assemblesys.com>
2025-08-09 12:48:32 -0700

    Run yapf.

--- sky/schemas/db/global_user_state/005_cluster_event.py
+++ sky/schemas/db/global_user_state/005_cluster_event.py
@@ -24,8 +24,8 @@ def upgrade():
     """Add new table for cluster events."""
     with op.get_context().autocommit_block():
         # Add new table for cluster events.
-        db_utils.add_table_to_db_sqlalchemy(
-            Base.metadata, op.get_bind(), 'cluster_events')
+        db_utils.add_table_to_db_sqlalchemy(Base.metadata, op.get_bind(),
+                                            'cluster_events')
 
 
 def downgrade():
