d1a2b9326d43ff6677183b9d591599f4a9b6e9c9
Kevin Mingtarja <kevin.mingtarja@gmail.com>
2025-08-12 13:45:03 -0700

    regenerate protos, fix pre-commit isort exclusion list

--- sky/schemas/generated/autostopv1_pb2.pyi
+++ sky/schemas/generated/autostopv1_pb2.pyi
@@ -1,8 +1,7 @@
-from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
-
+from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
+from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
