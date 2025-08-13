09e30e836951f1a6591b6dca8bba5dd96f108b0c
Seung Jin Yang <seungjin219@gmail.com>
2025-08-13 00:05:36 -0700

    heh

---
+++ sky/utils/directory_utils.py
@@ -0,0 +1,17 @@
+import os
+
+# This file is in '<project_root>/sky/utils/directory_utils.py'
+# So we need to go up 2 levels to get to the '<project_root>/sky' directory
+SKY_DIR = os.path.dirname(os.path.dirname(__file__))
+
+def get_sky_dir():
+    """Get the sky root directory."""
+    return SKY_DIR
+
+# sky dir is in '<project_root>/sky'
+# So we need to go up 1 level to get to the '<project_root>' directory
+PROJECT_ROOT_DIR = os.path.dirname(SKY_DIR)
+
+def get_project_root_dir():
+    """Get the project root directory."""
+    return PROJECT_ROOT_DIR
