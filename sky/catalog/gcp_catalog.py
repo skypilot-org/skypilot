ccc9838d65e9104b9ab601bcd8bac1fe45520a55
zpoint <zp0int@qq.com>
2025-08-14 13:44:55 +0800

    revert catalog debug change

--- sky/catalog/gcp_catalog.py
+++ sky/catalog/gcp_catalog.py
@@ -685,12 +685,6 @@ def get_image_id_from_tag(tag: str, region: Optional[str]) -> Optional[str]:
         _image_df = common.read_catalog('gcp/images.csv',
                                         pull_frequency_hours=0)
         image_id = common.get_image_id_from_tag_impl(_image_df, tag, region)
-    if tag == 'skypilot:custom-cpu-ubuntu-2204-250813':
-        image_id = ('projects/sky-dev-465/global/images/'
-                    'skypilot-gcp-cpu-ubuntu-250813')
-    elif tag == 'skypilot:custom-gpu-ubuntu-2204-250813':
-        image_id = ('projects/sky-dev-465/global/images/'
-                    'skypilot-gcp-gpu-ubuntu-250813')
     return image_id
 
 
