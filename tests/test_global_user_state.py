345a3aafadb08e1b01bf521abeed67faaaab8e59
lloyd-brown <lloyd@assemblesys.com>
2025-08-14 11:06:43 -0700

    Remove TODO.

--- tests/test_global_user_state.py
+++ tests/test_global_user_state.py
@@ -13,8 +13,6 @@ def test_enabled_clouds_empty():
     assert sky.global_user_state.get_cached_enabled_clouds(
         sky.clouds.cloud.CloudCapability.COMPUTE, workspace='default') == []
 
-
-# TODO (lloyd-brown): Add test for cluster event retention daemon.
 @pytest.mark.asyncio
 async def test_cluster_event_retention_daemon():
     """Test the cluster event retention daemon runs correctly."""
