68106e6bf8e0311fac433493387a48a58f36f998
lloyd-brown <lloyd@assemblesys.com>
2025-08-15 22:08:35 -0700

    [UX] Provide last event info for clusters stored in cluster_history (#6658)

--- sky/backends/backend_utils.py
+++ sky/backends/backend_utils.py
@@ -2079,7 +2079,15 @@ def _update_cluster_status(cluster_name: str) -> Optional[Dict[str, Any]]:
     if handle.cluster_yaml is None:
         # Remove cluster from db since this cluster does not have a config file
         # or any other ongoing requests
-        global_user_state.remove_cluster(cluster_name, terminate=True)
+        global_user_state.add_cluster_event(
+            cluster_name,
+            None,
+            'Cluster has no YAML file. Removing the cluster from cache.',
+            global_user_state.ClusterEventType.STATUS_CHANGE,
+            nop_if_duplicate=True)
+        global_user_state.remove_cluster(cluster_name,
+                                         terminate=True,
+                                         remove_events=True)
         logger.debug(f'Cluster {cluster_name!r} has no YAML file. '
                      'Removing the cluster from cache.')
         return None
@@ -2414,10 +2422,10 @@ def _update_cluster_status(cluster_name: str) -> Optional[Dict[str, Any]]:
     # Now is_abnormal is False: either node_statuses is empty or all nodes are
     # STOPPED.
     backend = backends.CloudVmRayBackend()
-    backend.post_teardown_cleanup(handle, terminate=to_terminate, purge=False)
     global_user_state.add_cluster_event(
         cluster_name, None, 'All nodes stopped, terminating cluster.',
         global_user_state.ClusterEventType.STATUS_CHANGE)
+    backend.post_teardown_cleanup(handle, terminate=to_terminate, purge=False)
     return global_user_state.get_cluster_from_name(cluster_name)
 
 
