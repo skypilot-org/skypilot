5a07890fa0836ca17945bcdf78d5ac5e75ed0de2
Romil Bhardwaj <romil.bhardwaj@gmail.com>
2025-08-17 18:41:37 -0700

    Remove meetup announcement

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -117,7 +117,8 @@ html_theme_options = {
         'icon': 'fab fa-github',
     }],
     'use_edit_page_button': True,
-    'announcement': '<div style="padding: 24px; text-align: center; font-size: 0.8rem; line-height: 1;">👋 Join us for the <b>SkyPilot AI Infra Meetup</b> in San Francisco on August 14! <a href="https://lu.ma/q1rfsjxk?utm_source=skydocs">Register here</a></div>',
+    # Add meetup or other announcements here.
+    # 'announcement': '<div style="padding: 24px; text-align: center; font-size: 0.8rem; line-height: 1;">👋 Join us for the <b>SkyPilot AI Infra Meetup</b> in San Francisco on August 14! <a href="https://lu.ma/q1rfsjxk?utm_source=skydocs">Register here</a></div>',
     'secondary_sidebar_items': [
         'page-toc',
         'edit-this-page',
