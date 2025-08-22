2569ecd478709437a6c546bf36fa72e4fb77cb7c
lloyd-brown <lloyd@assemblesys.com>
2025-08-22 16:21:03 -0700

    Add test for custom ami.

--- tests/smoke_tests/test_ssm.py
+++ tests/smoke_tests/test_ssm.py
@@ -7,8 +7,15 @@
 # Terminate failed clusters after test finishes
 # > pytest tests/smoke_tests/test_ssm.py --terminate-on-failure
 
+import tempfile
+import textwrap
+
 import pytest
 from smoke_tests import smoke_tests_utils
+
+import sky
+from sky import skypilot_config
+from sky.skylet import constants
 
 
 @pytest.mark.aws
@@ -54,3 +61,42 @@ def test_ssm_private():
         timeout=smoke_tests_utils.get_timeout('aws'),
     )
     smoke_tests_utils.run_one_test(test)
+
+
+@pytest.mark.aws
+def test_ssm_private_custom_ami():
+    """Test that ssm works with private IP addresses and a custom AMI.
+    """
+
+    ami_id = "ami-04b95a57b104f3c92"
+
+    config = textwrap.dedent(f"""
+    aws:
+        ssh_user: my-own-user-name
+        use_internal_ips: true
+        vpc_name: DO_NOT_DELETE_lloyd-airgapped-plus-gateway
+        use_ssm: true
+        post_provision_runcmd:
+            - systemctl enable amazon-ssm-agent
+            - systemctl start amazon-ssm-agent
+    """)
+
+    with tempfile.NamedTemporaryFile(delete=True) as f:
+        f.write(config.encode('utf-8'))
+        f.flush()
+
+        name = smoke_tests_utils.get_cluster_name()
+        test = smoke_tests_utils.Test(
+            'ssm_private_custom_ami',
+            [
+                f's=$(SKYPILOT_DEBUG=0 sky launch -y -c {name} --infra aws/us-west-1 {smoke_tests_utils.LOW_RESOURCE_ARG} --image-id {ami_id} tests/test_yamls/minimal.yaml) && {smoke_tests_utils.VALIDATE_LAUNCH_OUTPUT}',
+            ],
+            teardown=f'sky down -y {name}',
+            timeout=smoke_tests_utils.get_timeout('aws'),
+            env={
+                skypilot_config.ENV_VAR_GLOBAL_CONFIG: f.name,
+                constants.SKY_API_SERVER_URL_ENV_VAR:
+                    sky.server.common.get_server_url()
+            },
+        )
+        smoke_tests_utils.run_one_test(test)
