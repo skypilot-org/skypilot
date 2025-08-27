39aa8fd585eb9f4e31a968a8c15eec329328c8c9
Maslov Nikita <webconn@users.noreply.github.com>
2025-08-27 09:46:41 +0500

    api server: add proper hashing support for internal basic auth (#6806)
    * api server: add proper hashing support for internal basic auth
    
    * fix formatting
    
    * formatting again
    
    * make isort happy
    
    * fix unit tests
    
    * fix pylint
    
    * fix codestyle

--- sky/server/server.py
+++ sky/server/server.py
@@ -23,7 +23,6 @@ import zipfile
 import aiofiles
 import fastapi
 from fastapi.middleware import cors
-from passlib.hash import apr_md5_crypt
 import starlette.middleware.base
 import uvloop
 
@@ -121,7 +120,7 @@ def _try_set_basic_auth_user(request: fastapi.Request):
         username_encoded = username.encode('utf8')
         db_username_encoded = user.name.encode('utf8')
         if (username_encoded == db_username_encoded and
-                apr_md5_crypt.verify(password, user.password)):
+                common.crypt_ctx.verify(password, user.password)):
             request.state.auth_user = user
             break
 
@@ -221,7 +220,7 @@ class BasicAuthMiddleware(starlette.middleware.base.BaseHTTPMiddleware):
             username_encoded = username.encode('utf8')
             db_username_encoded = user.name.encode('utf8')
             if (username_encoded == db_username_encoded and
-                    apr_md5_crypt.verify(password, user.password)):
+                    common.crypt_ctx.verify(password, user.password)):
                 valid_user = True
                 request.state.auth_user = user
                 await authn.override_user_info_in_request_body(request, user)
