6006625e86f29008311fc5a1d3ea1a1dfc8d386a
Aiman Ismail <aiman@primeintellect.ai>
2025-08-27 21:32:39 +0800

    leave file blank since were not importing any sdk for prime intellect

--- sky/adaptors/primeintellect.py
+++ sky/adaptors/primeintellect.py
@@ -1,8 +1,1 @@
 """Prime Intellect cloud adaptor."""
-
-from sky.adaptors import common
-
-primeintellect = common.LazyImport(
-    'primeintellect',
-    import_error_message='Failed to import dependencies for Prime Intellect. '
-    'Try running: pip install "skypilot[primeintellect]"')
