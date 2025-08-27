876392d6682725f073df8b0c6694818230f87b39
Kevin Mingtarja <kevin.mingtarja@gmail.com>
2025-08-27 00:12:59 -0700

    fix

--- tests/smoke_tests/test_region_and_zone.py
+++ tests/smoke_tests/test_region_and_zone.py
@@ -239,7 +239,7 @@ def test_gcp_zone():
 # is running remotely. We should fix this.
 @pytest.mark.no_vast  # Requires AWS
 @pytest.mark.no_hyperbolic  # Requires AWS
-@pytest.mark.no_nebius  # Nebius image does not have Docker installed.
+@pytest.mark.no_nebius  # Nebius images does not have Docker installed.
 @pytest.mark.parametrize(
     'image_id',
     [
