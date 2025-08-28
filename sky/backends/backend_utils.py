6a5a180ef0463ebe87a160c39dce5c8ac3c463eb
Seung Jin <seungjin219@gmail.com>
2025-08-27 17:06:53 -0700

    correct regex expression for duplicate event matching (#6901)
    correction

--- sky/backends/backend_utils.py
+++ sky/backends/backend_utils.py
@@ -2367,7 +2367,7 @@ def _update_cluster_status(cluster_name: str) -> Optional[Dict[str, Any]]:
             # Some status reason clears after a certain time (e.g. k8s events
             # are only stored for an hour by default), so it is possible that
             # the previous event has a status reason, but now it does not.
-            init_reason_regex = f'^Cluster is abnormal because {init_reason} .*'
+            init_reason_regex = f'^Cluster is abnormal because {init_reason}.*'
         log_message = f'Cluster is abnormal because {init_reason}'
         if status_reason:
             log_message += f' ({status_reason})'
