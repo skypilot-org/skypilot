5c15e31166f569a7074f1464b2bd56d3747e106e
Christopher Cooper <christopher@cg505.com>
2025-08-29 19:02:30 -0400

    Reapply "[Core] Pool Job Consolidation" (#6817)
    This reverts commit 6f98299c6272db57df4b41ac901dcaefc167a75d.

--- sky/utils/db/migration_utils.py
+++ sky/utils/db/migration_utils.py
@@ -10,6 +10,7 @@ from alembic.config import Config
 from alembic.runtime import migration
 import filelock
 import sqlalchemy
+from sqlalchemy.ext import asyncio as sqlalchemy_async
 
 from sky import sky_logging
 from sky.skylet import constants
@@ -31,17 +32,29 @@ SERVE_VERSION = '001'
 SERVE_LOCK_PATH = '~/.sky/locks/.serve_db.lock'
 
 
-def get_engine(db_name: str):
+def get_engine(db_name: str, async_engine: bool = False):
     conn_string = None
     if os.environ.get(constants.ENV_VAR_IS_SKYPILOT_SERVER) is not None:
         conn_string = os.environ.get(constants.ENV_VAR_DB_CONNECTION_URI)
     if conn_string:
-        engine = sqlalchemy.create_engine(conn_string,
-                                          poolclass=sqlalchemy.NullPool)
+        logger.debug(f'using db URI from {conn_string}')
+        if async_engine:
+            conn_string = conn_string.replace('postgresql://',
+                                              'postgresql+asyncpg://')
+            engine = sqlalchemy_async.create_async_engine(
+                conn_string, poolclass=sqlalchemy.NullPool)
+        else:
+            engine = sqlalchemy.create_engine(conn_string,
+                                              poolclass=sqlalchemy.NullPool)
     else:
         db_path = os.path.expanduser(f'~/.sky/{db_name}.db')
         pathlib.Path(db_path).parents[0].mkdir(parents=True, exist_ok=True)
-        engine = sqlalchemy.create_engine('sqlite:///' + db_path)
+        if async_engine:
+            engine = sqlalchemy_async.create_async_engine(
+                'sqlite+aiosqlite:///' + db_path, connect_args={'timeout': 30})
+        else:
+            engine = sqlalchemy.create_engine('sqlite:///' + db_path,
+                                              connect_args={'timeout': 30})
     return engine
 
 
