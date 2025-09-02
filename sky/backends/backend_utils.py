03f88eae57c470775f61f812a2c6736b6cf147f6
Aylei <rayingecho@gmail.com>
2025-09-02 21:48:40 +0800

    Fix
    Signed-off-by: Aylei <rayingecho@gmail.com>

--- sky/backends/backend_utils.py
+++ sky/backends/backend_utils.py
@@ -3098,9 +3098,10 @@ def get_clusters(
         # Refactor this to use some other info to
         # determine if a launch is in progress.
         request = requests_lib.get_request_tasks(
-            status=[requests_lib.RequestStatus.RUNNING],
-            cluster_names=[cluster_name],
-            include_request_names=['sky.launch'])
+            req_filter=requests_lib.RequestTaskFilter(
+                status=[requests_lib.RequestStatus.RUNNING],
+                cluster_names=[cluster_name],
+                include_request_names=['sky.launch']))
         if len(request) > 0:
             # There is an active launch request on the cluster,
             # so we don't want to update the cluster status until
