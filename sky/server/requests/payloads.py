d04d22e9ebc64ec649c90474a597aa292aac68aa
Seung Jin Yang <seungjin219@gmail.com>
2025-09-02 13:06:12 -0700

    fix deprecation target

--- sky/server/requests/payloads.py
+++ sky/server/requests/payloads.py
@@ -309,7 +309,7 @@ class StatusBody(RequestBody):
     cluster_names: Optional[List[str]] = None
     refresh: common_lib.StatusRefreshMode = common_lib.StatusRefreshMode.NONE
     all_users: bool = True
-    # TODO (kyuds): default to False post 0.11.0
+    # TODO (kyuds): default to False post 0.10.5
     include_credentials: bool = True
 
 
