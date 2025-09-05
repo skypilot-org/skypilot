6f8b60a84ef0aae223bc6a850804decd716e38d5
Aylei <rayingecho@gmail.com>
2025-09-05 16:46:37 +0800

    Appen
    Signed-off-by: Aylei <rayingecho@gmail.com>

--- refresh.py
+++ refresh.py
@@ -8,11 +8,11 @@ proc = psutil.Process()
 
 count = int(os.getenv("RUN_COUNT", "200"))
 
-def rss():
+def rss(i: int = 0):
     gc.collect()
-    print(f'RSS: {proc.memory_info().rss / 1024 / 1024:.2f}MB')
+    print(f'Round: {i}, RSS: {proc.memory_info().rss / 1024 / 1024:.2f}MB')
 
 print(f'SKYPILOT_DISABLE_LRU_CACHE: {os.getenv("SKYPILOT_DISABLE_LRU_CACHE", "false")}')
-for _ in range(count):
+for i in range(count):
     core.status(refresh=common.StatusRefreshMode.FORCE, all_users=True)
-    rss()
+    rss(i)
