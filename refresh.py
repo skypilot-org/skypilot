547f818efcce9fe221deb6e272e20baa9b9d64e4
Aylei <rayingecho@gmail.com>
2025-09-05 15:52:07 +0800

    Fixxx
    Signed-off-by: Aylei <rayingecho@gmail.com>

---
+++ refresh.py
@@ -0,0 +1,16 @@
+from sky import core
+from sky.utils import common
+import gc
+import psutil
+import os
+
+proc = psutil.Process()
+
+def rss():
+    gc.collect()
+    print(f'{proc.memory_info().rss / 1024 / 1024:.2f}MB')
+
+print(f'SKYPILOT_DISABLE_LRU_CACHE: {os.getenv('SKYPILOT_DISABLE_LRU_CACHE', 'false')}')
+for _ in range(50):
+    core.status(refresh=common.StatusRefreshMode.FORCE, all_users=True)
+    rss()
