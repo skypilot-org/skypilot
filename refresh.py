0ef9c9fcb58fa7b169806a266ed382ca2b0c7162
Aylei <rayingecho@gmail.com>
2025-09-05 15:55:58 +0800

    Fix
    Signed-off-by: Aylei <rayingecho@gmail.com>

--- refresh.py
+++ refresh.py
@@ -10,7 +10,7 @@ def rss():
     gc.collect()
     print(f'{proc.memory_info().rss / 1024 / 1024:.2f}MB')
 
-print(f'SKYPILOT_DISABLE_LRU_CACHE: {os.getenv('SKYPILOT_DISABLE_LRU_CACHE', 'false')}')
+print(f'SKYPILOT_DISABLE_LRU_CACHE: {os.getenv("SKYPILOT_DISABLE_LRU_CACHE", "false")}')
 for _ in range(50):
     core.status(refresh=common.StatusRefreshMode.FORCE, all_users=True)
     rss()
