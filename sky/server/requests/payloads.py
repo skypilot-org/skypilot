19525bccf6f512b2af49c508a0f68892b297365d
Seung Jin <seungjin219@gmail.com>
2025-09-05 14:05:07 -0700

    [API server] Do not let client set db string on server side (#7042)
    * do not let client set db string on srv side
    
    * format
    
    * add dev comment

--- sky/server/requests/payloads.py
+++ sky/server/requests/payloads.py
@@ -86,6 +86,11 @@ def request_body_env_vars() -> dict:
     env_vars.pop(skypilot_config.ENV_VAR_SKYPILOT_CONFIG, None)
     env_vars.pop(skypilot_config.ENV_VAR_GLOBAL_CONFIG, None)
     env_vars.pop(skypilot_config.ENV_VAR_PROJECT_CONFIG, None)
+    # Remove the config related env vars, as the client config override
+    # should be passed in the request body.
+    # Any new environment variables that are server-specific should
+    # use SKYPILOT_SERVER_ENV_VAR_PREFIX.
+    env_vars.pop(constants.ENV_VAR_DB_CONNECTION_URI, None)
     return env_vars
 
 
