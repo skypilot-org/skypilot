f6838f6c5b23d4c65bc00d353fb5f3186ca0a5c1
Seung Jin Yang <seungjin219@gmail.com>
2025-09-05 17:18:40 -0700

    unittest

---
+++ tests/unit_tests/test_memory_usage.py
@@ -0,0 +1,21 @@
+import pytest
+
+from sky import check as sky_check
+import tracemalloc
+import gc
+
+# if run with other tests, the memory usage may be
+# affected by other tests, so we run it separately.
+@pytest.mark.serial
+def test_sky_check_memory_usage():
+    tracemalloc.start()
+    initial_memory = tracemalloc.get_traced_memory()[0]
+    print(f'Initial memory: {initial_memory}')
+    sky_check.check(quiet=True, verbose=True)
+    gc.collect()
+    final_memory = tracemalloc.get_traced_memory()[0]
+    tracemalloc.stop()
+    print(f'Final memory: {final_memory}')
+    memory_diff_mb = (final_memory - initial_memory) / 1024 / 1024
+    assert memory_diff_mb < 100, (
+        f'Memory usage increased by {memory_diff_mb} MB')
