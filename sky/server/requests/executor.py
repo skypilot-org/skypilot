ed8f6da5b8872f0a8675dda2dfab4e34de2c8400
Seung Jin Yang <seungjin219@gmail.com>
2025-09-08 13:29:30 -0700

    even more

--- sky/server/requests/executor.py
+++ sky/server/requests/executor.py
@@ -284,15 +284,15 @@ def override_request_env_and_config(
         request_id: str) -> Generator[None, None, None]:
     """Override the environment and SkyPilot config for a request."""
     original_env = os.environ.copy()
-    # Unset SKYPILOT_DEBUG by default, to avoid the value set on the API server
-    # affecting client requests. If set on the client side, it will be
-    # overridden by the request body.
-    os.environ.pop('SKYPILOT_DEBUG', None)
-    # Remove the db connection uri from client supplied env vars, as the
-    # client should not set the db string on server side.
-    request_body.env_vars.pop(constants.ENV_VAR_DB_CONNECTION_URI, None)
-    os.environ.update(request_body.env_vars)
     try:
+        # Unset SKYPILOT_DEBUG by default, to avoid the value set on the API server
+        # affecting client requests. If set on the client side, it will be
+        # overridden by the request body.
+        os.environ.pop('SKYPILOT_DEBUG', None)
+        # Remove the db connection uri from client supplied env vars, as the
+        # client should not set the db string on server side.
+        request_body.env_vars.pop(constants.ENV_VAR_DB_CONNECTION_URI, None)
+        os.environ.update(request_body.env_vars)
         # Note: may be overridden by AuthProxyMiddleware.
         # TODO(zhwu): we need to make the entire request a context available to the
         # entire request execution, so that we can access info like user through
