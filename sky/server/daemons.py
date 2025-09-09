1fc54eab797116055b9938424bb361b18ac596bc
Seung Jin Yang <seungjin219@gmail.com>
2025-09-09 00:30:54 -0700

    format

--- sky/server/daemons.py
+++ sky/server/daemons.py
@@ -11,8 +11,8 @@ from sky.utils import annotations
 from sky.utils import common
 from sky.utils import common_utils
 from sky.utils import env_options
-from sky.utils import timeline
 from sky.utils import subprocess_utils
+from sky.utils import timeline
 from sky.utils import ux_utils
 
 logger = sky_logging.init_logger(__name__)
@@ -75,13 +75,11 @@ class InternalRequestDaemon:
                 # using too much memory.
                 annotations.clear_request_level_cache()
                 timeline.save_timeline()
-                common_utils.release_memory()
                 # Kill all children processes related to this request.
-                # Each executor handles a single request, so we can safely kill all
-                # children processes related to this request.
-                # This is required as python does not pass the KeyboardInterrupt
-                # to the threads that are not main thread.
+                # Each executor handles a single request, so we can safely
+                # kill all children processes related to this request.
                 subprocess_utils.kill_children_processes()
+                common_utils.release_memory()
             except Exception:  # pylint: disable=broad-except
                 # It is OK to fail to run the event, as the event is not
                 # critical, but we should log the error.
