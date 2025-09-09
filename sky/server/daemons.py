eee3931111b640f42941893e3dd7c2d1d2de2a01
Seung Jin <seungjin219@gmail.com>
2025-09-09 08:30:27 -0700

    [core] kill subprocesses after each daemon run (#7077)
    * kill subprocesses after each daemon run
    
    * format

--- sky/server/daemons.py
+++ sky/server/daemons.py
@@ -11,6 +11,7 @@ from sky.utils import annotations
 from sky.utils import common
 from sky.utils import common_utils
 from sky.utils import env_options
+from sky.utils import subprocess_utils
 from sky.utils import timeline
 from sky.utils import ux_utils
 
@@ -74,6 +75,10 @@ class InternalRequestDaemon:
                 # using too much memory.
                 annotations.clear_request_level_cache()
                 timeline.save_timeline()
+                # Kill all children processes related to this request.
+                # Each executor handles a single request, so we can safely
+                # kill all children processes related to this request.
+                subprocess_utils.kill_children_processes()
                 common_utils.release_memory()
             except Exception:  # pylint: disable=broad-except
                 # It is OK to fail to run the event, as the event is not
