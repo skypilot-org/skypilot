d343bf014820c1ed5c01641d424ce03da5e702a7
Zhanghao Wu <zhanghao.wu@outlook.com>
2025-09-12 15:34:45 -0700

    [config] allow active workspace in task (#7160)
    allow active workspace in task

--- sky/skylet/constants.py
+++ sky/skylet/constants.py
@@ -384,6 +384,7 @@ OVERRIDEABLE_CONFIG_KEYS_IN_TASK: List[Tuple[str, ...]] = [
     ('gcp', 'enable_gvnic'),
     ('gcp', 'enable_gpu_direct'),
     ('gcp', 'placement_policy'),
+    ('active_workspace',),
 ]
 # When overriding the SkyPilot configs on the API server with the client one,
 # we skip the following keys because they are meant to be client-side configs.
