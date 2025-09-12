7b915550fabe766793ed21863270143ed547119a
Marco Cristofanilli <m4oc@users.noreply.github.com>
2025-09-12 02:15:55 +0000

    Revert and fix test

--- tests/smoke_tests/test_sky_serve.py
+++ tests/smoke_tests/test_sky_serve.py
@@ -338,8 +338,8 @@ def test_skyserve_oci_http():
 @pytest.mark.no_fluidstack  # Fluidstack does not support T4 gpus for now
 @pytest.mark.no_vast  # Vast has low availability of T4 GPUs
 @pytest.mark.no_hyperbolic  # Hyperbolic has low availability of T4 GPUs
-@pytest.mark.no_seeweb  # Seeweb does not support T4 GPUs
 @pytest.mark.parametrize('accelerator', [{'do': 'H100', 'nebius': 'H100'}])
+@pytest.mark.no_seeweb  # Seeweb does not support T4 GPUs
 @pytest.mark.serve
 @pytest.mark.resource_heavy
 def test_skyserve_llm(generic_cloud: str, accelerator: Dict[str, str]):
