e5802aa72feb8536d92f4880e93f34675b33f37b
Zhanghao Wu <zhanghao.wu@outlook.com>
2025-09-13 11:14:02 -0700

    [Docs] Fix distributed examples (#7173)
    * Fix distributed training job yaml and fix installation
    
    * update test
    
    * format
    
    * fix
    
    * fix file name
    
    * Allow tests for clouds other than k8s
    
    * Add no clouds
    
    * fix write

--- tests/smoke_tests/test_cluster_job.py
+++ tests/smoke_tests/test_cluster_job.py
@@ -2018,39 +2018,49 @@ def test_long_setup_run_script(generic_cloud: str):
         smoke_tests_utils.run_one_test(test)
 
 
-# ---------- Test min-gpt on Kubernetes ----------
-@pytest.mark.kubernetes
+# ---------- Test min-gpt ----------
+@pytest.mark.no_scp  # SCP does not support num_nodes > 1 yet
+@pytest.mark.no_hyperbolic  # Hyperbolic not support num_nodes > 1 yet
+@pytest.mark.no_seeweb  # Seeweb does not support multi-node
 @pytest.mark.resource_heavy
-def test_min_gpt_kubernetes():
+@pytest.mark.parametrize('train_file', [
+    'examples/distributed-pytorch/train.yaml',
+    'examples/distributed-pytorch/train-rdzv.yaml'
+])
+def test_min_gpt(generic_cloud: str, train_file: str):
     accelerator = smoke_tests_utils.get_avaliabe_gpus_for_k8s_tests()
     name = smoke_tests_utils.get_cluster_name()
-    original_yaml_path = 'examples/distributed-pytorch/train.yaml'
 
-    with open(original_yaml_path, 'r') as f:
-        content = f.read()
+    def read_and_modify(file_path: str) -> str:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            content = f.read()
+        # Let the train exit after 1 epoch
+        modified_content = content.replace(
+            'main.py', 'main.py trainer_config.max_epochs=1')
+        modified_content = re.sub(r'accelerators:\s*[^\n]+',
+                                  f'accelerators: {accelerator}',
+                                  modified_content)
 
-    # Let the train exit after 1 epoch
-    modified_content = content.replace('main.py',
-                                       'main.py trainer_config.max_epochs=1')
+        # Create a temporary YAML file with the modified content
+        with tempfile.NamedTemporaryFile(mode='w', suffix='.yaml',
+                                         delete=False) as f:
+            f.write(modified_content)
+            f.flush()
+            train_file_path = f.name
+        return train_file_path
 
-    modified_content = re.sub(r'accelerators:\s*[^\n]+',
-                              f'accelerators: {accelerator}', modified_content)
+    dist_train_file = read_and_modify(train_file)
 
-    # Create a temporary YAML file with the modified content
-    with tempfile.NamedTemporaryFile(mode='w', suffix='.yaml') as f:
-        f.write(modified_content)
-        f.flush()
-
-        test = smoke_tests_utils.Test(
-            'min_gpt_kubernetes',
-            [
-                f'sky launch -y -c {name} --infra kubernetes {f.name}',
-                f'sky logs {name} 1 --status',
-            ],
-            f'sky down -y {name}',
-            timeout=20 * 60,
-        )
-        smoke_tests_utils.run_one_test(test)
+    test = smoke_tests_utils.Test(
+        'min_gpt_kubernetes',
+        [
+            f'sky launch -y -c {name} --infra {generic_cloud} {dist_train_file}',
+            f'sky logs {name} 1 --status',
+        ],
+        f'sky down -y {name}',
+        timeout=20 * 60,
+    )
+    smoke_tests_utils.run_one_test(test)
 
 
 # ---------- Test GCP network tier ----------
