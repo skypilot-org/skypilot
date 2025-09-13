f61d530fbf9004fdf9e4e8a19ba1149c72e97ff7
lloyd-brown <lloyd@assemblesys.com>
2025-09-12 20:34:38 -0700

    Ignore sky check.

--- tests/unit_tests/test_validation.py
+++ tests/unit_tests/test_validation.py
@@ -2,8 +2,10 @@ from unittest.mock import patch
 
 import pytest
 
+from sky import check as sky_check
 from sky import resources as resources_lib
 from sky import task as task_lib
+from sky.clouds.kubernetes import Kubernetes
 from sky.optimizer import _fill_in_launchable_resources
 
 
@@ -30,6 +32,10 @@ def test_resources_validate_with_cloud(monkeypatch):
 
         # Call the original validate method
         return original_validate(resources_instance)
+
+    # Patch sky check so we don't fail if the cloud is not enabled.
+    monkeypatch.setattr(sky_check, 'get_cached_enabled_clouds_or_refresh',
+                        lambda *args, **kwargs: [Kubernetes()])
 
     # Patch the validate method to intercept calls and check cloud is set
     with patch.object(resources_lib.Resources, 'validate', new=mock_validate):
