1a641fcd3ed333d8c1695ef172b0a62882c5193e
Aylei <rayingecho@gmail.com>
2025-09-17 13:58:09 +0800

    Debug

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 from sky.provision.kubernetes import utils
 
-print(utils.get_kubernetes_node_info('nebius-k8s-gpu-sa@nebius-shopify-k8s-cluster'))
+print(utils.get_kubernetes_node_info('in-cluster'))
+print(utils.get_kubernetes_node_info('in-cluster'))
