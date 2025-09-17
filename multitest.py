b4e6a43cabae46143d18701e3e95720b5fb64adc
Aylei <rayingecho@gmail.com>
2025-09-17 16:28:24 +0800

    Debug

---
+++ multitest.py
@@ -0,0 +1,11 @@
+from sky import core
+from sky.provision.kubernetes import utils
+from sky.utils import annotations
+from sky.utils import common_utils
+
+# print(utils.get_kubernetes_node_info('in-cluster'))
+print(len(core.cost_report(days=30)))
+annotations.clear_request_level_cache()
+common_utils.release_memory()
+# print(utils.get_kubernetes_node_info('in-cluster'))
+print(len(core.cost_report(days=30)))
--- test.py
+++ test.py
@@ -1,11 +1,23 @@
 from sky import core
+import psutil
 from sky.provision.kubernetes import utils
 from sky.utils import annotations
 from sky.utils import common_utils
 
-# print(utils.get_kubernetes_node_info('in-cluster'))
-print(core.realtime_kubernetes_gpu_availability())
-annotations.clear_request_level_cache()
-common_utils.release_memory()
-# print(utils.get_kubernetes_node_info('in-cluster'))
-print(core.realtime_kubernetes_gpu_availability())
+proc = psutil.Process()
+
+print(f'RSS start: {proc.memory_info().rss / 1024 / 1024} MB')
+
+def run(fn, *args, **kwargs):
+    annotations.clear_request_level_cache()
+    print(f'RSS before {fn.__name__}: {proc.memory_info().rss / 1024 / 1024} MB')
+    fn(*args, **kwargs)
+    common_utils.release_memory()
+    print(f'RSS after {fn.__name__}: {proc.memory_info().rss / 1024 / 1024} MB')
+
+run(utils.get_kubernetes_node_info, 'in-cluster')
+run(utils.get_kubernetes_node_info, 'nebius-mk8s-nebius-shopify-k8s-cluster')
+run(core.realtime_kubernetes_gpu_availability)
+run(core.cost_report, days=30)
+
+print(f'RSS end: {proc.memory_info().rss / 1024 / 1024} MB')
