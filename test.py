48845554cbc38feb16480a523f1a083ae82f7f93
Aylei <rayingecho@gmail.com>
2025-09-17 14:03:30 +0800

    Debug

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from sky.provision.kubernetes import utils
 from sky.utils import common_utils
+from sky.utils import annotations
 
 print(utils.get_kubernetes_node_info('in-cluster'))
+annotations.clear_request_level_cache()
 common_utils.release_memory()
 print(utils.get_kubernetes_node_info('in-cluster'))
