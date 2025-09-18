f492656117b7453c6ec12b2c425bc4c0a377a321
Seung Jin Yang <seungjin219@gmail.com>
2025-09-17 17:44:53 -0700

    format

--- tests/smoke_tests/test_setup.py
+++ tests/smoke_tests/test_setup.py
@@ -3,12 +3,10 @@ from smoke_tests import smoke_tests_utils
 
 
 # ---------- Test launching a cluster that has pyproject.toml in the workdir ----------
-@pytest.mark.parametrize(
-    'image_id',
-    [
-        'docker:us-docker.pkg.dev/sky-dev-465/buildkite-test-images/test-workdir-pyproject:latest',
-        'docker:us-docker.pkg.dev/sky-dev-465/buildkite-test-images/test-root-pyproject:latest',
-    ])
+@pytest.mark.parametrize('image_id', [
+    'docker:us-docker.pkg.dev/sky-dev-465/buildkite-test-images/test-workdir-pyproject:latest',
+    'docker:us-docker.pkg.dev/sky-dev-465/buildkite-test-images/test-root-pyproject:latest',
+])
 def test_workdir_with_pyproject(generic_cloud: str, image_id: str):
     name = smoke_tests_utils.get_cluster_name()
     test = smoke_tests_utils.Test(
