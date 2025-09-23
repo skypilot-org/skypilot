53c18efea7f49a58e615079093d3a839ac385f9e
ZePing Guo <zp0int@qq.com>
2025-09-23 22:37:09 +0800

    debug

--- sky/catalog/gcp_catalog.py
+++ sky/catalog/gcp_catalog.py
@@ -685,6 +685,12 @@ def get_image_id_from_tag(tag: str, region: Optional[str]) -> Optional[str]:
         _image_df = common.read_catalog('gcp/images.csv',
                                         pull_frequency_hours=0)
         image_id = common.get_image_id_from_tag_impl(_image_df, tag, region)
+    if tag == 'skypilot:custom-cpu-ubuntu-2204-250923':
+        image_id = ('projects/sky-dev-465/global/images/'
+                    'skypilot-gcp-cpu-ubuntu-250923')
+    elif tag == 'skypilot:custom-gpu-ubuntu-2204-250923':
+        image_id = ('projects/sky-dev-465/global/images/'
+                    'skypilot-gcp-gpu-ubuntu-250923')
     return image_id
 
 
