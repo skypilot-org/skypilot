87a5f72cbacc1e71a7405b9f035f691813a4194f
ZePing Guo <zp0int@qq.com>
2025-09-23 20:43:46 +0800

    resolve comment and rebuild

--- sky/catalog/images/provisioners/cuda.sh
+++ sky/catalog/images/provisioners/cuda.sh
@@ -17,6 +17,17 @@ else
     ARCH_PATH="x86_64"
 fi
 
+# Install GCC 12 and set as default compiler
+# This is required because newer Ubuntu 22.04 kernels (6.5.0+ and 6.8.0+) are built with GCC 12,
+# but Ubuntu 22.04 LTS defaults to GCC 11. Without GCC 12, NVIDIA DKMS driver compilation
+# will fail with error: "unrecognized command-line option '-ftrivial-auto-var-init=zero'"
+# This flag was introduced in GCC 12 and is not recognized by GCC 11.
+echo "Installing GCC 12 to match kernel compiler version..."
+sudo apt-get update
+sudo apt-get install -y gcc-12 g++-12
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 100
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-12 100
+
 # Download architecture-specific CUDA keyring package
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/${ARCH_PATH}/cuda-keyring_1.1-1_all.deb
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
