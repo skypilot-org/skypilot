b552b0734101f63c1af191902ab825955b1d008b
lloyd-brown <lloyd@assemblesys.com>
2025-09-25 15:26:49 -0700

    Remove smoke test.

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -36,7 +36,6 @@ from smoke_tests.test_managed_job import *
 from smoke_tests.test_mount_and_storage import *
 from smoke_tests.test_pools import *
 from smoke_tests.test_region_and_zone import *
-from smoke_tests.test_runpod import *
 from smoke_tests.test_sky_serve import *
 from smoke_tests.test_ssm import *
 from smoke_tests.test_workspaces import *
