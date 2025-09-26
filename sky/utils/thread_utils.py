a082e29461df78145a40b9dc888ae3bf830872bb
cblmemo <cblmemo@gmail.com>
2025-09-26 12:56:11 -0700

    fix thread

--- sky/utils/thread_utils.py
+++ sky/utils/thread_utils.py
@@ -6,19 +6,16 @@ import threading
 class SafeThread(threading.Thread):
     """A thread that can catch exceptions."""
 
-    def __init__(self, target, args=(), kwargs=None):
-        super().__init__()
-        self._target = target
-        self._args = args
-        self._kwargs = kwargs or {}
-        self.exc = None
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._exc = None
 
     def run(self):
         try:
-            self._target(*self._args, **self._kwargs)
+            super().run()
         except Exception as e:  # pylint: disable=broad-except
-            self.exc = e
+            self._exc = e
 
     @property
     def exitcode(self) -> int:
-        return 1 if self.exc is not None else 0
+        return 1 if self._exc is not None else 0
