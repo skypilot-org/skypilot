8cc7063a789bf96dc627739c3267f69c5c32b7c9
Seung Jin Yang <seungjin219@gmail.com>
2025-10-01 17:11:02 -0700

    testfix

--- tests/unit_tests/test_sky/storage/test_storage_utils.py
+++ tests/unit_tests/test_sky/storage/test_storage_utils.py
@@ -171,7 +171,7 @@ def test_zip_files_and_folders(ignore_dir_name, request):
             actual_zipped_files = zipf.namelist()
 
         expected_zipped_files = [
-            'ln-keep.py', 'ln-dir-keep.py', 'dir/subdir/ln-keep.py',
+            '.', 'ln-keep.py', 'ln-dir-keep.py', 'dir/subdir/ln-keep.py',
             'dir/subdir/remove.py', 'keep.py', 'dir/keep.txt', 'dir/keep.a',
             'dir/subdir/keep.b', 'ln-folder', 'empty-folder/', 'dir/',
             'dir/subdir/', 'dir/subdir/remove_dir/'
@@ -289,7 +289,9 @@ def test_zip_files_and_folders_excluded_directories():
 
             # Double-check by verifying the total number of files
             # We should only have the main directory files
-            assert len(zipped_files) == 2  # main_file.txt and .skyignore
+            assert len(
+                zipped_files
+            ) == 3  # the directory itself, main_file.txt and .skyignore
 
 
 def test_get_excluded_files_from_gitignore_with_submodules():
