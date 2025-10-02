63394c9e631c13ab07f5e1599a2506d120ffc908
Aylei <rayingecho@gmail.com>
2025-10-03 04:49:51 +0800

    Fixed k8s container ephemeral storage not reclaimed when using fusermount (#7398)
    * Fixed fusermount storage
    
    Signed-off-by: Aylei <rayingecho@gmail.com>
    
    * Bump fusermount server
    
    Signed-off-by: Aylei <rayingecho@gmail.com>
    
    * Fix CI
    
    Signed-off-by: Aylei <rayingecho@gmail.com>
    
    * Address review comments
    
    Signed-off-by: Aylei <rayingecho@gmail.com>
    
    ---------
    
    Signed-off-by: Aylei <rayingecho@gmail.com>
    Co-authored-by: Seung Jin Yang <seungjin219@gmail.com>

--- addons/fuse-proxy/pkg/server/server.go
+++ addons/fuse-proxy/pkg/server/server.go
@@ -89,6 +89,9 @@ func (s *Server) handleConnection(conn net.Conn) {
 		log.Errorf("Failed to receive namespace fd: %v", err)
 		return
 	}
+	// Close fd to avoid holding reference to the mnt namespace, which will
+	// block the ephmeral storage cleanup.
+	defer syscall.Close(nsFd)
 	if err := json.Unmarshal(msg, req); err != nil {
 		log.Errorf("Failed to unmarshal request: %v", err)
 		return
