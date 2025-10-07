17b250844b8c2f4a7417140fe4138580849e7edf
Seung Jin <seungjin219@gmail.com>
2025-10-06 20:55:49 -0700

    proper user hash calculation for ssh keys (#7502)
    peoper user hash calculation for ssh keys

--- sky/authentication.py
+++ sky/authentication.py
@@ -65,10 +65,7 @@ MAX_TRIALS = 64
 _SSH_KEY_PATH_PREFIX = '~/.sky/clients/{user_hash}/ssh'
 
 
-def get_ssh_key_and_lock_path(
-        user_hash: Optional[str] = None) -> Tuple[str, str, str]:
-    if user_hash is None:
-        user_hash = common_utils.get_user_hash()
+def get_ssh_key_and_lock_path(user_hash: str) -> Tuple[str, str, str]:
     user_ssh_key_prefix = _SSH_KEY_PATH_PREFIX.format(user_hash=user_hash)
 
     os.makedirs(os.path.expanduser(user_ssh_key_prefix),
@@ -105,7 +102,7 @@ def _generate_rsa_key_pair() -> Tuple[str, str]:
     return public_key, private_key
 
 
-def _save_key_pair(private_key_path: str, public_key_path: str,
+def _save_key_pair(user_hash: str, private_key_path: str, public_key_path: str,
                    private_key: str, public_key: str) -> None:
     key_dir = os.path.dirname(private_key_path)
     os.makedirs(key_dir, exist_ok=True, mode=0o700)
@@ -124,13 +121,14 @@ def _save_key_pair(private_key_path: str, public_key_path: str,
               opener=functools.partial(os.open, mode=0o644)) as f:
         f.write(public_key)
 
-    global_user_state.set_ssh_keys(common_utils.get_user_hash(), public_key,
-                                   private_key)
+    global_user_state.set_ssh_keys(user_hash, public_key, private_key)
 
 
 def get_or_generate_keys() -> Tuple[str, str]:
     """Returns the absolute private and public key paths."""
-    private_key_path, public_key_path, lock_path = get_ssh_key_and_lock_path()
+    user_hash = common_utils.get_user_hash()
+    private_key_path, public_key_path, lock_path = get_ssh_key_and_lock_path(
+        user_hash)
     private_key_path = os.path.expanduser(private_key_path)
     public_key_path = os.path.expanduser(public_key_path)
     lock_path = os.path.expanduser(lock_path)
@@ -146,8 +144,8 @@ def get_or_generate_keys() -> Tuple[str, str]:
                 global_user_state.get_ssh_keys(common_utils.get_user_hash()))
             if not exists:
                 ssh_public_key, ssh_private_key = _generate_rsa_key_pair()
-            _save_key_pair(private_key_path, public_key_path, ssh_private_key,
-                           ssh_public_key)
+            _save_key_pair(user_hash, private_key_path, public_key_path,
+                           ssh_private_key, ssh_public_key)
     assert os.path.exists(public_key_path), (
         'Private key found, but associated public key '
         f'{public_key_path} does not exist.')
@@ -185,8 +183,8 @@ def create_ssh_key_files_from_db(private_key_path: Optional[str] = None):
                 global_user_state.get_ssh_keys(user_hash))
             if not exists:
                 raise RuntimeError(f'SSH keys not found for user {user_hash}')
-            _save_key_pair(private_key_path, public_key_path, ssh_private_key,
-                           ssh_public_key)
+            _save_key_pair(user_hash, private_key_path, public_key_path,
+                           ssh_private_key, ssh_public_key)
     assert os.path.exists(public_key_path), (
         'Private key found, but associated public key '
         f'{public_key_path} does not exist.')
