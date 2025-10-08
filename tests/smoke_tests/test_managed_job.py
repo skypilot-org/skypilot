854fe06821a77566d3d0edcdf3d48d3f35c3c50b
Kevin Mingtarja <kevin.mingtarja@gmail.com>
2025-10-08 15:28:03 -0700

    dbg

--- tests/smoke_tests/test_managed_job.py
+++ tests/smoke_tests/test_managed_job.py
@@ -78,6 +78,8 @@ def test_managed_jobs_basic(generic_cloud: str):
                 job_status=[sky.ManagedJobStatus.CANCELLED],
                 timeout=230),
             # Test the functionality for logging.
+            f'sky jobs queue',
+            f'sky jobs logs -n {name}-2 --no-follow',
             f's=$(sky jobs logs -n {name}-2 --no-follow); echo "$s"; echo "$s" | grep "start counting"',
             f's=$(sky jobs logs --controller -n {name}-2 --no-follow); echo "$s"; echo "$s" | grep "Cluster launched:"',
             rf'{smoke_tests_utils.GET_JOB_QUEUE} | grep {name}-2 | head -n1 | grep "RUNNING\|SUCCEEDED"',
