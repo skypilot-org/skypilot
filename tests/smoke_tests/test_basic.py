1c8951dee1bae8eeaaf3dc2eef3dc5e0dc4f69a1
Seung Jin Yang <seungjin219@gmail.com>
2025-10-16 13:53:10 -0700

    for real this time

--- tests/smoke_tests/test_basic.py
+++ tests/smoke_tests/test_basic.py
@@ -1072,10 +1072,12 @@ def test_kubernetes_show_gpus(generic_cloud: str):
             (
             's=$(SKYPILOT_DEBUG=0 sky show-gpus --infra kubernetes) && '
             'echo "$s" && '
-            # # Grab the table header by querying for `REQUESTABLE_QTY_PER_NODE`
-            # # using -A 1 to grab the next line as well.
-            # # On the next line, search for the correct utilization string.
-            'echo $s | grep "REQUESTABLE_QTY_PER_NODE" -A 1 | grep "8 of 8 free"'
+            # Grab the table header by querying for `REQUESTABLE_QTY_PER_NODE`
+            # using -A 1 to grab the next line as well.
+            # Then get the last line of the output 
+            # (only the first line of values, exluding the table header.)
+            # Then, search for the correct utilization string.
+            'echo "$s" | grep "REQUESTABLE_QTY_PER_NODE" -A 1 | tail -n 1 | grep "8 of 8 free"'
             )
         ],
     )
