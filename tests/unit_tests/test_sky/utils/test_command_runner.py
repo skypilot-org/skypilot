71ed925c53a0ea3c3c2ebe45b33f0652268cc3dc
Seung Jin <seungjin219@gmail.com>
2025-10-16 18:34:05 -0700

    pull down key file from db on ssh command runner initialization (#7650)
    pull down key file from db gd

--- tests/unit_tests/test_sky/utils/test_command_runner.py
+++ tests/unit_tests/test_sky/utils/test_command_runner.py
@@ -1,15 +1,22 @@
 """Unit tests for sky.utils.command_runner."""
 
+import os
+
+from sky import authentication
 from sky.utils import command_runner
+from sky.utils import common_utils
 
 
 def test_docker_runner_passes_proxy_command_to_inner_hop() -> None:
     """Ensure docker-mode runners reuse user proxy for the host hop."""
     proxy_cmd = 'ssh -W %h:%p jump@host'
+    user_hash = common_utils.get_user_hash()
+    private_key_path, _, _ = authentication.get_ssh_key_and_lock_path(user_hash)
+
     runner = command_runner.SSHCommandRunner(
         node=('10.0.0.5', 22),
         ssh_user='ubuntu',
-        ssh_private_key='/tmp/fake-key',
+        ssh_private_key=os.path.expanduser(private_key_path),
         ssh_proxy_command=proxy_cmd,
         docker_user='container',
         ssh_control_name='unit-test-control',
