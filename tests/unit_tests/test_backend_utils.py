4445335e3cf1dc26d1fa466357993b651066e294
kyuds <kyuseung1016@gmail.com>
2025-10-20 09:25:28 +0900

    make test actually correct.

--- tests/unit_tests/test_backend_utils.py
+++ tests/unit_tests/test_backend_utils.py
@@ -208,7 +208,11 @@ def test_get_clusters_launch_refresh(monkeypatch):
         }
 
     def get_clusters_mock(*args, **kwargs):
-        return [_mock_cluster(False), _mock_cluster(True), _mock_cluster(True, 'None')]
+        return [
+            _mock_cluster(False),
+            _mock_cluster(True),
+            _mock_cluster(True, 'None')
+        ]
 
     def get_readable_resources_repr(handle, simplify):
         return ''
@@ -242,4 +246,4 @@ def test_get_clusters_launch_refresh(monkeypatch):
                         get_request_tasks)
 
     assert len(
-        backend_utils.get_clusters(refresh=common.StatusRefreshMode.FORCE)) == 2
+        backend_utils.get_clusters(refresh=common.StatusRefreshMode.FORCE)) == 3
