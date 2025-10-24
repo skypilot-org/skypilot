6632d895f894ac12cb4b94bbe69ef65b8d156a69
Aylei <rayingecho@gmail.com>
2025-10-24 11:28:12 +0800

    Fix smoke test
    Signed-off-by: Aylei <rayingecho@gmail.com>

--- tests/smoke_tests/test_managed_job.py
+++ tests/smoke_tests/test_managed_job.py
@@ -1626,7 +1626,7 @@ def test_managed_jobs_logs_gc(generic_cloud: str):
             }
         },
         commands=[
-            f'sky jobs launch -n {name} --infra {generic_cloud} {smoke_tests_utils.LOW_RESOURCE_ARG} -y examples/managed_job.yaml -d',
+            f'sky jobs launch -n {name} --infra {generic_cloud} {smoke_tests_utils.LOW_RESOURCE_ARG} -y "echo hello" -d',
             smoke_tests_utils.
             get_cmd_wait_until_managed_job_status_contains_matching_job_name(
                 job_name=f'{name}',
