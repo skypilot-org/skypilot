0bbf932a246da5c4e44b5a776f94a98530affdeb
Kevin Mingtarja <kevin.mingtarja@gmail.com>
2025-10-25 11:33:01 -0700

    [Requests] Use orjson to speed up encoding/decoding return_value and error

--- sky/server/server.py
+++ sky/server/server.py
@@ -26,6 +26,7 @@ import aiofiles
 import anyio
 import fastapi
 from fastapi.middleware import cors
+from fastapi import responses as fastapi_responses
 import starlette.middleware.base
 import uvloop
 
@@ -1498,7 +1499,7 @@ async def local_down(request: fastapi.Request,
 
 
 # === API server related APIs ===
-@app.get('/api/get')
+@app.get('/api/get', response_class=fastapi_responses.ORJSONResponse)
 async def api_get(request_id: str) -> payloads.RequestPayload:
     """Gets a request with a given request ID prefix."""
     while True:
