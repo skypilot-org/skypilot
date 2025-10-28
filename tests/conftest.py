02a251d971cedb1281e8acd843c4199b06a639b0
Atem Aguer <atemsunday@gmail.com>
2025-10-28 07:14:33 -0700

    Register Mithril in test configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -66,7 +66,7 @@ all_clouds_in_smoke_tests = [
     'aws', 'gcp', 'azure', 'lambda', 'cloudflare', 'ibm', 'scp', 'oci', 'do',
     'kubernetes', 'vsphere', 'cudo', 'fluidstack', 'paperspace',
     'primeintellect', 'runpod', 'vast', 'nebius', 'hyperbolic', 'seeweb',
-    'shadeform', 'coreweave'
+    'shadeform', 'coreweave', 'mithril'
 ]
 default_clouds_to_run = ['aws', 'azure']
 
@@ -97,6 +97,7 @@ cloud_to_pytest_keyword = {
     'shadeform': 'shadeform',
     'seeweb': 'seeweb',
     'coreweave': 'coreweave',
+    'mithril': 'mithril',
 }
 
 
