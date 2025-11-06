fdcea6222e891271a4621862c7a939e4cce29508
tv <tv@tvs-MacBook-Pro.attlocal.net>
2025-11-05 17:51:22 -0800

    shutil

--- sky/server/server.py
+++ sky/server/server.py
@@ -1023,7 +1023,7 @@ async def unzip_file(zip_file_path: pathlib.Path,
 
     def _do_unzip() -> None:
         try:
-            os.rmdir(client_file_mounts_dir)
+            shutil.rmtree(client_file_mounts_dir, ignore_errors=True)
             with zipfile.ZipFile(zip_file_path, 'r') as zipf:
                 for member in zipf.infolist():
                     # Determine the new path
