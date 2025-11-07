767213dc7c8a4241dc172db028fe61e447a36b47
Rohan Sonecha <rohan@sonecha.com>
2025-11-06 21:18:25 -0800

    [k8s] retry k8s api list namespaced pod on empty response (#7854)
    * add more debug logs around k8s list_namespaced_pod
    
    * label selector
    
    * [k8s] retry k8s api list namespaced pod on empty response
    
    * tests
    
    * reorder retry logic
    
    * only retry for status refresh daemon and status -r
    
    * remove unnecessary retry_if_missing
    
    * remove defaults
    
    * fix test
    
    * update retry if missing logic to only run on sky status -r
    
    * clarity
    
    * change retry if missing logic for refresh
    
    * update logic
    
    * update docstring
    
    * only kubernetes docstring
    
    * adjust logic and add comment
    
    * tighten
    
    * update test

--- sky/provision/__init__.py
+++ sky/provision/__init__.py
@@ -80,6 +80,7 @@ def query_instances(
     cluster_name_on_cloud: str,
     provider_config: Optional[Dict[str, Any]] = None,
     non_terminated_only: bool = True,
+    retry_if_missing: bool = False,
 ) -> Dict[str, Tuple[Optional['status_lib.ClusterStatus'], Optional[str]]]:
     """Query instances.
 
@@ -88,6 +89,11 @@ def query_instances(
 
     A None status means the instance is marked as "terminated"
     or "terminating".
+
+    Args:
+        retry_if_missing: Whether to retry the call to the cloud api if the
+          cluster is not found when querying the live status on the cloud.
+          NOTE: This is currently only used on kubernetes.
     """
     raise NotImplementedError
 
