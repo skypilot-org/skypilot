e07d94d3b5a31cba9ba9264b1414144a1873cb4a
DanielZhangQD <36026334+DanielZhangQD@users.noreply.github.com>
2025-11-07 09:16:05 +0800

    [Dashboard] Return resources_str_full for cluster records of the dashboard requests (#7880)
    return resources_str_full for cluster records of the dashboard requests

--- sky/backends/backend_utils.py
+++ sky/backends/backend_utils.py
@@ -3243,11 +3243,10 @@ def get_clusters(
             handle = record['handle']
             resource_str_simple, resource_str_full = (
                 resources_utils.get_readable_resources_repr(
-                    handle, simplified_only=summary_response))
+                    handle, simplified_only=False))
             record['resources_str'] = resource_str_simple
+            record['resources_str_full'] = resource_str_full
             if not summary_response:
-                assert resource_str_full is not None
-                record['resources_str_full'] = resource_str_full
                 record['cluster_name_on_cloud'] = handle.cluster_name_on_cloud
 
     def _update_records_with_credentials(
