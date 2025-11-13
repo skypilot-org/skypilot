0a3c3099ef132f0c3216a9435924939016c8c0f8
Seung Jin Yang <seungjin219@gmail.com>
2025-11-13 13:58:25 -0800

    format

--- tests/unit_tests/test_sky/db/test_kv_cache.py
+++ tests/unit_tests/test_sky/db/test_kv_cache.py
@@ -13,20 +13,25 @@ def isolated_database(tmp_path):
     temp_log_path = tmp_path / 'logs'
     temp_log_path.mkdir()
 
-
-    kv_cache._SQLALCHEMY_ENGINE = sqlalchemy.create_engine(f'sqlite:///{temp_db_path}')
+    kv_cache._SQLALCHEMY_ENGINE = sqlalchemy.create_engine(
+        f'sqlite:///{temp_db_path}')
     kv_cache.create_table(kv_cache._SQLALCHEMY_ENGINE)
     yield
     kv_cache._SQLALCHEMY_ENGINE = None
 
+
 def test_cache_entry_basic(isolated_database):
-    kv_cache.add_or_update_cache_entry('test_key', 'test_value', time.time() + 3600)
+    kv_cache.add_or_update_cache_entry('test_key', 'test_value',
+                                       time.time() + 3600)
     assert kv_cache.get_cache_entry('test_key') == 'test_value'
 
+
 def test_get_cache_entry_expired(isolated_database):
-    # add a cache entry that is expired 
-    kv_cache.add_or_update_cache_entry('test_key', 'test_value', time.time() - 3600)
+    # add a cache entry that is expired
+    kv_cache.add_or_update_cache_entry('test_key', 'test_value',
+                                       time.time() - 3600)
     assert kv_cache.get_cache_entry('test_key') is None
     # add a cache entry that is not expired
-    kv_cache.add_or_update_cache_entry('test_key', 'test_value', time.time() + 3600)
+    kv_cache.add_or_update_cache_entry('test_key', 'test_value',
+                                       time.time() + 3600)
     assert kv_cache.get_cache_entry('test_key') == 'test_value'
