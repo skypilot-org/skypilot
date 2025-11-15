5b719988934f3223da6ddf828704a29abfbc8a5c
Aylei <rayingecho@gmail.com>
2025-11-15 09:49:08 +0800

    Allow job controller specifying server config for local API server (#7966)
    * Allow specifying server config for local API server
    
    Signed-off-by: Aylei <rayingecho@gmail.com>
    
    * Refine
    
    Signed-off-by: Aylei <rayingecho@gmail.com>
    
    * Better comments
    
    Signed-off-by: Aylei <rayingecho@gmail.com>
    
    * Refine
    
    Signed-off-by: Aylei <rayingecho@gmail.com>
    
    ---------
    
    Signed-off-by: Aylei <rayingecho@gmail.com>

--- sky/server/requests/payloads.py
+++ sky/server/requests/payloads.py
@@ -81,9 +81,12 @@ def request_body_env_vars() -> dict:
     env_vars[constants.USER_ENV_VAR] = common_utils.get_current_user_name()
     env_vars[
         usage_constants.USAGE_RUN_ID_ENV_VAR] = usage_lib.messages.usage.run_id
+    if not common.is_api_server_local():
+        # Used in job controller, for local API server, keep the
+        # SKYPILOT_CONFIG env var to use the config for the managed job.
+        env_vars.pop(skypilot_config.ENV_VAR_SKYPILOT_CONFIG, None)
     # Remove the path to config file, as the config content is included in the
     # request body and will be merged with the config on the server side.
-    env_vars.pop(skypilot_config.ENV_VAR_SKYPILOT_CONFIG, None)
     env_vars.pop(skypilot_config.ENV_VAR_GLOBAL_CONFIG, None)
     env_vars.pop(skypilot_config.ENV_VAR_PROJECT_CONFIG, None)
     # Remove the config related env vars, as the client config override
