ea4742218e0643188495e6f6b320d1464b9056aa
ZePing Guo <zp0int@qq.com>
2025-11-19 23:32:32 +0800

    revert lock event

--- sky/utils/lock_events.py
+++ sky/utils/lock_events.py
@@ -63,17 +63,17 @@ class FileLockEvent:
             f'[FileLock.hold]:{self._lockfile}')
 
     def acquire(self):
-        was_locked = self._lock.is_locked()
+        was_locked = self._lock.is_locked
         with timeline.Event(f'[FileLock.acquire]:{self._lockfile}'):
             self._lock.acquire()
-        if not was_locked and self._lock.is_locked():
+        if not was_locked and self._lock.is_locked:
             # start holding the lock after initial acquiring
             self._hold_lock_event.begin()
 
     def release(self):
-        was_locked = self._lock.is_locked()
+        was_locked = self._lock.is_locked
         self._lock.release()
-        if was_locked and not self._lock.is_locked():
+        if was_locked and not self._lock.is_locked:
             # stop holding the lock after initial releasing
             self._hold_lock_event.end()
 
