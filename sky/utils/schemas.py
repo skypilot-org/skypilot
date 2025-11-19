ce40acdfe11e68663071efd8a80f23a87a3158f9
Romil Bhardwaj <romil.bhardwaj@gmail.com>
2025-11-19 15:09:57 -0800

    multislurm support

--- sky/utils/schemas.py
+++ sky/utils/schemas.py
@@ -1404,6 +1404,24 @@ def get_config_schema():
                 **_CONTEXT_CONFIG_SCHEMA_MINIMAL,
             }
         },
+        'slurm': {
+            'type': 'object',
+            'required': [],
+            'additionalProperties': False,
+            'properties': {
+                'allowed_clusters': {
+                    'oneOf': [{
+                        'type': 'array',
+                        'items': {
+                            'type': 'string',
+                        },
+                    }, {
+                        'type': 'string',
+                        'pattern': '^all$'
+                    }]
+                },
+            }
+        },
         'oci': {
             'type': 'object',
             'required': [],
