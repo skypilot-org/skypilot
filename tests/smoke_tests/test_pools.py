a0053dca5af6915d69e94c641b0ba78efd25ac69
lloyd-brown <lloyd@assemblesys.com>
2025-11-21 17:56:28 -0800

    Don't check ID.

--- tests/smoke_tests/test_pools.py
+++ tests/smoke_tests/test_pools.py
@@ -1214,7 +1214,7 @@ def test_pools_single_yaml(generic_cloud: str):
                     pool_name=pool_name, pool_yaml=one_config_yaml.name),
                 (f's=$(sky jobs launch --pool {pool_name} {one_config_yaml.name} --name {job_name} -d -y); '
                  'echo "$s"; '
-                 'echo; echo; echo "$s" | grep "Job submitted, ID: 1"; '
+                 'echo; echo; echo "$s" | grep "Job submitted"; '
                  'echo "$s" | grep "Unified job"'),
                 wait_until_job_status(job_name, ['SUCCEEDED'], timeout=timeout),
             ],
