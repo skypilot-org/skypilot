0a86a936a72d53c97440f1da97c745395883c5a2
kyuds <kyuseung1016@gmail.com>
2025-12-05 08:57:32 +0900

    add comments

--- sky/utils/kubernetes/ssh-tunnel.sh
+++ sky/utils/kubernetes/ssh-tunnel.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# This redirect stub is needed because we use this script in the
+# exec auth section when creating our kubeconfig. Therefore, node pools
+# launched in older versions of SkyPilot will have kubeconfigs pointing
+# to this path.
+
 # TODO (kyuds): remove this script after v0.13.0. Kept here for backwards compat.
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 exec "$SCRIPT_DIR/../../ssh_node_pools/deploy/tunnel/ssh-tunnel.sh" "$@"
