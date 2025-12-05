bb73286bc3e5fa857dfb8ba4e017bd745ac94719
Seung Jin <seungjin219@gmail.com>
2025-12-05 12:51:33 -0800

    [Slurm] robust slurm check against nonexistent config file (#8207)
    * robust slurm check against nonexistent config file
    
    * better error handline
    
    * better error message
    
    * format

--- sky/clouds/slurm.py
+++ sky/clouds/slurm.py
@@ -1,10 +1,7 @@
 """Slurm."""
 
-import os
 import typing
 from typing import Dict, Iterator, List, Optional, Tuple, Union
-
-from paramiko.config import SSHConfig
 
 from sky import catalog
 from sky import clouds
@@ -58,6 +55,7 @@ class Slurm(clouds.Cloud):
     }
     _MAX_CLUSTER_NAME_LEN_LIMIT = 120
     _regions: List[clouds.Region] = []
+    _INDENT_PREFIX = '    '
 
     # Using the latest SkyPilot provisioner API to provision and check status.
     PROVISIONER_VERSION = clouds.ProvisionerVersion.SKYPILOT
@@ -287,7 +285,7 @@ class Slurm(clouds.Cloud):
         assert cluster is not None, 'No available Slurm cluster found.'
 
         # cluster is our target slurmctld host.
-        ssh_config = SSHConfig.from_path(os.path.expanduser(CREDENTIAL_PATH))
+        ssh_config = slurm_utils.get_slurm_ssh_config()
         ssh_config_dict = ssh_config.lookup(cluster)
 
         resources = resources.assert_launchable()
@@ -416,7 +414,20 @@ class Slurm(clouds.Cloud):
     def _check_compute_credentials(
             cls) -> Tuple[bool, Optional[Union[str, Dict[str, str]]]]:
         """Checks if the user has access credentials to the Slurm cluster."""
-        ssh_config = SSHConfig.from_path(os.path.expanduser(CREDENTIAL_PATH))
+        try:
+            ssh_config = slurm_utils.get_slurm_ssh_config()
+        except FileNotFoundError:
+            return (
+                False,
+                f'Slurm configuration file {slurm_utils.DEFAULT_SLURM_PATH} '
+                'does not exist.\n'
+                f'{cls._INDENT_PREFIX}For more info: '
+                'https://docs.skypilot.co/en/latest/getting-started/'
+                'installation.html#slurm-installation')
+        except Exception as e:  # pylint: disable=broad-except
+            return (False, 'Failed to load SSH configuration from '
+                    f'{slurm_utils.DEFAULT_SLURM_PATH}: '
+                    f'{common_utils.format_exception(e)}.')
         existing_allowed_clusters = cls.existing_allowed_clusters()
 
         if not existing_allowed_clusters:
