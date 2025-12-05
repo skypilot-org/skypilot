b8389e54817e46f28c595c7f4e395a24c3af1faa
massaindustries <francescomassa06@gmail.com>
2025-12-05 12:19:35 +0100

    seeweb-storage-3

--- sky/data/storage.py
+++ sky/data/storage.py
@@ -4799,6 +4799,8 @@ class CoreWeaveStore(S3CompatibleStore):
 class SeewebStore(S3CompatibleStore):
     """S3-compatible backend for Seeweb Cloud Object Storage."""
 
+    _DEFAULT_REGION = 'eu-milano-1'
+
     @classmethod
     def get_config(cls) -> S3CompatibleConfig:
         return S3CompatibleConfig(
@@ -4812,6 +4814,7 @@ class SeewebStore(S3CompatibleStore):
             aws_profile=seeweb_adaptor.SEEWEB_PROFILE_NAME,
             get_endpoint_url=seeweb_adaptor.get_endpoint,
             cloud_name=str(clouds.Seeweb()),
+            default_region=cls._DEFAULT_REGION,
             mount_cmd_factory=cls._get_seeweb_mount_cmd,
         )
 
