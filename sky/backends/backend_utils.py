fc362ff1a0fb163d85af2e03f424612ab7c98ce9
Rohan Sonecha <rohan@sonecha.com>
2025-12-12 17:00:28 -0800

    cluster_failures log

--- sky/backends/backend_utils.py
+++ sky/backends/backend_utils.py
@@ -2265,6 +2265,8 @@ def _update_cluster_status(
     cluster_failures = global_user_state.get_cluster_failures(
         record['cluster_hash'])
 
+    logger.info(f'cluster_failures: {cluster_failures}')
+
     def get_node_counts_from_ray_status(
             runner: command_runner.CommandRunner) -> Tuple[int, int, str, str]:
         rc, output, stderr = runner.run(
