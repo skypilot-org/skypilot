7a9d620309275e175ad43528fda756b0e2670067
ZePing Guo <zp0int@qq.com>
2026-01-09 23:32:27 +0800

    enforce that hook_timeout is only set if hook is set

--- sky/client/sdk.py
+++ sky/client/sdk.py
@@ -1342,6 +1342,7 @@ def autostop(
         None
 
     Request Raises:
+        sky.exceptions.ArgumentValidationError: if arguments are invalid.
         sky.exceptions.ClusterDoesNotExist: if the cluster does not exist.
         sky.exceptions.ClusterNotUpError: if the cluster is not UP.
         sky.exceptions.NotSupportedError: if the cluster is not based on
@@ -1351,6 +1352,10 @@ def autostop(
         sky.exceptions.CloudUserIdentityError: if we fail to get the current
             user identity.
     """
+    if hook_timeout is not None and hook is None:
+        raise exceptions.ArgumentValidationError(
+            'hook_timeout can only be set if hook is set.')
+
     remote_api_version = versions.get_remote_api_version()
     if wait_for is not None and (remote_api_version is None or
                                  remote_api_version < 13):
