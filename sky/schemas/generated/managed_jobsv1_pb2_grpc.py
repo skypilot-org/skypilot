af5ec3534b999c38f75be5cc8ebf2616b24c1f74
lloyd-brown <lloyd@assemblesys.com>
2026-01-21 18:34:36 -0800

    Compile protos.

--- sky/schemas/generated/managed_jobsv1_pb2_grpc.py
+++ sky/schemas/generated/managed_jobsv1_pb2_grpc.py
@@ -20,7 +20,7 @@ except ImportError:
 if _version_not_supported:
     warnings.warn(
         f'The grpc package installed is at version {GRPC_VERSION},'
-        + f' but the generated code in sky/schemas/proto/managed_jobsv1_pb2_grpc.py depends on'
+        + f' but the generated code in sky/schemas/generated/managed_jobsv1_pb2_grpc.py depends on'
         + f' grpcio>={GRPC_GENERATED_VERSION}.'
         + f' Please upgrade your grpc module to grpcio>={GRPC_GENERATED_VERSION}'
         + f' or downgrade your generated code using grpcio-tools<={GRPC_VERSION}.'
