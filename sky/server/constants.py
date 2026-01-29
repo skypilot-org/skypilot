3157aa0a1b0903377972678ac73ac3aba314b381
lloyd-brown <lloyd@assemblesys.com>
2026-01-28 16:15:14 -0800

    Formatting.

--- sky/server/constants.py
+++ sky/server/constants.py
@@ -10,7 +10,7 @@ from sky.skylet import constants
 # based on version info is needed.
 # For more details and code guidelines, refer to:
 # https://docs.skypilot.co/en/latest/developers/CONTRIBUTING.html#backward-compatibility-guidelines
-API_VERSION = 31  # add is_primary_in_job_group field for job groups
+API_VERSION = 32  # add recipes endpoint
 
 # The minimum peer API version that the code should still work with.
 # Notes (dev):
