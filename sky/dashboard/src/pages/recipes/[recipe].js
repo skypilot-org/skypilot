80e3973aabde4ecb8ae758a85a75a96c1f57c991
lloyd-brown <lloyd@assemblesys.com>
2026-01-28 17:24:18 -0800

    Change title for tab.

--- sky/dashboard/src/pages/recipes/[recipe].js
+++ sky/dashboard/src/pages/recipes/[recipe].js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
 
 const RecipeDetail = dynamic(
   () => import('@/components/recipe-detail').then((mod) => mod.RecipeDetail),
@@ -8,10 +9,17 @@ const RecipeDetail = dynamic(
 );
 
 export default function RecipeDetailPage() {
+  const router = useRouter();
+  const { recipe } = router.query;
+
+  const title = recipe
+    ? `${recipe} | Recipes | SkyPilot Dashboard`
+    : 'Recipes | SkyPilot Dashboard';
+
   return (
     <>
       <Head>
-        <title>Recipe | SkyPilot Dashboard</title>
+        <title>{title}</title>
       </Head>
       <RecipeDetail />
     </>
