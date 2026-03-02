58fa4f5106ff83ec2d86c5f549289d0fdd2d069d
Muhammad Ibrahim <muhammad.ibrahim@multiversecomputing.com>
2026-03-02 20:18:48 +0100

    [Kubernetes] Stop sanitizing workspace for pod metadata

--- sky/backends/backend_utils.py
+++ sky/backends/backend_utils.py
@@ -971,8 +971,7 @@ def write_cluster_config(
             # controller_utils.shared_controller_vars_to_fill().
             'user': common_utils.get_cleaned_username(
                 os.environ.get(constants.USER_ENV_VAR, '')),
-            'workspace': common_utils.get_cleaned_username(
-                skypilot_config.get_active_workspace()),
+            'workspace': skypilot_config.get_active_workspace(),
 
             # Networking configs
             'use_internal_ips': skypilot_config.get_effective_region_config(
