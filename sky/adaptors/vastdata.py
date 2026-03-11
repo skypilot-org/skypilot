96c51d84e42d0f958448867f50583887033c354b
Haim Marko <haim.marko@vastdata.com>
2026-03-11 01:14:00 -0700

    Update VastData error hints to suggest installation of "skypilot[aws]" instead of boto3 for improved user guidance

--- sky/adaptors/vastdata.py
+++ sky/adaptors/vastdata.py
@@ -226,7 +226,7 @@ def check_storage_credentials() -> Tuple[bool, Optional[str]]:
     if hints:
         hints += ' Run the following commands:'
         if not profile_in_cred:
-            hints += f'\n{_INDENT_PREFIX}  $ pip install boto3'
+            hints += f'\n{_INDENT_PREFIX}  $ pip install "skypilot[aws]"'
             hints += (f'\n{_INDENT_PREFIX}  $ AWS_SHARED_CREDENTIALS_FILE='
                       f'{VASTDATA_CREDENTIALS_PATH} aws configure --profile '
                       f'{VASTDATA_PROFILE_NAME}')
