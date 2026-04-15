34e98e57fd39c3258d6d8298f862c5661e4f7a3a
Christopher Cooper <christopher@cg505.com>
2026-04-15 22:59:22 +0000

    disable subpath for automount

--- sky/backends/backend_utils.py
+++ sky/backends/backend_utils.py
@@ -1002,23 +1002,26 @@ def write_cluster_config(
                         f'auto_mounts. Skipping.')
                     continue
                 for path in mount_paths:
+                    # Disable sub_path usage, which turns out to be confusing,
+                    # for now. See #9353.
                     if path.startswith('/'):
                         mount_path = path
-                        sub_path = path.lstrip('/')
+                        # sub_path = path.lstrip('/')
                     elif path.startswith('~/'):
                         mount_path = f'{home_dir}/{path[2:]}'
-                        sub_path = path[2:]
+                        # sub_path = path[2:]
                     elif path == '~':
                         mount_path = home_dir
-                        sub_path = None
+                        # sub_path = None
                     else:
+                        logger.warning(f'Malformed automount path {path}')
                         continue
                     vol_info = volume_utils.VolumeInfo(
                         name=volume_name,
                         path=mount_path,
                         volume_name_on_cloud=volume_config.name_on_cloud,
                         volume_id_on_cloud=volume_config.id_on_cloud,
-                        sub_path=sub_path if sub_path else None,
+                        # sub_path=sub_path if sub_path else None,
                         volume_type=volume_config.type,
                         host_path=volume_config.config.get('host_path'),
                     )
