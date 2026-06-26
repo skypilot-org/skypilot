96e54bd82a9556c036b822de15b206e1661eea92
Kevin Mingtarja <69668484+kevinmingtarja@users.noreply.github.com>
2026-06-26 01:08:14 -0700

    [Kubernetes] Refactor FUSE setup command (#9820)
    * [Kubernetes] Refactor FUSE setup command
    
    * [Kubernetes] Fix formatting in FUSE setup command refactor

---
+++ tests/unit_tests/kubernetes/test_fuse.py
@@ -0,0 +1,22 @@
+"""Tests for Kubernetes FUSE helpers."""
+
+from sky.provision.kubernetes import fuse
+
+
+def test_fusermount_shim_setup_command_uses_supplied_sudo_and_shared_dir():
+    command = fuse.get_fusermount_shim_setup_command(
+        sudo_cmd='$(prefix_cmd)', shared_dir='/var/run/fusermount')
+
+    assert '$(prefix_cmd) cp -p "$FUSERMOUNT_PATH"' in command
+    assert ('$(prefix_cmd) ln -sf "/var/run/fusermount/fusermount-shim" '
+            '"$FUSERMOUNT_PATH"') in command
+    assert ('$(prefix_cmd) cp -p "/var/run/fusermount/fusermount-wrapper" '
+            '/bin/fusermount-wrapper') in command
+    assert 'wait_for_fusermount()' in command
+
+
+def test_fusermount_shim_setup_command_allows_empty_sudo():
+    command = fuse.get_fusermount_shim_setup_command(sudo_cmd='')
+
+    assert 'sudo cp -p' not in command
+    assert 'cp -p "$FUSERMOUNT_PATH"' in command
