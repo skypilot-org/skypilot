a38f698ff3b5245b144c2ac73a20f0befca6328d
MitaliMSahoo <sahmitali21@gmail.com>
2026-08-16 21:11:25 -0700

    [CI]  Fix formatting, lint issue

--- sky/jobs/controller.py
+++ sky/jobs/controller.py
@@ -1091,9 +1091,9 @@ class JobController:
                             f'Task {task_id} exceeded max_duration '
                             f'({task.max_duration}). Terminating the job.')
                         failure_reason = (
-                            f'Job exceeded max_duration of {task.max_duration}. '
-                            'The job was terminated by the controller.'
-                            )
+                            f'Job exceeded max_duration of '
+                            f'{task.max_duration}. '
+                            'The job was terminated by the controller.')
                         await managed_job_state.set_failed_async(
                             self._job_id,
                             task_id,
