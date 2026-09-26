d3da5e4f4aa7cf6fdad5b1e7acf391512e6cc516
Amitoj6210 <as6210@srmist.edu.in>
2026-09-26 22:43:21 +0530

    Update storage_backends.py

--- sky/data/storage_backends.py
+++ sky/data/storage_backends.py
@@ -9,17 +9,17 @@ class R2CloudStorage(CloudStorage):
     """Cloudflare Cloud Storage."""
 
     _GET_AWSCLI = [
-    ('if ! aws --version >/dev/null 2>&1; then '
-     f'{constants.SKY_UV_PIP_CMD} install awscli; '
-     'fi; '
-     'awscli_path=$(which aws || echo '
-     f'{constants.SKY_REMOTE_PYTHON_ENV}/bin/aws)'),
-]
+        ('if ! aws --version >/dev/null 2>&1; then '
+         f'{constants.SKY_UV_PIP_CMD} install awscli; '
+         'fi; '
+         'awscli_path=$(which aws || echo '
+         f'{constants.SKY_REMOTE_PYTHON_ENV}/bin/aws)'),
+    ]
 
     def __init__(self,
-             name: str,
-             source: str,
-             mode: StorageMode = StorageMode.MOUNT):
+                 name: str,
+                 source: str,
+                 mode: StorageMode = StorageMode.MOUNT):
         super().__init__()
         self.name = name
         self.source = source
@@ -33,5 +33,4 @@ class R2CloudStorage(CloudStorage):
             f'export AWS_SHARED_CREDENTIALS_FILE={self.credentials_path} && '
             f'$awscli_path s3 sync {source} {target} '
             f'--endpoint-url={self.endpoint_url}')
-        
         return ' && '.join([*self._GET_AWSCLI, sync_command])
